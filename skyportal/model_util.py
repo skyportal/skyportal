d7c7988b804c1c1e6aec15e872ed37ef01daeb97
Brett Naul <brettnaul@gmail.com>
2017-06-22 21:52:45 -0700

    Connect to db before test data insert

--- skyportal/model_util.py
+++ skyportal/model_util.py
@@ -1,10 +1,14 @@
 import numpy as np
 import pandas as pd
+from baselayer.app import cfg
 from baselayer.app.model_util import status, create_tables, drop_tables
 from skyportal import models
 
 
 def insert_test_data():
+    with status(f"Connecting to database {cfg['database']['database']}"):
+        models.init_db(**cfg['database'])
+
     with status("Dropping all tables"):
         drop_tables()
 
@@ -20,22 +24,23 @@ def insert_test_data():
         models.DBSession().add(u)
         models.DBSession().commit()
 
-    s = models.Source(ra=353.36647, dec=33.646149, red_shift=0.063,
-                      name='14gqr')
-    s.comments = [models.Comment(text="No source at transient location to R>26"
-                                 "in LRIS imaging", user=u),
-                  models.Comment(text="Strong calcium lines have emerged.",
-                                 user=u)]
-    start = pd.Timestamp('2015-07-01')
-    end = pd.Timestamp('2015-08-01')
-    times = pd.to_datetime(np.linspace(start.value, end.value, 100))
-    s.photometry = [models.Photometry(obs_time=t, mag=np.random.uniform(19, 22),
-                                      e_mag=np.random.uniform(0, 0.5),
-                                      lim_mag=np.random.uniform(22, 25),
-                                      filter=np.random.choice(['R', 'G']))
-                    for t in times]
-    models.DBSession().add(s)
-    models.DBSession().commit()
+    with status(f"Creating dummy source 14gqr"):
+        s = models.Source(ra=353.36647, dec=33.646149, red_shift=0.063,
+                          name='14gqr')
+        s.comments = [models.Comment(text="No source at transient location to"
+                                     "R>26 in LRIS imaging", user=u),
+                      models.Comment(text="Strong calcium lines have emerged.",
+                                     user=u)]
+        start = pd.Timestamp('2015-07-01')
+        end = pd.Timestamp('2015-08-01')
+        times = pd.to_datetime(np.linspace(start.value, end.value, 100))
+        s.photometry = [models.Photometry(obs_time=t, mag=np.random.uniform(19, 22),
+                                          e_mag=np.random.uniform(0, 0.5),
+                                          lim_mag=np.random.uniform(22, 25),
+                                          filter=np.random.choice(['R', 'G']))
+                        for t in times]
+        models.DBSession().add(s)
+        models.DBSession().commit()
 
 if __name__ == "__main__":
     insert_test_data()
