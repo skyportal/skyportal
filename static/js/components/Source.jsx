bf6141861ba661f632b58c69c3611d028996d0fa
Stefan van der Walt <stefanv@berkeley.edu>
2017-06-23 14:21:23 -0700

    Add plotting

--- static/js/components/Source.jsx
+++ static/js/components/Source.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import Plot from './Plot'
 
-const Source = ({ ra, dec, red_shift, name }) => (
+const Source = ({ ra, dec, red_shift, name, id }) => (
   <div>
     <hr/>
     <b>{name} (location: {ra}, {dec})</b>
     <hr/>
     <b>Red Shift:</b>{red_shift}<br/>
-    <b>Photometry:</b>
-
+    <b>Photometry ({id}):</b><br/>
+    <Plot url={`/plot_photometry/${id}`}/>
   </div>
 );
 
