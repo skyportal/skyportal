51758f231e896f9d693da3f248946e3367ccd40d
Stefan van der Walt <stefanv@berkeley.edu>
2017-06-23 22:25:33 -0700

    Fix frontend tests

--- skyportal/tests/frontend/test_frontpage.py
+++ skyportal/tests/frontend/test_frontpage.py
@@ -5,7 +5,5 @@ from selenium import webdriver
 def test_front_page(driver):
     driver.get("/")
     assert 'localhost' in driver.current_url
-    button = driver.wait_for_xpath("//div[contains(@title,'connected')]")
-    link = driver.find_element_by_partial_link_text('Click here')
-    link.click()
-    driver.wait_for_xpath("//div[contains(text(),'Hello')]")
+    driver.wait_for_xpath("//div[contains(@title,'connected')]")
+    driver.wait_for_xpath('//h1[contains(text(), SkyPortal)]')
