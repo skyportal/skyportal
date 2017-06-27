d3d2ad7ac4afe4af7852d0d98185b5643767b20d
Brett Naul <brettnaul@gmail.com>
2017-06-27 12:50:10 -0700

    More preliminary schema and test data

--- skyportal/model_util.py
+++ skyportal/model_util.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import numpy as np
 import pandas as pd
@@ -29,16 +30,31 @@ if __name__ == "__main__":
 
     with status(f"Creating dummy source 14gqr"):
         s = models.Source(ra=353.36647, dec=33.646149, red_shift=0.063,
-                          name='14gqr')
+                          id='14gqr')
         s.comments = [models.Comment(text="No source at transient location to"
                                      "R>26 in LRIS imaging", user=u),
                       models.Comment(text="Strong calcium lines have emerged.",
                                      user=u)]
+
+        t = models.Telescope(name='Palomar 1.5m', nickname='P60',
+                             lat=33.3633675, lon=-116.8361345, elevation=1870,
+                             diameter=1.5)
+        i = models.Instrument(telescope=t, name='P60 Camera', type='phot',
+                              band='optical')
+
         phot_file = os.path.join(os.path.dirname(__file__), 'tests', 'data',
                                  'phot.csv')
         phot_data = pd.read_csv(phot_file)
-        s.photometry = [models.Photometry(**row)
-                        for i, row in phot_data.iterrows()]
+        s.photometry = [models.Photometry(instrument=i, **row)
+                        for j, row in phot_data.iterrows()]
+
+        spec_file = os.path.join(os.path.dirname(__file__), 'tests', 'data',
+                                 'spec.csv')
+        spec_data = np.loadtxt(spec_file)
+        s.spectra = [models.Spectrum(instrument=i,
+                                     observed_at=datetime.datetime(2014, 10, 24),
+                                     wavelengths=spec_data[:, 0],
+                                     fluxes=spec_data[:, 1], errors=None)]
 
         models.DBSession().add(s)
         models.DBSession().commit()
--- skyportal/models.py
+++ skyportal/models.py
@@ -3,15 +3,42 @@ import pandas as pd
 
 import sqlalchemy as sa
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects import postgresql as psql
 
 from baselayer.app.models import init_db, Base, DBSession, User
+
+
+class Source(Base):
+    id = sa.Column(sa.String(5), primary_key=True)
+    ra = sa.Column(sa.Float)
+    dec = sa.Column(sa.Float)
+    red_shift = sa.Column(sa.Float, nullable=True)
+
+
+class Telescope(Base):
+    name = sa.Column(sa.String, nullable=False)
+    nickname = sa.Column(sa.String, nullable=False)
+    lat = sa.Column(sa.Float, nullable=False)
+    lon = sa.Column(sa.Float, nullable=False)
+    elevation = sa.Column(sa.Float, nullable=False)
+    diameter = sa.Column(sa.Float, nullable=False)
+
+
+class Instrument(Base):
+    telescope_id = sa.Column(sa.ForeignKey('telescopes.id',
+                                           ondelete='CASCADE'),
+                             nullable=False, index=True)
+    telescope = relationship('Telescope', backref='instruments')
+    name = sa.Column(sa.String, nullable=False)
+    type = sa.Column(sa.String, nullable=False)
+    band = sa.Column(sa.String, nullable=False)
 
 
 class Comment(Base):
     user_id = sa.Column(sa.ForeignKey('users.id', ondelete='CASCADE'),
                         nullable=False, index=True)
     user = relationship('User', backref='comments')
-    text = sa.Column(sa.String(), nullable=False)
+    text = sa.Column(sa.String, nullable=False)
     source_id = sa.Column(sa.ForeignKey('sources.id', ondelete='CASCADE'),
                           nullable=False, index=True)
     source = relationship('Source', backref='comments')
@@ -22,15 +49,29 @@ class Photometry(Base):
     source_id = sa.Column(sa.ForeignKey('sources.id', ondelete='CASCADE'),
                           nullable=False, index=True)
     source = relationship('Source', backref='photometry')
-    obs_time = sa.Column(sa.DateTime())
-    mag = sa.Column(sa.Float())
-    e_mag = sa.Column(sa.Float())
-    lim_mag = sa.Column(sa.Float())
-    filter = sa.Column(sa.String())
+    instrument_id = sa.Column(sa.ForeignKey('instruments.id',
+                                            ondelete='CASCADE'),
+                              nullable=False, index=True)
+    instrument = relationship('Instrument', backref='photometry')
+    obs_time = sa.Column(sa.DateTime)
+    mag = sa.Column(sa.Float)
+    e_mag = sa.Column(sa.Float)
+    lim_mag = sa.Column(sa.Float)
+    filter = sa.Column(sa.String)  # TODO -> filters table?
 
 
-class Source(Base):
-    name = sa.Column(sa.String())
-    ra = sa.Column(sa.Float())
-    dec = sa.Column(sa.Float())
-    red_shift = sa.Column(sa.Float(), nullable=True)
+class Spectrum(Base):
+    __tablename__ = 'spectra'
+    source_id = sa.Column(sa.ForeignKey('sources.id', ondelete='CASCADE'),
+                          nullable=False, index=True)
+    source = relationship('Source', backref='spectra')
+    observed_at = sa.Column(sa.DateTime, nullable=False)
+    # TODO program?
+    instrument_id = sa.Column(sa.ForeignKey('instruments.id',
+                                            ondelete='CASCADE'),
+                              nullable=False, index=True)
+    instrument = relationship('Instrument', backref='spectra')
+    # TODO better numpy integration
+    wavelengths = sa.Column(psql.ARRAY(sa.Float), nullable=False)
+    fluxes = sa.Column(psql.ARRAY(sa.Float), nullable=False)
+    errors = sa.Column(psql.ARRAY(sa.Float))
