832cecb560a0724869b7f268141b409cc0465b70
Stefan van der Walt <stefanv@berkeley.edu>
2017-06-30 12:03:45 -0700

    Minor restyling of source page

--- static/js/components/Source.jsx
+++ static/js/components/Source.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import PlotContainer from '../containers/PlotContainer'
 
+import styles from "./Source.css";
+
 
 const Source = ({ ra, dec, red_shift, id }) => {
   if (id === undefined) {
     return <div>Source not found</div>
   } else {
     return (
-      <div>
-        <hr/>
-        <b>{id} (location: {ra}, {dec})</b>
-        <hr/>
-        <b>Red Shift: </b>{red_shift}<br/>
-        <b>Photometry:</b><br/>
+      <div className={styles.source}>
+        <div className={styles.name}>{id}</div>
+
+        <b>Location:</b> {ra}, {dec}<br/>
+        <b>Red Shift: </b>{red_shift}
+
+        <br/>
+        <b>Photometry:</b>
         <PlotContainer url={`/plot_photometry/${id}`}/>
+
+        <br/>
         <b>Spectroscopy:</b><br/>
         <PlotContainer url={`/plot_spectroscopy/${id}`}/>
     </div>
