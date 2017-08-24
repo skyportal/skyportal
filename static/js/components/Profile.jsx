81a8fadfcb4a02793882ee587dff7e8b8d3b7dcb
Stefan van der Walt <sjvdwalt@gmail.com>
2017-08-23 17:44:24 -0700

    Add dropdown profile menu (#38)

--- static/js/components/Profile.jsx
+++ static/js/components/Profile.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Dropdown, { DropdownTrigger, DropdownContent } from 'react-simple-dropdown';
+
 import styles from "./Profile.css";
 
 
 const Profile = ({ username }) => (
   <div className={styles.profile}>
-    { username } &nbsp;▾
+    <Dropdown>
+
+      <DropdownTrigger>
+        { username } &nbsp;▾
+      </DropdownTrigger>
+
+      <DropdownContent className={styles.dropdownContent}>
+
+        <Link to="/profile">
+          <div className={styles.entry}>
+            Profile
+          </div>
+        </Link>
+
+        <div className={styles.rule} />
+
+        <div className={styles.entry}>
+          Groups
+        </div>
+
+        <div className={styles.rule} />
+
+        <a href="https://github.com/skyportal/skyportal/issues/new">
+          <div className={styles.entry}>
+            File an issue
+          </div>
+        </a>
+
+        <a href="https://github.com/skyportal/skyportal">
+          <div className={styles.entry}>
+            Help
+          </div>
+        </a>
+
+      </DropdownContent>
+    </Dropdown>
   </div>
 );
 
