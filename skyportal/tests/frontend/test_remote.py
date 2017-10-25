743e9c655546456b21b95bcc9997345a35c54ff6
Brett Naul <brettnaul@gmail.com>
2017-10-25 15:35:21 -0700

    Add simple test for verifying Docker images

---
+++ skyportal/tests/frontend/test_remote.py
@@ -0,0 +1,12 @@
+# These tests can be run against a hosted instance and interact only with the
+# server through the public-facing web interface (rather than injecting
+# fixture data directly into the database). Used for testing broadly that a
+# server or Docker image was started successfully.
+
+
+def test_remote(driver):
+    # TODO expand to cover the basics of all site functionality
+    # (c.f. `test_pipeline_sequentially` from `cesium_web`)
+    driver.get("/")
+    assert 'localhost' in driver.current_url
+    driver.wait_for_xpath('//h2[contains(text(), "List of Sources")]')
