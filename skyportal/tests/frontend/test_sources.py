d2daff3bb190bfe792b2721c8d99cfeaae828fa1
Ari Crellin-Quick <a.crellinquick@gmail.com>
2018-06-08 17:04:29 -0700

    Add create token user & token auth / get source test

--- skyportal/tests/frontend/test_sources.py
+++ skyportal/tests/frontend/test_sources.py
@@ -1,6 +1,8 @@
 import pytest
+import uuid
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from skyportal.model_util import create_token_user
 
 
 def test_public_source_page(driver, user, public_source):
@@ -20,3 +22,13 @@ def test_comments(driver, user, public_source):
     comment_box.send_keys(comment_text)
     driver.find_element_by_css_selector('[type=submit]').click()
     driver.wait_for_xpath(f'//div[text()="{comment_text}"]')
+
+
+def test_token_user_retrieving_source(driver, public_group, public_source):
+    auth_token = create_token_user(uuid.uuid4(), [public_group.id])
+    response = driver.request(
+        'GET', f'{driver.server_url}/api/sources/{public_source.id}',
+        json={'token': auth_token}).json()
+    assert response['status'] == 'success'
+    assert all(k in response['data'] for k in ['ra', 'dec', 'red_shift',
+                                               'created_at', 'id'])
