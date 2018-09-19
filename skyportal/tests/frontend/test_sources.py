53937a5891a55e3a0a39bfd13aaf5064245cae25
Ari Crellin-Quick <a.crellinquick@gmail.com>
2018-09-19 11:02:33 -0700

    Add comment attachment tests

--- skyportal/tests/frontend/test_sources.py
+++ skyportal/tests/frontend/test_sources.py
@@ -1,8 +1,15 @@
 import pytest
 import uuid
+import os
+from os.path import join as pjoin
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from skyportal.model_util import create_token
+from baselayer.app.config import load_config
+
+
+cfg = load_config()
 
 
 def test_public_source_page(driver, user, public_source):
@@ -22,6 +29,45 @@ def test_comments(driver, user, public_source):
     comment_box.send_keys(comment_text)
     driver.find_element_by_css_selector('[type=submit]').click()
     driver.wait_for_xpath(f'//div[text()="{comment_text}"]')
+
+
+def test_upload_comment_attachment(driver, user, public_source):
+    driver.get(f"/become_user/{user.id}")  # TODO decorator/context manager?
+    driver.get(f"/source/{public_source.id}")
+    driver.wait_for_xpath(f'//div[text()="{public_source.id}"]')
+    comment_box = driver.find_element_by_css_selector('[name=comment]')
+    comment_text = 'Test comment'
+    comment_box.send_keys(comment_text)
+    attachment_file = driver.find_element_by_css_selector('input[type=file]')
+    attachment_file.send_keys(pjoin(os.path.dirname(os.path.dirname(__file__)),
+                                    'data', 'spec.csv'))
+    driver.find_element_by_css_selector('[type=submit]').click()
+    driver.wait_for_xpath(f'//div[text()="{comment_text}"]')
+    driver.wait_for_xpath('//a[text()="spec.csv"]')
+
+
+def test_download_comment_attachment(driver, user, public_source):
+    driver.get(f"/become_user/{user.id}")  # TODO decorator/context manager?
+    driver.get(f"/source/{public_source.id}")
+    driver.wait_for_xpath(f'//div[text()="{public_source.id}"]')
+    comment_box = driver.find_element_by_css_selector('[name=comment]')
+    comment_text = 'Test comment'
+    comment_box.send_keys(comment_text)
+    attachment_file = driver.find_element_by_css_selector('input[type=file]')
+    attachment_file.send_keys(pjoin(os.path.dirname(os.path.dirname(__file__)),
+                                    'data', 'spec.csv'))
+    driver.find_element_by_css_selector('[type=submit]').click()
+    driver.wait_for_xpath(f'//div[text()="{comment_text}"]')
+    driver.wait_for_xpath('//a[text()="spec.csv"]').click()
+    time.sleep(0.5)
+    fpath = str(os.path.abspath(pjoin(cfg['paths:downloads_folder'], 'spec.csv')))
+    assert(os.path.exists(fpath))
+    try:
+        with open(fpath) as f:
+            l = f.read()
+        assert l.split('\n')[0] == 'wavelength,flux,instrument_id'
+    finally:
+        os.remove(fpath)
 
 
 def test_token_user_retrieving_source(driver, public_group, public_source):
