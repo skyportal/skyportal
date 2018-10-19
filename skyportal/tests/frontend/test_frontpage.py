b18b5d1385ca1e4cfd7d3b8c49e033d2933eda14
Ari Crellin-Quick <a.crellinquick@gmail.com>
2018-10-19 00:12:37 -0700

    Update frontend tests

--- skyportal/tests/frontend/test_frontpage.py
+++ skyportal/tests/frontend/test_frontpage.py
@@ -9,4 +9,5 @@ def test_front_page(driver, user, public_source, private_source):
     driver.wait_for_xpath("//div[contains(@title,'connected')]")
     driver.wait_for_xpath('//h2[contains(text(), "Sources")]')
     driver.wait_for_xpath(f'//a[text()="{public_source.id}"]')
+    driver.wait_for_xpath(f'//td[text()="{public_source.simbad_class}"]')
     driver.wait_for_xpath_missing('//a[text()="{private_source.id}"]')
