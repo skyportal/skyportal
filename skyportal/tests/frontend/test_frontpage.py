1621a39c125ccb7e342279f6d2219e11a20b620b
Ari Crellin-Quick <a.crellinquick@gmail.com>
2018-11-15 16:23:43 -0800

    Add source filtering test

--- skyportal/tests/frontend/test_frontpage.py
+++ skyportal/tests/frontend/test_frontpage.py
@@ -19,13 +19,14 @@ def test_source_list(driver, user, public_source, private_source):
     assert not el.is_enabled()
 
 
-def test_source_list_pagination(driver, user, public_sources_205):
+def test_source_filtering_and_pagination(driver, user, public_sources_205):
     driver.get(f"/become_user/{user.id}")  # TODO decorator/context manager?
     assert 'localhost' in driver.current_url
     driver.get('/')
     driver.wait_for_xpath("//div[contains(@title,'connected')]")
     driver.wait_for_xpath('//h2[contains(text(), "Sources")]')
     driver.wait_for_xpath(f'//td[text()="{public_sources_205[0].simbad_class}"]')
+    # Pagination
     next_button = driver.wait_for_xpath('//button[text()="View Next 100 Sources"]')
     assert next_button.is_enabled()
     prev_button = driver.wait_for_xpath('//button[text()="View Previous 100 Sources"]')
@@ -42,6 +43,14 @@ def test_source_list_pagination(driver, user, public_sources_205):
     prev_button.click()
     time.sleep(1)
     assert not prev_button.is_enabled()
+    # Source filtering
+    assert next_button.is_enabled()
+    source_id = driver.wait_for_xpath("//input[@name='sourceID']")
+    source_id.send_keys('aa')
+    submit = driver.wait_for_xpath("//input[@type='submit']")
+    submit.click()
+    time.sleep(1)
+    assert not next_button.is_enabled()
 
 
 def test_group_list(driver, user, public_group):
