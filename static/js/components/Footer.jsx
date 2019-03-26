09c7ac33b92da6e05033ee43b8a9104b03b30489
Ari Crellin-Quick <a.crellinquick@gmail.com>
2019-03-26 15:41:00 -0700

    Add Footer component and display SkyPortal version number

---
+++ static/js/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './Footer.css';
+
+
+const Footer = ({ version }) => (
+  <div className={styles.footer}>
+    <div className={styles.footerContent}>
+      SkyPortal v{version}. Please file issues at&nbsp;
+      <a href="https://github.com/skyportal/skyportal">
+        https://github.com/skyportal/skyportal
+      </a>
+      .
+    </div>
+  </div>
+);
+Footer.propTypes = {
+  version: PropTypes.string
+};
+Footer.defaultProps = {
+  version: '_unknown_'
+};
+
+export default Footer;
