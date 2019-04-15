80fa58e430c65b55da0cb44c0470bc8306102163
Stefan van der Walt <stefanv@berkeley.edu>
2019-04-15 16:24:20 -0700

    doctr: revert single file deployment instruction

--- .travis/deploy_docs.sh
+++ .travis/deploy_docs.sh
@@ -6,8 +6,7 @@ if [[ $TRAVIS_PULL_REQUEST == false && \
       $TRAVIS_BRANCH == "master" ]]
 then
     pip install doctr
-    doctr deploy . --deploy-repo "skyportal/docs"
-    doctr deploy . --built-docs redoc-static.html
+    doctr deploy . --deploy-repo skyportal/docs
 else
     echo "-- will only push docs from master --"
 fi
