9aee341eebfbaa1b9aaf38596f01087ccaaf9a87
Ari Crellin-Quick <a.crellinquick@gmail.com>
2019-05-01 10:08:11 -0700

    Change how __version__ is imported to avoid circular dependencies

--- skyportal/handlers/base.py
+++ skyportal/handlers/base.py
@@ -1,16 +1,16 @@
 from baselayer.app.handlers.base import BaseHandler as BaselayerHandler
-import skyportal
+from .. import __version__
 
 
 class BaseHandler(BaselayerHandler):
     def success(self, *args, **kwargs):
         data = kwargs.get('data', {})
-        data['version'] = skyportal.__version__
+        data['version'] = __version__
         kwargs['data'] = data
         super().success(*args, **kwargs)
 
     def error(self, *args, **kwargs):
         data = kwargs.get('data', {})
-        data['version'] = skyportal.__version__
+        data['version'] = __version__
         kwargs['data'] = data
         super().error(*args, **kwargs)
