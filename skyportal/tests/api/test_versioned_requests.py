653ebca9111bd930ebb1bf71ea50674d8932323f
Ari Crellin-Quick <a.crellinquick@gmail.com>
2019-05-01 16:29:19 -0700

    Add sysinfo API test

---
+++ skyportal/tests/api/test_versioned_requests.py
@@ -0,0 +1,8 @@
+from skyportal.tests import api
+import skyportal
+
+
+def test_versioned_request(view_only_token, public_source):
+    status, data = api('GET', 'sources/{public_source.id}',
+                   token=view_only_token)
+    assert data['data']['version'] == skyportal.__version__
