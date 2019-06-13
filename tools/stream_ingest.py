e7b88355bc5f614063b310484652cae64db37a7c
Ari Crellin-Quick <a.crellinquick@gmail.com>
2019-06-12 17:07:55 -0700

    Update stream_ingest with telescope, photometry, instrument entries

--- tools/stream_ingest.py
+++ tools/stream_ingest.py
@@ -9,11 +9,12 @@ import argparse
 import sys
 import os
 import requests
+import numpy as np
 
 from lsst.alert.stream import alertConsumer
 import baselayer
 from baselayer.app.config import load_config
-from skyportal.models import init_db, Token
+from skyportal.models import init_db, Token, Source, Telescope, Instrument, Photometry, DBSession
 from skyportal.model_util import create_token
 
 
@@ -46,34 +47,44 @@ def write_stamp_file(stamp_dict, output_dir):
     return
 
 
-def post_to_skyportal(data):
-    token = Token.query.filter(Token.name=='alert_stream_token').first()
-    if not token:
-        token = create_token(1, ['Upload data'], name='alert_stream_token')
-    else:
-        token = token.id
+def post_to_skyportal(data, token, instrument_id):
     headers = {'Authorization': f'token {token}'}
-    r = requests.post('http://localhost:5000/api/sources',
+    source = Source.query.get(str(data['diaSource']['diaSourceId']))
+    if not source:
+        r = requests.post('http://localhost:5000/api/sources',
                       headers=headers,
                       json={'ra': data['diaSource']['ra'],
                             'dec': data['diaSource']['decl'],
-                            'id': str(data['alertId']),
+                            'id': str(data['diaSource']['diaSourceId']),
                             'group_ids': [1]})
+    try:
+        r = requests.post('http://localhost:5000/api/photometry',
+                          headers=headers,
+                          json={'sourceID': str(data['diaSource']['diaSourceId']),
+                                'obsTime': data['diaObject']['radecTai'],
+                                'mag': data['diaSource']['totFlux'],
+                                'e_mag': data['diaSource']['totFluxErr'],
+                                'timeScale': 'tai',
+                                'timeFormat': 'mjd',
+                                'instrumentID': instrument_id,
+                                'lim_mag': 28,
+                                'filter': ''})
+    except:
+        print(data)
+        raise
     print(data['alertId'], r.json())
 
 
-def alert_filter(alert, stampdir=None):
+def alert_filter(alert, skyportal_token, instrument_id, stampdir=None):
     """Filter to apply to each alert.
        See schemas here: https://github.com/lsst-dm/sample-avro-alert
     """
     data = msg_text(alert)
     if data:  # Write your condition statement here
-        post_to_skyportal(data)
+        post_to_skyportal(data, skyportal_token, instrument_id)
         if stampdir:  # Collect all postage stamps **There are no stamps**
-            write_stamp_file(
-                alert.get('cutoutDifference'), stampdir)
-            write_stamp_file(
-                alert.get('cutoutTemplate'), stampdir)
+            write_stamp_file(alert.get('cutoutDifference'), stampdir)
+            write_stamp_file(alert.get('cutoutTemplate'), stampdir)
     return
 
 
@@ -104,7 +115,35 @@ def main():
     else:
         conf['group.id'] = os.environ['HOSTNAME']
 
+
+    # Retrieve or generate token for SkyPortal API auth
+    token = Token.query.filter(Token.name == 'alert_stream_token').first()
+    if not token:
+        token = create_token(1, ['Upload data'], name='alert_stream_token')
+    else:
+        token = token.id
+
+
+    # Retrieve or generate telescope & instrument ID
+    telescope = Telescope.query.filter(Telescope.name == 'LSST').first()
+    if not telescope:
+        telescope = Telescope(name='LSST', nickname='LSST', lat=-30.245, lon=-70.749,
+                              elevation=2663, diameter=8.4)
+    instrument = Instrument.query.filter(Instrument.name == 'LSST Camera').first()
+    if not instrument:
+        instrument = Instrument(telescope=telescope, name='LSST Camera', type='phot',
+                                band='optical')
+        DBSession.add(instrument)
+        DBSession.commit()
+    instrument_id = instrument.id
+
+
+
     # Start consumer and print alert stream
+    # alert_ids = []
+    # source_ids = []
+    # parent_source_ids = []
+    # dia_object_ids = []
     with alertConsumer.AlertConsumer(args.topic, **conf) as streamReader:
         msg_count = 0
         while True:
@@ -117,7 +156,17 @@ def main():
                     msg_count += 1
                     if msg_count % args.interval == 0:
                         # Apply filter to each alert
-                        alert_filter(msg, args.stampDir)
+                        alert_filter(msg, token, instrument_id, args.stampDir)
+                        # data = msg_text(msg)
+                        # source_ids.append(data['diaSource']['diaSourceId'])
+                        # alert_ids.append(data['alertId'])
+                        # parent_source_ids.append(data['diaSource']['parentDiaSourceId'])
+                        # dia_object_ids.append(data['diaSource']['diaObjectId'])
+                        # print("number of unique source IDs:", len(np.unique(source_ids)))
+                        # print("number of unique alert IDs:", len(np.unique(alert_ids)))
+                        # print("number of unique parent source IDs:", len(np.unique([x for x in parent_source_ids if x is not None])))
+                        # print("number of unique dia object IDs:", len(np.unique(dia_object_ids)))
+                        # print('\n')
 
             except alertConsumer.EopError as e:
                 # Write when reaching end of partition
@@ -129,7 +178,6 @@ def main():
             except KeyboardInterrupt:
                 sys.stderr.write('%% Aborted by user\n')
                 sys.exit()
-
 
 if __name__ == "__main__":
     main()
