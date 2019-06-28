044738cd42b199c317fb87852e227ec87b559e28
Ari Crellin-Quick <a.crellinquick@gmail.com>
2019-06-28 13:12:12 -0700

    Run alert stream demo test on Travis

--- .travis/travis_script.sh
+++ .travis/travis_script.sh
@@ -18,6 +18,13 @@ make ${TEST_TARGET}
 
 section_end "Tests"
 
+section "Test.alert.stream.demo"
+
+make load_demo_data
+make alert_stream_demo
+
+section_end "Test.alert.stream.demo"
+
 
 section "Build.docs"
 
