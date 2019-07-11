94ac9a652b57799b9b44d97db3123c42cffc700f
Ari Crellin-Quick <a.crellinquick@gmail.com>
2019-07-11 16:48:57 -0700

    Add stream ingestion test

--- .travis/travis_script.sh
+++ .travis/travis_script.sh
@@ -18,13 +18,6 @@ make ${TEST_TARGET}
 
 section_end "Tests"
 
-section "Test.alert.stream.demo"
-
-make load_demo_data
-make alert_stream_demo
-
-section_end "Test.alert.stream.demo"
-
 
 section "Build.docs"
 
---
+++ skyportal/tests/test_stream_ingest.py
@@ -0,0 +1,70 @@
+import time
+import os
+import shutil
+import subprocess
+import inspect
+import requests
+
+import baselayer
+from baselayer.app.config import load_config
+from skyportal.models import init_db, Token, Source, Telescope, Instrument, Photometry, DBSession
+from skyportal.model_util import create_token
+
+
+conn = init_db(**load_config()['database'])
+
+
+def test_stream_ingest():
+    #skyportal_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    skyportal_root = os.path.dirname(os.path.dirname(os.path.dirname(
+        os.path.abspath(inspect.getsourcefile(lambda:0)))))
+    print(skyportal_root)
+    os.chdir(skyportal_root)
+    print("\n\n", os.getcwd(), "\n\n")
+    db_proc = subprocess.run('make load_demo_data',
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             shell=True)
+    print('\n\nmake load_demo_data output:\n\n', str(db_proc.stdout), str(db_proc.stderr), '\n\n')
+    run_proc = subprocess.Popen("make run",
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                shell=True)
+    time.sleep(10)
+    # Retrieve or generate token for SkyPortal API auth
+    token = Token.query.filter(Token.name == 'alert_stream_token').first()
+    if not token:
+        token = create_token(1, ['Upload data'], name='alert_stream_token')
+    else:
+        token = token.id
+
+    r = requests.get('http://localhost:5000/api/sources',
+                     headers={'Authorization': f'token {token}'})
+    assert len(r.json()['data']['sources']) == 2
+    proc = subprocess.Popen("make alert_stream_demo",
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            shell=True)
+
+    for i in range(30):
+        out = str(proc.stdout.readline())
+        app_out = str(run_proc.stdout.readline())
+        r = requests.get('http://localhost:5000/api/sources',
+                         headers={'Authorization': f'token {token}'})
+        n_sources = len(r.json()['data']['sources'])
+        if n_sources > 2:
+            print("\n\n\n\nYES!!!!!!!!!!\n\n\n\n")
+            run_proc.terminate()
+            proc.terminate()
+            break
+        else:
+            print("\n\n\nNot yet...", out, '\n', app_out, "\n\n\n")
+            time.sleep(2)
+    else:
+        print('Stream ingestion test failed - no ouput indicating success.')
+        print(out)
+        raise Exception('test failed...')
+
+
+if __name__ == '__main__':
+    test_stream_ingest()
