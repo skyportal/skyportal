accd1658d47a435b37938603ad24622b62e42b50
Ari Crellin-Quick <a.crellinquick@gmail.com>
2019-09-23 13:10:44 -0700

    Add tests for creating/deleting tokens via front-end

---
+++ skyportal/tests/frontend/test_tokens.py
@@ -0,0 +1,36 @@
+import pytest
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+import time
+import uuid
+
+
+def test_add_token(driver, user, public_group):
+    token_name = str(uuid.uuid4())
+    driver.get(f'/become_user/{user.id}')
+    driver.get('/profile')
+    driver.wait_for_xpath('//input[@name="acls_Comment"]').click()
+    driver.wait_for_xpath('//input[@name="acls_Upload data"]').click()
+    group_select = Select(driver.wait_for_xpath('//select[@name="group_id"]'))
+    group_select.select_by_value(str(public_group.id))
+    driver.wait_for_xpath('//input[@name="name"]').send_keys(token_name)
+    driver.wait_for_xpath('//input[@value="Generate Token"]').click()
+    time.sleep(1)
+    driver.refresh()
+    driver.wait_for_xpath(f'//td[contains(.,"{token_name}")]')
+
+
+def test_delete_token(driver, user, public_group,
+                      view_only_token_created_by_fulluser):
+    driver.get(f'/become_user/{user.id}')
+    driver.get('/profile')
+    driver.wait_for_xpath(f'//input[@value="{view_only_token_created_by_fulluser}"]')
+    driver.wait_for_xpath('//a[contains(text(),"Delete")]').click()
+    time.sleep(1)
+    driver.refresh()
+    try:
+        driver.wait_for_xpath(f'//input[@value="{view_only_token_created_by_fulluser}"]')
+    except:
+        pass
+    else:
+        raise Exception('Token row not deleted...')
