024880dc6ce1469c826150773971cd33c26048c9
Ari Crellin-Quick <a.crellinquick@gmail.com>
2019-09-26 11:10:51 -0700

    Clear auth token on become_user and logout; update frontend tests

--- skyportal/handlers/logout.py
+++ skyportal/handlers/logout.py
@@ -8,4 +8,7 @@ class LogoutHandler(BaseHandler):
     @auth_or_token
     def get(self):
         self.clear_cookie('user_id')
+        # Not strictly speaking necessary -- upon next websocket connection,
+        # auth will fail and token will refresh
+        self.clear_cookie('auth_token')
         self.redirect('/')
