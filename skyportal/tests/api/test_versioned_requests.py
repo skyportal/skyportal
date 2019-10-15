cdfa8dc573e02970c4a242e3e9b6ec36dd5c72ac
Ari Crellin-Quick <a.crellinquick@gmail.com>
2019-10-15 10:59:24 -0700

    Add git commit date and hash to skyportal version number if dev version

--- skyportal/tests/api/test_versioned_requests.py
+++ skyportal/tests/api/test_versioned_requests.py
@@ -6,3 +6,5 @@ def test_versioned_request(view_only_token, public_source):
     status, data = api('GET', 'sources/{public_source.id}',
                    token=view_only_token)
     assert data['data']['version'] == skyportal.__version__
+    if 'dev' in skyportal.__version__:
+        assert '+git' in data['data']['version']
