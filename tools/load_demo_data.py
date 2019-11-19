316d843c18a8635aea1140e4d97814ea3f01b024
Ari Crellin-Quick <a.crellinquick@gmail.com>
2019-11-19 15:29:50 -0800

    Update baselayer

--- tools/load_demo_data.py
+++ tools/load_demo_data.py
@@ -5,4 +5,5 @@ from skyportal.model_util import load_demo_data
 if __name__ == "__main__":
     """Insert test data"""
     env, cfg = load_env()
+    print(f"Running load_demo_data with cfg['database']: {cfg['database']}")
     load_demo_data(cfg)
