a832af9a7e455a0c3ab8fb536069be7fefbbae30
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-02-21 17:18:00 -0800

    Move SourceView index from source_id to created_at column

--- skyportal/handlers/api/internal/source_views.py
+++ skyportal/handlers/api/internal/source_views.py
@@ -1,6 +1,7 @@
 import datetime
 from sqlalchemy import func, desc
-from baselayer.app.access import permissions, auth_or_token
+import tornado.web
+from baselayer.app.access import auth_or_token
 from ...base import BaseHandler
 from ....models import (
     DBSession, Source, SourceView, GroupSource
@@ -12,7 +13,7 @@ class SourceViewsHandler(BaseHandler):
     def get(self):
         prefs = (self.current_user.preferences if
                  hasattr(self.current_user, 'preferences') and
-                 self.current_user.preferences else {})
+                 self.current_user.preferences is not None else {})
         if 'topSources' in prefs and 'maxNumSources' in prefs['topSources']:
             max_num_sources = int(prefs['topSources']['maxNumSources'])
         else:
@@ -31,9 +32,9 @@ class SourceViewsHandler(BaseHandler):
              .order_by(desc('views')).limit(max_num_sources))
         return self.success(data={'sourceViews': q.all()})
 
-    @auth_or_token
+    @tornado.web.authenticated
     def post(self, source_id):
-        # Ensure token/user has access to source
+        # Ensure user has access to source
         s = Source.get_if_owned_by(source_id, self.current_user)
         # This endpoint will only be hit by front-end, so this will never be a token
         register_source_view(source_id=source_id,
--- skyportal/models.py
+++ skyportal/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os.path
 import re
 import requests
@@ -190,9 +191,11 @@ User.sources = relationship('Source', backref='users',
 
 class SourceView(Base):
     source_id = sa.Column(sa.ForeignKey('sources.id', ondelete='CASCADE'),
-                          nullable=False, index=True, unique=False)
+                          nullable=False, unique=False)
     username_or_token_id = sa.Column(sa.String, nullable=False, unique=False)
     is_token = sa.Column(sa.Boolean, nullable=False, default=False)
+    created_at = sa.Column(sa.DateTime, nullable=False, default=datetime.now,
+                           index=True)
 
 
 class Telescope(Base):
