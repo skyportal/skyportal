9b79a3b2f34f7636842d9a9953ef8b501834d31f
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-03-20 12:11:29 -0700

    Update delete candidates cron job

--- jobs/delete_unsaved_candidates.py
+++ jobs/delete_unsaved_candidates.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 
 import datetime
-from skyportal.models import Candidate, DBSession
+from skyportal.models import init_db, Candidate, DBSession
 from baselayer.app.env import load_env
 
 
 env, cfg = load_env()
+init_db(**cfg["database"])
+
 cutoff_datetime = datetime.datetime.now() - datetime.timedelta(
     days=cfg["misc"]["days_to_keep_unsaved_candidates"]
 )
 
-Candidate.query.filter(Candidate.source_id.is_(None)).filter(
+c = Candidate.query.filter(Candidate.source_id.is_(None)).filter(
     Candidate.created_at <= cutoff_datetime
 ).delete()
 
 DBSession.commit()
+
+print("Deleted", c, "unsaved candidates.")
