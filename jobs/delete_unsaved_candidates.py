8e737db57bd1b0e51ba79879017d1b708026de66
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-04-09 16:18:10 -0700

    Check that n_days config file value is an int between 1 and 30 in delete old candidates cron job

--- jobs/delete_unsaved_candidates.py
+++ jobs/delete_unsaved_candidates.py
@@ -8,9 +8,16 @@ from baselayer.app.env import load_env
 env, cfg = load_env()
 init_db(**cfg["database"])
 
-cutoff_datetime = datetime.datetime.now() - datetime.timedelta(
-    days=cfg["misc.days_to_keep_unsaved_candidates"]
-)
+try:
+    n_days = int(cfg["misc.days_to_keep_unsaved_candidates"])
+except ValueError:
+    raise ValueError("Invalid (non-integer) value provided for "
+                     "days_to_keep_unsaved_candidates in config file.")
+
+if not 1 <= n_days <= 30:
+    raise ValueError("days_to_keep_unsaved_candidates must be an integer between 1 and 30")
+
+cutoff_datetime = datetime.datetime.now() - datetime.timedelta(days=n_days)
 
 n_deleted = (
     Source.query
