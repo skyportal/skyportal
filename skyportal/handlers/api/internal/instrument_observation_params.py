cf6517c281a27950d48a71f14f5af25bcfa8d16d
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-04-21 13:54:33 -0700

    Gracefully handle missing instrument obs params file and invalid JSON cases in handler

--- skyportal/handlers/api/internal/instrument_observation_params.py
+++ skyportal/handlers/api/internal/instrument_observation_params.py
@@ -6,6 +6,12 @@ from ...base import BaseHandler
 class InstrumentObservationParamsHandler(BaseHandler):
     @auth_or_token
     def get(self):
-        with open("instrument_observation_params.json") as f:
-            instrument_data = json.load(f)
+        try:
+            with open("instrument_observation_params.json") as f:
+                instrument_data = json.load(f)
+        except FileNotFoundError:
+            return self.error("instrument_observation_params.json does not exist.")
+        except json.JSONDecodeError:
+            return self.error("JSON parse error: instrument_observation_params.json "
+                              "does not contain properly formatted JSON.")
         return self.success(data={"instrumentObsParams": instrument_data})
