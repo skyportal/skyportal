7221ea219926bcbb6a64f35e6fd62aa417ce23e7
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-05-02 13:24:22 -0700

    De-lint plotjs JS files

--- static/js/plotjs/download.js
+++ static/js/plotjs/download.js
@@ -1,54 +1,55 @@
+/* eslint-disable */
 function table_to_csv(source, write_header) {
-    const columns = ['mjd', 'filter', 'flux', 'fluxerr', 'zp', 'zpsys', 'lim_mag'];
-    const nrows = source.get_length();
-    const lines = [];
+  const columns = ['mjd', 'filter', 'flux', 'fluxerr', 'zp', 'zpsys', 'lim_mag'];
+  const nrows = source.get_length();
+  const lines = [];
 
-    if (write_header) {
-        lines.push(columns.join(','));
-    }
+  if (write_header) {
+    lines.push(columns.join(','));
+  }
 
-    for (let i = 0; i < nrows; i++) {
-        let row = [];
-        for (let j = 0; j < columns.length; j++) {
-            const column = columns[j];
-            try {
-                row.push(source.data[column][i].toString());
-            } catch (error) {
-                if (column === 'zp'){
-                    row.push(25.);
-                } else if (column === 'zpsys') {
-                    row.push('ab');
-                } else {
-                    throw error;
-                }
-            }
+  for (let i = 0; i < nrows; i++) {
+    const row = [];
+    for (let j = 0; j < columns.length; j++) {
+      const column = columns[j];
+      try {
+        row.push(source.data[column][i].toString());
+      } catch (error) {
+        if (column === 'zp') {
+          row.push(25.0);
+        } else if (column === 'zpsys') {
+          row.push('ab');
+        } else {
+          throw error;
         }
-        lines.push(row.join(','))
+      }
     }
-    return lines.join('\n').concat('\n')
+    lines.push(row.join(','));
+  }
+  return lines.join('\n').concat('\n');
 }
 
 
 const filename = 'objname.csv';
 filetext = '';
-for (let i=0; i < toggle.labels.length; i++){
-    let write_header = i === 0;
-    if (slider.value > 0) {
-        filetext += table_to_csv(eval('bin' + i).data_source, write_header);
-    } else {
-        filetext += table_to_csv(eval('obs' + i).data_source, write_header);
-    }
+for (let i=0; i < toggle.labels.length; i++) {
+  const write_header = i === 0;
+  if (slider.value > 0) {
+    filetext += table_to_csv(eval(`bin${i}`).data_source, write_header);
+  } else {
+    filetext += table_to_csv(eval(`obs${i}`).data_source, write_header);
+  }
 }
 const blob = new Blob([filetext], { type: 'text/csv;charset=utf-8;' });
 
-//addresses IE
+// addresses IE
 if (navigator.msSaveBlob) {
-    navigator.msSaveBlob(blob, filename)
+  navigator.msSaveBlob(blob, filename);
 } else {
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = filename;
-    link.target = '_blank';
-    link.style.visibility = 'hidden';
-    link.dispatchEvent(new MouseEvent('click'));
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  link.target = '_blank';
+  link.style.visibility = 'hidden';
+  link.dispatchEvent(new MouseEvent('click'));
 }
--- static/js/plotjs/stackm.js
+++ static/js/plotjs/stackm.js
@@ -1,151 +1,141 @@
+/* eslint-disable */
+const binsize = slider.value;
+const fluxalph = ((binsize == 0) ? 1.0 : 0.1);
 
- var binsize = slider.value;
- var fluxalph = ((binsize == 0) ? 1. : 0.1);
+for (let i = 0; i < toggle.labels.length; i++) {
+  const fluxsource = eval(`obs${i}`).data_source;
+  const binsource = eval(`bin${i}`).data_source;
 
- for (var i = 0; i < toggle.labels.length; i++) {
+  const fluxerrsource = eval(`obserr${i}`).data_source;
+  const binerrsource = eval(`binerr${i}`).data_source;
 
-     var fluxsource = eval("obs" + i).data_source;
-     var binsource = eval("bin" + i).data_source;
+  const unobssource = eval(`unobs${i}`).data_source;
+  const unobsbinsource = eval(`unobsbin${i}`).data_source;
 
-     var fluxerrsource = eval("obserr" + i).data_source;
-     var binerrsource = eval("binerr" + i).data_source;
+  const allsource = eval(`all${i}`);
 
-     var unobssource = eval("unobs" + i).data_source;
-     var unobsbinsource = eval("unobsbin" + i).data_source;
+  const minmjd = Math.min.apply(Math, allsource.data.mjd);
 
-     var allsource = eval("all" + i);
+  const date = new Date(); // a new date
+  const time = date.getTime(); // the timestamp, not neccessarely using UTC as current time
+  const maxmjd = ((time / 86400000) - (date.getTimezoneOffset()/1440) + 40587.0);
 
-     var minmjd = Math.min.apply(Math, allsource.data['mjd']);
+  binsource.data.mjd = [];
+  binsource.data.flux = [];
+  binsource.data.fluxerr = [];
+  binsource.data.filter = [];
+  binsource.data.color = [];
+  binsource.data.lim_mag = [];
+  binsource.data.mag = [];
+  binsource.data.magerr = [];
 
-     var date = new Date();     // a new date
-     var time = date.getTime(); // the timestamp, not neccessarely using UTC as current time
-     var maxmjd = ((time / 86400000) - (date.getTimezoneOffset()/1440) + 40587.);
+  binerrsource.data.xs = [];
+  binerrsource.data.ys = [];
+  binerrsource.data.color = [];
 
-     binsource.data['mjd'] = [];
-     binsource.data['flux'] = [];
-     binsource.data['fluxerr'] = [];
-     binsource.data['filter'] = [];
-     binsource.data['color'] = [];
-     binsource.data['lim_mag'] = [];
-     binsource.data['mag'] = [];
-     binsource.data['magerr'] = [];
+  unobsbinsource.data.mjd = [];
+  unobsbinsource.data.lim_mag = [];
+  unobsbinsource.data.color = [];
+  unobsbinsource.data.flux = [];
+  unobsbinsource.data.fluxerr = [];
+  unobsbinsource.data.mag = [];
+  unobsbinsource.data.magerr = [];
+  unobsbinsource.data.filter = [];
 
-     binerrsource.data['xs'] = [];
-     binerrsource.data['ys'] = [];
-     binerrsource.data['color'] = [];
+  for (var j = 0; j < fluxsource.get_length(); j++) {
+    fluxsource.data.alpha[j] = fluxalph;
+    fluxerrsource.data.alpha[j] = fluxalph;
+  }
 
-     unobsbinsource.data['mjd'] = [];
-     unobsbinsource.data['lim_mag'] = [];
-     unobsbinsource.data['color'] = [];
-     unobsbinsource.data['flux'] = [];
-     unobsbinsource.data['fluxerr'] = [];
-     unobsbinsource.data['mag'] = [];
-     unobsbinsource.data['magerr'] = [];
-     unobsbinsource.data['filter'] = [];
+  for (var j = 0; j < unobssource.get_length(); j++) {
+    unobssource.data.alpha[j] = fluxalph;
+  }
 
-     for (var j = 0; j < fluxsource.get_length(); j++){
-         fluxsource.data['alpha'][j] = fluxalph;
-         fluxerrsource.data['alpha'][j] = fluxalph;
-     }
+  if (binsize > 0) {
+    // now do the binning
+    const k = 0;
+    let curmjd = minmjd;
+    const mjdbins = [curmjd];
 
-     for (var j = 0; j < unobssource.get_length(); j++){
-         unobssource.data['alpha'][j] = fluxalph;
-     }
+    while (curmjd < maxmjd) {
+      curmjd += binsize;
+      mjdbins.push(curmjd);
+    }
 
-     if (binsize > 0){
+    const nbins = mjdbins.length - 1;
+    for (let l = 0; l < nbins; l++) {
+      // calculate the flux, fluxerror, and mjd of the bin
+      const flux = [];
+      const weight = [];
+      const mjd = [];
+      const limmag = [];
+      let ivarsum = 0;
 
-         // now do the binning
-         var k = 0;
-         var curmjd = minmjd;
-         var mjdbins = [curmjd];
+      for (let m = 0; m < allsource.get_length(); m++) {
+        if ((allsource.data.mjd[m] < mjdbins[l + 1]) && (allsource.data.mjd[m] >= mjdbins[l])) {
+          const fluxvar = allsource.data.fluxerr[m] * allsource.data.fluxerr[m];
+          const ivar = 1 / fluxvar;
 
-         while (curmjd < maxmjd){
-             curmjd += binsize;
-             mjdbins.push(curmjd);
-         }
+          weight.push(ivar);
+          flux.push(allsource.data.flux[m]);
+          mjd.push(allsource.data.mjd[m]);
+          limmag.push(allsource.data.lim_mag[m]);
+          ivarsum += ivar;
+        }
+      }
 
-         var nbins = mjdbins.length - 1;
-         for (var l = 0; l < nbins; l++) {
+      let myflux = 0;
+      let mymjd = 0;
 
+      if (weight.length == 0) {
+        continue;
+      }
 
-             // calculate the flux, fluxerror, and mjd of the bin
-             var flux = [];
-             var weight = [];
-             var mjd = [];
-             var limmag = [];
-             var ivarsum = 0;
+      for (let n = 0; n < weight.length; n++) {
+        myflux += weight[n] * flux[n] / ivarsum;
+        mymjd += weight[n] * mjd[n] / ivarsum;
+      }
 
-             for (var m = 0; m < allsource.get_length(); m++){
-                 if ((allsource.data['mjd'][m] < mjdbins[l + 1]) && (allsource.data['mjd'][m] >= mjdbins[l])){
+      const myfluxerr = Math.sqrt(1 / ivarsum);
+      const obs = myflux / myfluxerr > 5;
 
-                     let fluxvar = allsource.data['fluxerr'][m] * allsource.data['fluxerr'][m];
-                     let ivar = 1 / fluxvar;
+      if (obs) {
+        var mymag = -2.5 * Math.log10(myflux) + 25;
+        var mymagerr = Math.abs(-2.5 * myfluxerr / myflux / Math.log(10));
+        var mysource = binsource;
 
-                     weight.push(ivar);
-                     flux.push(allsource.data['flux'][m]);
-                     mjd.push(allsource.data['mjd'][m]);
-                     limmag.push(allsource.data['lim_mag'][m]);
-                     ivarsum += ivar;
-                 }
-             }
+        binerrsource.data.xs.push([mymjd, mymjd]);
+        binerrsource.data.ys.push([mymag - mymagerr, mymag + mymagerr]);
+        binerrsource.data.color.push(allsource.data.color[0]);
+      } else {
+        var mymag = null;
+        var mymagerr = null;
+        var mysource = unobsbinsource;
+      }
 
-             var myflux = 0;
-             var mymjd = 0;
+      if (weight.length > 1) {
+        var mymaglim = -2.5 * Math.log10(5 * myfluxerr) + 25;
+      } else {
+        var mymaglim = limmag[0];
+      }
 
-             if (weight.length == 0){
-                 continue;
-             }
+      mysource.data.mjd.push(mymjd);
+      mysource.data.flux.push(myflux);
+      mysource.data.fluxerr.push(myfluxerr);
+      mysource.data.filter.push(allsource.data.filter[0]);
+      mysource.data.color.push(allsource.data.color[0]);
+      mysource.data.mag.push(mymag);
+      mysource.data.magerr.push(mymagerr);
+      mysource.data.lim_mag.push(mymaglim);
+    }
+  }
 
-             for (var n = 0; n < weight.length; n++){
-                 myflux += weight[n] * flux[n] / ivarsum;
-                 mymjd += weight[n] * mjd[n] / ivarsum;
-             }
+  fluxsource.change.emit();
+  binsource.change.emit();
 
-             var myfluxerr = Math.sqrt(1 / ivarsum);
-             var obs = myflux / myfluxerr > 5;
+  fluxerrsource.change.emit();
+  binerrsource.change.emit();
 
-             if (obs){
-                 var mymag = -2.5 * Math.log10(myflux) + 25;
-                 var mymagerr = Math.abs(-2.5 * myfluxerr  / myflux / Math.log(10));
-                 var mysource = binsource;
-
-                 binerrsource.data['xs'].push([mymjd, mymjd]);
-                 binerrsource.data['ys'].push([mymag - mymagerr, mymag + mymagerr]);
-                 binerrsource.data['color'].push(allsource.data['color'][0]);
-
-             } else {
-                 var mymag = null;
-                 var mymagerr = null;
-                 var mysource = unobsbinsource;
-
-             }
-
-             if (weight.length > 1) {
-                 var mymaglim = -2.5 * Math.log10(5 * myfluxerr) + 25;
-             } else {
-                 var mymaglim = limmag[0];
-             }
-
-             mysource.data['mjd'].push(mymjd);
-             mysource.data['flux'].push(myflux);
-             mysource.data['fluxerr'].push(myfluxerr);
-             mysource.data['filter'].push(allsource.data['filter'][0]);
-             mysource.data['color'].push(allsource.data['color'][0]);
-             mysource.data['mag'].push(mymag);
-             mysource.data['magerr'].push(mymagerr);
-             mysource.data['lim_mag'].push(mymaglim);
-
-
-         }
-     }
-
-     fluxsource.change.emit();
-     binsource.change.emit();
-
-     fluxerrsource.change.emit();
-     binerrsource.change.emit();
-
-     unobssource.change.emit();
-     unobsbinsource.change.emit();
-
- }
+  unobssource.change.emit();
+  unobsbinsource.change.emit();
+}
