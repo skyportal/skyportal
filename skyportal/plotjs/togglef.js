30110a5c7fa4054e4c982a504dae5f378ea3233c
Danny Goldstein <dgold@berkeley.edu>
2020-05-02 11:35:03 -0400

    move plotjs to static

--- skyportal/plotjs/togglef.js
+++
@@ -1,6 +0,0 @@
-for (let i = 0; i < toggle.labels.length; i++) {
-    eval("obs" + i).visible = (toggle.active.includes(i));
-    eval("obserr" + i).visible = (toggle.active.includes(i));
-    eval("bin" + i).visible = (toggle.active.includes(i));
-    eval("binerr" + i).visible = (toggle.active.includes(i));
-}
