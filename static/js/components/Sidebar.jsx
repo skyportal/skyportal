ab73a2c4f11c5a809e95207a4ee91fb210edae6f
jialin-wu-02 <jialin.wu.02@gmail.com>
2020-05-01 17:57:14 -0700

    Given sidebar a darker color reflecting the design in https://www.figma.com/file/SYUco4bA2Rq6nMFX2LijlT/homepage?node-id=2%3A0
    - Adding custom icons.
    - Fixing the width of the sidebar.
    - TODO: seperate components into pages.
    - TODO: build a color theme (palette) for skyportal.

--- static/js/components/Sidebar.jsx
+++ static/js/components/Sidebar.jsx
@@ -7,11 +7,8 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import FlareIcon from '@material-ui/icons/Flare';
-import AccountBoxIcon from '@material-ui/icons/AccountBox';
 
-const drawerWidth = 170;
+const drawerWidth = 180;
 
 const useStyles = makeStyles(theme => ({
   drawer: {
@@ -20,40 +17,48 @@ const useStyles = makeStyles(theme => ({
   },
   drawerPaper: {
     width: drawerWidth,
+    paddingLeft: "0.4em",
+    background: "#00045C",
+    color: "#B8D2FF",
+    fontSize: "1.2em"
   },
   toolbar: theme.mixins.toolbar,
+  icon: {
+    width: "1.6em",
+    height: "1.6em"
+  }
 }));
 
 
 const drawer = () => {
     const classes = useStyles();
     const iconMap = {
-        Dashboard: <DashboardIcon/>,
-        Source: <FlareIcon/>,
-        Profile: <AccountBoxIcon />
+        Dashboard: <img className={classes.icon} src="/static/images/home.png" alt="" />,
+        Source: <img className={classes.icon} src="/static/images/search.png" alt="" />,
+        Profile: <img className={classes.icon} src="/static/images/profile.png" alt="" />
     }
 
     return (
-    <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-        paper: classes.drawerPaper,
-        }}
-        anchor="left"
-    >
-        <div className={classes.toolbar} />
-        <Divider />
-        <List>
-        {['Dashboard', 'Source', 'Profile'].map((text, index) => (
-            <ListItem button key={text}>
-            <ListItemIcon> {iconMap[text]} </ListItemIcon>
-            <ListItemText primary={text} />
-            </ListItem>
-        ))}
-        </List>
-        <Divider />
-    </Drawer>
+        <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+            paper: classes.drawerPaper,
+            }}
+            anchor="left"
+        >
+            <div className={classes.toolbar} />
+            <Divider />
+            <List>
+            {['Dashboard', 'Source', 'Profile'].map((text, index) => (
+                <ListItem button key={text}>
+                  <ListItemIcon> {iconMap[text]} </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItem>
+            ))}
+            </List>
+            <Divider />
+        </Drawer>
 )};
 
 export default drawer;
