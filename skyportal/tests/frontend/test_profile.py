93a2e474d1494377571e5315242c1ea6c1a279e0
Stefan van der Walt <stefanv@berkeley.edu>
2020-05-04 10:37:35 -0700

    Fix frontend tests

--- skyportal/tests/frontend/test_profile.py
+++ skyportal/tests/frontend/test_profile.py
@@ -6,16 +6,12 @@ import time
 def test_token_acls_options_rendering1(driver, user):
     driver.get(f'/become_user/{user.id}')
     driver.get('/profile')
-    driver.wait_for_xpath('//input[@name="acls_Comment"]')
-    driver.wait_for_xpath('//input[@name="acls_Upload data"]')
-    driver.wait_for_xpath_to_disappear('//input[@name="acls_Manage sources"]')
+    driver.wait_for_xpath('//input[@name="acls[0]"]')
+    driver.wait_for_xpath('//input[@name="acls[1]"]')
+    driver.wait_for_xpath_to_disappear('//input[@name="acls[2]"]')
 
 def test_token_acls_options_rendering2(driver, super_admin_user):
     driver.get(f'/become_user/{super_admin_user.id}')
     driver.get('/profile')
-    driver.wait_for_xpath('//input[@name="acls_Comment"]')
-    driver.wait_for_xpath('//input[@name="acls_Upload data"]')
-    driver.wait_for_xpath('//input[@name="acls_Manage sources"]')
-    driver.wait_for_xpath('//input[@name="acls_Manage groups"]')
-    driver.wait_for_xpath('//input[@name="acls_Become user"]')
-    driver.wait_for_xpath('//input[@name="acls_System admin"]')
+    for i in range(5):
+        driver.wait_for_xpath(f'//input[@name="acls[{i}]"]')
