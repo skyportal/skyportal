02a5aa2ce645bd05c61f313a0b52684d6be40890
jialin-wu-02 <jialin.wu.02@gmail.com>
2020-05-04 22:24:24 -0700

    Display profile picture, name, and description & update sidebar icons
    - Currently using the material-ui icons (since it is easier to use)
    - Display profile picture, name, and description at the top of the sidebar

--- static/js/components/Sidebar.jsx
+++ static/js/components/Sidebar.jsx
@@ -7,8 +7,15 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 
+import { blue } from '@material-ui/core/colors';
 
-const drawerWidth = 180;
+import FaceIcon from '@material-ui/icons/Face';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+
+
+const drawerWidth = 190;
 
 const useStyles = makeStyles(theme => ({
   drawer: {
@@ -22,7 +29,23 @@ const useStyles = makeStyles(theme => ({
     color: "#B8D2FF",
     fontSize: "1.2em"
   },
-  toolbar: theme.mixins.toolbar,
+  toolbar: {
+    display: "flex",
+    height: "4em",
+    padding: "1em 0em",
+    alignItems: "center",
+  },
+  profileNameBox: {
+    display: "block",
+    padding: "0em 0.3em"
+  },
+  description: {
+    margin: "0em",
+    padding: "0em",
+    fontSize: "0.6em",
+    color: "#ccc"
+  },
+  // theme.mixins.toolbar,
   icon: {
     width: "1.6em",
     height: "1.6em"
@@ -33,32 +56,44 @@ const useStyles = makeStyles(theme => ({
 const drawer = () => {
     const classes = useStyles();
     const iconMap = {
-        Dashboard: <img className={classes.icon} src="/static/images/home.png" alt="" />,
-        Source: <img className={classes.icon} src="/static/images/search.png" alt="" />,
-        Profile: <img className={classes.icon} src="/static/images/profile.png" alt="" />
+        // Dashboard: <img className={classes.icon} src="/static/images/home.png" alt="" />,
+        // Dashboard: <img className={classes.icon} src="/static/images/figma_icon/iconfinder_house_384890.svg" alt="" />,
+        // Source: <img className={classes.icon} src="/static/images/search.png" alt="" />,
+        // Source: <img className={classes.icon} src="/static/images/figma_icon/iconfinder_search_322497.svg" alt="" />,
+        // Profile: <img className={classes.icon} src="/static/images/profile.png" alt="" />
+        Dashboard: <HomeIcon style={{ color: blue[200] }} />,
+        Source: <SearchIcon style={{ color: blue[200] }} />,
+        Profile: <AccountBoxIcon style={{ color: blue[200] }} />
+
     }
 
     return (
-        <Drawer
-            className={classes.drawer}
-            variant="permanent"
-            classes={{
-            paper: classes.drawerPaper,
-            }}
-            anchor="left"
-        >
-            <div className={classes.toolbar} />
-            <Divider />
-            <List>
-            {['Dashboard', 'Source', 'Profile'].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon> {iconMap[text]} </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-            ))}
-            </List>
-            <Divider />
-        </Drawer>
+      <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+          paper: classes.drawerPaper,
+          }}
+          anchor="left"
+      >
+          <div className={classes.toolbar}> 
+            <FaceIcon style={{ fontSize: "4.2em" }}/> 
+            <div className={classes.profileNameBox}>
+              Alex Wu
+              <p className={classes.description}> Front End Dev </p>
+            </div>
+          </div>
+          <Divider />
+          <List>
+          {['Dashboard', 'Source', 'Profile'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon> {iconMap[text]} </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+          ))}
+          </List>
+          <Divider />
+      </Drawer>
 )};
 
 export default drawer;
