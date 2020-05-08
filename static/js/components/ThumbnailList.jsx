329fa45be4841eeaa2c35a021c7357929a85cbac
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-05-08 12:30:01 -0700

    De-lint Thumbnail.jsx

--- static/js/components/ThumbnailList.jsx
+++ static/js/components/ThumbnailList.jsx
@@ -11,7 +11,6 @@ dayjs.extend(calendar);
 
 
 const Thumbnail = ({ ra, dec, telescope, mjd, name, url }) => {
-
   const unixt = (mjd - 40587.0) * 86400000;
   const observed_at = new Date(unixt); // a new date
   const observed_at_str = dayjs(observed_at).toString();
