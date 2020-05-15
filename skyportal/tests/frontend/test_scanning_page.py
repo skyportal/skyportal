4a54a475134d09f927c504bcf505b97cce1ea173
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-05-15 09:57:30 -0700

    Update scanning page tests

--- skyportal/tests/frontend/test_scanning_page.py
+++ skyportal/tests/frontend/test_scanning_page.py
@@ -11,7 +11,13 @@ def test_candidates_page_render(driver, user, public_candidate):
 
 
 def test_candidate_group_filtering(
-    driver, user, public_candidate, public_group, upload_data_token, manage_groups_token
+    driver,
+    user,
+    public_candidate,
+    public_filter,
+    public_group,
+    upload_data_token,
+    manage_groups_token,
 ):
     candidate_id = str(uuid.uuid4())
     for i in range(20):
@@ -23,10 +29,10 @@ def test_candidate_group_filtering(
                 "ra": 234.22,
                 "dec": -22.33,
                 "redshift": 3,
-                "simbad_class": "RRLyr",
+                "altdata": {"simbad": {"class": "RRLyr"}},
                 "transient": False,
                 "ra_dis": 2.3,
-                "group_ids": [public_group.id],
+                "filter_ids": [public_filter.id],
             },
             token=upload_data_token,
         )
@@ -64,7 +70,13 @@ def test_candidate_group_filtering(
 
 
 def test_candidate_unsaved_only_filtering(
-    driver, user, public_candidate, public_group, upload_data_token, manage_groups_token
+    driver,
+    user,
+    public_candidate,
+    public_filter,
+    public_group,
+    upload_data_token,
+    manage_groups_token,
 ):
     candidate_id = str(uuid.uuid4())
     for i in range(20):
@@ -76,7 +88,7 @@ def test_candidate_unsaved_only_filtering(
                 "ra": 234.22,
                 "dec": -22.33,
                 "redshift": 3,
-                "simbad_class": "RRLyr",
+                "altdata": {"simbad": {"class": "RRLyr"}},
                 "transient": False,
                 "ra_dis": 2.3,
                 "group_ids": [public_group.id],
@@ -92,10 +104,10 @@ def test_candidate_unsaved_only_filtering(
                 "ra": 234.22,
                 "dec": -22.33,
                 "redshift": 3,
-                "simbad_class": "RRLyr",
+                "altdata": {"simbad": {"class": "RRLyr"}},
                 "transient": False,
                 "ra_dis": 2.3,
-                "group_ids": [public_group.id],
+                "filter_ids": [public_filter.id],
             },
             token=upload_data_token,
         )
@@ -131,7 +143,7 @@ def test_candidate_unsaved_only_filtering(
 
 
 def test_candidate_date_filtering(
-    driver, user, public_candidate, public_group, upload_data_token
+    driver, user, public_candidate, public_filter, public_group, upload_data_token
 ):
     candidate_id = str(uuid.uuid4())
     for i in range(20):
@@ -143,10 +155,10 @@ def test_candidate_date_filtering(
                 "ra": 234.22,
                 "dec": -22.33,
                 "redshift": 3,
-                "simbad_class": "RRLyr",
+                "altdata": {"simbad": {"class": "RRLyr"}},
                 "transient": False,
                 "ra_dis": 2.3,
-                "group_ids": [public_group.id],
+                "filter_ids": [public_filter.id],
             },
             token=upload_data_token,
         )
@@ -157,7 +169,7 @@ def test_candidate_date_filtering(
             "POST",
             "photometry",
             data={
-                "source_id": f"{candidate_id}_{i}",
+                "obj_id": f"{candidate_id}_{i}",
                 "observed_at": arrow.utcnow().isoformat(),
                 "time_format": "iso",
                 "time_scale": "utc",
@@ -198,7 +210,7 @@ def test_save_candidate(driver, group_admin_user, public_group, public_candidate
     driver.wait_for_xpath(f'//a[text()="{public_candidate.id}"]')
     first_save_button = driver.wait_for_xpath('//button[text()="Save as source"]')
     first_save_button.click()
-    driver.wait_for_xpath(f"//input[@name='{public_group.id}']")
+    driver.wait_for_xpath(f"//input[@name='{public_group.id}']").click()
     second_save_button = driver.wait_for_xpath('//button[text()="Save"]')
     second_save_button.click()
     driver.wait_for_xpath_to_disappear('//button[text()="Save as source"]')
@@ -213,126 +225,7 @@ def test_save_candidate_no_groups_error_message(
     driver.wait_for_xpath(f'//a[text()="{public_candidate.id}"]')
     first_save_button = driver.wait_for_xpath('//button[text()="Save as source"]')
     first_save_button.click()
-    driver.wait_for_xpath(f"//input[@name='{public_group.id}']").click()
+    driver.wait_for_xpath(f"//input[@name='{public_group.id}']")
     second_save_button = driver.wait_for_xpath('//button[text()="Save"]')
     second_save_button.click()
-    driver.wait_for_xpath('//div[contains(.,"Backend error: Invalid group ID")]')
-
-
-def test_save_candidate_single_group_membership(
-    driver, group_admin_user, public_group, upload_data_token, manage_groups_token
-):
-    candidate_id = str(uuid.uuid4())
-    status, data = api(
-        "POST",
-        "candidates",
-        data={
-            "id": f"{candidate_id}",
-            "ra": 234.22,
-            "dec": -22.33,
-            "redshift": 3,
-            "simbad_class": "RRLyr",
-            "transient": False,
-            "ra_dis": 2.3,
-            "group_ids": [public_group.id],
-        },
-        token=upload_data_token,
-    )
-    assert status == 200
-    assert data["data"]["id"] == f"{candidate_id}"
-
-    status, data = api(
-        "POST",
-        "groups",
-        data={"name": str(uuid.uuid4()), "group_admins": [group_admin_user.username]},
-        token=manage_groups_token,
-    )
-    assert status == 200
-    other_group_id = data["data"]["id"]
-
-    driver.get(f"/become_user/{group_admin_user.id}")
-    driver.get("/candidates")
-    first_save_button = driver.wait_for_xpath(
-        f"//button[@id='saveCandidateButton_{candidate_id}']"
-    )
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    first_save_button.click()
-    assert (
-        driver.wait_for_xpath(f"//input[@name='{public_group.id}']").get_attribute(
-            "checked"
-        )
-        == "true"
-    )
-    assert (
-        driver.wait_for_xpath(f"//input[@name='{other_group_id}']").get_attribute(
-            "checked"
-        )
-        is None
-    )
-    second_save_button = driver.wait_for_xpath('//button[text()="Save"]')
-    second_save_button.click()
-    driver.wait_for_xpath_to_disappear(
-        f"//button[@id='saveCandidateButton_{candidate_id}']"
-    )
-    status, data = api("GET", f"sources/{candidate_id}", token=upload_data_token)
-    assert [g["id"] for g in data["data"]["sources"]["groups"]] == [public_group.id]
-
-
-def test_save_candidate_multiple_group_membership(
-    driver, group_admin_user, public_group, upload_data_token, manage_groups_token
-):
-    candidate_id = str(uuid.uuid4())
-    status, data = api(
-        "POST",
-        "candidates",
-        data={
-            "id": f"{candidate_id}",
-            "ra": 234.22,
-            "dec": -22.33,
-            "redshift": 3,
-            "simbad_class": "RRLyr",
-            "transient": False,
-            "ra_dis": 2.3,
-            "group_ids": [public_group.id],
-        },
-        token=upload_data_token,
-    )
-    assert status == 200
-    assert data["data"]["id"] == f"{candidate_id}"
-
-    status, data = api(
-        "POST",
-        "groups",
-        data={"name": str(uuid.uuid4()), "group_admins": [group_admin_user.username]},
-        token=manage_groups_token,
-    )
-    assert status == 200
-    other_group_id = data["data"]["id"]
-
-    driver.get(f"/become_user/{group_admin_user.id}")
-    driver.get("/candidates")
-    first_save_button = driver.wait_for_xpath(
-        f"//button[@id='saveCandidateButton_{candidate_id}']"
-    )
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    first_save_button.click()
-    assert (
-        driver.wait_for_xpath(f"//input[@name='{public_group.id}']").get_attribute(
-            "checked"
-        )
-        == "true"
-    )
-    other_group_checkbox = driver.wait_for_xpath(f"//input[@name='{other_group_id}']")
-    assert other_group_checkbox.get_attribute("checked") is None
-    other_group_checkbox.click()
-    assert other_group_checkbox.get_attribute("checked") == "true"
-    second_save_button = driver.wait_for_xpath('//button[text()="Save"]')
-    second_save_button.click()
-    driver.wait_for_xpath_to_disappear(
-        f"//button[@id='saveCandidateButton_{candidate_id}']"
-    )
-    status, data = api("GET", f"sources/{candidate_id}", token=upload_data_token)
-    assert [g["id"] for g in data["data"]["sources"]["groups"]] == [
-        public_group.id,
-        other_group_id,
-    ]
+    driver.wait_for_xpath('//div[contains(.,"Invalid group_ids field")]')
