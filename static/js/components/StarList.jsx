d7e015c838f08c2a8c37f7fa97aeda2a3bd9e369
Stefan van der Walt <stefanv@berkeley.edu>
2020-06-11 00:40:21 +0200

    Add inline starlist display

---
+++ static/js/components/StarList.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { GET } from '../API';
+
+import styles from './StarList.css';
+
+
+const StarList = ({ sourceId }) => {
+  const [starList, setStarList] = useState([{ str: 'Loading starlist...' }]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchStarList = async () => {
+      const response = await dispatch(
+        GET(`/api/sources/${sourceId}/offsets?facility=Keck`, 'skyportal/FETCH_STARLIST')
+      );
+      setStarList(response.data.starlist_info);
+    };
+
+    fetchStarList();
+  }, [sourceId, dispatch]);
+
+  return (
+    <div className={styles.starListDiv}>
+      <code className={styles.starList}>
+        {
+          starList && starList.map((item, idx) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <React.Fragment key={idx}>
+              { item.str }
+              <br />
+            </React.Fragment>
+          ))
+        }
+      </code>
+    </div>
+  );
+};
+
+StarList.propTypes = {
+  sourceId: PropTypes.string.isRequired
+};
+
+export default StarList;
