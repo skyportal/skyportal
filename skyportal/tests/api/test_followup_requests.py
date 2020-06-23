9203f641c1142a8f25bcb22e5c5f2345b4ea7e18
Danny Goldstein <dgold@berkeley.edu>
2020-06-22 18:23:55 -0700

    more tests

--- skyportal/tests/api/test_followup_requests.py
+++ skyportal/tests/api/test_followup_requests.py
@@ -260,3 +260,26 @@ def test_token_user_delete_owned_assignment(red_transients_run,
                        token=upload_data_token)
     assert status == 200
     assert data['status'] == 'success'
+
+
+def test_regular_user_delete_super_admin_assignment(red_transients_run,
+                                                    public_source,
+                                                    upload_data_token,
+                                                    super_admin_token):
+
+    request_data = {'run_id': red_transients_run.id,
+                    'obj_id': public_source.id,
+                    'priority': '5',
+                    'comment': 'Please take spectrum only below airmass 1.5'}
+
+    status, data = api('POST', 'assignment',
+                       data=request_data,
+                       token=super_admin_token)
+    assert status == 200
+    assert data['status'] == 'success'
+    id = data['data']['id']
+
+    status, data = api('DELETE', f'assignment/{id}',
+                       token=upload_data_token)
+    assert status == 400
+    assert data['status'] == 'error'
