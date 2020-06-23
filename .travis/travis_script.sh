8aafd6376cf130d82e4971db9dd38e9005d6ff1b
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-06-23 09:28:06 -0700

    Do not run make lint-install on Travis, as req's are already installed (#510)
    * Do not run make lint-install on Travis, as req's are already installed; remove duplicate code
    
    * Print installed node package versions in travis_install.sh
    
    * Check that make load_demo_data exits with status code 0 on Travis CI builds
    
    * Update .eslintrc.yaml
    
    * Fix linting errors
    
    * Run lint after JS dependencies got installed by running SkyPortal once
    
    * Print eslint version
    
    * Update Travis image to Bionic
    
    Something was broken with NodeJS (npx, specifically)
    
    Co-authored-by: Stefan van der Walt <stefanv@berkeley.edu>

--- .travis/travis_script.sh
+++ .travis/travis_script.sh
@@ -2,25 +2,20 @@
 
 set -ex
 
+section "load_demo_data"
+make load_demo_data
+section_end "load_demo_data"
 
 section "ESLint"
-
-make lint-install
+npx eslint --version
 make lint
-
 section_end "ESLint"
 
-
 section "Tests"
-
 make log &
 make ${TEST_TARGET}
-
 section_end "Tests"
 
-
 section "Build.docs"
-
 make docs
-
 section_end "Build.docs"
