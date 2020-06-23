4cede94324b7a3dd9a5a491bebb4feb1fb0eb405
Danny Goldstein <dgold@berkeley.edu>
2020-06-22 18:13:39 -0700

    add more working tests

--- skyportal/tests/api/test_followup_requests.py
+++ skyportal/tests/api/test_followup_requests.py
@@ -186,3 +186,77 @@ def test_robotic_request_for_classical_instrument(lris, public_source,
     assert status == 400
     assert data['status'] == 'error'
 
+
+def test_invalid_robotic_request_empty_observations(sedm, public_source,
+                                                    upload_data_token):
+
+    request_data = {'instrument_id': sedm.id,
+                    'obj_id': public_source.id,
+                    'priority': '5',
+                    'comment': 'Classification',
+                    'start_date': datetime.datetime.utcnow().isoformat(),
+                    'end_date': (
+                            datetime.datetime.utcnow() + datetime.timedelta(days=7)
+                    ).isoformat(),
+                    'observations': []}
+
+    status, data = api('POST', 'followup_request',
+                       data=request_data,
+                       token=upload_data_token)
+    assert status == 400
+    assert data['status'] == 'error'
+
+
+def test_token_user_delete_followup_request(sedm, public_source,
+                                            upload_data_token):
+
+    request_data = {'instrument_id': sedm.id,
+                    'obj_id': public_source.id,
+                    'priority': '5',
+                    'comment': 'Classification',
+                    'start_date': datetime.datetime.utcnow().isoformat(),
+                    'end_date': (
+                            datetime.datetime.utcnow() + datetime.timedelta(days=7)
+                    ).isoformat(),
+                    'observations': [
+                        {'type': 'spectroscopy',
+                         'exposure_time': 60.},
+                        {'type': 'imaging',
+                         'filter': 'sdssg',
+                         'exposure_time': 360.}
+                    ]}
+
+    status, data = api('POST', 'followup_request',
+                       data=request_data,
+                       token=upload_data_token)
+    assert status == 200
+    assert data['status'] == 'success'
+    id = data['data']['id']
+
+    status, data = api('DELETE', f'followup_request/{id}',
+                       token=upload_data_token)
+
+    assert status == 200
+    assert data['status'] == 'success'
+
+
+def test_token_user_delete_owned_assignment(red_transients_run,
+                                            public_source,
+                                            upload_data_token):
+
+    request_data = {'run_id': red_transients_run.id,
+                    'obj_id': public_source.id,
+                    'priority': '5',
+                    'comment': 'Please take spectrum only below airmass 1.5'}
+
+    status, data = api('POST', 'assignment',
+                       data=request_data,
+                       token=upload_data_token)
+    assert status == 200
+    assert data['status'] == 'success'
+    id = data['data']['id']
+
+    status, data = api('DELETE', f'assignment/{id}',
+                       token=upload_data_token)
+    assert status == 200
+    assert data['status'] == 'success'
