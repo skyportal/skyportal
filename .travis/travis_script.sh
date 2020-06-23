de54950b8f3430442ba7990e34994732fc8529a1
Stefan van der Walt <stefanv@berkeley.edu>
2020-06-23 10:56:01 +0200

    Run lint after JS dependencies got installed by running SkyPortal once

--- .travis/travis_script.sh
+++ .travis/travis_script.sh
@@ -2,34 +2,19 @@
 
 set -ex
 
-
-section "ESLint"
-
-make lint
-
-section_end "ESLint"
-
-
-
 section "load_demo_data"
-
 make load_demo_data
-
 section_end "load_demo_data"
 
-
+section "ESLint"
+make lint
+section_end "ESLint"
 
 section "Tests"
-
 make log &
 make ${TEST_TARGET}
-
 section_end "Tests"
 
-
-
 section "Build.docs"
-
 make docs
-
 section_end "Build.docs"
