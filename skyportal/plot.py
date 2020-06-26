ea3cb991d7ec1f7cca5868dce869ed5a33a04022
Danny Goldstein <dgold@berkeley.edu>
2020-06-26 09:48:57 -0700

    typo

--- skyportal/plot.py
+++ skyportal/plot.py
@@ -192,7 +192,7 @@ def photometry_plot(obj_id, user, width=600, height=300):
             Telescope.nickname.label("telescope"),
             Instrument.name.label("instrument"),
         )
-        .join(Instrument, Instrument.id == Photometry.instrument_Id)
+        .join(Instrument, Instrument.id == Photometry.instrument_id)
         .join(Telescope, Telescope.id == Instrument.telescope_id)
         .filter(Photometry.obj_id == obj_id)
         .filter(Photometry.groups.any(Group.id.in_([g.id for g in user.groups])))
