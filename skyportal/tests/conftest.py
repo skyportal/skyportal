d125b028379961a530877af53dbd3be570268752
Joshua Bloom <profjsb@gmail.com>
2020-06-30 14:05:29 -0700

    PR#1 Classification Epic: Taxonomy Infrastructure (#519)
    * initial Taxonomy class added to models
    
    * initial API handlers for Taxonomy

--- skyportal/tests/conftest.py
+++ skyportal/tests/conftest.py
@@ -237,6 +237,13 @@ def comment_token(user):
     )
     return token_id
 
+@pytest.fixture()
+def taxonomy_token(user):
+    token_id = create_token(
+        permissions=["Post Taxonomy", "Delete Taxonomy"],
+        created_by_id=user.id, name=str(uuid.uuid4())
+    )
+    return token_id
 
 @pytest.fixture()
 def comment_token_two_groups(user_two_groups):
