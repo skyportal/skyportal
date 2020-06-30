ed5ab315ea0395cfda1cd2019791dd60f6929a06
Danny Goldstein <dgold@berkeley.edu>
2020-06-30 16:13:48 -0700

    Update skyportal/tests/api/test_photometry.py
    Co-authored-by: Joshua Bloom <profjsb@gmail.com>

--- skyportal/tests/api/test_photometry.py
+++ skyportal/tests/api/test_photometry.py
@@ -870,7 +870,6 @@ def test_token_user_retrieve_null_photometry(upload_data_token, public_source,
 def test_token_user_big_post(upload_data_token, public_source, ztf_camera,
                              public_group):
 
-    start = time.time()
     status, data = api('POST', 'photometry',
                        data={
                            'obj_id': str(public_source.id),
