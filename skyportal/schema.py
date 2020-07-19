1a64971725a270fad053382d261c53ef4a58f545
Danny Goldstein <dgold@berkeley.edu>
2020-07-19 15:27:29 -0700

    prevent a target from being assigned more than once to the same observing run

--- skyportal/schema.py
+++ skyportal/schema.py
@@ -584,6 +584,14 @@ class AssignmentSchema(_Schema):
         if run is None:
             raise ValidationError(f'Invalid observing run: "{run_id}"')
 
+        predecessor = ClassicalAssignment.query.filter(
+            ClassicalAssignment.obj_id == data['obj_id'],
+            ClassicalAssignment.run_id == run_id
+        ).first()
+
+        if predecessor is not None:
+            raise ValidationError(f'Object is already assigned to this run.')
+
         # check the object
         assignment = ClassicalAssignment(**data)
         return assignment
