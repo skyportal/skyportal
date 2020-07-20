51ac4e86fe092836018f656635ad064356357d68
Danny Goldstein <dgold@berkeley.edu>
2020-07-20 13:59:30 -0700

    add a many=True handler for telescope endpoint

--- skyportal/handlers/api/telescope.py
+++ skyportal/handlers/api/telescope.py
@@ -1,7 +1,7 @@
 from marshmallow.exceptions import ValidationError
 from baselayer.app.access import permissions, auth_or_token
 from ..base import BaseHandler
-from ...models import DBSession, Telescope, Group
+from ...models import DBSession, Telescope, Group, GroupTelescope
 
 
 class TelescopeHandler(BaseHandler):
@@ -66,32 +66,54 @@ class TelescopeHandler(BaseHandler):
         return self.success(data={"id": telescope.id})
 
     @auth_or_token
-    def get(self, telescope_id):
+    def get(self, telescope_id=None):
         """
         ---
-        description: Retrieve a telescope
-        parameters:
-          - in: path
-            name: telescope_id
-            required: true
-            schema:
-              type: integer
-        responses:
-          200:
-            content:
-              application/json:
-                schema: SingleTelescope
-          400:
-            content:
-              application/json:
-                schema: Error
+        single:
+          description: Retrieve a telescope
+          parameters:
+            - in: path
+              name: telescope_id
+              required: true
+              schema:
+                type: integer
+          responses:
+            200:
+              content:
+                application/json:
+                  schema: SingleTelescope
+            400:
+              content:
+                application/json:
+                  schema: Error
+        multiple:
+          description: Retrieve all telescopes
+          responses:
+            200:
+              content:
+                application/json:
+                  schema: ArrayOfTelescopes
+            400:
+              content:
+                application/json:
+                  schema: Error
         """
-        t = Telescope.get_if_owned_by(int(telescope_id), self.current_user)
 
-        if t is not None:
-            return self.success(data=t)
+        if telescope_id is not None:
+            t = Telescope.get_if_owned_by(int(telescope_id), self.current_user)
+            if t is not None:
+                return self.success(data=t)
+            else:
+                return self.error(
+                    f"Could not load telescope with ID {telescope_id}")
         else:
-            return self.error(f"Could not load telescope with ID {telescope_id}")
+            query = DBSession().query(Telescope).join(GroupTelescope).filter(
+                GroupTelescope.group_id.in_([g.id for g in self.current_user.groups])
+            )
+
+            tels = query.all()
+            return self.success(data=tels)
+
 
     @permissions(['Manage sources'])
     def put(self, telescope_id):
