348e8c9b42f9338a3dfc97cb39b0b2eb913cd71b
Danny Goldstein <dgold@berkeley.edu>
2020-07-29 21:12:08 -0700

    remove link

--- static/js/components/ObservingRunList.jsx
+++ static/js/components/ObservingRunList.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from "react-router-dom";
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
