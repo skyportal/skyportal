2a191419c1b9cb45b19736b5356ecbb6fba4b665
jbloom <profjsb@gmail.com>
2020-07-30 22:34:58 -0700

    working profile avatar and info updater

--- static/js/components/Profile.jsx
+++ static/js/components/Profile.jsx
@@ -1,44 +1,31 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 
 import NewTokenForm from './NewTokenForm';
 import TokenList from './TokenList';
 import UpdateProfileForm from './UpdateProfileForm';
+import ShowUserInfo from './ShowUserInfo';
 
 const Profile = () => {
   const profile = useSelector((state) => state.profile);
   const groups = useSelector((state) => state.groups.user);
   return (
     <div>
-      <Typography component="div">
-        <Box fontWeight="fontWeightBold" component="span" mr={1}>
-          Username:
-        </Box>
-        {profile.username}
-      </Typography>
-
-      <Typography component="div">
-        <Box pb={1}>
-          <Box fontWeight="fontWeightBold" component="span" mr={1}>
-            User roles:
-          </Box>
-          {profile.roles}
-        </Box>
-      </Typography>
-
-      <UpdateProfileForm
-        profile={profile}
-      />
-
-      <NewTokenForm
-        acls={profile.acls}
-        groups={groups}
-      />
-
-      <TokenList tokens={profile.tokens} />
-
+      <div>
+        <ShowUserInfo />
+      </div>
+      <div>
+        <UpdateProfileForm />
+      </div>
+      <div>
+        <NewTokenForm
+          acls={profile.acls}
+          groups={groups}
+        />
+      </div>
+      <div>
+        <TokenList tokens={profile.tokens} />
+      </div>
     </div>
   );
 };
