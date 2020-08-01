c213b598e339234f3567874f157934669ead5c4b
jbloom <profjsb@gmail.com>
2020-08-01 12:01:05 -0700

    add test for new User fields

--- skyportal/handlers/api/user.py
+++ skyportal/handlers/api/user.py
@@ -3,9 +3,21 @@ from baselayer.app.access import permissions
 from ...models import DBSession, User, Group, GroupUser, cfg
 
 
-def add_user_and_setup_groups(username, roles, group_ids_and_admin):
+def add_user_and_setup_groups(username,
+                              first_name=None,
+                              last_name=None,
+                              contact_phone=None,
+                              contact_email=None,
+                              roles=[],
+                              group_ids_and_admin=[]
+                              ):
     # Add user
-    user = User(username=username.lower(), role_ids=roles)
+    user = User(username=username.lower(), role_ids=roles,
+                first_name=first_name,
+                last_name=last_name,
+                contact_phone=contact_phone,
+                contact_email=contact_email
+                )
     DBSession().add(user)
     DBSession().flush()
 
