e44544c54f87472eae4ebf0f0d05410ec438b2ce
jbloom <profjsb@gmail.com>
2020-08-05 14:55:56 -0700

    remove double utc import

--- static/js/components/CommentList.jsx
+++ static/js/components/CommentList.jsx
@@ -8,7 +8,6 @@ import GroupIcon from "@material-ui/icons/Group";
 import dayjs from "dayjs";
 import utc from 'dayjs/plugin/utc';
 import relativeTime from "dayjs/plugin/relativeTime";
-import utc from 'dayjs/plugin/utc';
 
 import * as sourceActions from "../ducks/source";
 import styles from "./CommentList.css";
