b5a206ca944cd52d692dae272722a4a3e230ce48
Stefan van der Walt <stefanv@berkeley.edu>
2020-08-12 23:13:07 -0700

    Revert StarList

--- static/js/components/StarList.jsx
+++ static/js/components/StarList.jsx
@@ -1,25 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import PropTypes from "prop-types";
 
 import { GET } from "../API";
 
 import styles from "./StarList.css";
-
-const StarListBody = ({ starList }) => (
-  <div className={styles.starListDiv}>
-    <code className={styles.starList}>
-      {starList &&
-        starList.map((item, idx) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <React.Fragment key={idx}>
-            {item.str}
-            <br />
-          </React.Fragment>
-        ))}
-    </code>
-  </div>
-);
 
 const StarList = ({ sourceId }) => {
   const [starList, setStarList] = useState([{ str: "Loading starlist..." }]);
@@ -39,50 +24,24 @@ const StarList = ({ sourceId }) => {
     fetchStarList();
   }, [sourceId, dispatch]);
 
-  return <StarListBody starList={starList} />;
-};
-
-export const ObservingRunStarList = () => {
-  const [starList, setStarList] = useState([{ str: "Loading starlist..." }]);
-  const { assignments } = useSelector((state) => state.observingRun);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const fetchStarList = async () => {
-      const promises = assignments.map((assignment) =>
-        dispatch(
-          GET(
-            `/api/sources/${assignment.obj_id}/offsets?facility=Keck`,
-            "skyportal/FETCH_STARLIST"
-          )
-        )
-      );
-
-      const starlistInfo = [];
-      const values = await Promise.allSettled(promises);
-
-      values.forEach((response) =>
-        starlistInfo.push(...response.value.data.starlist_info)
-      );
-
-      setStarList(starlistInfo);
-    };
-    fetchStarList();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dispatch]);
-  return <StarListBody starList={starList} />;
+  return (
+    <div className={styles.starListDiv}>
+      <code className={styles.starList}>
+        {starList &&
+          starList.map((item, idx) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <React.Fragment key={idx}>
+              {item.str}
+              <br />
+            </React.Fragment>
+          ))}
+      </code>
+    </div>
+  );
 };
 
 StarList.propTypes = {
   sourceId: PropTypes.string.isRequired,
-};
-
-StarListBody.propTypes = {
-  starList: PropTypes.arrayOf(
-    PropTypes.shape({
-      str: PropTypes.string,
-    })
-  ).isRequired,
 };
 
 export default StarList;
