92a89cc509ab33f5905502eb64f57ff74c49452a
Joshua Bloom <profjsb@gmail.com>
2020-08-18 16:17:13 -0700

    Quick Search Bar for Sources (#675)
    * Add Quick Search Bar
    
    * Update data sharing test to account for new autocomplete rendered
    
    * Add quick search bar tests

--- skyportal/tests/frontend/test_data_sharing.py
+++ skyportal/tests/frontend/test_data_sharing.py
@@ -14,7 +14,7 @@ def test_share_data(
     driver.click_xpath('//*[text()="Share data"]')
     driver.wait_for_xpath(f"//div[text()='{public_group.name}']", 15)
     driver.click_xpath('//*[@id="MUIDataTableSelectCell-0"]', wait_clickable=False)
-    driver.click_xpath('//input[contains(@class, "MuiAutocomplete-input")]')
+    driver.click_xpath('//*[@id="dataSharingFormGroupsSelect"]')
     driver.click_xpath(f'//li[text()="{public_group2.name}"]')
     driver.click_xpath('//*[text()="Submit"]')
     driver.wait_for_xpath('//*[text()="Data successfully shared"]', 15)
--- static/js/components/HeaderContent.jsx
+++ static/js/components/HeaderContent.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Box from "@material-ui/core/Box";
 import Responsive from "./Responsive";
 import ProfileDropdown from "./ProfileDropdown";
 import Logo from "./Logo";
+import QuickSearchBar from "./QuickSearchBar";
 
 import styles from "./Main.css";
 
 const HeaderContent = () => (
   <div className={styles.topBannerContent}>
-    <div style={{ display: "inline-block", float: "left" }}>
+    <div style={{ display: "inline-flex", flexDirection: "row" }}>
       <Logo className={styles.logo} />
       <Link className={styles.title} to="/">
-        SkyPortal ∝
+        SkyPortal
       </Link>
+      <Box p={2} />
+      <QuickSearchBar id="search" />
     </div>
     <div style={{ position: "fixed", right: "1rem", top: "1rem" }}>
       <Responsive desktopElement={ProfileDropdown} />
