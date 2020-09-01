e7da94c1a0b4549cb9b05f3a178442b14766de35
Danny Goldstein <dgold@berkeley.edu>
2020-08-31 19:07:31 -0700

    remove followup request frontend for API-only pr

--- static/js/components/Source.jsx
+++ static/js/components/Source.jsx
@@ -22,9 +22,6 @@ import { ra_to_hours, dec_to_hours } from "../units";
 import styles from "./Source.css";
 import Responsive from "./Responsive";
 import FoldBox from "./FoldBox";
-import FollowupRequestForm from "./FollowupRequestForm";
-
-import FollowupRequestLists from "./FollowupRequestLists";
 import SharePage from "./SharePage";
 
 import AssignmentForm from "./AssignmentForm";
@@ -60,9 +57,6 @@ const Source = ({ route }) => {
       fetchSource();
     }
   }, [dispatch, isCached, route.id]);
-  const { instrumentList, instrumentFormParams } = useSelector(
-    (state) => state.instruments
-  );
   const { observingRunList } = useSelector((state) => state.observingRuns);
   const { taxonomyList } = useSelector((state) => state.taxonomies);
 
@@ -182,16 +176,6 @@ const Source = ({ route }) => {
           title="Follow-up"
           mobileProps={{ folded: true }}
         >
-          <FollowupRequestForm
-            obj_id={source.id}
-            instrumentList={instrumentList}
-            instrumentFormParams={instrumentFormParams}
-          />
-          <FollowupRequestLists
-            followupRequests={source.followup_requests}
-            instrumentList={instrumentList}
-            instrumentFormParams={instrumentFormParams}
-          />
           <AssignmentForm
             obj_id={source.id}
             observingRunList={observingRunList}
