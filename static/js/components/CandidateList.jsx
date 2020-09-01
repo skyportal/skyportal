1d080f01bd6c3dd168b082911496638459a8778a
Kyung Min Shin <kshin@caltech.edu>
2020-09-01 16:01:04 -0700

    Reskin Candidates page to use MUI (#773)

--- static/js/components/CandidateList.jsx
+++ static/js/components/CandidateList.jsx
@@ -2,6 +2,15 @@ import React, { useEffect, Suspense } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+
 import * as candidatesActions from "../ducks/candidates";
 import ThumbnailList from "./ThumbnailList";
 import CandidateCommentList from "./CandidateCommentList";
@@ -12,7 +21,18 @@ const VegaPlot = React.lazy(() =>
   import(/* webpackChunkName: "VegaPlot" */ "./VegaPlot")
 );
 
+const useStyles = makeStyles(() => ({
+  candidateListContainer: {
+    padding: "1rem",
+  },
+  title: {
+    marginBottom: "0.625rem",
+  },
+}));
+
 const CandidateList = () => {
+  const classes = useStyles();
+
   const { candidates } = useSelector((state) => state.candidates);
 
   const userGroups = useSelector((state) => state.groups.user);
@@ -26,108 +46,118 @@ const CandidateList = () => {
   }, [candidates, dispatch]);
 
   return (
-    <div style={{ border: "1px solid #DDD", padding: "10px" }}>
-      <h2>Scan candidates for sources</h2>
-      <FilterCandidateList userGroups={userGroups} />
-      <table>
-        <thead>
-          <tr>
-            <th>Last detected</th>
-            <th>Images</th>
-            <th>Info</th>
-            <th>Photometry</th>
-            <th>Autoannotations</th>
-          </tr>
-        </thead>
-        <tbody>
-          {!!candidates &&
-            candidates.map((candidateObj) => {
-              const thumbnails = candidateObj.thumbnails.filter(
-                (t) => t.type !== "dr8"
-              );
-              return (
-                <tr key={candidateObj.id}>
-                  <td>
-                    {candidateObj.last_detected && (
-                      <div>
+    <Paper elevation={1}>
+      <div className={classes.candidateListContainer}>
+        <Typography variant="h6" className={classes.title}>
+          Scan candidates for sources
+        </Typography>
+        <FilterCandidateList userGroups={userGroups} />
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Last detected</TableCell>
+              <TableCell>Images</TableCell>
+              <TableCell>Info</TableCell>
+              <TableCell>Photometry</TableCell>
+              <TableCell>Autoannotations</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {!!candidates &&
+              candidates.map((candidateObj) => {
+                const thumbnails = candidateObj.thumbnails.filter(
+                  (t) => t.type !== "dr8"
+                );
+                return (
+                  <TableRow key={candidateObj.id}>
+                    <TableCell>
+                      {candidateObj.last_detected && (
                         <div>
-                          {
-                            String(candidateObj.last_detected)
-                              .split(".")[0]
-                              .split("T")[1]
-                          }
+                          <div>
+                            {
+                              String(candidateObj.last_detected)
+                                .split(".")[0]
+                                .split("T")[1]
+                            }
+                          </div>
+                          <div>
+                            {
+                              String(candidateObj.last_detected)
+                                .split(".")[0]
+                                .split("T")[0]
+                            }
+                          </div>
                         </div>
-                        <div>
-                          {
-                            String(candidateObj.last_detected)
-                              .split(".")[0]
-                              .split("T")[0]
-                          }
-                        </div>
-                      </div>
-                    )}
-                  </td>
-                  <td>
-                    <ThumbnailList
-                      ra={candidateObj.ra}
-                      dec={candidateObj.dec}
-                      thumbnails={thumbnails}
-                    />
-                  </td>
-                  <td>
-                    ID:&nbsp;
-                    <Link to={`/candidate/${candidateObj.id}`}>
-                      {candidateObj.id}
-                    </Link>
-                    <br />
-                    {candidateObj.is_source ? (
-                      <div>
-                        <Link
-                          to={`/source/${candidateObj.id}`}
-                          style={{ color: "red", textDecoration: "underline" }}
-                        >
-                          Previously Saved
-                        </Link>
-                      </div>
-                    ) : (
-                      <div>
-                        NOT SAVED
-                        <br />
-                        <SaveCandidateButton
-                          candidate={candidateObj}
-                          userGroups={userGroups}
-                        />
-                      </div>
-                    )}
-                    <b>Coordinates</b>
-                    :&nbsp;
-                    {candidateObj.ra}
-                    &nbsp;
-                    {candidateObj.dec}
-                    <br />
-                    Gal. Coords (l,b):&nbsp;
-                    {candidateObj.gal_lon.toFixed(1)}, &nbsp;
-                    {candidateObj.gal_lat.toFixed(1)}
-                    <br />
-                  </td>
-                  <td>
-                    <Suspense fallback={<div>Loading plot...</div>}>
-                      <VegaPlot
-                        dataUrl={`/api/sources/${candidateObj.id}/photometry`}
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      <ThumbnailList
+                        ra={candidateObj.ra}
+                        dec={candidateObj.dec}
+                        thumbnails={thumbnails}
+                        size="8rem"
                       />
-                    </Suspense>
-                  </td>
-                  <td>
-                    {candidateObj.comments && (
-                      <CandidateCommentList comments={candidateObj.comments} />
-                    )}
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
-    </div>
+                    </TableCell>
+                    <TableCell>
+                      ID:&nbsp;
+                      <Link to={`/candidate/${candidateObj.id}`}>
+                        {candidateObj.id}
+                      </Link>
+                      <br />
+                      {candidateObj.is_source ? (
+                        <div>
+                          <Link
+                            to={`/source/${candidateObj.id}`}
+                            style={{
+                              color: "red",
+                              texTableCellecoration: "underline",
+                            }}
+                          >
+                            Previously Saved
+                          </Link>
+                        </div>
+                      ) : (
+                        <div>
+                          NOT SAVED
+                          <br />
+                          <SaveCandidateButton
+                            candidate={candidateObj}
+                            userGroups={userGroups}
+                          />
+                        </div>
+                      )}
+                      <b>Coordinates</b>
+                      :&nbsp;
+                      {candidateObj.ra}
+                      &nbsp;
+                      {candidateObj.dec}
+                      <br />
+                      Gal. Coords (l,b):&nbsp;
+                      {candidateObj.gal_lon.toFixed(1)}, &nbsp;
+                      {candidateObj.gal_lat.toFixed(1)}
+                      <br />
+                    </TableCell>
+                    <TableCell>
+                      <Suspense fallback={<div>Loading plot...</div>}>
+                        <VegaPlot
+                          dataUrl={`/api/sources/${candidateObj.id}/photometry`}
+                        />
+                      </Suspense>
+                    </TableCell>
+                    <TableCell>
+                      {candidateObj.comments && (
+                        <CandidateCommentList
+                          comments={candidateObj.comments}
+                        />
+                      )}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </div>
+    </Paper>
   );
 };
 
