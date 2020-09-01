d6bce1e77fe9cb84e875379e8662438560e816f3
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-09-01 09:42:26 -0700

    Only modify PSA pipeline if debug_login=False

--- skyportal/app_server.py
+++ skyportal/app_server.py
@@ -125,42 +125,43 @@ def make_app(cfg, baselayer_handlers, baselayer_settings):
     ]
 
     settings = baselayer_settings
-    settings.update(
-        {
-            'SOCIAL_AUTH_PIPELINE': (
-                # Get the information we can about the user and return it in a simple
-                # format to create the user instance later. In some cases the details are
-                # already part of the auth response from the provider, but sometimes this
-                # could hit a provider API.
-                'social_core.pipeline.social_auth.social_details',
-                # Get the social uid from whichever service we're authing thru. The uid is
-                # the unique identifier of the given user in the provider.
-                'social_core.pipeline.social_auth.social_uid',
-                # Verify that the current auth process is valid within the current
-                # project, this is where emails and domains whitelists are applied (if
-                # defined).
-                'social_core.pipeline.social_auth.auth_allowed',
-                # Checks if the current social-account is already associated in the site.
-                'social_core.pipeline.social_auth.social_user',
-                # Make up a username for this person, appends a random string at the end if
-                # there's any collision.
-                'social_core.pipeline.user.get_username',
-                'skyportal.onboarding.create_user',
-                # Create a user account if we haven't found one yet.
-                # 'social_core.pipeline.user.create_user',
-                # Create the record that associates the social account with the user.
-                'social_core.pipeline.social_auth.associate_user',
-                # Populate the extra_data field in the social record with the values
-                # specified by settings (and the default ones like access_token, etc).
-                'social_core.pipeline.social_auth.load_extra_data',
-                # Update the user record with any changed info from the auth service.
-                'social_core.pipeline.user.user_details',
-                'skyportal.onboarding.setup_invited_user_permissions',
-            ),
-            'SOCIAL_AUTH_NEW_USER_REDIRECT_URL': '/profile?newUser=true',
-            'SOCIAL_AUTH_FIELDS_STORED_IN_SESSION': ['invite_token'],
-        }
-    )
+    if not cfg["server.auth.debug_login"]:
+        settings.update(
+            {
+                'SOCIAL_AUTH_PIPELINE': (
+                    # Get the information we can about the user and return it in a simple
+                    # format to create the user instance later. In some cases the details are
+                    # already part of the auth response from the provider, but sometimes this
+                    # could hit a provider API.
+                    'social_core.pipeline.social_auth.social_details',
+                    # Get the social uid from whichever service we're authing thru. The uid is
+                    # the unique identifier of the given user in the provider.
+                    'social_core.pipeline.social_auth.social_uid',
+                    # Verify that the current auth process is valid within the current
+                    # project, this is where emails and domains whitelists are applied (if
+                    # defined).
+                    'social_core.pipeline.social_auth.auth_allowed',
+                    # Checks if the current social-account is already associated in the site.
+                    'social_core.pipeline.social_auth.social_user',
+                    # Make up a username for this person, appends a random string at the end if
+                    # there's any collision.
+                    'social_core.pipeline.user.get_username',
+                    'skyportal.onboarding.create_user',
+                    # Create a user account if we haven't found one yet.
+                    # 'social_core.pipeline.user.create_user',
+                    # Create the record that associates the social account with the user.
+                    'social_core.pipeline.social_auth.associate_user',
+                    # Populate the extra_data field in the social record with the values
+                    # specified by settings (and the default ones like access_token, etc).
+                    'social_core.pipeline.social_auth.load_extra_data',
+                    # Update the user record with any changed info from the auth service.
+                    'social_core.pipeline.user.user_details',
+                    'skyportal.onboarding.setup_invited_user_permissions',
+                ),
+                'SOCIAL_AUTH_NEW_USER_REDIRECT_URL': '/profile?newUser=true',
+                'SOCIAL_AUTH_FIELDS_STORED_IN_SESSION': ['invite_token'],
+            }
+        )
 
     app = tornado.web.Application(handlers, **settings)
     models.init_db(**cfg['database'])
