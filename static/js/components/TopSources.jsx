a9a0a05e3c4e782713bd9aadfa8b6b5ef77b853c
Kyung Min Shin <kshin@caltech.edu>
2020-09-03 10:59:24 -0700

    Add overflow to top sources

--- static/js/components/TopSources.jsx
+++ static/js/components/TopSources.jsx
@@ -39,25 +39,27 @@ const TopSources = ({ classes }) => {
           />
         </div>
         <p>Displaying most-viewed sources</p>
-        <ul className={styles.topSourceList}>
-          {sourceViews.map(({ obj_id, views, public_url }) => (
-            <li
-              key={`topSources_${obj_id}_${views}`}
-              className={styles.topSource}
-            >
-              <Link to={`/source/${obj_id}`}>
-                <img className={styles.stamp} src={public_url} alt={obj_id} />
-              </Link>
-              <span>
-                {" - "}
-                <Link to={`/source/${obj_id}`}>{obj_id}</Link>
-              </span>
-              <span>
-                <em>{` - ${views} view(s)`}</em>
-              </span>
-            </li>
-          ))}
-        </ul>
+        <div className={styles.topSourceListContainer}>
+          <ul className={styles.topSourceList}>
+            {sourceViews.map(({ obj_id, views, public_url }) => (
+              <li
+                key={`topSources_${obj_id}_${views}`}
+                className={styles.topSource}
+              >
+                <Link to={`/source/${obj_id}`}>
+                  <img className={styles.stamp} src={public_url} alt={obj_id} />
+                </Link>
+                <span>
+                  {" - "}
+                  <Link to={`/source/${obj_id}`}>{obj_id}</Link>
+                </span>
+                <span>
+                  <em>{` - ${views} view(s)`}</em>
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </Paper>
   );
