dd781664de26ef4482ccb8bb49c1b52b425c99ba
Danny Goldstein <dgold@berkeley.edu>
2020-09-03 00:50:49 -0700

    Update skyportal/handlers/api/internal/robotic_instruments.py
    Co-authored-by: Ari Crellin-Quick <a.crellinquick@gmail.com>

--- skyportal/handlers/api/internal/robotic_instruments.py
+++ skyportal/handlers/api/internal/robotic_instruments.py
@@ -7,7 +7,7 @@ class RoboticInstrumentsHandler(BaseHandler):
     @auth_or_token
     def get(self):
         instruments = Instrument.query.filter(
-            Instrument.api_classname.isnot(None)  # noqa
+            Instrument.api_classname.isnot(None)
         ).all()
         retval = {i.id: i.api_class.frontend_render_info() for i in instruments}
         return self.success(data=retval)
