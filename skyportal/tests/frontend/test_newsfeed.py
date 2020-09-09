058d0ed2289bec07dfb4044c32b91d155ac13286
Kyung Min Shin <kshin@caltech.edu>
2020-09-08 20:13:01 -0700

    Fix tests

--- skyportal/tests/frontend/test_newsfeed.py
+++ skyportal/tests/frontend/test_newsfeed.py
@@ -41,11 +41,11 @@ def test_news_feed(
     for i in range(2):
         # Source added item
         driver.wait_for_xpath(
-            f'//div[contains(@class, "NewsFeed__entryContent")][span[text()="New source added"]][.//a[@href="/source/{obj_id_base}_{i}"]]'
+            f'//div[contains(@class, "NewsFeed__entryContent")][.//p[text()="New source added"]][.//a[@href="/source/{obj_id_base}_{i}"]]'
         )
 
         # Comment item
-        driver.wait_for_xpath(f'//span[contains(text(),"comment_text_{i}")]')
+        driver.wait_for_xpath(f'//p[contains(text(),"comment_text_{i}")]')
 
 
 @pytest.mark.flaky(reruns=2)
@@ -85,18 +85,18 @@ def test_news_feed_prefs_widget(
     for i in range(2):
         # Source added item
         driver.wait_for_xpath(
-            f'//div[contains(@class, "NewsFeed__entryContent")][span[text()="New source added"]][.//a[@href="/source/{obj_id_base}_{i}"]]'
+            f'//div[contains(@class, "NewsFeed__entryContent")][.//p[text()="New source added"]][.//a[@href="/source/{obj_id_base}_{i}"]]'
         )
 
         # Comment item
-        driver.wait_for_xpath(f'//span[contains(text(),"comment_text_{i}")]')
+        driver.wait_for_xpath(f'//p[contains(text(),"comment_text_{i}")]')
 
     driver.wait_for_xpath('//*[@id="newsFeedSettingsIcon"]').click()
     n_items_input = driver.wait_for_xpath('//input[@id="numItems"]')
     n_items_input.clear()
     ActionChains(driver).click(n_items_input).send_keys("2").perform()
     driver.click_xpath('//button[contains(., "Save")]')
-    source_added_item_xpath = f'//div[contains(@class, "NewsFeed__entryContent")][span[text()="New source added"]][.//a[@href="/source/{obj_id_base}_0"]]'
+    source_added_item_xpath = f'//div[contains(@class, "NewsFeed__entryContent")][.//p[text()="New source added"]][.//a[@href="/source/{obj_id_base}_0"]]'
     driver.wait_for_xpath_to_disappear(source_added_item_xpath)
     driver.wait_for_xpath('//*[@id="newsFeedSettingsIcon"]').click()
     n_items_input = driver.wait_for_xpath('//input[@id="numItems"]')
