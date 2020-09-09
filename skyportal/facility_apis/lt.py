90163b13962a3d29f7d237e24abc7b3c61178705
Michael Coughlin <coughlin@physics.harvard.edu>
2020-09-09 13:04:34 -0500

    submit and delete... works!

--- skyportal/facility_apis/lt.py
+++ skyportal/facility_apis/lt.py
@@ -13,6 +13,7 @@ from . import FollowUpAPI
 from baselayer.app.access import auth_or_token
 from baselayer.app.env import load_env
 import requests
+from requests_toolbelt.utils import dump
 
 
 LT_SETTINGS = {
@@ -20,7 +21,6 @@ LT_SETTINGS = {
     'PASSWORD': '',
     'LT_HOST': '',
     'LT_PORT': '',
-    'DEBUG': True,
     'MAX_AIRMASS': 2.0,
     'MAX_SEEING': 1.2,
     'MAX_SKYBRI': 1.0,
@@ -34,7 +34,6 @@ LT_SETTINGS['USERNAME'] = cfg['app.lt_username']
 LT_SETTINGS['PASSWORD'] = cfg['app.lt_password']
 LT_SETTINGS['LT_HOST'] = cfg['app.lt_host']
 LT_SETTINGS['LT_PORT'] = cfg['app.lt_port']
-print(LT_SETTINGS)
 
 LT_XML_NS = 'http://www.rtml.org/v3.1a'
 LT_XSI_NS = 'http://www.w3.org/2001/XMLSchema-instance'
@@ -56,15 +55,18 @@ class LTRequest():
         }
         schemaLocation = etree.QName(LT_XSI_NS, 'schemaLocation')
         uid = format(str(int(time.time())))
-        return etree.Element('RTML', {schemaLocation: LT_SCHEMA_LOCATION}, xmlns=LT_XML_NS,
-                             mode='request', uid=uid, version='3.1a', nsmap=namespaces)
+        prolog = etree.Element('RTML', {schemaLocation: LT_SCHEMA_LOCATION},
+                               xmlns=LT_XML_NS,
+                               mode='request', uid=uid,
+                               version='3.1a', nsmap=namespaces)
+        return prolog
 
     def _build_project(self, payload, request):
         project = etree.Element('Project',
                                 ProjectID=request.payload["LT_proposalID"])
         contact = etree.SubElement(project, 'Contact')
-        etree.SubElement(contact, 'Username').text = request.payload["LT_username"]
-        etree.SubElement(contact, 'Name').text = ''
+        etree.SubElement(contact, 'Username').text = LT_SETTINGS['USERNAME']
+        etree.SubElement(contact, 'Name').text = LT_SETTINGS['USERNAME']
         payload.append(project)
 
     def _build_constraints(self, request):
@@ -82,8 +84,8 @@ class LTRequest():
         etree.SubElement(photom_const, 'Clouds').text = LT_SETTINGS['PHOTOMETRIC']
 
         date_const = etree.Element('DateTimeConstraint', type='include')
-        start = request.payload["start_date"] + 'T00:00+00:00'
-        end = request.payload["end_date"] + 'T00:00+00:00'
+        start = request.payload["start_date"] + 'T00:00:00+00:00'
+        end = request.payload["end_date"] + 'T00:00:00+00:00'
         etree.SubElement(date_const, 'DateTimeStart', system='UT', value=start)
         etree.SubElement(date_const, 'DateTimeEnd', system='UT', value=end)
 
@@ -127,7 +129,7 @@ class LTRequest():
         device = etree.SubElement(schedule, 'Device', name="IO:O", type="camera")
         etree.SubElement(device, 'SpectralRegion').text = 'optical'
         setup = etree.SubElement(device, 'Setup')
-        etree.SubElement(setup, 'Filter', type=filt)
+        etree.SubElement(setup, 'Filter', type=filt.upper())
         detector = etree.SubElement(setup, 'Detector')
         binning = etree.SubElement(detector, 'Binning')
         etree.SubElement(binning, 'X', units='pixels').text = LT_SETTINGS['IOO_BINNING'].split('x')[0]
@@ -171,69 +173,90 @@ class LTAPI(FollowUpAPI):
     # subclasses *must* implement the method below
     @staticmethod
     def submit(request):
+
+        from ..models import DBSession, FollowupRequestHTTPRequest
+
         ltreq = LTRequest()
         observation_payload = ltreq._build_prolog()
         ltreq._build_project(observation_payload, request)
         ltreq._build_inst_schedule(observation_payload, request)
 
-        if LT_SETTINGS['DEBUG']:
-            f = open("created.rtml", "w")
-            f.write(etree.tostring(observation_payload,
-                                   encoding="unicode", pretty_print=True))
-            f.close()
-            return [0]
-        else:
-            headers = {
-                'Username': LT_SETTINGS['username'],
-                'Password': LT_SETTINGS['password']
-            }
-            url = '{0}://{1}:{2}/node_agent2/node_agent?wsdl'.format('http', LT_SETTINGS['LT_HOST'],
-                                                                     LT_SETTINGS['LT_PORT'])
-            client = Client(url=url, headers=headers)
-            # Send payload, and receive response string, removing the encoding tag which causes issue with lxml parsing
-            response = client.service.handle_rtml(observation_payload).replace('encoding="ISO-8859-1"', '')
-            response_rtml = etree.fromstring(response)
-            mode = response_rtml.get('mode')
-            if mode == 'reject':
-                self.dump_request_response(observation_payload, response_rtml)
-            obs_id = response_rtml.get('uid')
-            return [obs_id]
+        headers = {
+            'Username': LT_SETTINGS['USERNAME'],
+            'Password': LT_SETTINGS['PASSWORD']
+        }
+        url = '{0}://{1}:{2}/node_agent2/node_agent?wsdl'.format('http', LT_SETTINGS['LT_HOST'],
+                                                                 LT_SETTINGS['LT_PORT'])
+        client = Client(url=url, headers=headers)
+        full_payload = etree.tostring(observation_payload, encoding="unicode", pretty_print=True)
+        # Send payload, and receive response string, removing the encoding tag which causes issue with lxml parsing
+        response = client.service.handle_rtml(full_payload).replace('encoding="ISO-8859-1"', '')
+        response_rtml = etree.fromstring(response)
+        mode = response_rtml.get('mode')
+        if mode == 'confirm':
+            message = FollowupRequestHTTPRequest(
+                content=response,
+                origin='skyportal',
+                request=request,
+            )
+            DBSession().add(message)
+            DBSession().add(request)
+            DBSession().commit()
 
-    def validate(self, observation_payload):
-        if LT_SETTINGS['DEBUG']:
-            return []
-        else:
-            headers = {
-                'Username': LT_SETTINGS['username'],
-                'Password': LT_SETTINGS['password']
-            }
-            url = '{0}://{1}:{2}/node_agent2/node_agent?wsdl'.format('http',
-                                                                     LT_SETTINGS['LT_HOST'],
-                                                                     LT_SETTINGS['LT_PORT'])
-            client = Client(url=url, headers=headers)
-            validate_payload = etree.fromstring(observation_payload)
-            # Change the payload to an inquiry mode document to test connectivity.
-            validate_payload.set('mode', 'inquiry')
-            # Send payload, and receive response string, removing the encoding tag which causes issue with lxml parsing
+    @staticmethod
+    def delete(request):
 
-            try:
-                response = client.service.handle_rtml(validate_payload).replace('encoding="ISO-8859-1"', '')
-            except:
-                return ['Error with connection to Liverpool Telescope',
-                        'This could be due to incorrect credentials, or IP / Port settings',
-                        'Occassionally, this could be due to the rebooting of systems at the Telescope Site',
-                        'Please retry at another time.',
-                        'If the problem persists please contact ltsupport_astronomer@ljmu.ac.uk']
+        from ..models import DBSession, FollowupRequest
 
-            response_rtml = etree.fromstring(response)
-            if response_rtml.get('mode') == 'offer':
-                return []
-            elif response_rtml.get('mode') == 'reject':
-                return ['Error with RTML submission to Liverpool Telescope',
-                        'This can occassionally happen due to systems rebooting at the Telescope Site',
-                        'Please retry at another time.',
-                        'If the problem persists please contact ltsupport_astronomer@ljmu.ac.uk']
+        req = DBSession().query(FollowupRequest).filter(
+                    FollowupRequest.id == request.id
+              ).one()
+        content = req.http_requests[0].content
+        response_rtml = etree.fromstring(content)
+        uid = response_rtml.get('uid')
 
+        headers = {
+            'Username': LT_SETTINGS['USERNAME'],
+            'Password': LT_SETTINGS['PASSWORD']
+        }
+        url = '{0}://{1}:{2}/node_agent2/node_agent?wsdl'.format('http',
+                                                                 LT_SETTINGS['LT_HOST'],
+                                                                 LT_SETTINGS['LT_PORT'])
+
+        namespaces = {
+            'xsi': LT_XSI_NS,
+        }
+        schemaLocation = etree.QName(LT_XSI_NS, 'schemaLocation')
+        cancel_payload = etree.Element('RTML',
+                                       {schemaLocation: LT_SCHEMA_LOCATION},
+                                       mode='abort',
+                                       uid=format(str(uid)),
+                                       version='3.1a',
+                                       nsmap=namespaces
+                                       )
+        project = etree.SubElement(cancel_payload,
+                                   'Project',
+                                   ProjectID=request.payload["LT_proposalID"])
+        contact = etree.SubElement(project, 'Contact')
+        etree.SubElement(contact, 'Username').text = LT_SETTINGS['USERNAME']
+        etree.SubElement(contact, 'Name').text = LT_SETTINGS['USERNAME']
+        etree.SubElement(contact, 'Communication')
+        cancel = etree.tostring(cancel_payload, encoding='unicode', pretty_print=True)
+
+        client = Client(url=url, headers=headers)
+        # Send cancel_payload, and receive response string, removing the encoding tag which causes issue with lxml parsing
+        try:
+            response = client.service.handle_rtml(cancel).replace('encoding="ISO-8859-1"', '')
+        except suds.WebFault:
+            return
+        response_rtml = etree.fromstring(response)
+        mode = response_rtml.get('mode')
+        uid = response_rtml.get('uid')
+        if mode == 'confirm':
+            DBSession().query(FollowupRequest).filter(
+                FollowupRequest.id == request.id
+            ).delete()
+            DBSession().commit()
 
     _instrument_configs = {}
 
@@ -293,9 +316,7 @@ class LTAPI(FollowUpAPI):
             },
             "start_date": {"type": "string", "format": "date"},
             "end_date": {"type": "string", "format": "date"},
-            "LT_proposalID": {"type": "string"},
-            "LT_username": {"type": "string"},
-            "LT_password": {"type": "string"}
+            "LT_proposalID": {"type": "string"}
         },
         "required": ["instrument_type", "priority", "start_date", "end_date"],
         "dependencies": _dependencies 
