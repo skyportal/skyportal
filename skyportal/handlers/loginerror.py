4b9ad746dc2e208e05da80e1703d5f957bf82583
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-09-09 20:48:12 -0700

    Revert "Mv loginerror.html -> loginerror.html.template; use fill_conf_values templating format"
    This reverts commit 290c5f79fe1162edd6c19082e811eb388a6e395e.

--- skyportal/handlers/loginerror.py
+++ skyportal/handlers/loginerror.py
@@ -1,6 +1,10 @@
 from baselayer.app.handlers.base import BaseHandler
+from baselayer.app.env import load_env
+
+
+env, cfg = load_env()
 
 
 class LoginErrorPageHandler(BaseHandler):
     def get(self):
-        self.render("loginerror.html")
+        self.render("loginerror.html", app=cfg["app"])
