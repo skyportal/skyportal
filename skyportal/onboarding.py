d139f6bc97fa62b9a923462e2e8bb66db417d30e
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-09-10 13:40:42 -0700

    Render login error page with error message (#879)
    * Render login error page with informative error message upon auth failure
    
    * Do not use os.environ to set/access auth error message; render more general error page instead
    
    * Mv loginerror.html -> loginerror.html.template; use fill_conf_values templating format
    
    * Revert "Mv loginerror.html -> loginerror.html.template; use fill_conf_values templating format"
    
    This reverts commit 290c5f79fe1162edd6c19082e811eb388a6e395e.
    
    * Render login error page with error message in PSAHandler.write_error; remove LoginErrorPageHandler
    
    * Update login error page banner message

--- skyportal/onboarding.py
+++ skyportal/onboarding.py
@@ -1,5 +1,4 @@
 import datetime
-from social_core.exceptions import AuthTokenError
 from .models import (
     DBSession,
     User,
@@ -27,7 +26,7 @@ def create_user(strategy, details, backend, uid, user=None, *args, **kwargs):
     if cfg["invitations.enabled"]:
 
         if existing_user is None and invite_token is None:
-            raise AuthTokenError("No invite token provided.")
+            raise Exception("Missing invite token. A valid invite token is required.")
         elif existing_user is not None:
             return {"is_new": False, "user": existing_user}
 
@@ -35,19 +34,18 @@ def create_user(strategy, details, backend, uid, user=None, *args, **kwargs):
             n_days = int(cfg["invitations.days_until_expiry"])
         except ValueError:
             raise ValueError(
-                "Invalid (non-integer) value provided for "
-                "invitations.days_until_expiry in config file."
+                "Invalid invitation configuration value: invitations.days_until_expiry cannot be cast to int"
             )
 
         invitation = Invitation.query.filter(Invitation.token == invite_token).first()
         if invitation is None:
-            raise AuthTokenError("Invalid invite token.")
+            raise Exception("Invalid invite token. A valid invite token is required.")
 
         cutoff_datetime = datetime.datetime.now() - datetime.timedelta(days=n_days)
         if invitation.created_at < cutoff_datetime:
-            raise AuthTokenError("Invite token has expired.")
+            raise Exception("Invite token expired.")
         if invitation.used:
-            raise AuthTokenError("Invite token has already been used.")
+            raise Exception("Invite token has already been used.")
 
         user = User(
             username=details["username"],
@@ -87,7 +85,7 @@ def create_user(strategy, details, backend, uid, user=None, *args, **kwargs):
 
 def get_username(strategy, details, backend, uid, user=None, *args, **kwargs):
     if 'username' not in backend.setting('USER_FIELDS', USER_FIELDS):
-        return
+        raise Exception("PSA configuration error: `username` not properly captured.")
     storage = strategy.storage
 
     existing_user = DBSession().query(User).filter(User.oauth_uid == uid).first()
@@ -114,13 +112,13 @@ def setup_invited_user_permissions(strategy, uid, details, user, *args, **kwargs
 
     invite_token = strategy.session_get("invite_token")
     if invite_token is None:
-        return
+        raise Exception("Missing invite token. A valid invite token is required.")
     invitation = Invitation.query.filter(Invitation.token == invite_token).first()
     if invitation is None:
-        raise AuthTokenError("Invalid invite token.")
+        raise Exception("Invalid invite token. A valid invite token is required.")
 
     if invitation.used:
-        raise AuthTokenError("Invite token has already been used.")
+        raise Exception("Invitation has already been used.")
 
     group_ids = [g.id for g in invitation.groups]
 
