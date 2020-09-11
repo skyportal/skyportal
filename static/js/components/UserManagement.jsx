d7220ce21168b47b136bb9c38b8e9985b82ce233
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-09-10 18:21:36 -0700

    Add title to user management page

--- static/js/components/UserManagement.jsx
+++ static/js/components/UserManagement.jsx
@@ -9,6 +9,7 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Chip from "@material-ui/core/Chip";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Typography from "@material-ui/core/Typography";
 
 import { showNotification } from "baselayer/components/Notifications";
 
@@ -72,56 +73,62 @@ const UserManagement = () => {
   };
 
   return (
-    <Paper elevation={1}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell>Username</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Groups</TableCell>
-            <TableCell>Streams</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {allUsers.map((user) => (
-            <TableRow key={user.id}>
-              <TableCell>
-                {`${user.first_name ? user.first_name : ""} ${
-                  user.last_name ? user.last_name : ""
-                }`}
-              </TableCell>
-              <TableCell>{user.username}</TableCell>
-              <TableCell>{user.contact_email}</TableCell>
-              <TableCell>
-                {user.groups
-                  .filter((group) => !group.single_user_group)
-                  .map((group) => (
+    <>
+      <Typography variant="h5">Manage users</Typography>
+      <Paper elevation={1}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Username</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Groups</TableCell>
+              <TableCell>Streams</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {allUsers.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>
+                  {`${user.first_name ? user.first_name : ""} ${
+                    user.last_name ? user.last_name : ""
+                  }`}
+                </TableCell>
+                <TableCell>{user.username}</TableCell>
+                <TableCell>{user.contact_email}</TableCell>
+                <TableCell>
+                  {user.groups
+                    .filter((group) => !group.single_user_group)
+                    .map((group) => (
+                      <Chip
+                        label={group.name}
+                        onDelete={() => {
+                          handleClickRemoveUserFromGroup(
+                            user.username,
+                            group.id
+                          );
+                        }}
+                        key={group.id}
+                      />
+                    ))}
+                </TableCell>
+                <TableCell>
+                  {user.streams.map((stream) => (
                     <Chip
-                      label={group.name}
+                      label={stream.name}
                       onDelete={() => {
-                        handleClickRemoveUserFromGroup(user.username, group.id);
+                        handleClickRemoveUserStreamAccess(user.id, stream.id);
                       }}
-                      key={group.id}
+                      key={stream.id}
                     />
                   ))}
-              </TableCell>
-              <TableCell>
-                {user.streams.map((stream) => (
-                  <Chip
-                    label={stream.name}
-                    onDelete={() => {
-                      handleClickRemoveUserStreamAccess(user.id, stream.id);
-                    }}
-                    key={stream.id}
-                  />
-                ))}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </>
   );
 };
 
