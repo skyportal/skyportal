28f4a5151fc0462abbe9c94e13339323a413fde9
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-09-15 14:51:11 -0700

    Add inviteUsers duck; refactor UserManagement.jsx

--- static/js/components/UserManagement.jsx
+++ static/js/components/UserManagement.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import Table from "@material-ui/core/Table";
@@ -10,17 +10,27 @@ import Paper from "@material-ui/core/Paper";
 import Chip from "@material-ui/core/Chip";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Typography from "@material-ui/core/Typography";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import PapaParse from "papaparse";
 
 import { showNotification } from "baselayer/components/Notifications";
 
 import * as groupsActions from "../ducks/groups";
 import * as usersActions from "../ducks/users";
 import * as streamsActions from "../ducks/streams";
+import * as inviteUsersActions from "../ducks/inviteUsers";
+
+const sampleCSVText = `example1@gmail.com,1,3,false
+example2@gmail.com,1 2 3,2 5 9,false false true`;
 
 const UserManagement = () => {
   const dispatch = useDispatch();
+  const { invitationsEnabled } = useSelector((state) => state.sysInfo);
   const currentUser = useSelector((state) => state.profile);
   const { allUsers } = useSelector((state) => state.users);
+  const [csvData, setCsvData] = useState("");
 
   useEffect(() => {
     const fetchUsers = () => {
@@ -62,6 +72,34 @@ const UserManagement = () => {
     if (result.status === "success") {
       dispatch(showNotification("Stream access successfully revoked."));
       dispatch(usersActions.fetchUsers());
+    }
+  };
+
+  const handleClickAddUsers = async () => {
+    let rows = PapaParse.parse(csvData.trim(), {
+      delimiter: ",",
+      skipEmptyLines: "greedy",
+    }).data;
+    rows = rows.map((row) => [
+      row[0].trim(),
+      PapaParse.parse(row[1].trim(), { delimiter: " " }).data[0],
+      PapaParse.parse(row[2].trim(), { delimiter: " " }).data[0],
+      PapaParse.parse(row[3].trim(), { delimiter: " " }).data[0],
+    ]);
+    const promises = rows.map((row) =>
+      dispatch(
+        inviteUsersActions.inviteUser({
+          userEmail: row[0],
+          streamIDs: row[1],
+          groupIDs: row[2],
+          groupAdmin: row[3],
+        })
+      )
+    );
+    const results = await Promise.all(promises);
+    if (results.every((result) => result.status === "success")) {
+      dispatch(showNotification("User(s) successfully invited."));
+      setCsvData("");
     }
   };
 
@@ -123,6 +161,36 @@ const UserManagement = () => {
           </TableBody>
         </Table>
       </Paper>
+      <br />
+      {invitationsEnabled && (
+        <>
+          <Typography variant="h5">Bulk Invite New Users</Typography>
+          <Paper elevation={1}>
+            <Box p={5}>
+              <code>
+                User Email,Stream IDs,Group IDs,true/false indicating admin
+                status for respective groups (list values space-separated, no
+                spaces after commas)
+              </code>
+              <br />
+              <TextareaAutosize
+                placeholder={sampleCSVText}
+                name="bulkInviteCSVInput"
+                style={{ height: "15rem", width: "50rem" }}
+                onChange={(e) => {
+                  setCsvData(e.target.value);
+                }}
+                value={csvData}
+              />
+            </Box>
+            <Box pl={5} pb={5}>
+              <Button variant="contained" onClick={handleClickAddUsers}>
+                Add Users
+              </Button>
+            </Box>
+          </Paper>
+        </>
+      )}
     </>
   );
 };
