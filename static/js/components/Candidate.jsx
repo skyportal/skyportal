27debff790c7ba46fab57ab5936c902ea2c0cd02
Joshua Bloom <profjsb@gmail.com>
2020-10-05 23:52:22 -0700

    Improved Source/Candidate classification and position view (#1053)
    * source view improvements
    
    * Candidate position display improvements
    
    * Apply suggestions from code review
    
    Co-authored-by: Kyung Min Shin <kshin@caltech.edu>
    
    * Candidate position display improvements
    
    * Apply suggestions from code review
    
    Co-authored-by: Kyung Min Shin <kshin@caltech.edu>
    
    Co-authored-by: Kyung Min Shin <kshin@caltech.edu>

--- static/js/components/Candidate.jsx
+++ static/js/components/Candidate.jsx
@@ -82,12 +82,13 @@ const Candidate = ({ route }) => {
               </div>
               <div>
                 <b>Position (J2000):</b>
-                &nbsp;
-                {candidate.ra}, &nbsp;
-                {candidate.dec}
-                &nbsp; (&alpha;,&delta;=
-                {ra_to_hours(candidate.ra)}, &nbsp;
-                {dec_to_hours(candidate.dec)}) &nbsp; (l,b=
+                &nbsp; &nbsp;
+                <span className={classes.position}>
+                  {ra_to_hours(candidate.ra)} &nbsp;
+                  {dec_to_hours(candidate.dec)}
+                </span>
+                &nbsp; (&alpha;,&delta;= {candidate.ra}, &nbsp;
+                {candidate.dec}; <i>l</i>,<i>b</i>=
                 {candidate.gal_lon.toFixed(6)}, &nbsp;
                 {candidate.gal_lat.toFixed(6)}
                 )
