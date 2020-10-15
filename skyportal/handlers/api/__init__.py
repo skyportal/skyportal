497371c1c028b49d8736c9b8f7755ccfb5cd0eb3
dmitryduev <dmitry.duev@gmail.com>
2020-10-15 01:42:21 -0700

    rm objshort handler

--- skyportal/handlers/api/__init__.py
+++ skyportal/handlers/api/__init__.py
@@ -11,7 +11,6 @@ from .instrument import InstrumentHandler
 from .invalid import InvalidEndpointHandler
 from .invitations import InvitationHandler
 from .news_feed import NewsFeedHandler
-from .obj import ObjShortHandler
 from .observingrun import ObservingRunHandler
 from .photometry import (
     PhotometryHandler,
