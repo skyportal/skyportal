1e13957e07e1577deed5327dde9cece1451f8beb
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-10-22 15:17:54 -0700

    Allow adding users to new group(s) from user management page

--- static/js/components/UserManagement.jsx
+++ static/js/components/UserManagement.jsx
@@ -12,11 +12,20 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import Typography from "@material-ui/core/Typography";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 import Box from "@material-ui/core/Box";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import IconButton from "@material-ui/core/IconButton";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import PapaParse from "papaparse";
+import { useForm, Controller } from "react-hook-form";
 
 import { showNotification } from "baselayer/components/Notifications";
 
+import FormValidationError from "./FormValidationError";
 import * as groupsActions from "../ducks/groups";
 import * as usersActions from "../ducks/users";
 import * as streamsActions from "../ducks/streams";
@@ -30,7 +39,12 @@ const UserManagement = () => {
   const { invitationsEnabled } = useSelector((state) => state.sysInfo);
   const currentUser = useSelector((state) => state.profile);
   const { allUsers } = useSelector((state) => state.users);
+  const { all: allGroups } = useSelector((state) => state.groups);
   const [csvData, setCsvData] = useState("");
+  const [addUserGroupsDialogOpen, setAddUserGroupsDialogOpen] = useState(false);
+  const [addGroupsFormUsername, setAddGroupsFormUsername] = useState("");
+
+  const { handleSubmit, errors, reset, control, getValues } = useForm();
 
   useEffect(() => {
     const fetchUsers = () => {
@@ -41,7 +55,11 @@ const UserManagement = () => {
     }
   }, [allUsers, dispatch]);
 
-  if (!allUsers?.length || !currentUser?.username?.length) {
+  if (
+    !allUsers?.length ||
+    !currentUser?.username?.length ||
+    !allGroups?.length
+  ) {
     return (
       <div>
         <CircularProgress />
@@ -57,6 +75,34 @@ const UserManagement = () => {
   ) {
     return <div>Access denied: Insufficient permissions.</div>;
   }
+
+  const validateGroups = () => {
+    const formState = getValues({ nest: true });
+    return formState.groups.length >= 1;
+  };
+
+  const handleAddUserToGroups = async (formData) => {
+    const groupIDs = formData.groups.map((g) => g.id);
+    const promises = groupIDs.map((gid) =>
+      dispatch(
+        groupsActions.addGroupUser({
+          username: addGroupsFormUsername,
+          admin: false,
+          group_id: gid,
+        })
+      )
+    );
+    const results = await Promise.all(promises);
+    if (results.every((result) => result.status === "success")) {
+      dispatch(
+        showNotification("User successfully added to specified group(s).")
+      );
+      reset({ groups: [] });
+      setAddUserGroupsDialogOpen(false);
+      dispatch(usersActions.fetchUsers());
+      setAddGroupsFormUsername("");
+    }
+  };
 
   const handleClickRemoveUserFromGroup = async (username, group_id) => {
     const result = await dispatch(
@@ -133,6 +179,17 @@ const UserManagement = () => {
                 <TableCell>{user.username}</TableCell>
                 <TableCell>{user.contact_email}</TableCell>
                 <TableCell>
+                  <IconButton
+                    aria-label="add-group"
+                    data-testid={`addUserGroupsButton${user.id}`}
+                    onClick={() => {
+                      setAddGroupsFormUsername(user.username);
+                      setAddUserGroupsDialogOpen(true);
+                    }}
+                    size="small"
+                  >
+                    <AddCircleIcon color="disabled" />
+                  </IconButton>
                   {user.groups
                     .filter((group) => !group.single_user_group)
                     .map((group) => (
@@ -196,6 +253,65 @@ const UserManagement = () => {
           </Paper>
         </>
       )}
+      <Dialog
+        open={addUserGroupsDialogOpen}
+        onClose={() => {
+          setAddUserGroupsDialogOpen(false);
+        }}
+        style={{ position: "fixed" }}
+      >
+        <DialogTitle>{`Add user ${addGroupsFormUsername} to selected groups:`}</DialogTitle>
+        <DialogContent>
+          <form onSubmit={handleSubmit(handleAddUserToGroups)}>
+            {!!errors.groups && (
+              <FormValidationError message="Please select at least one group" />
+            )}
+            <Controller
+              name="groups"
+              id="addUsersFromGroupsSelect"
+              as={
+                <Autocomplete
+                  multiple
+                  options={allGroups.filter(
+                    (g) =>
+                      !allUsers
+                        .filter((u) => u.username === addGroupsFormUsername)[0]
+                        ?.groups?.map((gr) => gr.id)
+                        ?.includes(g.id)
+                  )}
+                  getOptionLabel={(group) => group.name}
+                  filterSelectedOptions
+                  data-testid="addUsersFromGroupsSelect"
+                  renderInput={(params) => (
+                    <TextField
+                      // eslint-disable-next-line react/jsx-props-no-spreading
+                      {...params}
+                      error={!!errors.groups}
+                      variant="outlined"
+                      label="Select Groups/Users"
+                      data-testid="addUsersFromGroupsTextField"
+                    />
+                  )}
+                />
+              }
+              control={control}
+              onChange={([, data]) => data}
+              rules={{ validate: validateGroups }}
+              defaultValue={[]}
+            />
+            <div>
+              <Button
+                variant="contained"
+                type="submit"
+                name="submitAddFromGroupsButton"
+                data-testid="submitAddFromGroupsButton"
+              >
+                Submit
+              </Button>
+            </div>
+          </form>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
