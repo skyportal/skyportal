0aff737ea7fc6c89b82f3fd4304d8ed0923a7733
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-10-22 16:23:32 -0700

    Allow adding users to new groups from user management page (#1149)
    * Allow adding users to new group(s) from user management page
    
    * Add tests
    
    * Incorporate feedback from Dima

--- skyportal/tests/frontend/test_user_management.py
+++ skyportal/tests/frontend/test_user_management.py
@@ -25,3 +25,21 @@ def test_delete_stream_user(driver, super_admin_user, user, stream_with_users):
         f"//div[@id='deleteStreamUserButton_{user.id}_{stream.id}']//*[contains(@class, 'MuiChip-deleteIcon')]"
     )
     driver.wait_for_xpath(f"//div[text()='Stream access successfully revoked.']")
+
+
+def test_add_user_to_group(driver, user, super_admin_user, public_group, public_group2):
+    driver.get(f'/become_user/{super_admin_user.id}')
+    driver.get('/user_management')
+    driver.wait_for_xpath(
+        f"//div[@id='deleteGroupUserButton_{user.id}_{public_group.id}']"
+    )
+    driver.click_xpath(f'//*[@data-testid="addUserGroupsButton{user.id}"]')
+    driver.click_xpath('//*[@id="addUserToGroupsSelect"]')
+    driver.click_xpath(f'//li[text()="{public_group2.name}"]')
+    driver.click_xpath('//*[text()="Submit"]')
+    driver.wait_for_xpath(
+        '//*[text()="User successfully added to specified group(s)."]'
+    )
+    driver.wait_for_xpath(
+        f"//div[@id='deleteGroupUserButton_{user.id}_{public_group2.id}']"
+    )
