67c5a8e079213828b37bc2636b216cec77fab40b
Danny Goldstein <dgold@berkeley.edu>
2020-10-23 14:04:25 -0700

    link new migration script to old one

--- alembic/versions/d0195eb7ecd0_migration_script_for_1135.py
+++ alembic/versions/d0195eb7ecd0_migration_script_for_1135.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'd0195eb7ecd0'
-down_revision = None
+down_revision = '93d1e086aafb'
 branch_labels = None
 depends_on = None
 
