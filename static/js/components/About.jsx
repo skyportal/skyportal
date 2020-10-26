c183bb4c80a0ebd974d36b73354de245ba90da1d
Kyung Min Shin <kshin@caltech.edu>
2020-10-26 11:04:51 -0700

    Make the about JSX component the base template to simplify structure

--- static/js/components/About.jsx
+++ static/js/components/About.jsx
@@ -1,4 +1,26 @@
-{% block imports -%}
+/*
+{# Projects extending Skyportal can modify various segments of the About page by
+  writing their own About.jsx.template which extends BaseAbout.jsx.template and
+  overwrites the Jinja template blocks they wish to customize. Here, we just
+  render the base template as is. This way, just the specific parts that need to
+  be customized/appended to can be overwritten without having to overwrite the
+  entire About.jsx component.
+
+  Note that the base template uses comments to comment out Jinja-specific syntax
+  like the block declarations in order to make the template double as a valid
+  JSX file. Thus, further care should be taken when extending the template in
+  order to keep those comment characters from commenting out the blocks you
+  choose to overwrite. For example, you should place "*\/}" (without the escape
+  backslash) after {% block %} declarations within the JSX component render()
+  segment, and "{/*" immediately before the {% endblock %} declarations within
+  the render().
+
+  See https://jinja.palletsprojects.com/en/2.11.x/templates/#template-inheritance
+  for documentation on usage of Jinja templates.
+#}
+*/
+
+// {/* {% block imports -%} */}
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -15,17 +37,18 @@ import Paper from "@material-ui/core/Paper";
 
 import clsx from "clsx";
 import dayjs from "dayjs";
-{% endblock -%}
+
+// {% endblock -%}
 
 const useStyles = makeStyles((theme) => ({
-  {% block styles -%}
+  // {% block styles %}
   root: {
     padding: "1rem 2rem 2rem 2rem",
     fontSize: "1rem",
     "& .MuiTypography-h5": {
       margin: "1rem 0 0.83rem 0",
       fontWeight: 600,
-      wordBreak: "break-all"
+      wordBreak: "break-all",
     },
     "& .MuiTypography-body1": {
       margin: "1rem 0",
@@ -58,7 +81,7 @@ const useStyles = makeStyles((theme) => ({
   gitlogPR: {
     color: theme.palette.primary.dark,
   },
-  {% endblock -%}
+  // {% endblock -%}
 }));
 
 const BibLink = ({ bibtex, children }) => {
@@ -91,53 +114,58 @@ BibLink.propTypes = {
 };
 
 const About = () => {
-  {% block componentConsts -%}
+  // {% block componentConsts -%}
   const classes = useStyles();
   const version = useSelector((state) => state.sysInfo.version);
   const cosmology = useSelector((state) => state.sysInfo.cosmology);
   const cosmoref = useSelector((state) => state.sysInfo.cosmoref);
   const gitlog = useSelector((state) => state.sysInfo.gitlog);
-  {% endblock -%}
+  // {% endblock -%}
+
+  // {% block configVariables -%}
+  // Replace these with Jinja double-brace injection when extending
+  const appTitle = "SkyPortal";
+  // {% endblock -%}
 
   return (
     <Paper className={classes.root}>
-      {% block header -%}
+      {/* {% block header -%} */}
       <Typography variant="h5">
-        This is {{ app.title }}&nbsp;
+        This is {appTitle}&nbsp;
         <code>v{version}</code>.
       </Typography>
-      {% endblock -%}
-      {% block projectIntro -%}
+      {/* {% endblock -%} */}
+      {/* {% block projectIntro -%} */}
       <Typography variant="body1">
         The project homepage is at&nbsp;
         <a href="https://skyportal.io">https://skyportal.io</a>
       </Typography>
-      {% endblock -%}
-      {% block documentation -%}
+      {/* {% endblock -%} */}
+      {/* {% block documentation -%} */}
       <Typography variant="body1">
         Documentation lives at&nbsp;
         <a href="https://skyportal.io">https://skyportal.io/docs/</a>
       </Typography>
       <Typography variant="body1">
-        You may also interact with {{ app.title }} through its API. Generate a token
+        You may also interact with {appTitle} through its API. Generate a token
         from your&nbsp;
         <Link to="/profile">profile</Link>&nbsp; page, then refer to the&nbsp;
         <a href="https://skyportal.io/docs/api.html">API documentation</a>.
       </Typography>
-      {% endblock -%}
-      {% block githubIssues -%}
+      {/* {% endblock -%} */}
+      {/* {% block githubIssues -%} */}
       <Typography variant="body1">
         Please file issues on our GitHub page at&nbsp;
         <a href="https://github.com/skyportal/skyportal">
           https://github.com/skyportal/skyportal
         </a>
       </Typography>
-      {% endblock -%}
-      {% block citation -%}
+      {/* {% endblock -%} */}
+      {/* {% block citation -%} */}
       <div>
         If you found SkyPortal useful, please consider citing our work:
+        {/* {% raw -%} */}
         <BibLink
-          {% raw -%}
           bibtex={`@article{skyportal2019,
   author = {St\\'efan J. van der Walt and Arien Crellin-Quick and Joshua S. Bloom},
   title = {{SkyPortal}: An Astronomical Data Platform},
@@ -149,8 +177,9 @@ const About = () => {
   month = {may},
   doi = {10.21105/joss.01247},
   url = {http://joss.theoj.org/papers/10.21105/joss.01247}
-}`} {%- endraw -%}
+}`}
         >
+          {/* {%- endraw -%} */}
           Stéfan J. van der Walt, Arien Crellin-Quick, Joshua S. Bloom,{" "}
           <em>SkyPortal: An Astronomical Data Platform.</em> Journal of Open
           Source Software, 4(37) 1247, May 2019.{" "}
@@ -160,9 +189,10 @@ const About = () => {
           .
         </BibLink>
       </div>
-      {% endblock -%}
-      {% block acknowledgements -%}{% endblock -%}
-      {% block gitlog -%}
+      {/* {% endblock -%} */}
+      {/* {% block acknowledgements -%} */}
+      {/* {% endblock -%} */}
+      {/* {% block gitlog -%} */}
       {gitlog && (
         <>
           <Typography variant="h5">Recent Changelog</Typography>
@@ -197,8 +227,8 @@ const About = () => {
           </Paper>
         </>
       )}
-      {% endblock -%}
-      {% block cosmology -%}
+      {/* {% endblock -%} */}
+      {/* {% block cosmology -%} */}
       <Typography variant="h5">Cosmology</Typography>
       <span>
         The cosmology currently used here is an instance of{" "}
@@ -216,7 +246,7 @@ const About = () => {
           </>
         )}
       </span>
-      {% endblock -%}
+      {/* {% endblock -%} */}
     </Paper>
   );
 };
