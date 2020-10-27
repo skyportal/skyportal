00813f8e7683efd586dbc75ee0bba40329840a4b
guynir42 <guyynir@gmail.com>
2020-10-27 18:01:16 +0200

    added VegaSpectrum for viewing source spectra on groups sources and source list pages

---
+++ static/js/components/VegaSpectrum.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import PropTypes from "prop-types";
+import embed from "vega-embed";
+
+const spec = (url) => ({
+  $schema: "https://vega.github.io/schema/vega-lite/v4.json",
+  data: {
+    url,
+    format: {
+      type: "json",
+      property: "data", // where on the JSON does the data live
+    },
+  },
+  transform: [
+    {
+      flatten: ["wavelengths", "fluxes"],
+    },
+  ],
+  scales: [
+    {
+      name: "wavelengths",
+      type: "point",
+      range: "width",
+      domain: { field: "wavelengths" },
+    },
+    {
+      name: "fluxes",
+      type: "linear",
+      range: "height",
+      nice: true,
+      zero: true,
+      domain: { data: "table", field: "fluxes" },
+    },
+    {
+      name: "color",
+      type: "ordinal",
+      range: "category",
+      domain: { field: "observed_at" },
+    },
+  ],
+  mark: {
+    type: "line",
+    interpolate: "linear",
+  },
+  encoding: {
+    x: {
+      field: "wavelengths",
+      type: "quantitative",
+    },
+    y: {
+      field: "fluxes",
+      type: "quantitative",
+      axis: { format: ".2e" },
+    },
+    color: {
+      field: "observed_at",
+      type: "nominal",
+    },
+  },
+  background: "transparent",
+});
+
+const VegaSpectrum = React.memo((props) => {
+  const { dataUrl } = props;
+  return (
+    <div
+      ref={(node) => {
+        if (node) {
+          embed(node, spec(dataUrl), {
+            actions: false,
+          });
+        }
+      }}
+    />
+  );
+});
+
+VegaSpectrum.propTypes = {
+  dataUrl: PropTypes.string.isRequired,
+};
+
+VegaSpectrum.displayName = "VegaSpectrum";
+
+export default VegaSpectrum;
