cee2229b80e2a4497781122dafbaf01cc47aae59
Dmitry Duev <dmitryduev@users.noreply.github.com>
2020-10-27 23:33:32 -0700

    Fix candidate passed_at parsing (#1197)
    * fix candidate passed_at parsing
    
    * report filter_id,passed_at,passing_alert_id in /api/candidates.GET
    
    * implement /api/candidates.PUT
    
    * fix test_token_user_update_candidate
    
    * fix test_cannot_update_candidate_without_permission

--- skyportal/handlers/api/candidate.py
+++ skyportal/handlers/api/candidate.py
@@ -277,8 +277,18 @@ class CandidateHandler(BaseHandler):
             )
             filter_ids = [cand.filter_id for cand in accessible_candidates]
 
+            passing_alerts = [
+                {
+                    "filter_id": cand.filter_id,
+                    "passing_alert_id": cand.passing_alert_id,
+                    "passed_at": cand.passed_at,
+                }
+                for cand in accessible_candidates
+            ]
+
             candidate_info = c.to_dict()
             candidate_info["filter_ids"] = filter_ids
+            candidate_info["passing_alerts"] = passing_alerts
             candidate_info["comments"] = sorted(
                 [cmt.to_dict() for cmt in c.get_comments_owned_by(self.current_user)],
                 key=lambda x: x["created_at"],
@@ -630,7 +640,7 @@ class CandidateHandler(BaseHandler):
         passing_alert_id = data.pop("passing_alert_id", None)
         passed_at = data.pop("passed_at", None)
         if passed_at is not None:
-            passed_at = arrow.get(passed_at)
+            passed_at = arrow.get(passed_at).datetime
         try:
             filter_ids = data.pop("filter_ids")
         except KeyError:
@@ -678,7 +688,7 @@ class CandidateHandler(BaseHandler):
         return self.success(data={"id": obj.id})
 
     @permissions(["Manage sources"])
-    def patch(self, obj_id):
+    def put(self, obj_id):
         """
         ---
         description: Update a candidate
@@ -691,7 +701,24 @@ class CandidateHandler(BaseHandler):
         requestBody:
           content:
             application/json:
-              schema: ObjNoID
+              schema:
+                type: object
+                properties:
+                  filter_ids:
+                    type: array
+                    items:
+                      type: integer
+                    description: List of associated filter IDs
+                  passing_alert_id:
+                    type: integer
+                    description: ID of associated filter that created candidate
+                    nullable: true
+                  passed_at:
+                    type: string
+                    description: Arrow-parseable datetime string indicating when alert passed filter.
+                    nullable: true
+                required:
+                  - filter_ids
         responses:
           200:
             content:
@@ -702,21 +729,51 @@ class CandidateHandler(BaseHandler):
               application/json:
                 schema: Error
         """
-        # Ensure user has access to candidate
-        c = Candidate.get_obj_if_owned_by(obj_id, self.current_user)
-        if c is None:
-            return self.error("Invalid ID.")
         data = self.get_json()
-        data["id"] = obj_id
+        data["obj_id"] = obj_id
 
-        schema = Obj.__schema__()
+        # Ensure user has access to candidate
+        if (
+            DBSession().query(Candidate).filter(Candidate.obj_id == obj_id).first()
+            is None
+        ):
+            return self.error("Invalid ID.")
+
+        passing_alert_id = data.pop("passing_alert_id", None)
+        passed_at = data.pop("passed_at", None)
+        if passed_at is not None:
+            passed_at = arrow.get(passed_at).datetime
         try:
-            obj = schema.load(data, partial=True)
-        except ValidationError as e:
+            filter_ids = data.pop("filter_ids")
+        except KeyError:
+            return self.error("Missing required filter_ids parameter.")
+        user_accessible_filter_ids = [
+            filtr.id
+            for g in self.current_user.accessible_groups
+            for filtr in g.filters
+            if g.filters is not None
+        ]
+        if not all([fid in user_accessible_filter_ids for fid in filter_ids]):
             return self.error(
-                "Invalid/missing parameters: " f"{e.normalized_messages()}"
+                "Insufficient permissions - you must only specify "
+                "filters that you have access to."
             )
-        update_redshift_history_if_relevant(data, obj, self.associated_user_object)
+
+        filters = Filter.query.filter(Filter.id.in_(filter_ids)).all()
+        if not filters:
+            return self.error("At least one valid filter ID must be provided.")
+
+        candidates = (
+            DBSession()
+            .query(Candidate)
+            .filter(Candidate.obj_id == obj_id, Candidate.filter_id.in_(filter_ids))
+            .all()
+        )
+
+        for candidate in candidates:
+            candidate.passed_at = passed_at
+            candidate.passing_alert_id = passing_alert_id
+
         DBSession().commit()
 
         self.push_all(action="skyportal/FETCH_CANDIDATES")
