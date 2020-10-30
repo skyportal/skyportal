bbfa8fc7c62dbcb074d196ee311baf6e1ace4a80
Danny Goldstein <dgold@berkeley.edu>
2020-10-30 15:52:42 -0700

    remove unused tests

--- skyportal/tests/frontend/test_user.py
+++
@@ -1,8 +0,0 @@
-def test_user_info(driver, super_admin_user):
-    user = super_admin_user
-    driver.get(f'/become_user/{user.id}')
-    driver.get(f'/user/{user.id}')
-    driver.wait_for_xpath(f'//div[contains(.,"{user.username}")]')
-    pg_src = driver.page_source
-    for acl in user.permissions:
-        assert acl in pg_src
