4c5f3d72ecf46722016b08591f3ebe868fe38561
Danny Goldstein <dgold@berkeley.edu>
2020-10-30 18:16:05 -0700

    Serialize console logs  as artifacts on test failure (#1212)
    * add the browser console log to the test_results package
    
    * write browser console logs to a file on test failure
    
    * upload webdriver console logs to a different artifact
    
    * truncate geckodriver log to save space
    
    * touch geckodriver log if it dont exist
    
    * pin new baselayer

--- skyportal/tests/conftest.py
+++ skyportal/tests/conftest.py
@@ -63,6 +63,7 @@ def pytest_runtest_setup(item):
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     # execute all other hooks to obtain the report object
+
     outcome = yield
     rep = outcome.get_result()
 
@@ -75,12 +76,33 @@ def pytest_runtest_makereport(item, call):
 # check if a test has failed
 @pytest.fixture(scope="function", autouse=True)
 def test_failed_check(request):
+
+    gecko = Path('geckodriver.log')
+    gecko.touch(exist_ok=True)
+
+    # get the number of bytes in the file currently
+    log_bytes = os.path.getsize(gecko)
+
+    # add a separator to the geckodriver logs
+    with open(gecko, 'a') as f:
+        f.write(f'BEGIN {request.node.nodeid}\n')
+
     yield
     # request.node is an "item" because we use the default
     # "function" scope
+
+    # add a separator to the geckodriver logs
+    with open(gecko, 'a') as f:
+        f.write(f'END {request.node.nodeid}\n')
+
     if request.node.rep_call.failed and 'driver' in request.node.funcargs:
         webdriver = request.node.funcargs['driver']
         take_screenshot_and_page_source(webdriver, request.node.nodeid)
+
+    # delete the interstitial data from the geckodriver log by
+    # truncating the file back to its original number of bytes
+    with open(gecko, 'a') as f:
+        f.truncate(log_bytes)
 
 
 # make a screenshot with a name of the test, date and time.
@@ -91,9 +113,27 @@ def take_screenshot_and_page_source(webdriver, nodeid):
     ).replace(":", "_")
     file_name = os.path.join(os.path.dirname(__file__), '../../test-results', file_name)
     Path(file_name).parent.mkdir(parents=True, exist_ok=True)
+
     webdriver.save_screenshot(file_name)
     with open(file_name.replace('png', 'html'), 'w') as f:
         f.write(webdriver.page_source)
+
+    file_name = f'{nodeid}_{datetime.today().strftime("%Y-%m-%d_%H:%M")}.console.log'.replace(
+        "/", "_"
+    ).replace(
+        ":", "_"
+    )
+    file_name = os.path.join(
+        os.path.dirname(__file__), '../../webdriver-console', file_name
+    )
+    Path(file_name).parent.mkdir(parents=True, exist_ok=True)
+
+    with open(file_name, 'w') as f, open('geckodriver.log', 'r') as gl:
+        lines = gl.readlines()
+        revlines = list(reversed(lines))
+        istart = revlines.index(f'BEGIN {nodeid}\n')
+        iend = revlines.index(f'END {nodeid}\n')
+        f.write('\n'.join(list(reversed(revlines[iend : (istart + 1)]))))  # noqa: E203
 
 
 @pytest.fixture()
