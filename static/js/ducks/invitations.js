87615556fc438d53fe08f8b4c95af4edbe441be6
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-11-03 16:03:50 -0800

    Add invitations reducer and new actions

--- static/js/ducks/invitations.js
+++ static/js/ducks/invitations.js
@@ -1,7 +1,11 @@
+import store from "../store";
 import * as API from "../API";
 
 export const INVITE_USER = "skyportal/INVITE_USER";
 export const INVITE_USER_OK = "skyportal/INVITE_USER_OK";
+
+export const FETCH_INVITATIONS = "skyportal/FETCH_INVITATIONS";
+export const FETCH_INVITATIONS_OK = "skyportal/FETCH_INVITATIONS_OK";
 
 export const inviteUser = ({ userEmail, streamIDs, groupIDs, groupAdmin }) =>
   API.POST("/api/invitations", INVITE_USER, {
@@ -10,3 +14,18 @@ export const inviteUser = ({ userEmail, streamIDs, groupIDs, groupAdmin }) =>
     groupIDs,
     groupAdmin,
   });
+
+export const fetchInvitations = () =>
+  API.GET("/api/invitations", FETCH_INVITATIONS);
+
+function reducer(state = null, action) {
+  switch (action.type) {
+    case FETCH_INVITATIONS_OK: {
+      return action.data;
+    }
+    default:
+      return state;
+  }
+}
+
+store.injectReducer("invitations", reducer);
