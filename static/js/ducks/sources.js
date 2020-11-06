ac3e0275f2e86e084dadb65cdb04c214013eff1f
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-11-05 16:45:44 -0800

    Update sources duck to separate out savedGroupSources and pendingGroupSources state branches

--- static/js/ducks/sources.js
+++ static/js/ducks/sources.js
@@ -18,78 +18,117 @@ export const FETCH_PENDING_GROUP_SOURCES_OK =
 export const FETCH_PENDING_GROUP_SOURCES_FAIL =
   "skyportal/FETCH_PENDING_GROUP_SOURCES_FAIL";
 
-const addFilterParamDefaults = (filterParams) => {
+export function fetchSources(filterParams = {}) {
   if (!Object.keys(filterParams).includes("pageNumber")) {
     filterParams.pageNumber = 1;
   }
-  if (!Object.keys(filterParams).includes("numPerPage")) {
-    filterParams.numPerPage = 10;
-  }
-};
-
-export function fetchSources(filterParams = {}) {
-  addFilterParamDefaults(filterParams);
   const params = new URLSearchParams(filterParams);
   const queryString = params.toString();
   return API.GET(`/api/sources?${queryString}`, FETCH_SOURCES);
 }
 
-export function fetchGroupSources(filterParams = {}) {
+export function fetchSavedGroupSources(filterParams = {}) {
   if (!Object.keys(filterParams).includes("pageNumber")) {
     filterParams.pageNumber = 1;
   }
   const params = new URLSearchParams(filterParams);
   const queryString = params.toString();
-  return API.GET(`/api/sources?${queryString}`, FETCH_PENDING_GROUP_SOURCES);
+  return API.GET(`/api/sources?${queryString}`, FETCH_SAVED_GROUP_SOURCES);
+}
+
+export function fetchPendingGroupSources(filterParams = {}) {
+  if (!Object.keys(filterParams).includes("pageNumber")) {
+    filterParams.pageNumber = 1;
+  }
+  filterParams.pendingOnly = true;
+  const params = new URLSearchParams(filterParams);
+  const queryString = params.toString();
+  return API.GET(`/api/sources?${queryString}`, FETCH_SAVED_GROUP_SOURCES);
 }
 
 const initialState = {
-  sources: null,
+  latest: null,
+  savedGroupSources: null,
+  pendingGroupSources: null,
   pageNumber: 1,
+  lastPage: false,
   totalMatches: 0,
-  numPerPage: 10,
+  numberingStart: 0,
+  numberingEnd: 0,
 };
 
-const reducer = (
-  state = {
-    latest: initialState,
-    savedGroupSources: initialState,
-    pendingGroupSources: initialState,
-  },
-  action
-) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_SOURCES: {
       return {
         ...state,
-        latest: {
-          ...state.latest,
-          queryInProgress: action.parameters.body.pageNumber === undefined,
-        },
+        queryInProgress: action.parameters.body.pageNumber === undefined,
       };
     }
     case FETCH_SOURCES_OK: {
+      const {
+        sources,
+        pageNumber,
+        lastPage,
+        totalMatches,
+        numberingStart,
+        numberingEnd,
+      } = action.data;
       return {
         ...state,
-        latest: { ...action.data, queryInProgress: false },
+        latest: sources,
+        queryInProgress: false,
+        pageNumber,
+        lastPage,
+        totalMatches,
+        numberingStart,
+        numberingEnd,
       };
     }
     case FETCH_SOURCES_FAIL: {
       return {
         ...state,
-        latest: { ...state.latest, queryInProgress: false },
+        queryInProgress: false,
+      };
+    }
+    case FETCH_SAVED_GROUP_SOURCES: {
+      return {
+        ...state,
+        queryInProgress: action.parameters.body.pageNumber === undefined,
       };
     }
     case FETCH_SAVED_GROUP_SOURCES_OK: {
+      const { sources } = action.data;
       return {
         ...state,
-        savedGroupSources: action.data,
+        savedGroupSources: sources,
+        queryInProgress: false,
+      };
+    }
+    case FETCH_SAVED_GROUP_SOURCES_FAIL: {
+      return {
+        ...state,
+        queryInProgress: false,
+      };
+    }
+    case FETCH_PENDING_GROUP_SOURCES: {
+      return {
+        ...state,
+        queryInProgress: action.parameters.body.pageNumber === undefined,
       };
     }
     case FETCH_PENDING_GROUP_SOURCES_OK: {
+      const { sources } = action.data;
       return {
         ...state,
-        pendingGroupSources: action.data,
+        pendingGroupSources: sources,
+        queryInProgress: false,
+      };
+    }
+    case FETCH_PENDING_GROUP_SOURCES_FAIL: {
+      return {
+        ...state,
+        queryInProgress: false,
       };
     }
     default:
