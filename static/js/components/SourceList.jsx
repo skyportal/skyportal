1876cadff63e0ec6d70bdfa2a773b4a96b1194a5
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-11-05 17:16:10 -0800

    Combine SourceList and SourceListFilterForm into SourceList

--- static/js/components/SourceList.jsx
+++ static/js/components/SourceList.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
@@ -8,274 +8,227 @@ import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import TextField from "@material-ui/core/TextField";
-import InputLabel from "@material-ui/core/InputLabel";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
 import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import { useForm, Controller } from "react-hook-form";
 
-import * as Actions from "../ducks/sources";
+import * as sourcesActions from "../ducks/sources";
+import UninitializedDBMessage from "./UninitializedDBMessage";
+import SourceTable from "./SourceTable";
 
-const SourceListFilterForm = ({ sources }) => {
+const useStyles = makeStyles((theme) => ({
+  paperDiv: {
+    padding: "1rem",
+    height: "100%",
+  },
+  tableGrid: {
+    width: "100%",
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  paper: {
+    padding: "1rem",
+    marginTop: "0.625rem",
+  },
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& .MuiTextField-root": {
+      margin: theme.spacing(0.2),
+      width: "10rem",
+    },
+  },
+  blockWrapper: {
+    width: "100%",
+  },
+  title: {
+    margin: "1rem 0rem 0rem 0rem",
+  },
+}));
+
+const SourceList = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
+
+  const sources = useSelector((state) => state.sources);
+  const sourceTableEmpty = useSelector(
+    (state) => state.dbInfo.source_table_empty
+  );
+
+  useEffect(() => {
+    if (!sources.latest) {
+      dispatch(sourcesActions.fetchSources());
+    }
+  }, [sources.latest, dispatch]);
 
   const { handleSubmit, register, getValues, control, reset } = useForm();
 
-  const [jumpToPageInputValue, setJumpToPageInputValue] = useState("");
-
   const onSubmit = (data) => {
-    dispatch(Actions.fetchSources(data));
+    dispatch(sourcesActions.fetchSources(data));
   };
 
   const handleClickReset = () => {
     reset({ numPerPage: 100 });
-    dispatch(Actions.fetchSources());
+    dispatch(sourcesActions.fetchSources());
   };
 
-  const handleClickNextPage = (event) => {
-    event.preventDefault();
+  const handleSourceTablePagination = (pageNumber, numPerPage) => {
     const data = {
       ...getValues(),
-      pageNumber: sources.pageNumber + 1,
+      pageNumber,
+      numPerPage,
       totalMatches: sources.totalMatches,
     };
-    dispatch(Actions.fetchSources(data));
+    dispatch(sourcesActions.fetchSources(data));
   };
 
-  const handleClickPreviousPage = (event) => {
-    event.preventDefault();
-    const data = {
-      ...getValues(),
-      pageNumber: sources.pageNumber - 1,
-      totalMatches: sources.totalMatches,
-    };
-    dispatch(Actions.fetchSources(data));
-  };
-
-  const handleJumpToPageInputChange = (event) => {
-    setJumpToPageInputValue(event.target.value);
-  };
-
-  const handleClickJumpToPage = (event) => {
-    event.preventDefault();
-    const formState = getValues();
-    const data = {
-      ...formState,
-      pageNumber: jumpToPageInputValue,
-      totalMatches: sources.totalMatches,
-    };
-    if (jumpToPageInputValue < 1) {
-      data.pageNumber = 1;
-    } else if (
-      jumpToPageInputValue >
-      Math.ceil(sources.totalMatches / formState.numPerPage)
-    ) {
-      data.pageNumber = Math.ceil(sources.totalMatches / formState.numPerPage);
-    }
-    dispatch(Actions.fetchSources(data));
-  };
-
-  const useStyles = makeStyles((theme) => ({
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
-    },
-    paper: {
-      padding: "1rem",
-      marginTop: "0.625rem",
-    },
-    root: {
-      display: "flex",
-      flexWrap: "wrap",
-      "& .MuiTextField-root": {
-        margin: theme.spacing(0.2),
-        width: "10rem",
-      },
-    },
-    blockWrapper: {
-      width: "100%",
-    },
-    title: {
-      margin: "1rem 0rem 0rem 0rem",
-    },
-  }));
-
-  const classes = useStyles();
+  if (sourceTableEmpty) {
+    return <UninitializedDBMessage />;
+  }
+  if (!sources.latest) {
+    return <div>Loading sources...</div>;
+  }
 
   return (
-    <Paper className={classes.paper} variant="outlined">
-      <form className={classes.root} onSubmit={handleSubmit(onSubmit)}>
-        <div className={classes.blockWrapper}>
-          <h4> Filter Sources </h4>
-        </div>
-        <div className={classes.blockWrapper}>
-          <h5 className={classes.title}> Filter by Name or ID </h5>
-        </div>
-        <div className={classes.blockWrapper}>
-          <TextField
-            fullWidth
-            label="Source ID/Name"
-            name="sourceID"
-            inputRef={register}
-          />
-        </div>
+    <Paper elevation={1}>
+      <div className={classes.paperDiv}>
+        <Typography variant="h6" display="inline">
+          Sources
+        </Typography>
+        <Paper className={classes.paper} variant="outlined">
+          <form className={classes.root} onSubmit={handleSubmit(onSubmit)}>
+            <div className={classes.blockWrapper}>
+              <h4> Filter Sources </h4>
+            </div>
+            <div className={classes.blockWrapper}>
+              <h5 className={classes.title}> Filter by Name or ID </h5>
+            </div>
+            <div className={classes.blockWrapper}>
+              <TextField
+                fullWidth
+                label="Source ID/Name"
+                name="sourceID"
+                inputRef={register}
+              />
+            </div>
 
-        <div className={classes.blockWrapper}>
-          <h5 className={classes.title}> Filter by Position </h5>
-        </div>
-        <div className={classes.blockWrapper}>
-          <TextField
-            size="small"
-            label="RA (degrees)"
-            name="ra"
-            inputRef={register}
-          />
-          <TextField
-            size="small"
-            label="Dec (degrees)"
-            name="dec"
-            inputRef={register}
-          />
-          <TextField
-            size="small"
-            label="Radius (degrees)"
-            name="radius"
-            inputRef={register}
-          />
-        </div>
-        <div className={classes.blockWrapper}>
-          <h5 className={classes.title}>Filter by Time Last Detected (UTC)</h5>
-        </div>
-        <div className={classes.blockWrapper}>
-          <TextField
-            size="small"
-            label="Start Date"
-            name="startDate"
-            inputRef={register}
-            placeholder="2012-08-30T00:00:00"
-          />
-          <TextField
-            size="small"
-            label="End Date"
-            name="endDate"
-            inputRef={register}
-            placeholder="2012-08-30T00:00:00"
-          />
-        </div>
-        <div className={classes.blockWrapper}>
-          <h5 className={classes.title}> Filter by Simbad Class </h5>
-        </div>
-        <div className={classes.blockWrapper}>
-          <TextField
-            size="small"
-            label="Class Name"
-            type="text"
-            name="simbadClass"
-            inputRef={register}
-          />
-          <FormControlLabel
-            label="TNS Name"
-            labelPlacement="start"
-            control={
-              <Controller
-                as={<Checkbox color="primary" type="checkbox" />}
-                name="hasTNSname"
-                control={control}
-                defaultValue={false}
+            <div className={classes.blockWrapper}>
+              <h5 className={classes.title}> Filter by Position </h5>
+            </div>
+            <div className={classes.blockWrapper}>
+              <TextField
+                size="small"
+                label="RA (degrees)"
+                name="ra"
+                inputRef={register}
               />
-            }
-          />
-        </div>
-        <div className={classes.blockWrapper}>
-          <ButtonGroup
-            variant="contained"
-            color="primary"
-            aria-label="contained primary button group"
-          >
-            <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-              disabled={sources.queryInProgress}
-            >
-              Submit
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleClickReset}
-            >
-              Reset
-            </Button>
-          </ButtonGroup>
-        </div>
-        {sources && (
-          <div>
-            <div>
-              <Button
-                type="button"
-                onClick={handleClickPreviousPage}
-                disabled={sources.pageNumber === 1}
-              >
-                Previous Page
-              </Button>
-              <i>
-                Displaying&nbsp;
-                {sources.numberingStart}-{sources.numberingEnd}
-                &nbsp; of&nbsp;
-                {sources.totalMatches}
-                &nbsp; matching sources.
-              </i>
-              <Button
-                type="button"
-                onClick={handleClickNextPage}
-                disabled={sources.lastPage}
-              >
-                Next Page
-              </Button>
+              <TextField
+                size="small"
+                label="Dec (degrees)"
+                name="dec"
+                inputRef={register}
+              />
+              <TextField
+                size="small"
+                label="Radius (degrees)"
+                name="radius"
+                inputRef={register}
+              />
             </div>
-            <div>
-              <FormControl variant="filled" className={classes.formControl}>
-                <InputLabel id="nPerPageInputLabel">Num Per Page</InputLabel>
-                <Controller
-                  as={
-                    <Select labelId="nPerPageInputLabel">
-                      {[10, 25, 50, 100, 500].map((n) => (
-                        <MenuItem value={n} key={n}>
-                          {n}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  }
-                  name="numPerPage"
-                  control={control}
-                  defaultValue={100}
-                />
-              </FormControl>
+            <div className={classes.blockWrapper}>
+              <h5 className={classes.title}>
+                Filter by Time Last Detected (UTC)
+              </h5>
             </div>
-            <div>
-              <i>or&nbsp;&nbsp;</i>
-              <Button type="button" onClick={handleClickJumpToPage}>
-                Jump to page:
-              </Button>
-              &nbsp;&nbsp;
-              <input
+            <div className={classes.blockWrapper}>
+              <TextField
+                size="small"
+                label="Start Date"
+                name="startDate"
+                inputRef={register}
+                placeholder="2012-08-30T00:00:00"
+              />
+              <TextField
+                size="small"
+                label="End Date"
+                name="endDate"
+                inputRef={register}
+                placeholder="2012-08-30T00:00:00"
+              />
+            </div>
+            <div className={classes.blockWrapper}>
+              <h5 className={classes.title}> Filter by Simbad Class </h5>
+            </div>
+            <div className={classes.blockWrapper}>
+              <TextField
+                size="small"
+                label="Class Name"
                 type="text"
-                style={{ width: "25px" }}
-                onChange={handleJumpToPageInputChange}
-                value={jumpToPageInputValue}
-                name="jumpToPageInputField"
+                name="simbadClass"
+                inputRef={register}
+              />
+              <FormControlLabel
+                label="TNS Name"
+                labelPlacement="start"
+                control={
+                  <Controller
+                    as={<Checkbox color="primary" type="checkbox" />}
+                    name="hasTNSname"
+                    control={control}
+                    defaultValue={false}
+                  />
+                }
               />
             </div>
+            <div className={classes.blockWrapper}>
+              <ButtonGroup
+                variant="contained"
+                color="primary"
+                aria-label="contained primary button group"
+              >
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  disabled={sources.queryInProgress}
+                >
+                  Submit
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleClickReset}
+                >
+                  Reset
+                </Button>
+              </ButtonGroup>
+            </div>
+          </form>
+        </Paper>
+        {!sources.queryInProgress && (
+          <Grid item className={classes.tableGrid}>
+            <SourceTable
+              sources={sources.latest}
+              paginateCallback={handleSourceTablePagination}
+            />
+          </Grid>
+        )}
+        {sources.queryInProgress && (
+          <div>
+            <br />
+            <br />
+            <i>Query in progress...</i>
           </div>
         )}
-      </form>
+      </div>
     </Paper>
   );
 };
 
-SourceListFilterForm.propTypes = {
+SourceList.propTypes = {
   sources: PropTypes.shape({
     lastPage: PropTypes.bool,
     pageNumber: PropTypes.number,
@@ -286,4 +239,4 @@ SourceListFilterForm.propTypes = {
   }).isRequired,
 };
 
-export default SourceListFilterForm;
+export default SourceList;
