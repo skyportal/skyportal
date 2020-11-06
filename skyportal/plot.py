c9c2ebd87e7dd6754a49243e4b557cf4c5d603d8
Stefan van der Walt <stefanv@berkeley.edu>
2020-11-05 23:00:25 -0800

    Fix slider and file download

--- skyportal/plot.py
+++ skyportal/plot.py
@@ -785,7 +785,7 @@ def spectroscopy_plot(obj_id, spec_id=None):
         CustomJS(
             args={'slider': z_slider, 'textinput': z_textinput},
             code="""
-            textinput.value = slider.value.toFixed(3).toString();
+            textinput.value = parseFloat(slider.value).toFixed(3);
             textinput.change.emit();
         """,
         ),
@@ -800,7 +800,7 @@ def spectroscopy_plot(obj_id, spec_id=None):
         CustomJS(
             args={'slider': v_exp_slider, 'textinput': v_exp_textinput},
             code="""
-            textinput.value = slider.value.toFixed(0).toString();
+            textinput.value = parseFloat(slider.value).toFixed(0);
             textinput.change.emit();
         """,
         ),
