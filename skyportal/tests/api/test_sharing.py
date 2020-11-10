3afb9e9bf6870b50f8ceaf51f258879f5e8a3547
Danny Goldstein <dgold@berkeley.edu>
2020-11-09 17:34:53 -0800

    test that super admin can share any data

--- skyportal/tests/api/test_sharing.py
+++ skyportal/tests/api/test_sharing.py
@@ -244,3 +244,66 @@ def test_cannot_share_unowned_photometry(
     assert status == 400
     assert data['status'] == 'error'
     assert 'owner' in data['message'].lower()
+
+
+def test_system_admin_can_share_unowned_photometry(
+    upload_data_token,
+    super_admin_token,
+    public_source,
+    public_group,
+    public_group2,
+    view_only_token_group2,
+    ztf_camera,
+):
+
+    status, data = api(
+        "GET", f"sources/{public_source.id}", token=view_only_token_group2
+    )
+    # `view_only_token only` belongs to `public_group`, but not `public_group2`
+    # so the photometry is invisible to it
+    assert status == 400
+    assert data["status"] == "error"
+    assert 'permissions' in data['message'].lower()
+
+    status, data = api(
+        "POST",
+        "photometry",
+        data={
+            "obj_id": str(public_source.id),
+            "mjd": 58000.0,
+            "instrument_id": ztf_camera.id,
+            "flux": 12.24,
+            "fluxerr": 0.031,
+            "zp": 25.0,
+            "magsys": "ab",
+            "filter": "ztfg",
+            "group_ids": [public_group.id],
+        },
+        token=upload_data_token,
+    )
+    assert status == 200
+    assert data["status"] == "success"
+
+    photometry_id = data["data"]["ids"][0]
+    status, data = api(
+        "GET", f"photometry/{photometry_id}?format=flux", token=upload_data_token
+    )
+    assert status == 200
+    assert data["status"] == "success"
+
+    status, data = api(
+        "POST",
+        "sharing",
+        data={"photometryIDs": [photometry_id], "groupIDs": [public_group2.id]},
+        token=super_admin_token,
+    )
+
+    assert status == 200
+    assert data['status'] == 'success'
+
+    status, data = api(
+        "GET", f"photometry/{photometry_id}?format=flux", token=view_only_token_group2
+    )
+    # `view_only_token only` belongs to `public_group`, but not `public_group2`
+    assert status == 200
+    assert data["status"] == "success"
