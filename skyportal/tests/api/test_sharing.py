460ec142247b1269605d631b279bc871620f11f9
Danny Goldstein <dgold@berkeley.edu>
2020-11-09 17:38:35 -0800

    test sharing spectrum does not share source

--- skyportal/tests/api/test_sharing.py
+++ skyportal/tests/api/test_sharing.py
@@ -1,4 +1,5 @@
 from skyportal.tests import api
+import datetime
 
 
 def test_sharing_photometry(
@@ -187,6 +188,64 @@ def test_sharing_photometry_shares_source(
     # so the photometry is invisible to it
     assert status == 200
     assert data["status"] == "success"
+
+
+def test_sharing_spectroscopy_does_not_share_source(
+    upload_data_token,
+    public_source,
+    public_group,
+    public_group2,
+    view_only_token_group2,
+    lris,
+):
+
+    status, data = api(
+        "GET", f"sources/{public_source.id}", token=view_only_token_group2
+    )
+    # `view_only_token only` belongs to `public_group`, but not `public_group2`
+    # so the photometry is invisible to it
+    assert status == 400
+    assert data["status"] == "error"
+    assert 'permissions' in data['message'].lower()
+
+    status, data = api(
+        "POST",
+        "spectrum",
+        data={
+            'obj_id': str(public_source.id),
+            'observed_at': str(datetime.datetime.now()),
+            'wavelengths': [664, 665, 666],
+            'fluxes': [234.2, 232.1, 235.3],
+            'group_ids': [public_group.id],
+            'instrument_id': lris.id,
+        },
+        token=upload_data_token,
+    )
+    assert status == 200
+    assert data["status"] == "success"
+
+    spectrum_id = data["data"]["id"]
+    status, data = api("GET", f"spectrum/{spectrum_id}", token=upload_data_token)
+    assert status == 200
+    assert data["status"] == "success"
+
+    status, data = api(
+        "POST",
+        "sharing",
+        data={"spectrumIDs": [spectrum_id], "groupIDs": [public_group2.id]},
+        token=upload_data_token,
+    )
+
+    assert status == 200
+    assert data['status'] == 'success'
+
+    status, data = api(
+        "GET", f"sources/{public_source.id}", token=view_only_token_group2
+    )
+    # `view_only_token only` belongs to `public_group`, but not `public_group2`
+    # so the photometry is invisible to it
+    assert status == 400
+    assert data["status"] == "error"
 
 
 def test_cannot_share_unowned_photometry(
