07d37f5095ab6fc9f69c16d0cbe9fc12c150e101
Danny Goldstein <dgold@berkeley.edu>
2020-11-19 15:22:00 -0800

    ok -> valid

--- skyportal/handlers/api/sharing.py
+++ skyportal/handlers/api/sharing.py
@@ -56,24 +56,24 @@ class SharingHandler(BaseHandler):
 
         valid_groups = Group.query.filter(Group.id.in_(group_ids))
         valid_group_ids = [g.id for g in valid_groups]
-        invalid_group_ids = [gid for gid in group_ids if gid not in ok_group_ids]
+        invalid_group_ids = [gid for gid in group_ids if gid not in valid_group_ids]
 
         if len(invalid_group_ids) > 0:
             return self.error(f'Invalid group IDs: {invalid_group_ids}.')
-        groups = ok_groups
+        groups = valid_groups
 
         phot_obj_ids = []
         spec_obj_ids = []
 
         if phot_ids:
-            ok_phot = Photometry.query.filter(Photometry.id.in_(phot_ids))
-            ok_phot_ids = [op.id for op in ok_phot]
-            invalid_phot_ids = [pid for pid in phot_ids if pid not in ok_phot_ids]
+            valid_phot = Photometry.query.filter(Photometry.id.in_(phot_ids))
+            valid_phot_ids = [op.id for op in valid_phot]
+            invalid_phot_ids = [pid for pid in phot_ids if pid not in valid_phot_ids]
 
             if len(invalid_phot_ids) > 0:
                 return self.error(f'Invalid photometry IDs: {invalid_phot_ids}.')
 
-            for phot in ok_phot:
+            for phot in valid_phot:
                 # Ensure user has access to data being shared
                 if (
                     phot.owner_id != self.associated_user_object.id
@@ -88,14 +88,14 @@ class SharingHandler(BaseHandler):
                 phot_obj_ids.append(phot.obj_id)
 
         if spec_ids:
-            ok_spec = Spectrum.query.filter(Spectrum.id.in_(spec_ids))
-            ok_spec_ids = [os.id for os in ok_spec]
-            invalid_spec_ids = [sid for sid in spec_ids if sid not in ok_spec_ids]
+            valid_spec = Spectrum.query.filter(Spectrum.id.in_(spec_ids))
+            valid_spec_ids = [os.id for os in valid_spec]
+            invalid_spec_ids = [sid for sid in spec_ids if sid not in valid_spec_ids]
 
             if len(invalid_spec_ids) > 0:
                 return self.error(f'Invalid spectrum IDs: {invalid_spec_ids}.')
 
-            for spec in ok_spec:
+            for spec in valid_spec:
                 # Ensure user has access to data being shared
                 if (
                     spec.owner_id != self.associated_user_object.id
