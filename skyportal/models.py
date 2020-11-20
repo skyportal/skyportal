fix vectorization of ephemeris calculation

skyportal/models.py
@@ -2574,8 +2574,10 @@ class ObservingRun(Base):
     def rise_time(self, target_or_targets, altitude=30 * u.degree):
         """The rise time of the specified targets as an astropy.time.Time."""
         observer = self.instrument.telescope.observer
-        sunset = self.instrument.telescope.next_sunset(self.calendar_noon)
-        sunrise = self.instrument.telescope.next_sunrise(self.calendar_noon)
+        sunset = self.instrument.telescope.next_sunset(self.calendar_noon).reshape((1,))
+        sunrise = self.instrument.telescope.next_sunrise(self.calendar_noon).reshape(
+            (1,)
+        )
         original_shape = np.asarray(target_or_targets).shape
         target_array = (
             [target_or_targets] if len(original_shape) == 0 else target_or_targets
@@ -2583,7 +2585,7 @@ class ObservingRun(Base):
 
         next_rise = observer.target_rise_time(
             sunset, target_array, which='next', horizon=altitude
-        )
+        ).reshape((len(target_array),))
 
         # if next rise time is after next sunrise, the target rises before
         # sunset. show the previous rise so that the target is shown to be
@@ -2591,9 +2593,10 @@ class ObservingRun(Base):
 
         recalc = next_rise > sunrise
         if recalc.any():
+            target_subarr = [t for t, b in zip(target_array, recalc) if b]
             next_rise[recalc] = observer.target_rise_time(
-                sunset, target_or_targets[recalc], which='previous', horizon=altitude
-            )
+                sunset, target_subarr, which='previous', horizon=altitude
+            ).reshape((len(target_subarr),))
 
         return next_rise.reshape(original_shape)
 