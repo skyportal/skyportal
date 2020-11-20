f8951c99124a416d82d542a6eb799ae5772698bc
Danny Goldstein <dgold@berkeley.edu>
2020-11-19 16:07:37 -0800

    Simplify data sharing handler  (#1323)
    * begin to remove candidate components
    
    * remove references to candidate.js and candidate page test
    
    * change candidate page to source page
    
    * refactor get_if_owned_by logic in SourceHandler to get_if_visible_to
    
    * alter spectrum permissions to use Obj.get_if_visible_to
    
    * refactor existing get_obj_if_owned_by
    
    * remove spectrum.py-e
    
    * remove source.py-e
    
    * move construct joinedload to within get_obj_if_owned_by
    
    * update source sharing handler
    
    * fix test_sharing_photometry_shares_source
    
    * fix get_obj_if_owned_by()
    
    * let system admins share any data
    
    * test that super admin can share any data
    
    * test sharing spectrum does not share source
    
    * remove test_obj_visibility_source
    
    * Update skyportal/handlers/api/sharing.py
    
    Co-authored-by: Ari Crellin-Quick <a.crellinquick@gmail.com>
    
    * ok -> valid
    
    Co-authored-by: Ari Crellin-Quick <a.crellinquick@gmail.com>

--- skyportal/tests/conftest.py
+++ skyportal/tests/conftest.py
@@ -378,6 +378,13 @@ def user_group2(public_group2):
 
 
 @pytest.fixture()
+def user2(public_group):
+    return UserFactory(
+        groups=[public_group], roles=[models.Role.query.get("Full user")]
+    )
+
+
+@pytest.fixture()
 def user_no_groups():
     return UserFactory(roles=[models.Role.query.get("Full user")])
 
@@ -391,6 +398,13 @@ def user_two_groups(public_group, public_group2):
 
 @pytest.fixture()
 def view_only_user(public_group):
+    return UserFactory(
+        groups=[public_group], roles=[models.Role.query.get("View only")]
+    )
+
+
+@pytest.fixture()
+def view_only_user2(public_group):
     return UserFactory(
         groups=[public_group], roles=[models.Role.query.get("View only")]
     )
@@ -429,6 +443,12 @@ def super_admin_user_two_groups(public_group, public_group2):
 @pytest.fixture()
 def view_only_token(user):
     token_id = create_token(ACLs=[], user_id=user.id, name=str(uuid.uuid4()))
+    return token_id
+
+
+@pytest.fixture()
+def view_only_token2(user2):
+    token_id = create_token(ACLs=[], user_id=user2.id, name=str(uuid.uuid4()))
     return token_id
 
 
