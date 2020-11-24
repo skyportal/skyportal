4f79dcf8bfb3a03709539108088f4073182b3816
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-11-24 15:23:16 -0800

    Update assignment form front-end tests

--- skyportal/tests/frontend/test_observing_planner.py
+++ skyportal/tests/frontend/test_observing_planner.py
@@ -28,13 +28,7 @@ def test_source_is_added_to_observing_run_via_frontend(
 ):
     driver.get(f"/become_user/{super_admin_user.id}")
     driver.get(f"/source/{public_source.id}")
-
-    # wait for plots to load
-    driver.wait_for_xpath('//div[@class="bk-root"]//span[text()="Flux"]', timeout=20)
-    # this waits for the spectroscopy plot by looking for the element Mg
-    driver.wait_for_xpath('//div[@class="bk-root"]//label[text()="Mg"]', timeout=20)
-
-    run_select = driver.wait_for_xpath('//*[@id="mui-component-select-run_id"]')
+    run_select = driver.wait_for_xpath('//*[@data-testid="assignmentSelect"]')
     driver.scroll_to_element_and_click(run_select)
     observingrun_title = (
         f"{red_transients_run.calendar_date} "
@@ -48,11 +42,11 @@ def test_source_is_added_to_observing_run_via_frontend(
         driver.wait_for_xpath(f'//li[@data-value="{red_transients_run.id}"]')
     )
 
-    comment_box = driver.wait_for_xpath("//textarea[@name='comment']")
+    comment_box = driver.wait_for_xpath("//*[@data-testid='assignmentCommentInput']")
     comment_text = str(uuid.uuid4())
     comment_box.send_keys(comment_text)
 
-    submit_button = driver.wait_for_xpath('//*[@name="assignmentSubmitButton"]')
+    submit_button = driver.wait_for_xpath('//*[@data-testid="assignmentSubmitButton"]')
 
     driver.scroll_to_element_and_click(submit_button)
     driver.get(f"/run/{red_transients_run.id}")
