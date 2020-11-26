203c805a7a4b41445c1899f84530b136baada24c
Danny Goldstein <dgold@berkeley.edu>
2020-11-25 18:54:32 -0800

    update usercontact card

--- static/js/components/UserProfileInfo.jsx
+++ static/js/components/UserProfileInfo.jsx
@@ -5,57 +5,34 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
 import PropTypes from "prop-types";
-
-import md5 from "md5";
-
-import { makeStyles } from "@material-ui/core/styles";
 
 import UserAvatar from "./UserAvatar";
 
-const useStyles = makeStyles({
-  entryContent: {
-    margin: "0.25rem",
-  },
-  card: {
-    display: "inline-block",
-  },
-});
+export const UserContactInfo = ({ user }) => {
+  let contact_string = "";
+  if (user.first_name || user.last_name) {
+    contact_string += `${user.first_name} ${user.last_name}`;
+  } else {
+    contact_string += `${user.username}`;
+  }
 
-function gravatar_url(user) {
-  const email = user.contact_email ? user.contact_email : user.username;
-  const hash = md5(email);
-  return `https://secure.gravatar.com/avatar/${hash}?d=blank`;
-}
+  const contact = [];
+  if (user.contact_email) {
+    contact.push(user.contact_email);
+  }
 
-export const UserContactCard = ({ user }) => {
-  const styles = useStyles();
-  return (
-    <Paper elevation={1} className={styles.card}>
-      <div className={styles.entryContent}>
-        <Grid container spacing={1} alignItems="center" justify="center">
-          <Grid item xs={3}>
-            <UserAvatar
-              size={32}
-              firstName={user.first_name}
-              lastName={user.last_name}
-              username={user.username}
-              gravatarUrl={gravatar_url(user)}
-            />
-          </Grid>
-          <Grid item xs={9}>
-            {user.username} ({user.first_name} {user.last_name};
-            {user.contact_email}; {user.contact_phone})
-          </Grid>
-        </Grid>
-      </div>
-    </Paper>
-  );
+  if (user.contact_phone) {
+    contact.push(user.contact_phone);
+  }
+
+  if (contact.length > 0) {
+    contact_string += `(${contact.join(";")})`;
+  }
+  return <p>{contact_string}</p>;
 };
 
-UserContactCard.propTypes = {
+UserContactInfo.propTypes = {
   user: PropTypes.shape({
     first_name: PropTypes.string,
     last_name: PropTypes.string,
