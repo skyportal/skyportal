b74ea16a85be77646e92cbc5fe7bc806771363d7
guynir42 <guyynir@gmail.com>
2020-12-02 09:27:58 +0200

    fixed some comments by reviewer

--- skyportal/handlers/api/listing.py
+++ skyportal/handlers/api/listing.py
@@ -10,26 +10,26 @@ from ...models import (
 )
 
 
-class ListingHandler(BaseHandler):
+class UserOBjListHandler(BaseHandler):
     @auth_or_token
     def get(self):
         """
         ---
             description: get all objects corresponding to a specific user and that match a list name.
-            requestBody:
-              content:
-                application/json:
-                  schema:
-                    type: object
-                    properties:
-                      user_id:
-                        type: string
-                      list_name:
-                         type: string
-                         description: |
-                            find all objects saved to this list.
-                            If empty, will return an array of strings
-                            specifying all non-empty lists associated with this user.
+            - in: query
+              name: user_id
+              required: true
+              type: string
+            - in: query
+              name: list_name
+              required: false
+              type: string
+
+            description: |
+                find all objects saved to this list.
+                If not given will return all objects
+                saved by the user to all lists.
+
             responses:
               200:
                 content:
@@ -38,99 +38,78 @@ class ListingHandler(BaseHandler):
                       - $ref: '#/components/schemas/Success'
                       - type: array
                         items:
-                          description: array of object ids or array of list names.
+                          description: array of object ids.
         """
-        data = self.get_json()
 
-        schema = Listing.__schema__(exclude=["obj_id"])
-        try:
-            schema.load(data)
-        except ValidationError as e:
-            return self.error(f'Invalid/missing parameters: {e.normalized_messages()}')
+        user_id = self.get_query_argument("user_id")
 
-        # should we make user_id optional, defaulting to self.associated_user_object?
-        user_id = data.get("user_id")
         if User.query.get(user_id) is None:  # verify that user exists
             return self.error(f'User "{user_id}" does not exist!')
 
         # verify that poster has read access to user_id's lists?
 
-        list_name = data.get("list_name")
+        list_name = self.get_query_argument("list_name", None)
 
-        if list_name is None:
-            listings = (
-                DBSession().query(Listing).filter(Listing.user_id == user_id)
-            ).all()
-
-            list_names = [L.list_name for L in listings]
-            list_names = list(set(list_names))  # show only unique list names
-
-            return self.success(data=list_names)
-
-        else:
-            listings = (
-                DBSession()
-                .query(Listing)
-                .filter(Listing.user_id == user_id)
-                .filter(Listing.list_name == list_name)
-            ).all()
-
-            object_ids = [L.obj_id for L in listings]
-
-            return self.success(data=object_ids)
+        objects = DBSession().query(Listing).filter(Listing.user_id == user_id)
+        if list_name is not None:
+            objects = objects.filter(Listing.list_name == list_name)
+        return self.success(data=objects.all())
 
     @auth_or_token
     def put(self):
         """
         ---
         description: Add a listing, if it doesn't exist yet
-        requestBody:
-          content:
-            application/json:
-              schema:
-                type: object
-                properties:
-                  user_id:
-                    type: string
-                  obj_id:
-                    type: string
-                  list_name:
-                     type: string
-                     description: |
-                        Listing name for this item, e.g., "favorites".
-                        Multiple objects can be saved by the same user to different
-                        lists, where the list names are user-defined.
-                        List name must be a non-empty string starting with an
-                        alphanumeric character or underscore.
-                        (it must match the regex: /^\\w+/)
+        - in: query
+            name: user_id
+            required: true
+            type: string
+        - in: query
+            name: obj_id
+            required: true
+            type: string
+        - in: query
+            name: list_name
+            required: true
+            type: string
+        description: |
+            Listing name for this item, e.g., "favorites".
+            Multiple objects can be saved by the same user to different
+            lists, where the list names are user-defined.
+            List name must be a non-empty string starting with an
+            alphanumeric character or underscore.
+            (it must match the regex: /^\\w+/)
 
+        responses:
+          200:
+            content:
+              application/json:
+                schema:
+                  allOf:
+                    - $ref: '#/components/schemas/Success'
+                    - type: object
+                      properties:
+                        data:
+                          type: object
+                          properties:
+                            id:
+                              type: integer
+                              description: New listing ID
 
-                required:
-                  - user_id
-                  - obj_id
-                  - list_name
-
-                """
-        data = self.get_json()
-
-        schema = Listing.__schema__()
-        try:
-            schema.load(data)
-        except ValidationError as e:
-            return self.error(f'Invalid/missing parameters: {e.normalized_messages()}')
+        """
 
         # should we make user_id optional, defaulting to self.associated_user_object?
-        user_id = data.get("user_id")
+        user_id = self.get_query_argument("user_id")
         if User.query.get(user_id) is None:  # verify that user exists
             return self.error(f'User "{user_id}" does not exist!')
 
         # verify that poster has write access to user_id's lists?
 
-        obj_id = data.get("obj_id")
+        obj_id = self.get_query_argument("obj_id")
         if Obj.query.get(obj_id) is None:  # verify that object exists!
             return self.error(f'Object "{obj_id}" does not exist!')
 
-        list_name = data.get("list_name")
+        list_name = self.get_query_argument("list_name")
         if not re.search(r'^\w+', list_name):
             return self.error(
                 "Input `list_name` must begin with alphanumeric/underscore"
@@ -141,34 +120,27 @@ class ListingHandler(BaseHandler):
         DBSession.add(listing)
         DBSession.commit()
 
-        return self.success(data={})  # should we return something?
+        return self.success(data={listing.id})
 
     @auth_or_token
     def delete(self):
         """
-                ---
-                description: Remove an existing listing
-                requestBody:
-                  content:
-                    application/json:
-                      schema:
-                        type: object
-                        properties:
-                          user_id:
-                            type: string
-                          obj_id:
-                            type: string
-                          list_name:
-                             type: string
-                             description: |
-                                Listing name for this item, e.g., "favorites".
+        ---
+        description: Remove an existing listing
+        - in: query
+            name: user_id
+            required: true
+            type: string
+        - in: query
+            name: obj_id
+            required: true
+            type: string
+        - in: query
+            name: list_name
+            required: true
+            type: string
 
-                        required:
-                          - user_id
-                          - obj_id
-                          - list_name
-
-                        """
+        """
         data = self.get_json()
 
         schema = Listing.__schema__()
@@ -177,7 +149,6 @@ class ListingHandler(BaseHandler):
         except ValidationError as e:
             return self.error(f'Invalid/missing parameters: {e.normalized_messages()}')
 
-        # should we make user_id optional, defaulting to self.associated_user_object?
         user_id = data.get("user_id")
         if User.query.get(user_id) is None:  # verify that user exists
             return self.error(f'User "{user_id}" does not exist!')
