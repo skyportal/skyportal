850dececf8d9f52719be8851613325ffa6b54cd0
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-12-03 15:41:33 -0800

    Never include attachment bytes; add flag to include comments

--- skyportal/handlers/api/source.py
+++ skyportal/handlers/api/source.py
@@ -289,12 +289,12 @@ class SourceHandler(BaseHandler):
             description: |
               The sort order - either "asc" or "desc". Defaults to "asc"
           - in: query
-            name: includeCommentAttachmentData
+            name: includeComments
             nullable: true
             schema:
               type: boolean
             description: |
-              Boolean indicating whether to include comment attachment data in response.
+              Boolean indicating whether to include comment metadata in response.
               Defaults to false.
           responses:
             200:
@@ -341,9 +341,7 @@ class SourceHandler(BaseHandler):
         save_summary = self.get_query_argument('saveSummary', False)
         sort_by = self.get_query_argument("sortBy", None)
         sort_order = self.get_query_argument("sortOrder", "asc")
-        include_attachment_data = self.get_query_argument(
-            "includeCommentAttachmentData", False
-        )
+        include_comments = self.get_query_argument("includeComments", False)
 
         # These are just throwaway helper classes to help with deserialization
         class UTCTZnaiveDateTime(fields.DateTime):
@@ -445,20 +443,21 @@ class SourceHandler(BaseHandler):
                 IOLoop.current().add_callback(
                     lambda: add_ps1_thumbnail_and_push_ws_msg(s, self)
                 )
-            comments = s.get_comments_readable_by(self.current_user)
             source_info = s.to_dict()
-            source_info["comments"] = sorted(
-                [
-                    {
-                        k: v
-                        for k, v in c.to_dict().items()
-                        if not (k == "attachment_bytes" and not include_attachment_data)
-                    }
-                    for c in comments
-                ],
-                key=lambda x: x["created_at"],
-                reverse=True,
-            )
+            if include_comments:
+                comments = s.get_comments_readable_by(self.current_user)
+                source_info["comments"] = sorted(
+                    [
+                        {
+                            k: v
+                            for k, v in c.to_dict().items()
+                            if k != "attachment_bytes"
+                        }
+                        for c in comments
+                    ],
+                    key=lambda x: x["created_at"],
+                    reverse=True,
+                )
             source_info["annotations"] = sorted(
                 s.get_annotations_readable_by(self.current_user),
                 key=lambda x: x.origin,
@@ -659,20 +658,19 @@ class SourceHandler(BaseHandler):
             source_list = []
             for source in query_results["sources"]:
                 source_list.append(source.to_dict())
-                source_list[-1]["comments"] = sorted(
-                    [
-                        {
-                            k: v
-                            for k, v in c.to_dict().items()
-                            if not (
-                                k == "attachment_bytes" and not include_attachment_data
-                            )
-                        }
-                        for c in source.get_comments_readable_by(self.current_user)
-                    ],
-                    key=lambda x: x["created_at"],
-                    reverse=True,
-                )
+                if include_comments:
+                    source_list[-1]["comments"] = sorted(
+                        [
+                            {
+                                k: v
+                                for k, v in c.to_dict().items()
+                                if k != "attachment_bytes"
+                            }
+                            for c in source.get_comments_readable_by(self.current_user)
+                        ],
+                        key=lambda x: x["created_at"],
+                        reverse=True,
+                    )
                 source_list[-1][
                     "classifications"
                 ] = source.get_classifications_readable_by(self.current_user)
