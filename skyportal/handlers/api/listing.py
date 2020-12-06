1aeee222fe20e83b9855bd57627d906c52c5a475
guynir42 <guyynir@gmail.com>
2020-12-06 17:49:20 +0200

    minor bug in api parser

--- skyportal/handlers/api/listing.py
+++ skyportal/handlers/api/listing.py
@@ -158,7 +158,7 @@ class UserObjListHandler(BaseHandler):
           200:
             content:
               application/json:
-                schema: success
+                schema: Success
 
 
         """
