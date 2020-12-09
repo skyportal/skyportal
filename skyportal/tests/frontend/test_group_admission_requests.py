4ab5c52153a16ecbaf7d9fb70fabe1e3fb6fd187
Ari Crellin-Quick <a.crellinquick@gmail.com>
2020-12-09 10:15:44 -0800

    Filter datatable for desired row before attempting to click

--- skyportal/tests/frontend/test_group_admission_requests.py
+++ skyportal/tests/frontend/test_group_admission_requests.py
@@ -1,18 +1,32 @@
 # Comment to avert black & flake8 disagreement
 
 
+def filter_for_value(driver, value, last=False):
+    if last:
+        xpath = '(//*[@data-testid="Search-iconButton"])[last()]'
+    else:
+        xpath = '//*[@data-testid="Search-iconButton"]'
+    driver.click_xpath(xpath)
+    search_input_xpath = "//input[@aria-label='Search']"
+    search_input = driver.wait_for_xpath(search_input_xpath)
+    driver.click_xpath(search_input_xpath)
+    search_input.send_keys(value)
+
+
 def test_group_admission_request_and_acceptance(
     driver, user, super_admin_user_two_groups, public_group, public_group2
 ):
     driver.get(f'/become_user/{user.id}')
     driver.get('/groups')
     driver.wait_for_xpath('//h6[text()="My Groups"]')
+    filter_for_value(driver, public_group2.name)
     driver.click_xpath(f'//*[@data-testid="requestAdmissionButton{public_group2.id}"]')
     driver.wait_for_xpath(
         f'//*[@data-testid="deleteAdmissionRequestButton{public_group2.id}"]'
     )
     driver.get(f"/become_user/{super_admin_user_two_groups.id}")
     driver.get(f"/group/{public_group2.id}")
+    filter_for_value(driver, user.username, last=True)
     driver.wait_for_xpath('//div[text()="pending"]')
     driver.click_xpath(f'//*[@data-testid="acceptRequestButton{user.id}"]')
     driver.wait_for_xpath('//div[text()="accepted"]')
