22b44db1d6a6d26895e78be984996c1c4728cf42
guynir42 <guyynir@gmail.com>
2020-12-10 10:49:09 +0200

    fixed input arguments, added PATCH and POST, added tests for everything

--- skyportal/handlers/api/listing.py
+++ skyportal/handlers/api/listing.py
@@ -1,4 +1,5 @@
 import re
+from marshmallow.exceptions import ValidationError
 
 from baselayer.app.access import auth_or_token
 from ..base import BaseHandler
@@ -12,12 +13,12 @@ from ...models import (
 
 class UserObjListHandler(BaseHandler):
     @auth_or_token
-    def get(self):
+    def get(self, user_id):
         """
         ---
-            description: get all objects corresponding to a specific user and that match a list name.
+            description: get all listings corresponding to a specific user and that match a list name.
             parameters:
-            - in: query
+            - in: path
               name: user_id
               required: true
               type: string
@@ -36,8 +37,6 @@ class UserObjListHandler(BaseHandler):
                 content:
                   schema: ArrayOFListings
         """
-
-        user_id = self.get_query_argument("user_id")
 
         if User.query.get(user_id) is None:  # verify that user exists
             return self.error(f'User "{user_id}" does not exist!')
@@ -62,31 +61,100 @@ class UserObjListHandler(BaseHandler):
 
         return self.success(data=objects.all())
 
+    def add_listing(self, error_if_exists):
+        """
+        Add the listing given in the request body.
+        Two different behaviors when error_if_exists is true/false:
+        - true: will throw an error if listing exists (POST)
+        - false: will ignore if listing already exists (PUT)
+        """
+
+        data = self.get_json()
+
+        schema = Listing.__schema__()
+        try:
+            schema.load(data)
+        except ValidationError as e:
+            return self.error(f'Invalid/missing parameters: {e.normalized_messages()}')
+
+        user_id = data.get('user_id')
+
+        if User.query.get(user_id) is None:  # verify that user exists
+            return self.error(f'User "{user_id}" does not exist!')
+
+        user_id = int(user_id)
+
+        # verify that poster has write access to user_id's lists
+        if (
+            not self.associated_user_object.id == user_id
+            and "System admin" not in self.associated_user_object.permissions
+        ):
+            return self.error('Insufficient permissions to access this listing. ')
+
+        obj_id = data.get('obj_id')
+        if Obj.query.get(obj_id) is None:  # verify that object exists!
+            return self.error(f'Object "{obj_id}" does not exist!')
+
+        list_name = data.get('list_name')
+        if not re.search(r'^\w+', list_name):
+            return self.error(
+                "Input `list_name` must begin with alphanumeric/underscore"
+            )
+
+        objects = (
+            DBSession()
+            .query(Listing)
+            .filter(
+                Listing.user_id == user_id,
+                Listing.obj_id == obj_id,
+                Listing.list_name == list_name,
+            )
+        )
+
+        # what to do if listing already exists...
+        if objects.first() is not None:
+            if error_if_exists:
+                return self.error(
+                    f'Listing already exists with user_id={user_id}, obj_id={obj_id} and list_name={list_name}'
+                )
+            else:
+                return self.success(data={'listing_id': objects.first().id})
+
+        # no such listing, can just add a new one!
+        listing = Listing(user_id=user_id, obj_id=obj_id, list_name=list_name)
+
+        DBSession().add(listing)
+        DBSession().commit()
+
+        return self.success(data={'listing_id': listing.id})
+
     @auth_or_token
-    def put(self):
+    def post(self):
         """
         ---
-        description: Add a listing, if it doesn't exist yet
-        parameters:
-        - in: query
-          name: user_id
-          required: true
-          type: string
-        - in: query
-          name: obj_id
-          required: true
-          type: string
-        - in: query
-          name: list_name
-          required: true
-          type: string
-        description: |
-            Listing name for this item, e.g., "favorites".
-            Multiple objects can be saved by the same user to different
-            lists, where the list names are user-defined.
-            List name must be a non-empty string starting with an
-            alphanumeric character or underscore.
-            (it must match the regex: /^\\w+/)
+        description: Add a listing. If it exists, return an error
+        requestBody:
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  user_ud:
+                    type: integer
+                    required: true
+                  obj_id:
+                    type: string
+                    required: true
+                  list_name:
+                    type: string
+                    required: true
+                    description: |
+                        Listing name for this item, e.g., "favorites".
+                        Multiple objects can be saved by the same user to different
+                        lists, where the list names are user-defined.
+                        List name must be a non-empty string starting with an
+                        alphanumeric character or underscore.
+                        (it must match the regex: /^\\w+/)
 
         responses:
           200:
@@ -100,41 +168,158 @@ class UserObjListHandler(BaseHandler):
                         data:
                           type: object
                           properties:
-                            id:
+                            listing_id:
                               type: integer
                               description: New listing ID
 
         """
 
-        user_id = self.get_query_argument("user_id")
+        return self.add_listing(True)
+
+    @auth_or_token
+    def put(self):
+        """
+        ---
+        description: Add a listing, if it doesn't exist yet
+        requestBody:
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  user_ud:
+                    type: integer
+                    required: true
+                  obj_id:
+                    type: string
+                    required: true
+                  list_name:
+                    type: string
+                    required: true
+                    description: |
+                        Listing name for this item, e.g., "favorites".
+                        Multiple objects can be saved by the same user to different
+                        lists, where the list names are user-defined.
+                        List name must be a non-empty string starting with an
+                        alphanumeric character or underscore.
+                        (it must match the regex: /^\\w+/)
+
+        responses:
+          200:
+            content:
+              application/json:
+                schema:
+                  allOf:
+                    - $ref: '#/components/schemas/Success'
+                    - type: object
+                      properties:
+                        data:
+                          type: object
+                          properties:
+                            listing_id:
+                              type: integer
+                              description: New listing ID
+
+        """
+
+        return self.add_listing(False)
+
+    @auth_or_token
+    def patch(self, listing_id):
+        """
+        ---
+        description: Update a listing with new parameters
+        parameters:
+        - in: path
+          name: listing_id
+          required: true
+          schema:
+            type: integer
+        requestBody:
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  user_ud:
+                    type: integer
+                    required: true
+                  obj_id:
+                    type: string
+                    required: true
+                  list_name:
+                    type: string
+                    required: true
+                    description: |
+                        Listing name for this item, e.g., "favorites".
+                        Multiple objects can be saved by the same user to different
+                        lists, where the list names are user-defined.
+                        List name must be a non-empty string starting with an
+                        alphanumeric character or underscore.
+                        (it must match the regex: /^\\w+/)
+
+        responses:
+          200:
+            content:
+              application/json:
+                schema: Success
+
+        """
+
+        listing = Listing.query.get(int(listing_id))
+
+        if listing is None:
+            return self.error("Listing does not exist.")
+
+        # verify that poster has write access to user_id's lists
+        if (
+            not self.associated_user_object.id == listing.user_id
+            and "System admin" not in self.associated_user_object.permissions
+        ):
+            return self.error('Insufficient permissions to access this listing. ')
+
+        # get the data from the request body
+        data = self.get_json()
+
+        schema = Listing.__schema__()
+        try:
+            schema.load(data)
+        except ValidationError as e:
+            return self.error(f'Invalid/missing parameters: {e.normalized_messages()}')
+
+        user_id = data.get('user_id')
+
         if User.query.get(user_id) is None:  # verify that user exists
             return self.error(f'User "{user_id}" does not exist!')
 
         user_id = int(user_id)
 
-        # verify that poster has write access to user_id's lists
+        # verify that poster has write access to the new user_id
         if (
             not self.associated_user_object.id == user_id
             and "System admin" not in self.associated_user_object.permissions
         ):
-            return self.error('Insufficient permissions to access this listing. ')
+            return self.error(
+                f'Insufficient permissions to change listings for user ID: {user_id}. '
+            )
 
-        obj_id = self.get_query_argument("obj_id")
+        obj_id = data.get('obj_id')
         if Obj.query.get(obj_id) is None:  # verify that object exists!
             return self.error(f'Object "{obj_id}" does not exist!')
 
-        list_name = self.get_query_argument("list_name")
+        list_name = data.get('list_name')
         if not re.search(r'^\w+', list_name):
             return self.error(
                 "Input `list_name` must begin with alphanumeric/underscore"
             )
 
-        listing = Listing(user_id=user_id, obj_id=obj_id, list_name=list_name,)
+        listing.user_id = user_id
+        listing.obj_id = obj_id
+        listing.list_name = list_name
 
-        DBSession().add(listing)
         DBSession().commit()
 
-        return self.success(data={'listing_id': listing.id})
+        return self.success()
 
     @auth_or_token
     def delete(self, listing_id):
--- skyportal/tests/api/test_listings.py
+++ skyportal/tests/api/test_listings.py
@@ -3,9 +3,7 @@ from skyportal.tests import api
 from skyportal.model_util import create_token
 
 
-def test_add_objects_to_list(
-    user, upload_data_token, public_candidate, public_candidate2
-):
+def test_add_objects_to_list(user, public_candidate, public_candidate2):
 
     token_id = create_token(
         ACLs=["Upload data"], user_id=user.id, name=str(uuid.uuid4())
@@ -13,7 +11,12 @@ def test_add_objects_to_list(
 
     status, data = api(
         'PUT',
-        f'listing?user_id={user.id}&obj_id={public_candidate.id}&list_name=favorites',
+        'listing',
+        data={
+            'user_id': user.id,
+            'obj_id': public_candidate.id,
+            'list_name': 'favorites',
+        },
         token=token_id,
     )
 
@@ -22,7 +25,12 @@ def test_add_objects_to_list(
 
     status, data = api(
         'PUT',
-        f'listing?user_id={user.id}&obj_id={public_candidate2.id}&list_name=favorites',
+        'listing',
+        data={
+            'user_id': user.id,
+            'obj_id': public_candidate2.id,
+            'list_name': 'favorites',
+        },
         token=token_id,
     )
 
@@ -30,21 +38,81 @@ def test_add_objects_to_list(
     item2 = data["data"]["listing_id"]  # get the list item ID
 
     # get the list back, should include only two items
-    status, data = api(
-        'GET', f'listing?user_id={user.id}&list_name=favorites', token=token_id
-    )
+    status, data = api('GET', f'listing/{user.id}?list_name=favorites', token=token_id)
 
     assert status == 200
     new_list = data["data"]
 
     items = [item["id"] for item in new_list]
 
-    assert set(items) == set([item1, item2])
+    assert set(items) == {item1, item2}
+
+    # try to put a listing to a non-existing object
+    fake_obj_id = str(uuid.uuid4())
+
+    status, data = api(
+        'PUT',
+        'listing',
+        data={'user_id': user.id, 'obj_id': fake_obj_id, 'list_name': 'favorites'},
+        token=token_id,
+    )
+
+    assert status == 400
+    assert 'does not exist' in data["message"]
 
 
-def test_add_remove_objects(
-    user, upload_data_token, public_candidate, public_candidate2
-):
+def test_double_posting(user, public_candidate):
+    token_id = create_token(
+        ACLs=["Upload data"], user_id=user.id, name=str(uuid.uuid4())
+    )
+
+    status, data = api(
+        'POST',
+        'listing',
+        data={
+            'user_id': user.id,
+            'obj_id': public_candidate.id,
+            'list_name': 'favorites',
+        },
+        token=token_id,
+    )
+
+    assert status == 200
+    listing_id1 = data['data']['listing_id']
+
+    # try posting the same listing again!
+    status, data = api(
+        'POST',
+        'listing',
+        data={
+            'user_id': user.id,
+            'obj_id': public_candidate.id,
+            'list_name': 'favorites',
+        },
+        token=token_id,
+    )
+
+    assert status == 400
+
+    # doing a PUT instead of POST should be fine (simply ignored)
+    status, data = api(
+        'PUT',
+        'listing',
+        data={
+            'user_id': user.id,
+            'obj_id': public_candidate.id,
+            'list_name': 'favorites',
+        },
+        token=token_id,
+    )
+
+    assert status == 200
+    listing_id2 = data['data']['listing_id']
+
+    assert listing_id1 == listing_id2  # check the PUT returns the same identifier
+
+
+def test_add_remove_objects(user, public_candidate, public_candidate2):
 
     token_id = create_token(
         ACLs=["Upload data"], user_id=user.id, name=str(uuid.uuid4())
@@ -52,7 +120,12 @@ def test_add_remove_objects(
 
     status, data = api(
         'PUT',
-        f'listing?user_id={user.id}&obj_id={public_candidate.id}&list_name=favorites',
+        'listing',
+        data={
+            'user_id': user.id,
+            'obj_id': public_candidate.id,
+            'list_name': 'favorites',
+        },
         token=token_id,
     )
     assert status == 200
@@ -60,7 +133,12 @@ def test_add_remove_objects(
 
     status, data = api(
         'PUT',
-        f'listing?user_id={user.id}&obj_id={public_candidate2.id}&list_name=favorites',
+        'listing',
+        data={
+            'user_id': user.id,
+            'obj_id': public_candidate2.id,
+            'list_name': 'favorites',
+        },
         token=token_id,
     )
 
@@ -71,54 +149,174 @@ def test_add_remove_objects(
 
     assert status == 200
 
-    # get the list back, should include only two items
-    status, data = api(
-        'GET', f'listing?user_id={user.id}&list_name=favorites', token=token_id
-    )
+    # get the list back, should include only one item
+    status, data = api('GET', f'listing/{user.id}?list_name=favorites', token=token_id)
 
     assert status == 200
     new_list = data["data"]
 
     items = [item["id"] for item in new_list]
 
-    assert set(items) == set([item2])
+    assert set(items) == {item2}
 
 
-def test_add_objects_to_different_lists(
-    user, upload_data_token, public_candidate, public_candidate2
-):
+def test_add_objects_to_different_lists(user, public_candidate, public_candidate2):
 
     token_id = create_token(
         ACLs=["Upload data"], user_id=user.id, name=str(uuid.uuid4())
     )
 
-    list1 = uuid.uuid4()
+    list1 = str(uuid.uuid4())
+
     status, data = api(
         'PUT',
-        f'listing?user_id={user.id}&obj_id={public_candidate.id}&list_name={list1}',
+        'listing',
+        data={'user_id': user.id, 'obj_id': public_candidate.id, 'list_name': list1},
         token=token_id,
     )
 
     assert status == 200
     item1 = data["data"]["listing_id"]  # get the list item ID
 
-    list2 = uuid.uuid4()
+    list2 = str(uuid.uuid4())
     status, data = api(
         'PUT',
-        f'listing?user_id={user.id}&obj_id={public_candidate2.id}&list_name={list2}',
+        'listing',
+        data={'user_id': user.id, 'obj_id': public_candidate2.id, 'list_name': list2},
         token=token_id,
     )
 
     assert status == 200
 
-    # get the list back, should include only two items
-    status, data = api(
-        'GET', f'listing?user_id={user.id}&list_name={list1}', token=token_id
-    )
+    # get the list back, should include only one item that matches list1
+    status, data = api('GET', f'listing/{user.id}?list_name={list1}', token=token_id)
 
     assert status == 200
     new_list = data["data"]
 
     items = [item["id"] for item in new_list]
 
-    assert set(items) == set([item1])
+    assert set(items) == {item1}
+
+
+def test_patching_listing(user, user2, public_candidate, public_candidate2):
+    token_id = create_token(
+        ACLs=["Upload data"], user_id=user.id, name=str(uuid.uuid4())
+    )
+
+    list1 = str(uuid.uuid4())
+
+    status, data = api(
+        'PUT',
+        'listing',
+        data={'user_id': user.id, 'obj_id': public_candidate.id, 'list_name': list1},
+        token=token_id,
+    )
+
+    assert status == 200
+    item1 = data["data"]["listing_id"]  # get the list item ID
+
+    list2 = str(uuid.uuid4())
+    status, data = api(
+        'PATCH',
+        f'listing/{item1}',
+        data={'user_id': user.id, 'obj_id': public_candidate2.id, 'list_name': list2},
+        token=token_id,
+    )
+
+    assert status == 200
+
+    # get the list back, should include only one item that matches list1
+    status, data = api('GET', f'listing/{user.id}?list_name={list2}', token=token_id)
+
+    assert status == 200
+    new_list = data["data"]
+
+    assert len(new_list) == 1
+
+    assert new_list[0]['id'] == item1  # the listing ID is the same
+
+    assert new_list[0]['user_id'] == user.id  # user stays the same
+    assert new_list[0]['obj_id'] == public_candidate2.id  # obj id is new
+    assert new_list[0]['list_name'] == list2  # list name is new
+
+    # try to patch with an invalid user id
+    status, data = api(
+        'PATCH',
+        f'listing/{item1}',
+        data={'user_id': user2.id, 'obj_id': public_candidate2.id, 'list_name': list2},
+        token=token_id,
+    )
+
+    assert status == 400
+    assert 'Insufficient permission' in data['message']
+
+
+def test_listings_user_permissions(
+    user, user2, super_admin_user, public_candidate, public_candidate2
+):
+    token_id = create_token(
+        ACLs=["Upload data"], user_id=user.id, name=str(uuid.uuid4())
+    )
+
+    status, data = api(
+        'PUT',
+        'listing',
+        data={
+            'user_id': user.id,
+            'obj_id': public_candidate.id,
+            'list_name': 'favorites',
+        },
+        token=token_id,
+    )
+
+    assert status == 200
+    item1 = data["data"]["listing_id"]  # get the list item ID
+
+    # try to add this to a different user
+    status, data = api(
+        'PATCH',
+        f'listing/{item1}',
+        data={
+            'user_id': user2.id,
+            'obj_id': public_candidate.id,
+            'list_name': 'favorites',
+        },
+        token=token_id,
+    )
+
+    assert status == 400
+    assert 'Insufficient permission' in data['message']
+
+    # setup a token for the super user
+    super_token_id = create_token(
+        ACLs=["Upload data"], user_id=super_admin_user.id, name=str(uuid.uuid4())
+    )
+
+    # try to add this to a different user, but with super admin privileges
+    status, data = api(
+        'PATCH',
+        f'listing/{item1}',
+        data={
+            'user_id': user2.id,
+            'obj_id': public_candidate.id,
+            'list_name': 'favorites',
+        },
+        token=super_token_id,
+    )
+
+    assert status == 200
+
+    # get the list back, should include only one item that matches user2
+    status, data = api(
+        'GET', f'listing/{user2.id}?list_name=favorites', token=super_token_id
+    )
+
+    assert status == 200
+    new_list = data["data"]
+
+    assert len(new_list) == 1
+
+    assert new_list[0]['id'] == item1  # the listing ID is the same
+
+    assert new_list[0]['obj_id'] == public_candidate.id  # obj stays the same
