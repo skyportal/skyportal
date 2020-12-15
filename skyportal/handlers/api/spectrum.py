5f02a60f4416c3fd6b6ddd4e301819e375d86117
Don Neill <neilljd@gmail.com>
2020-12-15 13:57:55 -0800

    Clean out serialize function and replace with return of all query data, tried utc.isot format comparison for filter but still seems to not work, all in spectrum.py

--- skyportal/handlers/api/spectrum.py
+++ skyportal/handlers/api/spectrum.py
@@ -32,34 +32,6 @@ from ...schema import (
 _, cfg = load_env()
 
 
-def serialize(spec):
-
-    return_value = {
-        'wavelengths': spec.wavelengths,
-        'followup_request_id': spec.followup_request_id,
-        'fluxes': spec.fluxes,
-        'assignment_id': spec.assignment_id,
-        'errors': spec.errors,
-        'altdata': spec.altdata,
-        'obj_id': spec.obj_id,
-        'original_file_string': spec.original_file_string,
-        'observed_at': spec.observed_at,
-        'original_file_filename': spec.original_file_filename,
-        'owner_id': spec.owner_id,
-        'created_at': spec.created_at,
-        'modified': spec.modified,
-        'instrument_id': spec.instrument_id,
-        'instrument_name': spec.instrument.name,
-        'origin': spec.origin,
-        'id': spec.id,
-        'groups': spec.groups,
-        'reducers': spec.reducers,
-        'observers': spec.observers
-    }
-
-    return return_value
-
-
 class SpectrumHandler(BaseHandler):
     @permissions(['Upload data'])
     def post(self):
@@ -608,12 +580,11 @@ class SpectrumRangeHandler(BaseHandler):
             query = query.filter(Spectrum.instrument_id.in_(instrument_ids))
         if min_date is not None:
             utc = Time(min_date, format='isot', scale='utc')
-            query = query.filter(Spectrum.observed_at >= utc)
+            query = query.filter(Spectrum.observed_at >= utc.isot)
         if max_date is not None:
             utc = Time(max_date, format='isot', scale='utc')
-            query = query.filter(Spectrum.observed_at <= utc)
+            query = query.filter(Spectrum.observed_at <= utc.isot)
 
-        output = [serialize(p) for p in query]
-        return self.success(data=output)
+        return self.success(data=query.all())
 
 
