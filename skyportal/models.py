df85486fa1c7d49d1b8a258c31ca16e4472a068b
Guy Nir <37179063+guynir42@users.noreply.github.com>
2021-01-07 17:59:38 +0200

    Favorites list (#1432)

--- skyportal/models.py
+++ skyportal/models.py
@@ -956,6 +956,14 @@ Candidate.get_obj_if_readable_by = get_candidate_if_readable_by
 Candidate.is_readable_by = candidate_is_readable_by
 
 
+User.listings = relationship(
+    'Listing',
+    back_populates='user',
+    passive_deletes=True,
+    doc='The listings saved by this user',
+)
+
+
 Source = join_model("sources", Group, Obj)
 Source.__doc__ = (
     "An Obj that has been saved to a Group. Once an Obj is saved as a Source, "
@@ -2571,6 +2579,57 @@ User.transactions = relationship(
     'FacilityTransaction',
     back_populates='initiator',
     doc="The FacilityTransactions initiated by this User.",
+)
+
+
+class Listing(Base):
+
+    user_id = sa.Column(
+        sa.ForeignKey('users.id', ondelete='CASCADE'),
+        nullable=False,
+        index=True,
+        doc="The ID of the User who created this Listing.",
+    )
+
+    user = relationship(
+        "User",
+        foreign_keys=user_id,
+        back_populates="listings",
+        doc="The user that saved this object/listing",
+    )
+
+    obj_id = sa.Column(
+        sa.ForeignKey('objs.id', ondelete='CASCADE'),
+        nullable=False,
+        index=True,
+        doc="The ID of the object that is on this Listing",
+    )
+
+    obj = relationship("Obj", doc="The object referenced by this listing",)
+
+    list_name = sa.Column(
+        sa.String,
+        index=True,
+        nullable=False,
+        doc="Name of the list, e.g., 'favorites'. ",
+    )
+
+
+Listing.__table_args__ = (
+    sa.Index(
+        "listings_main_index",
+        Listing.user_id,
+        Listing.obj_id,
+        Listing.list_name,
+        unique=True,
+    ),
+    sa.Index(
+        "listings_reverse_index",
+        Listing.list_name,
+        Listing.obj_id,
+        Listing.user_id,
+        unique=True,
+    ),
 )
 
 
