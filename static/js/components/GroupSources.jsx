67ee55144b8dc86950425849e80ed385b09d5c7d
kshin <kshin@caltech.edu>
2021-01-07 10:45:15 -0500

    Enable filtering for GroupSources

--- static/js/components/GroupSources.jsx
+++ static/js/components/GroupSources.jsx
@@ -69,9 +69,10 @@ const GroupSources = ({ route }) => {
 
   const groupName = groups.filter((g) => g.id === groupID)[0]?.name || "";
 
-  const handleSavedSourcesTableSorting = (sortData) => {
+  const handleSavedSourcesTableSorting = (sortData, filterData) => {
     dispatch(
       sourcesActions.fetchSavedGroupSources({
+        ...filterData,
         group_ids: [route.id],
         pageNumber: 1,
         numPerPage: savedSourcesRowsPerPage,
@@ -84,10 +85,12 @@ const GroupSources = ({ route }) => {
   const handleSavedSourcesTablePagination = (
     pageNumber,
     numPerPage,
-    sortData
+    sortData,
+    filterData
   ) => {
     setSavedSourcesRowsPerPage(numPerPage);
     const data = {
+      ...filterData,
       group_ids: [route.id],
       pageNumber,
       numPerPage,
@@ -99,9 +102,10 @@ const GroupSources = ({ route }) => {
     dispatch(sourcesActions.fetchSavedGroupSources(data));
   };
 
-  const handlePendingSourcesTableSorting = (sortData) => {
+  const handlePendingSourcesTableSorting = (sortData, filterData) => {
     dispatch(
       sourcesActions.fetchPendingGroupSources({
+        ...filterData,
         group_ids: [route.id],
         pageNumber: 1,
         numPerPage: pendingSourcesRowsPerPage,
@@ -114,10 +118,12 @@ const GroupSources = ({ route }) => {
   const handlePendingSourcesTablePagination = (
     pageNumber,
     numPerPage,
-    sortData
+    sortData,
+    filterData
   ) => {
     setPendingSourcesRowsPerPage(numPerPage);
     const data = {
+      ...filterData,
       group_ids: [route.id],
       pageNumber,
       numPerPage,
