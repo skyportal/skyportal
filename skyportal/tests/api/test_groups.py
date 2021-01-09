a29f60c29c5e8693e36f78ea62c27f4288b3fee1
Danny Goldstein <dgold@berkeley.edu>
2021-01-09 10:52:49 -0800

    fix groups tests

--- skyportal/tests/api/test_groups.py
+++ skyportal/tests/api/test_groups.py
@@ -6,54 +6,54 @@ from baselayer.app.env import load_env
 _, cfg = load_env()
 
 
-def test_token_user_create_new_group(manage_groups_token, super_admin_user):
+def test_token_user_create_new_group(super_admin_token, super_admin_user):
     group_name = str(uuid.uuid4())
     status, data = api(
         "POST",
         "groups",
         data={"name": group_name, "group_admins": [super_admin_user.id]},
-        token=manage_groups_token,
+        token=super_admin_token,
     )
     assert status == 200
     assert data["status"] == "success"
     new_group_id = data["data"]["id"]
 
-    status, data = api("GET", f"groups/{new_group_id}", token=manage_groups_token)
+    status, data = api("GET", f"groups/{new_group_id}", token=super_admin_token)
     assert data["status"] == "success"
     assert data["data"]["name"] == group_name
 
 
-def test_fetch_group_by_name(manage_groups_token, super_admin_user):
+def test_fetch_group_by_name(super_admin_token, super_admin_user):
     group_name = str(uuid.uuid4())
     status, data = api(
         "POST",
         "groups",
         data={"name": group_name, "group_admins": [super_admin_user.id]},
-        token=manage_groups_token,
+        token=super_admin_token,
     )
     assert status == 200
     assert data["status"] == "success"
     new_group_id = data["data"]["id"]
 
-    status, data = api("GET", f"groups?name={group_name}", token=manage_groups_token)
+    status, data = api("GET", f"groups?name={group_name}", token=super_admin_token)
     assert data["status"] == "success"
     assert len(data["data"]) == 1
     assert data["data"][0]["name"] == group_name
     assert data["data"][0]["id"] == new_group_id
 
 
-def test_token_user_request_all_groups(manage_groups_token, super_admin_user):
+def test_token_user_request_all_groups(super_admin_token, super_admin_user):
     group_name = str(uuid.uuid4())
     status, data = api(
         "POST",
         "groups",
         data={"name": group_name, "group_admins": [super_admin_user.id]},
-        token=manage_groups_token,
+        token=super_admin_token,
     )
     assert status == 200
     assert data["status"] == "success"
 
-    status, data = api("GET", "groups", token=manage_groups_token)
+    status, data = api("GET", "groups", token=super_admin_token)
     assert data["status"] == "success"
     assert any(
         [user_group["name"] == group_name for user_group in data["data"]["user_groups"]]
@@ -80,40 +80,40 @@ def test_token_user_request_all_groups(manage_groups_token, super_admin_user):
     )
 
 
-def test_token_user_update_group(manage_groups_token, public_group):
+def test_token_user_update_group(super_admin_token, public_group):
     new_name = str(uuid.uuid4())
     status, data = api(
         "PUT",
         f"groups/{public_group.id}",
         data={"name": new_name},
-        token=manage_groups_token,
+        token=super_admin_token,
     )
     assert status == 200
     assert data["status"] == "success"
 
-    status, data = api("GET", f"groups/{public_group.id}", token=manage_groups_token)
+    status, data = api("GET", f"groups/{public_group.id}", token=super_admin_token)
     assert data["status"] == "success"
     assert data["data"]["name"] == new_name
 
 
-def test_token_user_delete_group(manage_groups_token, public_group):
-    status, data = api("DELETE", f"groups/{public_group.id}", token=manage_groups_token)
+def test_token_user_delete_group(super_admin_token, public_group):
+    status, data = api("DELETE", f"groups/{public_group.id}", token=super_admin_token)
     assert status == 200
     assert data["status"] == "success"
 
-    status, data = api("GET", f"groups/{public_group.id}", token=manage_groups_token)
+    status, data = api("GET", f"groups/{public_group.id}", token=super_admin_token)
     assert status == 400
 
 
 def test_manage_groups_token_get_unowned_group(
-    manage_groups_token, user, super_admin_user
+    super_admin_token, user, super_admin_user
 ):
     group_name = str(uuid.uuid4())
     status, data = api(
         "POST",
         "groups",
         data={"name": group_name, "group_admins": [user.id]},
-        token=manage_groups_token,
+        token=super_admin_token,
     )
     assert status == 200
     assert data["status"] == "success"
@@ -282,7 +282,7 @@ def test_add_stream_to_group_delete_stream(
 
 
 def test_post_new_filter_delete_group_deletes_filter(
-    manage_groups_token, group_with_stream, public_stream
+    super_admin_token, group_with_stream, public_stream
 ):
     status, data = api(
         "POST",
@@ -292,27 +292,27 @@ def test_post_new_filter_delete_group_deletes_filter(
             "stream_id": public_stream.id,
             "group_id": group_with_stream.id,
         },
-        token=manage_groups_token,
+        token=super_admin_token,
     )
     assert status == 200
     filter_id = data["data"]["id"]
 
-    status, data = api("GET", f"filters/{filter_id}", token=manage_groups_token)
+    status, data = api("GET", f"filters/{filter_id}", token=super_admin_token)
     assert status == 200
     assert data["data"]["id"] == filter_id
 
     status, data = api(
-        "DELETE", f"groups/{group_with_stream.id}", token=manage_groups_token
+        "DELETE", f"groups/{group_with_stream.id}", token=super_admin_token
     )
     assert status == 200
 
-    status, data = api("GET", f"filters/{filter_id}", token=manage_groups_token)
+    status, data = api("GET", f"filters/{filter_id}", token=super_admin_token)
     assert status == 400
     assert data["message"] == "Invalid filter ID."
 
 
 def test_post_new_filter_delete_stream_deletes_filter(
-    manage_groups_token, super_admin_token, group_with_stream, public_stream
+    super_admin_token, group_with_stream, public_stream
 ):
     status, data = api(
         "POST",
@@ -322,19 +322,19 @@ def test_post_new_filter_delete_stream_deletes_filter(
             "stream_id": public_stream.id,
             "group_id": group_with_stream.id,
         },
-        token=manage_groups_token,
+        token=super_admin_token,
     )
     assert status == 200
     filter_id = data["data"]["id"]
 
-    status, data = api("GET", f"filters/{filter_id}", token=manage_groups_token)
+    status, data = api("GET", f"filters/{filter_id}", token=super_admin_token)
     assert status == 200
     assert data["data"]["id"] == filter_id
 
     status, data = api("DELETE", f"streams/{public_stream.id}", token=super_admin_token)
     assert status == 200
 
-    status, data = api("GET", f"filters/{filter_id}", token=manage_groups_token)
+    status, data = api("GET", f"filters/{filter_id}", token=super_admin_token)
     assert status == 400
     assert data["message"] == "Invalid filter ID."
 
