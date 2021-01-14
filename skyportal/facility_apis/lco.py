ba2155aa25d85209f5689bd0f8fb1a49725c6955
Michael Coughlin <cough052@umn.edu>
2021-01-14 08:40:38 -0600

    Update skyportal/facility_apis/lco.py
    Co-authored-by: Kyung Min Shin <kshin@caltech.edu>

--- skyportal/facility_apis/lco.py
+++ skyportal/facility_apis/lco.py
@@ -730,8 +730,8 @@ class SPECTRALAPI(LCOAPI):
         requestgroup = lcoreq.requestgroup
 
         r = requests.post(
-            'https://observe.lco.global/api/requestgroups/',
-            headers={'Authorization': 'Token {}'.format(altdata["API_TOKEN"])},
+            "https://observe.lco.global/api/requestgroups/",
+            headers={"Authorization": f'Token {altdata["API_TOKEN"]}'},
             json=requestgroup,  # Make sure you use json!
         )
 
