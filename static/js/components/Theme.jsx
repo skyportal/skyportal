37080733b7b267ad391b543b891aaa2b2136f93b
joe <joe@boldmetrics.com>
2021-01-31 23:07:52 -0600

    Udpate MUI Theme with updated palette colors.

--- static/js/components/Theme.jsx
+++ static/js/components/Theme.jsx
@@ -8,10 +8,37 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 const Theme = ({ disableTransitions, children }) => {
   const theme = useSelector((state) => state.profile.preferences.theme);
   const materialTheme = createMuiTheme({
+    // Custom colors. These are from: https://coolors.co/e63946-f1faee-a8dadc-457b9d-1d3557
+    colors: {
+      darkBlue: '#1d3557', // Prussian Blue
+      midBlue: '#457b9d', // Celadon Blue
+      lightBlue: '#a8dadc', //Powder Blue
+      white: '#f1faee', // Honeydew
+      red: '#e63946', // Imperial Red
+    },
     palette: {
       type: theme || "light",
-      background:
-        theme === "dark" ? { default: "#303030" } : { default: "#f0f2f5" },
+      primary: {
+        main: '#1d3557', // darkBlue
+        light: '#4a5e84',
+        dark: '#000e2e',
+        contrastText: '#fff',
+      },
+      secondary: {
+        main: '#a8dadc', // lightBlue
+        light: '#76aace',
+        dark: '#a8dadc',
+        contrastText: '#fff',
+      },
+      info: {
+        main: '#f1faee', // white
+      },
+      warning: {
+        main: '#fca311',
+      },
+      error: {
+        main: '#e63946', // red
+      },
     },
     // Only added during testing; removes animations, transitions, and
     // rippple effects
