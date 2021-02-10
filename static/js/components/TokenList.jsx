d0687abac2e15567ac07fdde05d97ecc4371e22b
kshin <kshin@caltech.edu>
2021-02-10 16:23:16 -0500

    Address reviewer feedback

--- static/js/components/TokenList.jsx
+++ static/js/components/TokenList.jsx
@@ -44,7 +44,7 @@ const getMuiTheme = (theme) =>
         selectRoot: {
           marginRight: "0.5rem",
           [theme.breakpoints.up("sm")]: {
-            marginLeft: "0.rem",
+            marginLeft: "0",
             marginRight: "2rem",
           },
         },
