75fdfb7e4184b47e7aaedfa6c950f2b6b8711b99
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-02-22 12:53:48 -0800

    Explicitly create primary key for id column of subclasses of JoinModel

---
+++ alembic/versions/5365c004a923_add_primarykey_constraint_to_joinmodel_.py
@@ -0,0 +1,31 @@
+"""Add PrimaryKey constraint to JoinModel.id
+
+Revision ID: 5365c004a923
+Revises: 1c2d593e9778
+Create Date: 2021-02-22 12:46:46.137261
+
+"""
+from alembic import op
+from baselayer.app.models import JoinModel
+
+
+# revision identifiers, used by Alembic.
+revision = '5365c004a923'
+down_revision = '1c2d593e9778'
+branch_labels = None
+depends_on = None
+
+
+mapped_classes = JoinModel.__subclasses__()
+
+
+def upgrade():
+    for cls in mapped_classes:
+        table = cls.__tablename__
+        op.create_primary_key(f"pk_{table}", table, ["id"])
+
+
+def downgrade():
+    for cls in mapped_classes:
+        table = cls.__tablename__
+        op.drop_constraint(f"pk_{table}", table)
