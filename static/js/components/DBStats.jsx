383ae98234d503d356cb97fa471150a925171fd3
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-03-03 20:14:29 -0800

    Show cron job output in Dialog on click rather than Popover on hover on DB Stats page (#1737)

--- static/js/components/DBStats.jsx
+++ static/js/components/DBStats.jsx
@@ -6,36 +6,26 @@ import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
 import Typography from "@material-ui/core/Typography";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import Popover from "@material-ui/core/Popover";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import Box from "@material-ui/core/Box";
-import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
 
 import * as dbStatsActions from "../ducks/dbStats";
 
-const useStyles = makeStyles((theme) => ({
-  popover: {
-    pointerEvents: "none",
-  },
-  paper: {
-    padding: theme.spacing(1),
-  },
-}));
-
 const DBStats = () => {
-  const classes = useStyles();
   const dispatch = useDispatch();
   const dbStats = useSelector((state) => state.dbStats);
-  const [popoverAnchorEl, setPopoverAnchorEl] = useState(null);
+  const [clickedCronjobOutput, setClickedCronjobOutput] = useState(null);
 
-  const handlePopoverOpen = (event) => {
-    setPopoverAnchorEl(event.currentTarget);
+  const handleDialogClose = () => {
+    setClickedCronjobOutput(null);
   };
 
-  const handlePopoverClose = () => {
-    setPopoverAnchorEl(null);
-  };
-
-  const popoverOpen = Boolean(popoverAnchorEl);
+  const dialogOpen = Boolean(clickedCronjobOutput);
 
   useEffect(() => {
     if (dbStats === null) {
@@ -66,60 +56,49 @@ const DBStats = () => {
               </TableCell>
               <TableCell>
                 {Array.isArray(dbStats[key]) && (
-                  <ul style={{ paddingLeft: "0.8rem" }}>
-                    {dbStats[key].map((item, idx) => (
-                      <li key={item.summary}>
-                        {item.summary}
-                        {item.output && (
-                          <>
-                            <Typography
-                              variant="caption"
-                              aria-owns={
-                                popoverOpen
-                                  ? `mouse-over-popover${idx}`
-                                  : undefined
-                              }
-                              aria-haspopup="true"
-                              onMouseEnter={handlePopoverOpen}
-                              onMouseLeave={handlePopoverClose}
-                            >
-                              <em> (mouse over to see output)</em>
-                            </Typography>
-                            <Popover
-                              id={`mouse-over-popover${idx}`}
-                              open={popoverOpen}
-                              className={classes.popover}
-                              classes={{
-                                paper: classes.paper,
-                              }}
-                              anchorEl={popoverAnchorEl}
-                              anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "left",
-                              }}
-                              transformOrigin={{
-                                vertical: "top",
-                                horizontal: "left",
-                              }}
-                              onClose={handlePopoverClose}
-                              disableRestoreFocus
-                            >
-                              <Typography>
-                                <Box fontFamily="Monospace">
-                                  {item.output.split("\n").map((line) => (
-                                    <>
-                                      <span>{line}</span>
-                                      <br />
-                                    </>
-                                  ))}
-                                </Box>
-                              </Typography>
-                            </Popover>
-                          </>
-                        )}
-                      </li>
-                    ))}
-                  </ul>
+                  <>
+                    <List>
+                      {dbStats[key].map((item) => (
+                        <ListItem key={item.summary}>
+                          {item.summary}
+                          {item.output && (
+                            <>
+                              <Button
+                                variant="contained"
+                                size="small"
+                                style={{ marginLeft: "1rem" }}
+                                onClick={() =>
+                                  setClickedCronjobOutput(item.output)
+                                }
+                              >
+                                See job output
+                              </Button>
+                            </>
+                          )}
+                        </ListItem>
+                      ))}
+                    </List>
+                    <Dialog
+                      open={dialogOpen}
+                      style={{ position: "fixed" }}
+                      onClose={handleDialogClose}
+                    >
+                      <DialogTitle>Process Output</DialogTitle>
+                      <DialogContent>
+                        <Typography>
+                          <Box fontFamily="Monospace">
+                            {clickedCronjobOutput &&
+                              clickedCronjobOutput.split("\n").map((line) => (
+                                <>
+                                  <span>{line}</span>
+                                  <br />
+                                </>
+                              ))}
+                          </Box>
+                        </Typography>
+                      </DialogContent>
+                    </Dialog>
+                  </>
                 )}
                 {!Array.isArray(dbStats[key]) &&
                   (!Number.isNaN(Number(dbStats[key]))
