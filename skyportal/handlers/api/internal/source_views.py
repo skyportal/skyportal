f86428d57a70d6e56504f99189dc18ed3e624fe4
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-04-12 10:22:08 -0700

    Update SourceViewHandler access methods (#1868)
    * Remove outdated permissions check for source view registration
    
    * Update access method

--- skyportal/handlers/api/internal/source_views.py
+++ skyportal/handlers/api/internal/source_views.py
@@ -49,10 +49,10 @@ class SourceViewsHandler(BaseHandler):
         )
         sources = []
         for view, obj_id in query_results:
-            s = Source.get_obj_if_readable_by(  # Returns Source.obj
+            s = Obj.get_if_accessible_by(
                 obj_id,
                 self.current_user,
-                options=[joinedload(Source.obj).joinedload(Obj.thumbnails)],
+                options=[joinedload(Obj.thumbnails)],
             )
             info = first_thumbnail_info(s.thumbnails)
             sources.append(
@@ -71,10 +71,6 @@ class SourceViewsHandler(BaseHandler):
 
     @tornado.web.authenticated
     def post(self, obj_id):
-        # Ensure user has access to source
-        Obj.get_if_readable_by(obj_id, self.current_user)
-        # This endpoint will only be hit by front-end, so this will never be a token
-
         sv = SourceView(
             obj_id=obj_id,
             username_or_token_id=self.current_user.username,
