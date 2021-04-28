7a138196160b7d5b29b91c749c671ab80b890eb4
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-04-28 16:40:48 -0700

    Also update PATCH handler

--- skyportal/handlers/api/photometry.py
+++ skyportal/handlers/api/photometry.py
@@ -796,7 +796,9 @@ class PhotometryHandler(BaseHandler):
 
             # posting to new streams?
             if stream_ids:
-                streams = Stream.get_if_accessible_by(stream_ids, self.current_user)
+                streams = Stream.get_if_accessible_by(
+                    stream_ids, self.current_user, raise_if_none=True
+                )
                 # Add new stream_photometry rows if not already present
                 for stream in streams:
                     if (
@@ -933,12 +935,25 @@ class PhotometryHandler(BaseHandler):
 
         # Update streams, if relevant
         if stream_ids is not None:
-            streams = Stream.get_if_accessible_by(stream_ids, self.current_user)
-            if not streams:
-                return self.error(
-                    "Invalid stream_ids field. Specify at least one valid stream ID."
-                )
-            photometry.streams = streams
+            streams = Stream.get_if_accessible_by(
+                stream_ids, self.current_user, raise_if_none=True
+            )
+            # Add new stream_photometry rows if not already present
+            for stream in streams:
+                if (
+                    StreamPhotometry.query_records_accessible_by(self.current_user)
+                    .filter(
+                        StreamPhotometry.stream_id == stream.id,
+                        StreamPhotometry.photometr_id == photometry_id,
+                    )
+                    .first()
+                    is None
+                ):
+                    DBSession().add(
+                        StreamPhotometry(
+                            photometr_id=photometry_id, stream_id=stream.id
+                        )
+                    )
 
         self.verify_and_commit()
         return self.success()
