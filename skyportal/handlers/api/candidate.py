ec3b8db433e6a9f514407eaa8d1365d8c5536335
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-05-17 14:10:06 -0700

    Define cache_dir once and use throughout

--- skyportal/handlers/api/candidate.py
+++ skyportal/handlers/api/candidate.py
@@ -1068,25 +1068,26 @@ def grab_query_results(
 
     if page:
         if use_cache:
+            cache_dir = "cache/candidate_queries"
             if query_id is None:
                 query_id = str(uuid.uuid4())
-                if not os.path.exists("cache/candidate_queries"):
-                    os.mkdir("cache/candidate_queries")
+                if not os.path.exists(cache_dir):
+                    os.mkdir(cache_dir)
                 all_ids = ordered_ids.all()
-                np.save(f"cache/candidate_queries/{query_id}.npy", all_ids)
+                np.save(f"{cache_dir}/{query_id}.npy", all_ids)
                 results = all_ids[
                     ((page - 1) * n_items_per_page) : (page * n_items_per_page)  # noqa
                 ]
             else:
                 try:
-                    ids = np.load(f"cache/candidate_queries/{query_id}.npy")
+                    ids = np.load(f"{cache_dir}/{query_id}.npy")
                 except FileNotFoundError:
                     # Cache expired/removed; create new cache file
                     query_id = str(uuid.uuid4())
-                    if not os.path.exists("cache/candidate_queries"):
-                        os.mkdir("cache/candidate_queries")
+                    if not os.path.exists(cache_dir):
+                        os.mkdir(cache_dir)
                     all_ids = ordered_ids.all()
-                    np.save(f"cache/candidate_queries/{query_id}.npy", all_ids)
+                    np.save(f"{cache_dir}/{query_id}.npy", all_ids)
                     results = all_ids[
                         ((page - 1) * n_items_per_page) : (  # noqa
                             page * n_items_per_page
