327080f78dcd8904a829d77bd4288a9a07928119
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-05-19 12:40:13 -0700

    Update import order

--- skyportal/facility_apis/__init__.py
+++ skyportal/facility_apis/__init__.py
@@ -1,5 +1,5 @@
 from .interface import FollowUpAPI, Listener
 from .sedm import SEDMAPI, SEDMListener
 from .lt import IOOAPI, IOIAPI, SPRATAPI
-from .ztf import ZTFAPI
 from .lco import SINISTROAPI, SPECTRALAPI, FLOYDSAPI, MUSCATAPI
+from .ztf import ZTFAPI
