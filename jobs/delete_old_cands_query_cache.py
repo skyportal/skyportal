542bd68fd2e754b72d1faef5343c9b920a465033
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-05-19 11:24:48 -0700

    Specify cache expiration time in minutes rather than hours

--- jobs/delete_old_cands_query_cache.py
+++ jobs/delete_old_cands_query_cache.py
@@ -11,17 +11,17 @@ from baselayer.app.env import load_env
 _, cfg = load_env()
 
 try:
-    expire_after_n_hrs = float(cfg["misc.hours_to_keep_candidate_query_cache"])
+    expire_after_n_min = float(cfg["misc.minutes_to_keep_candidate_query_cache"])
 except ValueError:
     raise ValueError(
         "Invalid (non-numeric) value provided for "
-        "hours_to_keep_candidate_query_cache in config file."
+        "minutes_to_keep_candidate_query_cache in config file."
     )
 
 cache_dir = Path("cache/candidate_queries")
 
 for cache_file in cache_dir.glob("*.npy"):
-    if (time.time() - cache_file.stat().st_mtime) > (expire_after_n_hrs * 60 * 60):
+    if (time.time() - cache_file.stat().st_mtime) > (expire_after_n_min * 60):
         try:
             os.remove(cache_file.absolute())
         except FileNotFoundError:
