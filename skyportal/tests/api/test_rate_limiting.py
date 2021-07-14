1db037312b21ad612f7b5be37bf973922650782e
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-07-13 17:54:31 -0700

    Expand API tests

--- skyportal/tests/api/test_rate_limiting.py
+++ skyportal/tests/api/test_rate_limiting.py
@@ -1,5 +1,8 @@
 import time
+from baselayer.app.env import load_env
 from skyportal.tests import api
+
+_, cfg = load_env()
 
 
 def test_api_rate_limiting(view_only_token):
@@ -7,13 +10,24 @@ def test_api_rate_limiting(view_only_token):
     status = 200
     while status == 200:
         status, _ = api('GET', 'sysinfo', token=view_only_token)
-        n_successful_requests += 1
-    assert n_successful_requests == 100
+        if status == 200:
+            n_successful_requests += 1
+    assert (
+        100 * cfg["server"]["processes"] - 2
+        <= n_successful_requests
+        <= 100 * cfg["server"]["processes"] + 2
+    )
     status, data = api('GET', 'sysinfo', token=view_only_token)
-    assert status == 503
-    assert data["message"] == "API rate limit exceeded; please throttle your requests"
+    assert status != 200
+    assert "API rate limit exceeded; please throttle your requests" in data["message"]
 
-    time.sleep(5)
+    time.sleep(20)
 
     status, _ = api('GET', 'sysinfo', token=view_only_token)
     assert status == 200
+
+
+def test_sys_admins_not_rate_limited(super_admin_token):
+    for i in range(100 * cfg["server"]["processes"] + 5):
+        status, _ = api('GET', 'sysinfo', token=super_admin_token)
+        assert status == 200
