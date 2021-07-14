698b238e413fa3e2429aa4fe7c86aa80465a8413
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-07-14 12:37:23 -0700

    Update rate limit API tests

--- skyportal/tests/api/test_rate_limiting.py
+++ skyportal/tests/api/test_rate_limiting.py
@@ -1,8 +1,5 @@
 import time
-from baselayer.app.env import load_env
 from skyportal.tests import api
-
-_, cfg = load_env()
 
 
 def test_api_rate_limiting(view_only_token):
@@ -12,23 +9,11 @@ def test_api_rate_limiting(view_only_token):
         status, _ = api('GET', 'sysinfo', token=view_only_token)
         if status == 200:
             n_successful_requests += 1
-    assert (
-        100 * cfg["server"]["processes"] - 2
-        <= n_successful_requests
-        <= 100 * cfg["server"]["processes"] + 2
-    )
+    assert 25 <= n_successful_requests <= 26
     status, data = api('GET', 'sysinfo', token=view_only_token)
     assert status != 200
-    assert "API rate limit exceeded; please throttle your requests" in data["message"]
 
-    time.sleep(20)
+    time.sleep(5)
 
     status, _ = api('GET', 'sysinfo', token=view_only_token)
     assert status == 200
-
-
-# Disabling for now as this is not covered by nginx config approach
-def disabled_test_sys_admins_not_rate_limited(super_admin_token):
-    for i in range(100 * cfg["server"]["processes"] + 5):
-        status, _ = api('GET', 'sysinfo', token=super_admin_token)
-        assert status == 200
