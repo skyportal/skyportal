55f2dbe958d5be8336f409b0d47f6c6ca15ea0ac
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-07-15 07:31:36 -0700

    Update rate limiting API tests

--- skyportal/tests/api/test_rate_limiting.py
+++ skyportal/tests/api/test_rate_limiting.py
@@ -9,7 +9,7 @@ def test_api_rate_limiting(view_only_token):
         status, _ = api('GET', 'sysinfo', token=view_only_token)
         if status == 200:
             n_successful_requests += 1
-    assert 25 <= n_successful_requests <= 26
+    assert 15 <= n_successful_requests <= 16
     status, data = api('GET', 'sysinfo', token=view_only_token)
     assert status != 200
 
