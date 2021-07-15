13c4b34ecace9cb57a80037edb1470a69d0d9e24
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-07-15 10:57:44 -0700

    Add safety catch to rate limit test

--- skyportal/tests/rate_limiting/test_rate_limiting.py
+++ skyportal/tests/rate_limiting/test_rate_limiting.py
@@ -5,7 +5,7 @@ from skyportal.tests import api
 def test_api_rate_limiting(view_only_token):
     n_successful_requests = 0
     status = 200
-    while status == 200:
+    while status == 200 and n_successful_requests < 100:
         status, _ = api('GET', 'sysinfo', token=view_only_token)
         if status == 200:
             n_successful_requests += 1
