d1cc8d006cfb3f8a743e8fe4c163d73d51de4062
kshin <kshin@caltech.edu>
2021-07-23 17:33:39 -0400

    Update GCN notice types enum

---
+++ alembic/versions/4ef77725b330_update_gcn_notices_enum.py
@@ -0,0 +1,28 @@
+"""Update GCN notices enum
+
+Revision ID: 4ef77725b330
+Revises: 1f4da189227e
+Create Date: 2021-07-23 17:30:16.102665
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '4ef77725b330'
+down_revision = '1f4da189227e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # Add new values to the GCN noticetype ENUM type
+    with op.get_context().autocommit_block():
+        op.execute("ALTER TYPE noticetype ADD VALUE IF NOT EXISTS 'SK_SN'")
+
+
+def downgrade():
+    # There is no action taken to downgrade this migration.
+    # Values should only be added and never removed from ENUM types in order to
+    # avoid having to delete relevant data.
+    pass
