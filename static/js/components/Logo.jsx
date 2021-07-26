e7c0fc397788b44fb0385ce33bb4fa06e013c5e7
Kyung Min Shin <kshin@caltech.edu>
2021-07-26 18:56:58 -0400

    Convert CSS files to inline MUI makeStyles() (#2137)

--- static/js/components/Logo.jsx
+++ static/js/components/Logo.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
-import styles from "./Logo.css";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  rotateLogo: {
+    verticalAlign: "middle",
+    height: "3.125rem",
+    animationName: "$rotateUp",
+    animationDuration: "4s",
+  },
+  noRotateLogo: {
+    verticalAlign: "middle",
+    height: "3.125rem",
+  },
+  "@keyframes rotateUp": {
+    "0%": {
+      transform: "rotate(60deg)",
+    },
+    "50%": {
+      transform: "rotate(-10deg)",
+    },
+    "100%": {
+      transform: "rotate(0deg)",
+    },
+  },
+}));
 
 const Logo = ({ src, altText }) => {
   const rotateLogo = useSelector((state) => state.logo.rotateLogo);
+  const styles = useStyles();
   return (
     <div className={styles.logoContainer}>
       <img
