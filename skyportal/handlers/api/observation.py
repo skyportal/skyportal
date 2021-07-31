5142a8916409cce6807ca1d53501f102b50a4f55
Michael Coughlin <coughlin@physics.harvard.edu>
2021-07-30 21:01:18 -0500

    Get localization dependent probability

--- skyportal/handlers/api/observation.py
+++ skyportal/handlers/api/observation.py
@@ -1,12 +1,19 @@
 from baselayer.app.access import permissions, auth_or_token
 import arrow
+import healpix_alchemy as ha
+import numpy as np
 import pandas as pd
 from sqlalchemy.orm import joinedload
+from sqlalchemy import func, union
 from astropy.time import Time
 
 from ..base import BaseHandler
 from ...models import (
     DBSession,
+    GcnEvent,
+    InstrumentFieldTile,
+    Localization,
+    LocalizationTile,
     Telescope,
     Instrument,
     InstrumentField,
@@ -156,6 +163,8 @@ class ObservationHandler(BaseHandler):
         instrument_name = data.get('instrument_name')
         start_date = data.get('start_date')
         end_date = data.get('end_date')
+        dateobs = data.get('dateobs')
+        localization_name = data.get('localization_name')
 
         telescope = (
             Telescope.query_records_accessible_by(
@@ -203,5 +212,86 @@ class ObservationHandler(BaseHandler):
         query = query.filter(ExecutedObservation.obstime <= end_date)
 
         observations = query.all()
+
+        if dateobs is not None:
+            if localization_name is not None:
+                localization = (
+                    Localization.query_records_accessible_by(self.current_user)
+                    .filter(
+                        Localization.dateobs == dateobs,
+                        Localization.localization_name == localization_name,
+                    )
+                    .first()
+                )
+                if localization is None:
+                    return self.error("Localization not found", status=404)
+            else:
+                event = (
+                    GcnEvent.query_records_accessible_by(
+                        self.current_user,
+                        options=[
+                            joinedload(GcnEvent.localizations),
+                        ],
+                    )
+                    .filter(GcnEvent.dateobs == dateobs)
+                    .first()
+                )
+                if event is None:
+                    return self.error("GCN event not found", status=404)
+                localization = event.localizations[-1]
+
+            a, b = LocalizationTile, InstrumentFieldTile
+            a_lo = a.nested_lo.label('a_lo')
+            a_hi = a.nested_hi.label('a_hi')
+            b_lo = b.nested_lo.label('b_lo')
+            b_hi = b.nested_hi.label('b_hi')
+
+            query1 = DBSession().query(
+                a_lo,
+                a_hi,
+                b_lo,
+                b_hi,
+                InstrumentFieldTile.instrument_id.label('instrument_id'),
+                InstrumentFieldTile.instrument_field_id.label('instrument_field_id'),
+                LocalizationTile.localization_id.label('localization_id'),
+                LocalizationTile.probdensity.label('probdensity'),
+            )
+
+            query2 = union(
+                query1.join(b, a_lo.between(b_lo, b_hi)),
+                query1.join(b, b_lo.between(a_lo, a_hi)),
+            ).cte()
+
+            query3 = DBSession().query(ExecutedObservation.field_id)
+            query3 = query3.filter(ExecutedObservation.instrument_id == instrument.id)
+            query3 = query3.filter(ExecutedObservation.obstime >= start_date)
+            query3 = query3.filter(ExecutedObservation.obstime <= end_date)
+            subquery = query3.subquery()
+
+            lo = func.greatest(query2.c.a_lo, query2.c.b_lo)
+            hi = func.least(query2.c.a_hi, query2.c.b_hi)
+            area = (hi - lo + 1) * ha.healpix.PIXEL_AREA
+            prob = func.sum(query2.c.probdensity * area).label('probability')
+
+            query = (
+                DBSession()
+                .query(query2.c.instrument_field_id, prob)
+                .filter(query2.c.localization_id == localization.id)
+                .filter(query2.c.instrument_id == instrument.id)
+                .filter(query2.c.instrument_field_id.in_(subquery))
+                .group_by(query2.c.localization_id, query2.c.instrument_field_id)
+            )
+
+            tiles = query.all()
+            field_ids = np.array([tile[0] for tile in tiles])
+            probs = np.array([tile[1] for tile in tiles])
+            observations_all = []
+            for observation in observations:
+                idx = np.where(field_ids == observation.field_id)[0][0]
+                observations_all.append(
+                    {**observation.to_dict(), 'probability': probs[idx]}
+                )
+            observations = observations_all
+
         self.verify_and_commit()
         return self.success(data=observations)
