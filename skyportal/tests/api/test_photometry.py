96a7108b486f6869ce21188ed3b3d958b1bdff1c
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-08-02 18:22:06 -0700

    Further reduce data size in test_big_phot_post API test

--- skyportal/tests/api/test_photometry.py
+++ skyportal/tests/api/test_photometry.py
@@ -1446,10 +1446,10 @@ def test_token_user_big_post(
         'photometry',
         data={
             'obj_id': str(public_source.id),
-            'mjd': [58000 + i for i in range(40000)],
+            'mjd': [58000 + i for i in range(30000)],
             'instrument_id': ztf_camera.id,
-            'mag': np.random.uniform(low=18, high=22, size=40000).tolist(),
-            'magerr': np.random.uniform(low=0.1, high=0.3, size=40000).tolist(),
+            'mag': np.random.uniform(low=18, high=22, size=30000).tolist(),
+            'magerr': np.random.uniform(low=0.1, high=0.3, size=30000).tolist(),
             'limiting_mag': 22.3,
             'magsys': 'ab',
             'filter': 'ztfg',
