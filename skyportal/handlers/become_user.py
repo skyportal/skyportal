ca133ae05463ebfd6230d8816f0ff98ea754d0b9
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-08-05 12:42:53 -0700

    Return error message when user does not have sufficient permissions to access become_user

--- skyportal/handlers/become_user.py
+++ skyportal/handlers/become_user.py
@@ -4,7 +4,7 @@ from ..models import User
 
 class BecomeUserHandler(BaseHandler):
     def get(self, new_user_id=None):
-        if (
+        if not (
             self.cfg['server.auth.debug_login']
             or len(
                 {'System admin', 'Become user'}.intersection(
@@ -13,15 +13,17 @@ class BecomeUserHandler(BaseHandler):
             )
             > 0
         ):
-            user = User.query.get(new_user_id)
-            sa = user.social_auth.first()
-            if user:
-                self.clear_cookie('user_id')
-                self.clear_cookie('user_oauth_id')
-                self.clear_cookie('auth_token')
-                self.set_secure_cookie('user_id', new_user_id.encode('ascii'))
-                if sa is not None:
-                    self.set_secure_cookie('user_oauth_id', sa.uid.encode('ascii'))
-                return self.success()
-            else:
-                return self.error('Invalid user ID.')
+            return self.error("Insufficient permissions")
+
+        user = User.query.get(new_user_id)
+        sa = user.social_auth.first()
+        if user:
+            self.clear_cookie('user_id')
+            self.clear_cookie('user_oauth_id')
+            self.clear_cookie('auth_token')
+            self.set_secure_cookie('user_id', new_user_id.encode('ascii'))
+            if sa is not None:
+                self.set_secure_cookie('user_oauth_id', sa.uid.encode('ascii'))
+            return self.success()
+        else:
+            return self.error('Invalid user ID.')
