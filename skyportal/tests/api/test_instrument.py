f3d16e0cbaa0468f7a876b569ce1bf6537b22f81
Michael Coughlin <coughlin@physics.harvard.edu>
2021-11-21 16:06:05 -0600

    shorten time

--- skyportal/tests/api/test_instrument.py
+++ skyportal/tests/api/test_instrument.py
@@ -48,7 +48,7 @@ def test_token_user_post_get_instrument(super_admin_token):
     assert data['status'] == 'success'
 
     # wait for the fields to populate
-    time.sleep(120)
+    time.sleep(15)
 
     instrument_id = data['data']['id']
     status, data = api('GET', f'instrument/{instrument_id}', token=super_admin_token)
