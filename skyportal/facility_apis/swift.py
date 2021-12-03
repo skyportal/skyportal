5b2fa24637a073e25a62e71befa566636736dc27
Michael Coughlin <cough052@umn.edu>
2021-12-03 13:53:00 -0600

    Update skyportal/facility_apis/swift.py
    Co-authored-by: Ari Crellin-Quick <a.crellinquick@gmail.com>

--- skyportal/facility_apis/swift.py
+++ skyportal/facility_apis/swift.py
@@ -22,7 +22,6 @@ class UVOTRequest:
 
         Parameters
         ----------
-
         request: skyportal.models.FollowupRequest
             The request to add to the queue and the SkyPortal database.
 
