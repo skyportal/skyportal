8223bf8963a8ef8300f0458d0434a23cbfb72a57
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-01-06 15:28:53 +0100

    1.4

--- skyportal/models/schema.py
+++ skyportal/models/schema.py
@@ -107,7 +107,8 @@ def setup_schema():
     module.
 
     """
-    for class_ in _Base._decl_class_registry.values():
+    for mapper in _Base.registry.mappers:
+        class_ = mapper.class_
         if hasattr(class_, '__tablename__'):
             if class_.__name__.endswith('Schema'):
                 raise _ModelConversionError(
