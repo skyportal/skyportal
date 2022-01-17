6be848847291e7964d63ab0961b050c775396531
julietteBrt <juliette.barthet@gmail.com>
2022-01-17 13:58:32 +0100

    Modification of Next Shifts page

--- static/js/components/MyShifts.jsx
+++ static/js/components/MyShifts.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
+import {
+    makeStyles,
+    createMuiTheme,
+    MuiThemeProvider,
+    useTheme,
+  } from "@material-ui/core/styles";
+import MUIDataTable from "mui-datatables";
 
 
 import UserProfileInfo from "./UserProfileInfo";
@@ -37,14 +43,85 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
+const getMuiTheme = (theme) =>
+    createMuiTheme({
+    palette: theme.palette,
+    overrides: {
+        MUIDataTablePagination: {
+        toolbar: {
+            flexFlow: "row wrap",
+            justifyContent: "flex-end",
+            padding: "0.5rem 1rem 0",
+            [theme.breakpoints.up("sm")]: {
+            // Cancel out small screen styling and replace
+            padding: "0px",
+            paddingRight: "2px",
+            flexFlow: "row nowrap",
+            },
+        },
+        tableCellContainer: {
+            padding: "1rem",
+        },
+        selectRoot: {
+            marginRight: "0.5rem",
+            [theme.breakpoints.up("sm")]: {
+            marginLeft: "0",
+            marginRight: "2rem",
+            },
+        },
+        },
+    },
+});
 const MyShifts = () => {
   const profile = useSelector((state) => state.profile);
   const groups = useSelector((state) => state.groups.user);
   const classes = useStyles();
+  const theme = useTheme();
+  const columns = [
+        {
+        name: "start",
+        label: "Start",
+        options: {
+        },
+        },
+        {
+        name: "end",
+        label: "End",
+        options: {
+
+        },
+        },
+        {
+        name: "comments",
+        label: "Comments",
+        options: {
+        },
+        },
+        {
+        name: "other_shifters",
+        label: "Other Shifters",
+        options: {
+            },
+        },
+    ];
+
+    const options = {
+    search: true,
+    selectableRows: "none",
+    elevation: 0,
+    };
+    const shifts = [];
 
   return (
     <div>
         <Typography variant="h5">Your next shifts</Typography>
+            <MuiThemeProvider theme={getMuiTheme(theme)}>
+                <MUIDataTable
+                data={shifts}
+                options={options}
+                columns={columns}
+                />
+            </MuiThemeProvider>
       <div>
 
       </div>
