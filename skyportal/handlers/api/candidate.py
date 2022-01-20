a51c7c63d64be93662427c763357698bc40bd1b5
Joshua Bloom <profjsb@gmail.com>
2022-01-20 13:17:03 -0800

    SQLAlchemy 1.4.29 compat version (#2571)
    * Pin BL that requires SQLA v==1.4.29
    * Use SQLA 1.4-compatible registry mappers in schema
    * Refactor Values construct in photometry handler
    * Use select API instead of query in PhotometryHandler, PublicGroupHandler, conftest & fixtures
    * Add debug print statements
    * use a version that eralchemy which works with SQLA1.4
    * fix AttributeError on Base in schema.py
    * safe alias the group logic
    * query updates
    * use VALUES constructor in get_obj_id_values
    * use 1.4 func.jsonb_each instead of <1.4 formula
    * remove legacy code
    * pin sqla_1.4 from baselayer; fix queries (#2566)
    * Pin BL that requires SQLA v>=1.4.0
    * Use SQLA 1.4-compatible registry mappers in schema
    * Refactor Values construct in photometry handler
    * Use select API instead of query in PhotometryHandler, PublicGroupHandler, conftest & fixtures
    * fix AttributeError on Base in schema.py
    * safe alias the group logic
    * fix candidate.py
    * remove legacy code
    
    Co-authored-by: Ari Crellin-Quick <a.crellinquick@gmail.com>
    Co-authored-by: Michael Coughlin <michael.w.coughlin@gmail.com>
    
    * remove print statements from test suite
    * pin baselayer to allow SlugifiedStr
    * use a recent commit for testing migration
    * relax restrictions on react
    * Let migration manager handle table creation
    * Pin baselayer with app-creation-after-migration-manager fix
    * Revert "Let migration manager handle table creation"
    
    This reverts commit a3baeb72cb8cda964f4d78680fa603c7048301b5.
    
    * Pin to baselayer without migration-manager-table-creation
    
    * Pin baselayer in order to pin sqlalchemy to 1.4.29
    
    * robustify starlist tests to IRSA endpoint change; remove metadata warnings from schema.py
    
    Co-authored-by: Ari Crellin-Quick <a.crellinquick@gmail.com>
    Co-authored-by: Michael Coughlin <michael.w.coughlin@gmail.com>
    Co-authored-by: Stefan van der Walt <stefanv@berkeley.edu>

--- skyportal/handlers/api/candidate.py
+++ skyportal/handlers/api/candidate.py
@@ -9,11 +9,11 @@ import numpy as np
 
 from tornado.ioloop import IOLoop
 
+import sqlalchemy as sa
 from sqlalchemy.orm import joinedload
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.sql.expression import case, func, FromClause
-from sqlalchemy.ext.compiler import compiles
-from sqlalchemy.sql import column
+from sqlalchemy.sql.expression import case, func
+from sqlalchemy.sql import column, Values
 from sqlalchemy.types import Float, Boolean, String, Integer
 from marshmallow.exceptions import ValidationError
 
@@ -822,15 +822,16 @@ class CandidateHandler(BaseHandler):
             if "Page number out of range" in str(e):
                 return self.error("Page number out of range.")
             raise
+
         matching_source_ids = (
             Source.query_records_accessible_by(
                 self.current_user, columns=[Source.obj_id]
             )
-            .filter(Source.obj_id.in_([obj.id for obj in query_results["candidates"]]))
+            .filter(Source.obj_id.in_([obj.id for obj, in query_results["candidates"]]))
             .all()
         )
         candidate_list = []
-        for obj in query_results["candidates"]:
+        for (obj,) in query_results["candidates"]:
             with DBSession().no_autoflush:
                 obj.is_source = (obj.id,) in matching_source_ids
                 if obj.is_source:
@@ -1092,52 +1093,22 @@ def get_obj_id_values(obj_ids):
     values_table: `sqlalchemy.sql.expression.FromClause`
         The VALUES representation of the Obj IDs list.
     """
-    # https://github.com/sqlalchemy/sqlalchemy/wiki/PGValues
-    class _obj_id_values(FromClause):
-        named_with_column = True
-
-        def __init__(self, columns, *args, **kw):
-            self._column_args = columns
-            self.list = args
-            self.alias_name = self.name = kw.pop("alias_name", None)
-
-        def _populate_column_collection(self):
-            for c in self._column_args:
-                c._make_proxy(self)
-
-        @property
-        def _from_objects(self):
-            return [self]
-
-    # https://github.com/sqlalchemy/sqlalchemy/wiki/PGValues
-    @compiles(_obj_id_values)
-    def compile_values(element, compiler, asfrom=False, **kw):
-        columns = element.columns
-        v = "VALUES %s" % ", ".join(
-            "(%s)"
-            % ", ".join(
-                compiler.render_literal_value(elem, column.type)
-                for elem, column in zip(tup, columns)
-            )
-            for tup in element.list
+    values_table = (
+        Values(
+            column("id", String),
+            column("ordering", Integer),
         )
-        if asfrom:
-            if element.alias_name:
-                v = "(%s) AS %s (%s)" % (
-                    v,
-                    element.alias_name,
-                    (", ".join(c.name for c in element.columns)),
+        .data(
+            [
+                (
+                    obj_id,
+                    idx,
                 )
-            else:
-                v = "(%s)" % v
-        return v
-
-    values_table = _obj_id_values(
-        (column("id", String), column("ordering", Integer)),
-        *[(obj_id, idx) for idx, obj_id in enumerate(obj_ids)],
-        alias_name="values_table",
+                for idx, obj_id in enumerate(obj_ids)
+            ]
+        )
+        .alias("values_table")
     )
-
     return values_table
 
 
@@ -1252,19 +1223,33 @@ def grab_query_results(
             raise ValueError("Page number out of range.")
 
     items = []
-    query_options = [joinedload(Obj.thumbnails)] if include_thumbnails else []
-
     if len(obj_ids_in_page) > 0:
         # If there are no values, the VALUES statement above will cause a syntax error,
         # so only filter on the values if they exist
         obj_ids_values = get_obj_id_values(obj_ids_in_page)
-        items = (
-            DBSession()
-            .query(Obj)
-            .options(query_options)
-            .join(obj_ids_values, obj_ids_values.c.id == Obj.id)
-            .order_by(obj_ids_values.c.ordering)
-        )
+        if include_thumbnails:
+            items = (
+                DBSession()
+                .execute(
+                    sa.select(Obj)
+                    .options(joinedload(Obj.thumbnails))
+                    .join(obj_ids_values, obj_ids_values.c.id == Obj.id)
+                    .order_by(obj_ids_values.c.ordering)
+                )
+                .unique()
+                .all()
+            )
+        else:
+            items = (
+                DBSession()
+                .execute(
+                    sa.select(Obj)
+                    .join(obj_ids_values, obj_ids_values.c.id == Obj.id)
+                    .order_by(obj_ids_values.c.ordering)
+                )
+                .unique()
+                .all()
+            )
 
     info[items_name] = items
     return info
