54ddb3a8eb3ed1f815481740229c4b4ea07b73da
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-01-21 06:05:38 -0600

    add docstring

--- skyportal/facility_apis/fake_request.py
+++ skyportal/facility_apis/fake_request.py
@@ -2,6 +2,11 @@ from tornado import httputil
 
 
 class FakeApplication:
+    """Mimics the Application class of tornado.web.
+
+    Subclasses were minimum required to import the class.
+    """
+
     def __init__(
         self,
     ) -> None:
@@ -12,22 +17,26 @@ class FakeApplication:
 
 
 class FakeRequest(httputil.HTTPServerRequest):
+    """Mimics the HTTP request handler of tornado.web.
+
+    Subclasses were minimum required to import the class.
+    """
+
     def __init__(
         self,
-    ) -> None:
+    ):
         self.test = True
         super().__init__()
 
         class FakeConnection(object):
+            """Mimics the connection within the HTTP request handler."""
+
             def __init__(
                 self,
-            ) -> None:
+            ):
                 def set_close_callback(self):
                     return True
 
                 self.set_close_callback = set_close_callback
 
         self.connection = FakeConnection()
-
-
-fake_request = FakeRequest()
