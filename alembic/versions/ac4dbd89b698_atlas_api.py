00d881e816d7f5da7a1c810a51b0ccaa9e0b1523
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-01-26 16:38:08 -0600

    add default

--- alembic/versions/ac4dbd89b698_atlas_api.py
+++ alembic/versions/ac4dbd89b698_atlas_api.py
@@ -197,6 +197,7 @@ drop type "public"."followup_apis__old_version_to_be_dropped" cascade;
                 )
             ),
             nullable=False,
+            default=[],
         ),
     )
     # ### end Alembic commands ###
