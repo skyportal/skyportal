5995fa807b1d1b2e448a97ede0b27111945df02d
Guy Nir <guyynir@gmail.com>
2022-02-02 14:26:49 -0800

    update migration hash

--- alembic/versions/66ed6017e9ac_add_gcn_comments.py
+++ alembic/versions/66ed6017e9ac_add_gcn_comments.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '66ed6017e9ac'
-down_revision = 'f9611048498a'
+down_revision = '82f5a89fbe59'
 branch_labels = None
 depends_on = None
 
