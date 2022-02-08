dc0fc23ea17d76a3ca1c9730398bfbe54d89d217
Theodlz <theophile.dulaz@gmail.com>
2022-02-08 17:18:32 +0100

    Changes to be committed: 	modified:   static/js/components/SourceTableFilterForm.jsx

--- static/js/components/SourceTableFilterForm.jsx
+++ static/js/components/SourceTableFilterForm.jsx
@@ -209,6 +209,7 @@ const SourceTableFilterForm = ({ handleFilterSubmit }) => {
             Time Last Detected (UTC)
           </Typography>
           <TextField
+            disabled
             size="small"
             label="Last Detected After"
             name="startDate"
@@ -216,6 +217,7 @@ const SourceTableFilterForm = ({ handleFilterSubmit }) => {
             placeholder="2012-08-30T00:00:00"
           />
           <TextField
+            disabled
             size="small"
             label="Last Detected Before"
             name="endDate"
@@ -319,6 +321,7 @@ const SourceTableFilterForm = ({ handleFilterSubmit }) => {
             Peak Magnitude
           </Typography>
           <TextField
+            disabled
             size="small"
             label="Min"
             name="minPeakMagnitude"
@@ -329,6 +332,7 @@ const SourceTableFilterForm = ({ handleFilterSubmit }) => {
             inputRef={register}
           />
           <TextField
+            disabled
             size="small"
             label="Max"
             name="maxPeakMagnitude"
@@ -370,6 +374,7 @@ const SourceTableFilterForm = ({ handleFilterSubmit }) => {
             Latest Magnitude
           </Typography>
           <TextField
+            disabled
             size="small"
             label="Min"
             name="minLatestMagnitude"
@@ -380,6 +385,7 @@ const SourceTableFilterForm = ({ handleFilterSubmit }) => {
             inputRef={register}
           />
           <TextField
+            disabled
             size="small"
             label="Max"
             name="maxLatestMagnitude"
