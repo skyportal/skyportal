c32c6fcf93b0e0b071421a1cd63a7e901c55b786
Theodlz <theophile.dulaz@gmail.com>
2022-02-09 21:05:22 +0100

    Changes to be committed: 	new file:   static/js/components/NewShift.jsx 	new file:   static/js/components/ShiftPage.jsx 	new file:   static/js/ducks/shift.js 	new file:   static/js/ducks/shifts.js

---
+++ static/js/ducks/shift.js
@@ -0,0 +1,54 @@
+import messageHandler from "baselayer/MessageHandler";
+
+import * as API from "../API";
+import store from "../store";
+
+const REFRESH_SHIFT = "skyportal/REFRESH_SHIFT";
+
+const FETCH_SHIFT = "skyportal/FETCH_SHIFT";
+const FETCH_SHIFT_OK = "skyportal/FETCH_SHIFT_OK";
+
+const SUBMIT_SHIFT = "skyportal/SUBMIT_SHIFT";
+
+const DELETE_SHIFT = "skyportal/DELETE_SHIFT";
+
+export const fetchShift = (id) => API.GET(`/api/shift/${id}`, FETCH_SHIFT);
+
+export const submitShift = (run) => API.POST(`/api/shift`, SUBMIT_SHIFT, run);
+
+
+export function deleteShift(shiftID) {
+  return API.DELETE(
+    `/api/shift/${shiftID}`,
+    DELETE_SHIFT
+  );
+}
+
+
+// Websocket message handler
+messageHandler.add((actionType, payload, dispatch, getState) => {
+  const { shift } = getState();
+  if (actionType === REFRESH_SHIFT) {
+    const { shift_id } = payload;
+    if (shift_id === shift?.id) {
+      dispatch(fetchShift(shift_id));
+    }
+  }
+});
+
+const reducer = (state = null, action) => {
+  switch (action.type) {
+    case FETCH_SHIFT_OK: {
+      const shift = action.data;
+      return {
+        ...state,
+        ...shift,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+
+store.injectReducer("shift", reducer);
