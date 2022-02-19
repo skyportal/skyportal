020e1fa7c119852fedd129db118a20ab30709f5a
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-02-19 16:36:58 -0600

    Linting

--- static/js/components/GcnEventPage.jsx
+++ static/js/components/GcnEventPage.jsx
@@ -39,9 +39,6 @@ import GcnSelectionForm from "./GcnSelectionForm";
 import ObservationPlanRequestForm from "./ObservationPlanRequestForm";
 import ObservationPlanRequestLists from "./ObservationPlanRequestLists";
 
-import ObservationPlanRequestForm from "./ObservationPlanRequestForm";
-import ObservationPlanRequestLists from "./ObservationPlanRequestLists";
-
 dayjs.extend(relativeTime);
 dayjs.extend(utc);
 
