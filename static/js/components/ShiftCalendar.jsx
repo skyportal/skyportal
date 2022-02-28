59936c50359d18d6716004bb69e3ab5dcd238327
Theodlz <theophile.dulaz@gmail.com>
2022-02-28 16:24:42 +0100

    Changes to be committed: 	modified:   static/js/components/ShiftCalendar.jsx

--- static/js/components/ShiftCalendar.jsx
+++ static/js/components/ShiftCalendar.jsx
@@ -13,6 +13,7 @@ const allViews = Object.keys(Views).map((k) => Views[k]);
 let dispatch;
 let currentUser;
 const localizer = momentLocalizer(moment);
+let groups;
 
 function datestringToDate(shifts) {
   for (let i = 0; i < shifts.length; i += 1) {
@@ -22,13 +23,23 @@ function datestringToDate(shifts) {
   return shifts;
 }
 
-function handleSelectSlot({ start, end }) {
-  const name = window.prompt("New Event name");
-  const description = window.prompt("New Event description");
+async function handleSelectSlot({ start, end }) {
+  const name = window.prompt("New Shift name");
+  const description = window.prompt("New Shift description");
+  const group_ids = groups.map((group) => `   ${group.name}: ${group.id}\n`);
+  const group_id = await window.prompt(
+    `Choose shift group ID : \n ${group_ids}`
+  );
   const start_date = start.toISOString().replace("Z", "");
   const end_date = end.toISOString().replace("Z", "");
   dispatch(
-    shiftActions.submitShift({ name, description, start_date, end_date })
+    shiftActions.submitShift({
+      name,
+      description,
+      start_date,
+      end_date,
+      group_id,
+    })
   ).then((result) => {
     if (result.status === "success") {
       showNotification("Shift saved");
@@ -102,6 +113,7 @@ function MyCalendar({ shifts }) {
   shifts = datestringToDate(shifts);
   currentUser = useSelector((state) => state.profile);
   dispatch = useDispatch();
+  groups = useSelector((state) => state.groups.userAccessible);
   return (
     <div>
       <Calendar
