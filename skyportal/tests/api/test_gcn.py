8cee545fe1631e4ef00bdb8827c776b1411f6803
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-03-11 16:38:25 -0600

    fix tests

--- skyportal/tests/api/test_gcn.py
+++ skyportal/tests/api/test_gcn.py
@@ -64,6 +64,8 @@ def test_gcn_Fermi(super_admin_token, view_only_token):
     assert data['status'] == 'success'
 
     dateobs = "2018-01-16 00:36:53"
+    params = {"include2DMap": True}
+
     status, data = api('GET', f'gcn_event/{dateobs}', token=super_admin_token)
     assert status == 200
     data = data["data"]
@@ -75,6 +77,7 @@ def test_gcn_Fermi(super_admin_token, view_only_token):
         'GET',
         f'localization/{dateobs}/name/{skymap}',
         token=super_admin_token,
+        params=params,
     )
 
     data = data["data"]
