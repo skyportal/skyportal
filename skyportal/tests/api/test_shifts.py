3fab49b424dc33ff6d9eca4f514a5da963c00f07
Theodlz <theophile.dulaz@gmail.com>
2022-03-15 17:27:38 +0100

    added new api and flaky tests for the shifts

--- skyportal/tests/api/test_shifts.py
+++ skyportal/tests/api/test_shifts.py
@@ -8,17 +8,18 @@ def test_shift(public_group, super_admin_token, view_only_token, super_admin_use
         'group_id': public_group.id,
         'start_date': '3021-02-27T00:00:00',
         'end_date': '3021-07-20T00:00:00',
+        'description': 'the Night Shift',
+        'shift_admins': [super_admin_user.id],
     }
-
-    status, data = api('POST', 'shift', data=request_data, token=view_only_token)
+    status, data = api('POST', 'shifts', data=request_data, token=view_only_token)
     assert status == 401
     assert data['status'] == 'error'
 
-    status, data = api('POST', 'shift', data=request_data, token=super_admin_token)
+    status, data = api('POST', 'shifts', data=request_data, token=super_admin_token)
     assert status == 200
     assert data['status'] == 'success'
 
-    status, data = api('GET', f'shift/{public_group.id}', token=super_admin_token)
+    status, data = api('GET', f'shifts/{public_group.id}', token=super_admin_token)
     assert status == 200
     assert data['status'] == 'success'
 
