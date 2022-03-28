873b556985b1fbd1c4e5b202b27df1612dd151aa
Theodlz <theophile.dulaz@gmail.com>
2022-03-28 20:53:21 +0200

    linting of tests

--- skyportal/tests/api/test_shifts.py
+++ skyportal/tests/api/test_shifts.py
@@ -32,9 +32,9 @@ def test_shift(public_group, super_admin_token, view_only_token, super_admin_use
     assert any([request_data['start_date'] == s['start_date'] for s in data["data"]])
     assert any([request_data['end_date'] == s['end_date'] for s in data["data"]])
 
-    assert any([
-        len([s for s in s['users'] if s['id'] == super_admin_user.id])
-        == 1
-        for s in data['data']
-    ])
-    
+    assert any(
+        [
+            len([s for s in s['users'] if s['id'] == super_admin_user.id]) == 1
+            for s in data['data']
+        ]
+    )
