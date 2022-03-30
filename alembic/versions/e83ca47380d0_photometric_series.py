87b500d6c53e61a34ab64af608e0b4d3fafcf261
Guy Nir <guyynir@gmail.com>
2022-03-30 14:44:18 -0700

    fix alembic downrevision

--- alembic/versions/e83ca47380d0_photometric_series.py
+++ alembic/versions/e83ca47380d0_photometric_series.py
@@ -11,7 +11,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = 'e83ca47380d0'
-down_revision = '5df4b399aae2'
+down_revision = '1a43ee08a734'
 branch_labels = None
 depends_on = None
 
