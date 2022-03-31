8be34d4bac0cc671bcaad05fd64880f675df6178
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-03-31 13:20:05 -0500

    update head

--- alembic/versions/111d0c968332_synth_phot.py
+++ alembic/versions/111d0c968332_synth_phot.py
@@ -1,7 +1,7 @@
 """Synthetic photometry
 
 Revision ID: 111d0c968332
-Revises: 1a43ee08a734
+Revises: 0bc55992eb50
 Create Date: 2022-03-30 11:46:57.907566
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '111d0c968332'
-down_revision = '1a43ee08a734'
+down_revision = '0bc55992eb50'
 branch_labels = None
 depends_on = None
 
