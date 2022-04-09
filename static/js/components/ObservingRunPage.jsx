784e84d132e7413299b55b88c9f2827d5c4320d6
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-04-08 19:27:48 -0500

    Update static/js/components/ObservingRunPage.jsx
    Co-authored-by: Jada Lilleboe <82007190+jadalilleboe@users.noreply.github.com>

--- static/js/components/ObservingRunPage.jsx
+++ static/js/components/ObservingRunPage.jsx
@@ -69,7 +69,7 @@ const ObservingRunList = ({ observingRuns }) => {
 
   const [displayAll, setDisplayAll] = useState(false);
 
-  const toggleCheckbox = () => {
+  const toggleDisplayAllCheckbox = () => {
     setChecked(!checked);
   };
 
