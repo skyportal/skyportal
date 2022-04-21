39882fbb633540a4c6498b70767833d27e0ff42b
Theophile du Laz <theophile.dulaz@gmail.com>
2022-04-21 20:22:03 +0200

    Telescope map (#2835)
    Add a world map showing where the Telescopes are

---
+++ static/js/components/TelescopeMap.jsx
@@ -0,0 +1,184 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+  useZoomPan,
+} from "react-simple-maps";
+
+import world_map from "../../images/maps/world-110m.json";
+
+let dispatch;
+const width = 700;
+const height = 475;
+
+function CustomZoomableGroup({ children, ...restProps }) {
+  const { mapRef, transformString, position } = useZoomPan(restProps);
+  return (
+    <g ref={mapRef}>
+      <rect width={width} height={height} fill="transparent" />
+      <g transform={transformString}>{children(position)}</g>
+    </g>
+  );
+}
+function setCurrentTelescopes(currentTelescopes) {
+  const currentTelescopeMenu = "Telescope List";
+  dispatch({
+    type: "skyportal/CURRENT_TELESCOPES_AND_MENU",
+    data: { currentTelescopes, currentTelescopeMenu },
+  });
+}
+
+function telescopelabel(nestedTelescope) {
+  return nestedTelescope.telescopes
+    .map((telescope) => telescope.nickname)
+    .join(" / ");
+}
+
+function TelescopeMarker({ nestedTelescope, position }) {
+  return (
+    <Marker
+      id="telescope_marker"
+      key={`${nestedTelescope.lon},${nestedTelescope.lat}`}
+      coordinates={[nestedTelescope.lon, nestedTelescope.lat]}
+      onClick={() => setCurrentTelescopes(nestedTelescope)}
+    >
+      <circle r={6.5 / position.k} fill="#457B9C" />
+      <text
+        id="telescopes_label"
+        textAnchor="middle"
+        fontSize={10 / position.k}
+        y={-10 / position.k}
+      >
+        {telescopelabel(nestedTelescope)}
+      </text>
+    </Marker>
+  );
+}
+
+function normalizeLongitudeDiff(alpha, beta) {
+  return 180 - Math.abs(Math.abs(alpha - beta) - 180);
+}
+
+function normalizeLatitudeDiff(alpha, beta) {
+  return Math.abs(alpha - beta);
+}
+
+const TelescopeMap = ({ telescopes }) => {
+  const filteredTelescopes = telescopes.filter(
+    (telescope) => telescope.fixed_location
+  );
+  const nestedTelescopes = [];
+  for (let i = 0; i < filteredTelescopes.length; i += 1) {
+    if (i === 0) {
+      nestedTelescopes.push({
+        lat: filteredTelescopes[i].lat,
+        lon: filteredTelescopes[i].lon,
+        telescopes: [filteredTelescopes[i]],
+      });
+    } else {
+      for (let j = 0; j < nestedTelescopes.length; j += 1) {
+        if (
+          Math.abs(
+            normalizeLatitudeDiff(
+              filteredTelescopes[i].lat,
+              nestedTelescopes[j].lat
+            )
+          ) < 1 &&
+          Math.abs(
+            normalizeLongitudeDiff(
+              filteredTelescopes[i].lon,
+              nestedTelescopes[j].lon
+            )
+          ) < 2
+        ) {
+          nestedTelescopes[j].telescopes.push(filteredTelescopes[i]);
+          break;
+        } else if (j === nestedTelescopes.length - 1) {
+          nestedTelescopes.push({
+            lat: filteredTelescopes[i].lat,
+            lon: filteredTelescopes[i].lon,
+            telescopes: [filteredTelescopes[i]],
+          });
+          break;
+        }
+      }
+    }
+  }
+
+  dispatch = useDispatch();
+  return (
+    <ComposableMap width={width} height={height}>
+      <CustomZoomableGroup center={[0, 0]}>
+        {(position) => (
+          <>
+            <Geographies geography={world_map}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill="#EAEAEC"
+                    stroke="#D6D6DA"
+                  />
+                ))
+              }
+            </Geographies>
+            {nestedTelescopes.map(
+              (nestedTelescope) =>
+                nestedTelescope.lon &&
+                nestedTelescope.lat && (
+                  <TelescopeMarker
+                    nestedTelescope={nestedTelescope}
+                    position={position}
+                  />
+                )
+            )}
+          </>
+        )}
+      </CustomZoomableGroup>
+    </ComposableMap>
+  );
+};
+
+TelescopeMap.propTypes = {
+  telescopes: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      nickname: PropTypes.string,
+      lat: PropTypes.number,
+      lon: PropTypes.number,
+      fixed_location: PropTypes.bool,
+    })
+  ).isRequired,
+};
+
+TelescopeMarker.propTypes = {
+  nestedTelescope: PropTypes.shape({
+    lat: PropTypes.number,
+    lon: PropTypes.number,
+    telescopes: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        nickname: PropTypes.string,
+        lat: PropTypes.number,
+        lon: PropTypes.number,
+        fixed_location: PropTypes.bool,
+      })
+    ),
+  }).isRequired,
+  position: PropTypes.shape({
+    k: PropTypes.number,
+  }).isRequired,
+};
+
+CustomZoomableGroup.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default TelescopeMap;
---
+++ static/js/components/TelescopePageDesktop.jsx
@@ -0,0 +1,148 @@
+import React, { lazy, Suspense } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import NewTelescope from "./NewTelescope";
+import TelescopeInfo from "./TelescopeInfo";
+// lazy import the TelescopeMap component
+const TelescopeMap = lazy(() => import("./TelescopeMap"));
+
+let dispatch;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+    maxHeight: "90%",
+    overflowY: "auto",
+  },
+  paperContent: {
+    padding: "1rem",
+  },
+  menu: {
+    display: "flex",
+    direction: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  selectedMenu: {
+    height: "3rem",
+    backgroundColor: "lightblue",
+    fontSize: "1.2rem",
+  },
+  nonSelectedMenu: {
+    height: "3rem",
+    fontSize: "1.2rem",
+  },
+}));
+
+export function telescopeTitle(telescope) {
+  if (!telescope?.name) {
+    return (
+      <div>
+        <CircularProgress color="secondary" />
+      </div>
+    );
+  }
+
+  const result = `${telescope?.nickname}`;
+  return result;
+}
+
+export function telescopeInfo(telescope) {
+  if (!telescope?.name) {
+    return (
+      <div>
+        <CircularProgress color="secondary" />
+      </div>
+    );
+  }
+
+  const array = [
+    ...(telescope?.lat ? [`Latitude: ${telescope.lat}`] : []),
+    ...(telescope?.lon ? [`Longitude: ${telescope.lon}`] : []),
+    ...(telescope?.elevation ? [`Elevation: ${telescope.elevation}`] : []),
+  ];
+
+  // eslint-disable-next-line prefer-template
+  const result = "( " + array.join(" / ") + " )";
+
+  return result;
+}
+
+const TelescopePage = () => {
+  dispatch = useDispatch();
+  const { telescopeList } = useSelector((state) => state.telescopes);
+  const currentTelescopeMenu = useSelector(
+    (state) => state.telescope.currentTelescopeMenu
+  );
+
+  function setSelectedMenu(currentSelectedTelescopeMenu) {
+    const currentTelescopes = null;
+    dispatch({
+      type: "skyportal/CURRENT_TELESCOPES_AND_MENU",
+      data: {
+        currentTelescopes,
+        currentTelescopeMenu: currentSelectedTelescopeMenu,
+      },
+    });
+  }
+
+  const classes = useStyles();
+
+  function isMenuSelected(menu) {
+    let style;
+    if (menu === currentTelescopeMenu) {
+      style = classes.selectedMenu;
+    } else {
+      style = classes.nonSelectedMenu;
+    }
+    return style;
+  }
+  return (
+    <Suspense
+      fallback={
+        <div>
+          <CircularProgress color="secondary" />
+        </div>
+      }
+    >
+      <Grid container spacing={3}>
+        <Grid item md={8} sm={12}>
+          <Paper className={classes.paperContent}>
+            <TelescopeMap telescopes={telescopeList} />
+          </Paper>
+        </Grid>
+        <Grid item md={4} sm={12}>
+          <Paper className={classes.menu}>
+            <Button
+              id="telescope-list"
+              onClick={() => setSelectedMenu("Telescope List")}
+              className={isMenuSelected("Telescope List")}
+            >
+              Telescope List
+            </Button>
+            <Button
+              id="new-telescope"
+              onClick={() => setSelectedMenu("New Telescope")}
+              className={isMenuSelected("New Telescope")}
+            >
+              New Telescope
+            </Button>
+          </Paper>
+          <Paper className={classes.paperContent}>
+            {currentTelescopeMenu === "Telescope List" ? (
+              <TelescopeInfo />
+            ) : (
+              <NewTelescope />
+            )}
+          </Paper>
+        </Grid>
+      </Grid>
+    </Suspense>
+  );
+};
+
+export default TelescopePage;
