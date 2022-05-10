dd5b07fb42fe53e48163a31d560949d50e858483
Theodlz <theophile.dulaz@gmail.com>
2022-05-10 09:06:56 +0900

    added telescope with non fixed location

--- static/js/components/TelescopeInfo.jsx
+++ static/js/components/TelescopeInfo.jsx
@@ -47,17 +47,27 @@ const useStyles = makeStyles(() => ({
     padding: "0",
     margin: "0",
   },
-  canObserve: {
+  canObserveFixed: {
     height: "1rem",
     width: "1rem",
     backgroundColor: "#0c1445",
     borderRadius: "50%",
   },
-  cannotObserve: {
+  cannotObserveFixed: {
     height: "1rem",
     width: "1rem",
     backgroundColor: "#f9d71c",
     borderRadius: "50%",
+  },
+  canObserveNonFixed: {
+    height: "1rem",
+    width: "1rem",
+    backgroundColor: "#0c1445",
+  },
+  cannotObserveNonFixed: {
+    height: "1rem",
+    width: "1rem",
+    backgroundColor: "#f9d71c",
   },
   telescope_time: {
     display: "flex",
@@ -76,23 +86,37 @@ const TelescopeInfo = () => {
   return currentTelescopes ? (
     <List className={classes.root}>
       {currentTelescopes.telescopes.map((telescope) => (
-        <>
+        <div key={`${telescope.id}_list_item`}>
           <ListItem
             id={`${telescope.name}_info`}
             className={classes.listItem}
-            key={telescope.id}
+            key={`${telescope.id}_info`}
           >
-            <div className={classes.telescope_header}>
-              {telescope.is_night_astronomical ? (
-                <span className={classes.canObserve} />
-              ) : (
-                <span className={classes.cannotObserve} />
+            <div
+              className={classes.telescope_header}
+              key={`${telescope.id}_header`}
+            >
+              {telescope.is_night_astronomical && telescope.fixed_location && (
+                <span className={classes.canObserveFixed} />
               )}
+              {telescope.is_night_astronomical && !telescope.fixed_location && (
+                <span className={classes.canObserveNonFixed} />
+              )}
+              {!telescope.is_night_astronomical && telescope.fixed_location && (
+                <span className={classes.cannotObserveFixed} />
+              )}
+              {!telescope.is_night_astronomical &&
+                !telescope.fixed_location && (
+                  <span className={classes.cannotObserveNonFixed} />
+                )}
               <h2 className={classes.h2}>
                 {telescope.name} ({telescope.nickname})
               </h2>
             </div>
-            <div className={classes.telescope_time}>
+            <div
+              className={classes.telescope_time}
+              key={`${telescope.id}_time`}
+            >
               <i>
                 Next Sunrise (Astronomical):{" "}
                 {telescope.next_twilight_morning_astronomical.slice(0, -4)} UTC
@@ -102,31 +126,48 @@ const TelescopeInfo = () => {
                 {telescope.next_twilight_evening_astronomical.slice(0, -4)} UTC
               </i>
             </div>
-            {telescope.robotic ? (
-              <h3 className={classes.h3}>Robotic : Yes</h3>
-            ) : (
-              <h3 className={classes.h3}>Robotic : No</h3>
-            )}
-            <h3 className={classes.h3}>
+            <h3 className={classes.h3} key={`${telescope.id}_diameter`}>
               Diameter :{" "}
               {telescope.diameter ? telescope.diameter.toFixed(1) : null}
             </h3>
-            <h3 className={classes.h3}>
+            <h3 className={classes.h3} key={`${telescope.id}_location`}>
               Location : {telescope.lat ? telescope.lat.toFixed(4) : null},{" "}
               {telescope.lon ? telescope.lon.toFixed(4) : null}
             </h3>
-            <h3 className={classes.h3}>
+            <h3 className={classes.h3} key={`${telescope.id}_elevation`}>
               Elevation :{" "}
               {telescope.elevation ? telescope.elevation.toFixed(1) : null}
             </h3>
+            {telescope.robotic ? (
+              <h3 className={classes.h3} key={`${telescope.id}_robotic`}>
+                Robotic : Yes
+              </h3>
+            ) : (
+              <h3 className={classes.h3} key={`${telescope.id}_robotic`}>
+                Robotic : No
+              </h3>
+            )}
+            {telescope.fixed_location ? (
+              <h3 className={classes.h3} key={`${telescope.id}_fixed_location`}>
+                Fixed Location : Yes
+              </h3>
+            ) : (
+              <h3 className={classes.h3} key={`${telescope.id}_fixed_location`}>
+                Fixed Location : No
+              </h3>
+            )}
             {telescope.skycam_link && (
-              <a className={classes.a} href={telescope.skycam_link}>
+              <a
+                className={classes.a}
+                href={telescope.skycam_link}
+                key={`${telescope.id}_skycam_link`}
+              >
                 skycam link
               </a>
             )}
           </ListItem>
           <Divider />
-        </>
+        </div>
       ))}
     </List>
   ) : (
