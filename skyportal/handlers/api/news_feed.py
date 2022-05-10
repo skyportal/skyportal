546257c3a0fa5b981ed0191b0e8b3e953c4157de
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-05-10 16:31:47 -0500

    Stefan's newsfeed ideas (#2955)
    This PR adds improvements to the newsfeed checking including simplified parameter setting and an extra test.

--- skyportal/handlers/api/news_feed.py
+++ skyportal/handlers/api/news_feed.py
@@ -56,24 +56,19 @@ class NewsFeedHandler(BaseHandler):
                 schema: Error
         """
 
-        if hasattr(self.current_user, 'preferences') and isinstance(
-            self.current_user.preferences, dict
-        ):
-            preferences = self.current_user.preferences
-        else:
-            preferences = {}
+        preferences = getattr(self.current_user, 'preferences', None) or {}
+        n_items_query = self.get_query_argument("numItems", None)
+        if n_items_query is not None:
+            n_items_query = int(n_items_query)
+        n_items_feed = preferences.get('newsFeed', {}).get('numItems', None)
+        if n_items_feed is not None:
+            n_items_feed = int(n_items_feed)
 
-        n_items = self.get_query_argument("numItems", None)
-        if n_items is None:
-            if 'newsFeed' in preferences and 'numItems' in preferences['newsFeed']:
-                n_items = min(
-                    int(preferences['newsFeed']['numItems']), DEFAULT_NEWSFEED_ITEMS
-                )
-            else:
-                n_items = DEFAULT_NEWSFEED_ITEMS
+        n_items_list = [n_items_query, n_items_feed]
+        if all(v is None for v in n_items_list):
+            n_items = DEFAULT_NEWSFEED_ITEMS
         else:
-            n_items = int(n_items)
-
+            n_items = max(x for x in n_items_list if x is not None)
         if n_items > MAX_NEWSFEED_ITEMS:
             return self.error(
                 f'numItems should be no larger than {MAX_NEWSFEED_ITEMS}.'
