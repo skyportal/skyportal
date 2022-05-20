c917f76a0fb50ed3febbe6d1a6fb2c8940e0b10a
Stefan van der Walt <stefanv@berkeley.edu>
2022-05-20 14:13:33 -0700

    Fail tests if geckodriver is missing

--- skyportal/tests/conftest.py
+++ skyportal/tests/conftest.py
@@ -1,6 +1,7 @@
 """Test fixture configuration."""
 
 import os
+import shutil
 import uuid
 from datetime import datetime, timedelta
 from pathlib import Path
@@ -55,6 +56,14 @@ from skyportal.tests.fixtures import (
 )
 from skyportal.tests.fixtures import TMP_DIR  # noqa: F401
 from skyportal.models import Obj
+
+
+if shutil.which('geckodriver') is None:
+    raise RuntimeError(
+        'Geckodriver needs to be installed for browser automation.\n'
+        'See https://github.com/mozilla/geckodriver/releases'
+    )
+
 
 # Add a "test factory" User so that all factory-generated comments have a
 # proper author, if it doesn't already exist (the user may already be in
