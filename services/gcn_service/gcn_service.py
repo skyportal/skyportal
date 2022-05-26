76893c596b1aa04598c7d15b6638a4361783bcf1
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-05-26 15:15:17 -0500

    Refactor GcnEvent Handler

--- services/gcn_service/gcn_service.py
+++ services/gcn_service/gcn_service.py
@@ -1,23 +1,8 @@
-import yaml
-
 import gcn
 
 from baselayer.log import make_log
 
-from skyportal.tests import api
-
-
-def get_token():
-    try:
-        token = yaml.load(open('.tokens.yaml'), Loader=yaml.Loader)['INITIAL_ADMIN']
-        print('Token loaded from `.tokens.yaml`')
-        return token
-    except (FileNotFoundError, TypeError, KeyError):
-        print('Error: no token specified, and no suitable token found in .tokens.yaml')
-        return None
-
-
-admin_token = get_token()
+from skyportal.handlers.api.gcn import post_gcnevent
 
 
 @gcn.include_notice_types(
@@ -29,16 +14,16 @@ admin_token = get_token()
     gcn.NoticeType.LVC_INITIAL,
     gcn.NoticeType.LVC_UPDATE,
     gcn.NoticeType.LVC_RETRACTION,
-    # gcn.NoticeType.LVC_TEST,
+    gcn.NoticeType.LVC_TEST,
     gcn.NoticeType.AMON_ICECUBE_COINC,
     gcn.NoticeType.AMON_ICECUBE_HESE,
     gcn.NoticeType.ICECUBE_ASTROTRACK_GOLD,
     gcn.NoticeType.ICECUBE_ASTROTRACK_BRONZE,
 )
 def handle(payload, root):
-    response_status, data = api(
-        'POST', 'gcn_event', data={'xml': payload}, token=admin_token
-    )
+
+    user_id = 1
+    post_gcnevent(payload, user_id)
 
 
 if __name__ == "__main__":
