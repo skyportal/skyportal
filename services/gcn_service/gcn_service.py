30cd32698d8d9d2f8bac203266509dd43f6f06e5
Theodlz <theophile.dulaz@gmail.com>
2022-05-31 17:11:14 +0900

    added configuration for the gcn notice types

--- services/gcn_service/gcn_service.py
+++ services/gcn_service/gcn_service.py
@@ -12,26 +12,15 @@ env, cfg = load_env()
 init_db(**cfg['database'])
 
 
-@gcn.include_notice_types(
-    gcn.NoticeType.FERMI_GBM_FLT_POS,
-    gcn.NoticeType.FERMI_GBM_GND_POS,
-    gcn.NoticeType.FERMI_GBM_FIN_POS,
-    gcn.NoticeType.FERMI_GBM_SUBTHRESH,
-    gcn.NoticeType.LVC_PRELIMINARY,
-    gcn.NoticeType.LVC_INITIAL,
-    gcn.NoticeType.LVC_UPDATE,
-    gcn.NoticeType.LVC_RETRACTION,
-    # gcn.NoticeType.LVC_TEST,
-    gcn.NoticeType.AMON_ICECUBE_COINC,
-    gcn.NoticeType.AMON_ICECUBE_HESE,
-    gcn.NoticeType.ICECUBE_ASTROTRACK_GOLD,
-    gcn.NoticeType.ICECUBE_ASTROTRACK_BRONZE,
-)
 def handle(payload, root):
-
-    user_id = 1
-    with DBSession() as session:
-        post_gcnevent(payload, user_id, session)
+    notice_type = gcn.get_notice_type(root)
+    if notice_type in [
+        getattr(gcn.notice_types, notice_type)
+        for notice_type in cfg["gcn_notice_types"]
+    ]:
+        user_id = 1
+        with DBSession() as session:
+            post_gcnevent(payload, user_id, session)
 
 
 if __name__ == "__main__":
