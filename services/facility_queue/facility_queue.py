bd8fc7cda0cb1ad27f78f7896967dedb04d0b810
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-06-07 14:54:24 +0200

    Try to clean up sessions

--- services/facility_queue/facility_queue.py
+++ services/facility_queue/facility_queue.py
@@ -14,7 +14,6 @@ import tornado.escape
 import json
 import requests
 
-from sqlalchemy.orm import sessionmaker, scoped_session
 import sqlalchemy as sa
 
 from baselayer.app.models import init_db
@@ -34,8 +33,6 @@ request_session.trust_env = (
     False  # Otherwise pre-existing netrc config will override auth headers
 )
 
-Session = scoped_session(sessionmaker(bind=DBSession.session_factory.kw["bind"]))
-
 WAIT_TIME_BETWEEN_QUERIES = timedelta(seconds=120)
 
 
@@ -43,82 +40,83 @@ class FacilityQueue(asyncio.Queue):
     async def load_from_db(self):
         # Load items from database into queue
 
-        session = Session()
-        requests = (
-            session.query(FacilityTransactionRequest)
-            .where(FacilityTransactionRequest.status != "complete")
-            .all()
-        )
-        for req in requests:
-            await self.put(req)
+        with DBSession() as session:
+            requests = (
+                session.query(FacilityTransactionRequest)
+                .where(FacilityTransactionRequest.status != "complete")
+                .all()
+            )
+            for req in requests:
+                await self.put(req.id)
 
     async def service(self):
         while True:
-            req = await queue.get()
 
-            dt = datetime.utcnow() - req.last_query
-            if dt < WAIT_TIME_BETWEEN_QUERIES:
-                await self.put(req)
-            else:
-                print(f"Executing request {req.id}")
-                session = Session()
-                followup_request = session.query(FollowupRequest).get(
-                    req.followup_request_id
-                )
-                instrument = followup_request.allocation.instrument
-                altdata = followup_request.allocation.altdata
-
-                response = request_session.request(
-                    req.method,
-                    req.endpoint,
-                    json=req.data,
-                    params=req.params,
-                    headers=req.headers,
-                )
-
-                if instrument.name == "ATLAS":
-                    from skyportal.facility_apis.atlas import commit_photometry
-
-                    if response.status_code == 200:
-                        try:
-                            json_response = response.json()
-                        except Exception:
-                            raise ('No JSON data returned in request')
-
-                        if json_response['finishtimestamp']:
-                            req.status = "Committing photometry to database"
-                            commit_photometry(
-                                json_response,
-                                altdata,
-                                followup_request.id,
-                                instrument.id,
-                                followup_request.requester.id,
-                            )
-                            req.status = 'complete'
-                            session.add(req)
-                            print(f"Request {req.id} completed")
-
-                        elif json_response['starttimestamp']:
-                            followup_request.status = f"Task is running (started at {json_response['starttimestamp']})"
-                            req.last_query = datetime.utcnow()
-                            session.add(req)
-                            await self.put(req)
-                        else:
-                            followup_request.status = f"Waiting for job to start (queued at {json_response['timestamp']})"
-                            req.last_query = datetime.utcnow()
-                            session.add(req)
-                            await self.put(req)
-                    else:
-                        followup_request.status = f'error: {response.content}'
-
-                    session.add(followup_request)
-                    session.commit()
-
+            req_id = await queue.get()
+            with DBSession() as session:
+                req = session.query(FacilityTransactionRequest).get(req_id)
+                dt = datetime.utcnow() - req.last_query
+                if dt < WAIT_TIME_BETWEEN_QUERIES:
+                    await self.put(req.id)
                 else:
-                    raise ValueError(f'API for {instrument.name} unknown')
+                    print(f"Executing request {req.id}")
+                    followup_request = session.query(FollowupRequest).get(
+                        req.followup_request_id
+                    )
+                    instrument = followup_request.allocation.instrument
+                    altdata = followup_request.allocation.altdata
 
-            # Pause between requests
-            time.sleep(10)
+                    response = request_session.request(
+                        req.method,
+                        req.endpoint,
+                        json=req.data,
+                        params=req.params,
+                        headers=req.headers,
+                    )
+
+                    if instrument.name == "ATLAS":
+                        from skyportal.facility_apis.atlas import commit_photometry
+
+                        if response.status_code == 200:
+                            try:
+                                json_response = response.json()
+                            except Exception:
+                                raise ('No JSON data returned in request')
+
+                            if json_response['finishtimestamp']:
+                                req.status = "Committing photometry to database"
+                                commit_photometry(
+                                    json_response,
+                                    altdata,
+                                    followup_request.id,
+                                    instrument.id,
+                                    followup_request.requester.id,
+                                )
+                                req.status = 'complete'
+                                session.add(req)
+                                print(f"Request {req.id} completed")
+
+                            elif json_response['starttimestamp']:
+                                followup_request.status = f"Task is running (started at {json_response['starttimestamp']})"
+                                req.last_query = datetime.utcnow()
+                                session.add(req)
+                                await self.put(req)
+                            else:
+                                followup_request.status = f"Waiting for job to start (queued at {json_response['timestamp']})"
+                                req.last_query = datetime.utcnow()
+                                session.add(req)
+                                await self.put(req)
+                        else:
+                            followup_request.status = f'error: {response.content}'
+
+                        session.add(followup_request)
+                        session.commit()
+
+                    else:
+                        raise ValueError(f'API for {instrument.name} unknown')
+
+                # Pause between requests
+                time.sleep(10)
 
 
 queue = FacilityQueue()
@@ -149,51 +147,52 @@ class QueueHandler(tornado.web.RequestHandler):
                 }
             )
 
-        session = Session()
+        with DBSession() as session:
 
-        current_req = session.execute(
-            sa.select(FacilityTransactionRequest).where(
-                FacilityTransactionRequest.followup_request_id
-                == req.followup_request_id
-            )
-        ).first()
-        if current_req is not None:
-            self.write(
+            current_req = session.execute(
+                sa.select(FacilityTransactionRequest).where(
+                    FacilityTransactionRequest.followup_request_id
+                    == req.followup_request_id
+                )
+            ).first()
+            if current_req is not None:
+                self.set_status(400)
+                return self.write(
+                    {
+                        "status": "error",
+                        "message": f"Facility request {req.followup_request_id} already in the queue and/or complete",
+                        "data": {"followup_request_id": req.followup_request_id},
+                    }
+                )
+
+            endpoint_req = session.execute(
+                sa.select(FacilityTransactionRequest).where(
+                    FacilityTransactionRequest.endpoint == req.endpoint
+                )
+            ).first()
+            if endpoint_req is not None:
+                self.set_status(400)
+                return self.write(
+                    {
+                        "status": "error",
+                        "message": f"Facility request {req.followup_request_id} already reaches same endpoint {req.endpoint} as {endpoint_req.id}",
+                        "data": {"followup_request_id": req.followup_request_id},
+                    }
+                )
+
+            session.add(req)
+            session.commit()
+
+            await queue.put(req.id)
+
+            self.set_status(200)
+            return self.write(
                 {
-                    "status": "error",
-                    "message": f"Facility request {req.followup_request_id} already in the queue and/or complete",
-                    "data": {"followup_request_id": req.followup_request_id},
+                    "status": "success",
+                    "message": "Facility request accepted into queue",
+                    "data": {"queue_length": queue.qsize()},
                 }
             )
-            return
-
-        endpoint_req = session.execute(
-            sa.select(FacilityTransactionRequest).where(
-                FacilityTransactionRequest.endpoint == req.endpoint
-            )
-        ).first()
-        if endpoint_req is not None:
-            self.write(
-                {
-                    "status": "error",
-                    "message": f"Facility request {req.followup_request_id} already reaches same endpoint {req.endpoint} as {endpoint_req.id}",
-                    "data": {"followup_request_id": req.followup_request_id},
-                }
-            )
-            return
-
-        session.add(req)
-        session.commit()
-
-        await queue.put(req)
-
-        self.write(
-            {
-                "status": "success",
-                "message": "Facility request accepted into queue",
-                "data": {"queue_length": queue.qsize()},
-            }
-        )
 
 
 if __name__ == "__main__":
