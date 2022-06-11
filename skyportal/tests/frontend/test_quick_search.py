e6d2619461c9928063df10277ab9a2e3c45ae61c
Stephen <43051770+StephenTheCoder@users.noreply.github.com>
2022-06-11 12:31:35 -0500

    Update test_quick_search.py

--- skyportal/tests/frontend/test_quick_search.py
+++ skyportal/tests/frontend/test_quick_search.py
@@ -18,19 +18,3 @@ def test_quick_search(
     driver.wait_for_xpath('//*[@id="quick-search-bar"]').send_keys("invalid_source_id")
     driver.wait_for_xpath('//*[text()="No matching sources."]')
     
-def test_user_search(
-    driver,
-    regular_user,
-    public_source,
-    public_group,
-):
-    driver.get(f"/become_user/{regular_user.id}")
-    driver.get("/")
-    driver.wait_for_xpath('//*[@id="quick-search-bar"]').send_keys(public_source.id)
-    driver.click_xpath(f'//*[@id="quickSearchLinkTo{public_source.id}"]')
-    # Should be redirected to source page; check for elements that should render
-    driver.wait_for_xpath(f'//div[text()="{public_source.id}"]')
-    driver.wait_for_xpath(f'//span[text()="{public_group.name}"]')
-
-    driver.wait_for_xpath('//*[@id="quick-search-bar"]').send_keys("invalid_source_id")
-    driver.wait_for_xpath('//*[text()="No matching sources."]')
