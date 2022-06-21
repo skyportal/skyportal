98c9105a04dd0ddaaabf3e1aab34c7e7642501a1
Theophile du Laz <theophile.dulaz@gmail.com>
2022-06-21 15:52:08 +0900

    Update 336795744e40_user_affiliations.py

--- alembic/versions/336795744e40_user_affiliations.py
+++ alembic/versions/336795744e40_user_affiliations.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '336795744e40'
-down_revision = '438a795cbd15'
+down_revision = 'a75237f48f29'
 branch_labels = None
 depends_on = None
 
