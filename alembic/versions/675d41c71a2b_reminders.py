1a6972fc7e0dda55bd65e03a3a41067ae1b8f72f
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-06-28 08:56:07 -0500

    alembic

--- alembic/versions/675d41c71a2b_reminders.py
+++ alembic/versions/675d41c71a2b_reminders.py
@@ -1,7 +1,7 @@
 """Reminders migration
 
 Revision ID: 675d41c71a2b
-Revises: c8063723c76e
+Revises: f44a21fe8375
 Create Date: 2022-06-16 14:56:25.862884
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '675d41c71a2b'
-down_revision = 'c8063723c76e'
+down_revision = 'f44a21fe8375'
 branch_labels = None
 depends_on = None
 
