f82dd4625791bd7f39ca08514635cf77435b1f78
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-07-13 17:09:54 -0500

    Add an endpoint piechart

--- tools/nginx_parse.py
+++ tools/nginx_parse.py
@@ -11,6 +11,7 @@ import fire
 
 font = {'family': 'normal', 'weight': 'bold', 'size': 24}
 matplotlib.rc('font', **font)
+matplotlib.use("Agg")
 
 
 def create_ip_chart(filenames, outfile='requests.pdf'):
@@ -20,6 +21,47 @@ def create_ip_chart(filenames, outfile='requests.pdf'):
     outfile: str
         Path to the output file
     """
+
+    verbs = ['GET', 'PUT', 'POST', 'PATCH', 'PUT', 'HEAD', 'DELETE']
+    endpoints = [
+        '/index',
+        '/baselayer',
+        '/static',
+        '/source',
+        '/complete',
+        '/owa',
+        '/api/allocation',
+        '/api/listing',
+        '/api/candidates',
+        '/api/groups',
+        '/api/sources',
+        '/api/source',
+        '/api/filters',
+        '/api/instrument',
+        '/api/allocations',
+        '/api/internal',
+        '/api/telescope',
+        '/api/tns_robot',
+        '/api/tns_info',
+        '/api/archive',
+        '/api/alerts_aux',
+        '/api/spectrum',
+        '/api/newsfeed',
+        '/api/alerts_cutouts',
+        '/api/streams',
+        '/api/observing_run',
+        '/api/shifts',
+        '/api/default_observation_plan',
+        '/api/photometry',
+        '/api/observation',
+        '/api/followup_request',
+        '/api/galaxy_catalog',
+        '/api/user',
+        '/api/gcn_event',
+        '/api/spectra',
+        '/api/alerts',
+        '/api/roles',
+    ]
 
     for ii, fn in enumerate(filenames.split(",")):
         df = pd.read_csv(
@@ -31,20 +73,17 @@ def create_ip_chart(filenames, outfile='requests.pdf'):
             na_values='-',
             header=None,
         )
+
+        for verb in verbs:
+            for endpoint in endpoints:
+                df['request'][
+                    df['request'].str.contains(f"{verb} {endpoint}", case=False)
+                ] = f"{verb} {endpoint}"
+
         if ii == 0:
             df_all = df
         else:
             df_all = pd.concat([df_all, df])
-
-    df_group = df_all.groupby(['ip'])
-    labels = []
-    values = []
-    for name, group in df_group:
-        labels.append(name)
-        values.append(group.count()['ip'])
-
-    labels = [x for _, x in sorted(zip(values, labels), key=lambda pair: pair[0])]
-    values = sorted(values)
 
     def make_autopct(values, label="Hours"):
         def my_autopct(pct):
@@ -57,16 +96,49 @@ def create_ip_chart(filenames, outfile='requests.pdf'):
 
         return my_autopct
 
-    matplotlib.use("Agg")
-    plt.figure(figsize=(30, 30))
-    plt.pie(
+    df_group = df_all.groupby(['ip'])
+    labels = []
+    values = []
+    for name, group in df_group:
+        labels.append(name)
+        values.append(group.count()['ip'])
+
+    labels = [x for _, x in sorted(zip(values, labels), key=lambda pair: pair[0])]
+    values = sorted(values)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(60, 30))
+    ax1.pie(
         values,
         labels=labels,
         shadow=True,
         startangle=90,
         autopct=make_autopct(values, label="Requests"),
     )
-    plt.axis('equal')
+    ax1.axis('equal')
+    ax1.set_title('IP Addresses')
+
+    df_group = df_all.groupby(['request'])
+    labels = []
+    values = []
+    for name, group in df_group:
+        labels.append(name)
+        values.append(group.count()['request'])
+
+    labels = [x for _, x in sorted(zip(values, labels), key=lambda pair: pair[0])]
+    values = sorted(values)
+
+    print(labels)
+
+    ax2.pie(
+        values,
+        labels=labels,
+        shadow=True,
+        startangle=90,
+        autopct=make_autopct(values, label="Requests"),
+    )
+    ax2.axis('equal')
+    ax2.set_title('Requests')
+
     plt.savefig(outfile)
     plt.close()
 
