8c360c6ec47c31a255d132e8ec0816c18c620f06
Theodlz <theophile.dulaz@gmail.com>
2022-08-03 15:19:09 +0200

    updated migration's down revision id

--- alembic/versions/675d41c71a2b_reminders.py
+++ alembic/versions/675d41c71a2b_reminders.py
@@ -1,7 +1,7 @@
 """Reminders migration
 
 Revision ID: 675d41c71a2b
-Revises: 556169569def
+Revises: ef7f7859dbba
 Create Date: 2022-07-11 10:56:25.862884
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '675d41c71a2b'
-down_revision = '556169569def'
+down_revision = 'ef7f7859dbba'
 branch_labels = None
 depends_on = None
 
