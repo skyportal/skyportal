c67a2053356a951576e938fd48a271591df5a910
Theodlz <theophile.dulaz@gmail.com>
2022-08-03 15:38:50 +0200

    updated migration's down revision

--- alembic/versions/809e29e0c0c7_gcn_aliases.py
+++ alembic/versions/809e29e0c0c7_gcn_aliases.py
@@ -1,7 +1,7 @@
 """gcn_aliases
 
 Revision ID: 809e29e0c0c7
-Revises: 556169569def
+Revises: ef7f7859dbba
 Create Date: 2022-07-28 15:58:41.631666
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '809e29e0c0c7'
-down_revision = '556169569def'
+down_revision = 'ef7f7859dbba'
 branch_labels = None
 depends_on = None
 
