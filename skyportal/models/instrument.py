8b08e3c2f477811bded07070a601134cb17af0e3
Theophile du Laz <theophile.dulaz@gmail.com>
2022-08-09 19:43:52 +0200

    Telescope and Instruments - Permissions to add, modify and delete (#3259)
    This PR makes sure that the permissions to add telescopes and instruments are the same.

--- skyportal/models/instrument.py
+++ skyportal/models/instrument.py
@@ -12,7 +12,12 @@ from sqlalchemy.dialects.postgresql import ARRAY, JSONB
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import deferred
 
-from baselayer.app.models import Base, restricted
+from baselayer.app.models import (
+    Base,
+    CustomUserAccessControl,
+    DBSession,
+    public,
+)
 
 from skyportal import facility_apis
 
@@ -22,6 +27,13 @@ from ..enum_types import (
     listener_classnames,
     api_classnames,
 )
+
+from baselayer.app.env import load_env
+from baselayer.log import make_log
+
+_, cfg = load_env()
+
+log = make_log('model/instrument')
 
 
 class ArrayOfEnum(ARRAY):
@@ -43,10 +55,21 @@ class ArrayOfEnum(ARRAY):
         return process
 
 
+def manage_instrument_access_logic(cls, user_or_token):
+    if user_or_token.is_system_admin:
+        return DBSession().query(cls)
+    elif 'Manage allocations' in [acl.id for acl in user_or_token.acls]:
+        return DBSession().query(cls)
+    else:
+        # return an empty query
+        return DBSession().query(cls).filter(cls.id == -1)
+
+
 class Instrument(Base):
     """An instrument attached to a telescope."""
 
-    create = restricted
+    read = public
+    create = update = delete = CustomUserAccessControl(manage_instrument_access_logic)
 
     name = sa.Column(sa.String, unique=True, nullable=False, doc="Instrument name.")
     type = sa.Column(
