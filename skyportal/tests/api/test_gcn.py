4cda1c1569bea27224d03850ab5216363276987c
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-08-17 11:38:42 -0500

    Mark gcn summary test as flaky (#3361)

--- skyportal/tests/api/test_gcn.py
+++ skyportal/tests/api/test_gcn.py
@@ -10,6 +10,8 @@ import pandas as pd
 from regions import Regions
 from astropy.table import Table
 
+import pytest
+
 
 def test_gcn_GW(super_admin_token, view_only_token):
 
@@ -152,6 +154,7 @@ def test_gcn_from_moc(super_admin_token, view_only_token):
     assert 'IPN' in data["tags"]
 
 
+@pytest.mark.flaky(reruns=3)
 def test_gcn_summary_sources(
     super_admin_user,
     super_admin_token,
