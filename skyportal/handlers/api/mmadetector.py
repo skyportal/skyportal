833465e25ed13cfc8565871b101d0bc339763682
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-08-31 17:37:40 -0500

    Add GW Detectors (#3401)
    This PR adds MMA detectors.

---
+++ skyportal/handlers/api/mmadetector.py
@@ -0,0 +1,239 @@
+from sqlalchemy.orm import joinedload
+
+from marshmallow.exceptions import ValidationError
+from baselayer.app.access import permissions, auth_or_token
+
+from ..base import BaseHandler
+from ...models import MMADetector
+
+
+class MMADetectorHandler(BaseHandler):
+    @permissions(['Manage allocations'])
+    def post(self):
+        """
+        ---
+        description: Create a Multimessenger Astronomical Detector (MMADetector)
+        tags:
+          - mmadetectors
+        requestBody:
+          content:
+            application/json:
+              schema: MMADetectorNoID
+        responses:
+          200:
+            content:
+              application/json:
+                schema:
+                  allOf:
+                    - $ref: '#/components/schemas/Success'
+                    - type: object
+                      properties:
+                        data:
+                          type: object
+                          properties:
+                            id:
+                              type: integer
+                              description: New mmadetector ID
+          400:
+            content:
+              application/json:
+                schema: Error
+        """
+        data = self.get_json()
+
+        with self.Session() as session:
+
+            schema = MMADetector.__schema__()
+            try:
+                mmadetector = schema.load(data)
+            except ValidationError as e:
+                return self.error(
+                    'Invalid/missing parameters: ' f'{e.normalized_messages()}'
+                )
+            if data['fixed_location']:
+                if (
+                    data['lat'] < -90
+                    or data['lat'] > 90
+                    or data['lon'] < -180
+                    or data['lon'] > 180
+                ):
+                    return self.error(
+                        'Latitude must be between -90 and 90, longitude between -180 and 180'
+                    )
+            session.add(mmadetector)
+            session.commit()
+
+            self.push_all(action="skyportal/REFRESH_MMADETECTOR_LIST")
+            return self.success(data={"id": mmadetector.id})
+
+    @auth_or_token
+    def get(self, mmadetector_id=None):
+        """
+        ---
+        single:
+          description: Retrieve a Multimessenger Astronomical Detector (MMADetector)
+          tags:
+            - mmadetectors
+          parameters:
+            - in: path
+              name: mmadetector_id
+              required: true
+              schema:
+                type: integer
+          responses:
+            200:
+              content:
+                application/json:
+                  schema: SingleMMADetector
+            400:
+              content:
+                application/json:
+                  schema: Error
+        multiple:
+          description: Retrieve all Multimessenger Astronomical Detectors (MMADetectors)
+          tags:
+            - mmadetectors
+          parameters:
+            - in: query
+              name: name
+              schema:
+                type: string
+              description: Filter by name
+          responses:
+            200:
+              content:
+                application/json:
+                  schema: ArrayOfMMADetectors
+            400:
+              content:
+                application/json:
+                  schema: Error
+        """
+
+        with self.Session() as session:
+
+            if mmadetector_id is not None:
+                t = session.scalars(
+                    MMADetector.select(
+                        session.user_or_token, options=[joinedload(MMADetector.events)]
+                    ).where(MMADetector.id == int(mmadetector_id))
+                ).first()
+                if t is None:
+                    return self.error(
+                        f"Could not load MMA Detector with ID {mmadetector_id}"
+                    )
+                return self.success(data=t)
+
+            det_name = self.get_query_argument("name", None)
+            stmt = MMADetector.select(session.user_or_token)
+            if det_name is not None:
+                stmt = stmt.where(MMADetector.name.contains(det_name))
+
+            data = session.scalars(stmt).all()
+            return self.success(data=data)
+
+    @permissions(['Manage allocations'])
+    def patch(self, mmadetector_id):
+        """
+        ---
+        description: Update a Multimessenger Astronomical Detector (MMADetector)
+        tags:
+          - mmadetectors
+        parameters:
+          - in: path
+            name: mmadetector_id
+            required: true
+            schema:
+              type: integer
+        requestBody:
+          content:
+            application/json:
+              schema: MMADetectorNoID
+        responses:
+          200:
+            content:
+              application/json:
+                schema: Success
+          400:
+            content:
+              application/json:
+                schema: Error
+        """
+
+        with self.Session() as session:
+            t = session.scalars(
+                MMADetector.select(session.user_or_token, mode="update").where(
+                    MMADetector.id == int(mmadetector_id)
+                )
+            ).first()
+            if t is None:
+                return self.error('Invalid MMA Detector ID.')
+            data = self.get_json()
+            data['id'] = int(mmadetector_id)
+
+            schema = MMADetector.__schema__()
+            try:
+                schema.load(data)
+            except ValidationError as e:
+                return self.error(
+                    'Invalid/missing parameters: ' f'{e.normalized_messages()}'
+                )
+
+            if 'name' in data:
+                t.name = data['name']
+            if 'nickname' in data:
+                t.nickname = data['nickname']
+            if 'lat' in data:
+                if data['lat'] < -90 or data['lat'] > 90:
+                    return self.error('Latitude must be between -90 and 90')
+                t.lat = data['lat']
+            if 'lon' in data:
+                if data['lon'] < -180 or data['lon'] > 180:
+                    return self.error('Longitude between -180 and 180')
+                t.lon = data['lon']
+            if 'fixed_location' in data:
+                t.fixed_location = data['fixed_location']
+            if 'type' in data:
+                t.type = data['type']
+
+            session.commit()
+
+            self.push_all(action="skyportal/REFRESH_MMADETECTOR_LIST")
+            return self.success()
+
+    @permissions(['Manage allocations'])
+    def delete(self, mmadetector_id):
+        """
+        ---
+        description: Delete a Multimessenger Astronomical Detector (MMADetector)
+        tags:
+          - mmadetectors
+        parameters:
+          - in: path
+            name: mmadetector_id
+            required: true
+            schema:
+              type: integer
+        responses:
+          200:
+            content:
+              application/json:
+                schema: Success
+          400:
+            content:
+              application/json:
+                schema: Error
+        """
+
+        with self.Session() as session:
+            t = session.scalars(
+                MMADetector.select(session.user_or_token, mode="delete").where(
+                    MMADetector.id == int(mmadetector_id)
+                )
+            ).first()
+            if t is None:
+                return self.error('Invalid MMA Detector ID.')
+            session.delete(t)
+            session.commit()
+            self.push_all(action="skyportal/REFRESH_MMADETECTOR_LIST")
+            return self.success()
---
+++ static/js/components/MMADetectorPage.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState, useRef } from "react";
+
+import { useTheme } from "@mui/material/styles";
+import MMADetectorPageDesktop from "./MMADetectorPageDesktop";
+import MMADetectorPageMobile from "./MMADetectorPageMobile";
+
+const sidebarWidth = 190;
+
+const MMADetectorPage = () => {
+  const ref = useRef(null);
+  const theme = useTheme();
+  const initialWidth = window.innerWidth - sidebarWidth - 2 * theme.spacing(2);
+  const [width, setWidth] = useState(initialWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (ref.current !== null) {
+        setWidth(ref.current.offsetWidth);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+  }, [ref]);
+
+  return (
+    <div ref={ref}>
+      {width <= 600 ? <MMADetectorPageMobile /> : <MMADetectorPageDesktop />}
+    </div>
+  );
+};
+
+export default MMADetectorPage;
