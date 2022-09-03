ccac59cd61a5d8a6a025845403f737323f0b66b6
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-09-03 05:03:42 -0500

    MMADetector spectrum (#3406)
    This PR adds spectrum for MMA detectors.

--- skyportal/handlers/api/mmadetector.py
+++ skyportal/handlers/api/mmadetector.py
@@ -1,10 +1,20 @@
+import arrow
+from arrow import ParserError
+
 from sqlalchemy.orm import joinedload
+from sqlalchemy import or_
 
 from marshmallow.exceptions import ValidationError
 from baselayer.app.access import permissions, auth_or_token
+from baselayer.app.custom_exceptions import AccessError
 
+from .spectrum import parse_id_list
 from ..base import BaseHandler
-from ...models import MMADetector
+from ...models import MMADetector, MMADetectorSpectrum, Group
+
+from ...models.schema import (
+    MMADetectorSpectrumPost,
+)
 
 
 class MMADetectorHandler(BaseHandler):
@@ -236,4 +246,367 @@ class MMADetectorHandler(BaseHandler):
             session.delete(t)
             session.commit()
             self.push_all(action="skyportal/REFRESH_MMADETECTOR_LIST")
+            return self.success()
+
+
+class MMADetectorSpectrumHandler(BaseHandler):
+    @permissions(['Upload data'])
+    def post(self):
+        """
+        ---
+        description: Upload a Multimessenger Astronomical Detector (MMADetector) spectrum
+        tags:
+          - mmadetector_spectra
+        requestBody:
+          content:
+            application/json:
+              schema: MMADetectorSpectrumPost
+        responses:
+          200:
+            content:
+              application/json:
+                schema:
+                  allOf:
+                    - $ref: '#/components/schemas/Success'
+                    - type: object
+                      properties:
+                        data:
+                          type: object
+                          properties:
+                            id:
+                              type: integer
+                              description: New mmadetector spectrum ID
+          400:
+            content:
+              application/json:
+                schema: Error
+        """
+        json = self.get_json()
+        try:
+            data = MMADetectorSpectrumPost.load(json)
+        except ValidationError as e:
+            return self.error(
+                f'Invalid / missing parameters; {e.normalized_messages()}'
+            )
+
+        with self.Session() as session:
+            stmt = MMADetector.select(self.current_user).where(
+                MMADetector.id == data['detector_id']
+            )
+            mmadetector = session.scalars(stmt).first()
+            if mmadetector is None:
+                return self.error(
+                    f'Cannot find mmadetector with ID: {data["detector_id"]}'
+                )
+
+            owner_id = self.associated_user_object.id
+
+            # always append the single user group
+            single_user_group = self.associated_user_object.single_user_group
+
+            group_ids = data.pop("group_ids", None)
+            if group_ids == [] or group_ids is None:
+                group_ids = [single_user_group.id]
+            elif group_ids == "all":
+                group_ids = [g.id for g in self.current_user.accessible_groups]
+
+            if single_user_group.id not in group_ids:
+                group_ids.append(single_user_group.id)
+
+            groups = (
+                session.scalars(
+                    Group.select(self.current_user).where(Group.id.in_(group_ids))
+                )
+                .unique()
+                .all()
+            )
+            if {g.id for g in groups} != set(group_ids):
+                return self.error(
+                    f'Cannot find one or more groups with IDs: {group_ids}.'
+                )
+
+            spec = MMADetectorSpectrum(**data)
+            spec.mmadetector = mmadetector
+            spec.groups = groups
+            spec.owner_id = owner_id
+            session.add(spec)
+
+            session.commit()
+
+            self.push_all(
+                action='skyportal/REFRESH_MMADETECTOR',
+                payload={'detector_id': spec.detector.id},
+            )
+
+            self.push_all(
+                action='skyportal/REFRESH_MMADETECTOR_SPECTRA',
+                payload={'detector_id': spec.detector.id},
+            )
+
+            return self.success(data={"id": spec.id})
+
+    @auth_or_token
+    def get(self, spectrum_id=None):
+        """
+        ---
+        single:
+          description: Retrieve an mmadetector spectrum
+          tags:
+            - mmadetector_spectra
+          parameters:
+            - in: path
+              name: spectrum_id
+              required: true
+              schema:
+                type: integer
+          responses:
+            200:
+              content:
+                application/json:
+                  schema: SingleMMADetectorSpectrum
+            403:
+              content:
+                application/json:
+                  schema: Error
+        multiple:
+          description: Retrieve multiple spectra with given criteria
+          tags:
+            - mmadetector_spectra
+          parameters:
+            - in: query
+              name: observedBefore
+              nullable: true
+              schema:
+                type: string
+              description: |
+                Arrow-parseable date string (e.g. 2020-01-01). If provided,
+                return only spectra observed before this time.
+            - in: query
+              name: observedAfter
+              nullable: true
+              schema:
+                type: string
+              description: |
+                Arrow-parseable date string (e.g. 2020-01-01). If provided,
+                return only spectra observed after this time.
+            - in: query
+              name: detectorIDs
+              nullable: true
+              type: list
+              items:
+                type: integer
+              description: |
+                If provided, filter only spectra observed with one of these mmadetector IDs.
+            - in: query
+              name: groupIDs
+              nullable: true
+              schema:
+                type: list
+                items:
+                  type: integer
+              description: |
+                If provided, filter only spectra saved to one of these group IDs.
+        """
+
+        if spectrum_id is not None:
+            with self.Session() as session:
+                spectrum = session.scalars(
+                    MMADetectorSpectrum.select(session.user_or_token).where(
+                        MMADetectorSpectrum.id == spectrum_id
+                    )
+                ).first()
+                if spectrum is None:
+                    return self.error(
+                        f'Could not access spectrum {spectrum_id}.', status=403
+                    )
+                return self.success(data=spectrum)
+
+        # multiple spectra
+        observed_before = self.get_query_argument('observedBefore', None)
+        observed_after = self.get_query_argument('observedAfter', None)
+        detector_ids = self.get_query_argument('instrumentIDs', None)
+        group_ids = self.get_query_argument('groupIDs', None)
+
+        # validate inputs
+        try:
+            observed_before = (
+                arrow.get(observed_before).datetime if observed_before else None
+            )
+        except (TypeError, ParserError):
+            return self.error(f'Cannot parse time input value "{observed_before}".')
+
+        try:
+            observed_after = (
+                arrow.get(observed_after).datetime if observed_after else None
+            )
+        except (TypeError, ParserError):
+            return self.error(f'Cannot parse time input value "{observed_after}".')
+
+        with self.Session() as session:
+
+            try:
+                detector_ids = parse_id_list(detector_ids, MMADetector, session)
+                group_ids = parse_id_list(group_ids, Group, session)
+            except (ValueError, AccessError) as e:
+                return self.error(str(e))
+
+            # filter the spectra
+            spec_query = MMADetectorSpectrum.select(session.user_or_token)
+            if detector_ids:
+                spec_query = spec_query.where(
+                    MMADetectorSpectrum.detector_id.in_(detector_ids)
+                )
+
+            if group_ids:
+                spec_query = spec_query.where(
+                    or_(
+                        *[
+                            MMADetectorSpectrum.groups.any(Group.id == gid)
+                            for gid in group_ids
+                        ]
+                    )
+                )
+
+            if observed_before:
+                spec_query = spec_query.where(
+                    MMADetectorSpectrum.end_time <= observed_before
+                )
+
+            if observed_after:
+                spec_query = spec_query.where(
+                    MMADetectorSpectrum.start_time >= observed_after
+                )
+
+            spectra = session.scalars(spec_query).unique().all()
+
+            return self.success(data=spectra)
+
+    @permissions(['Upload data'])
+    def patch(self, spectrum_id):
+        """
+        ---
+        description: Update mmadetector spectrum
+        tags:
+          - mmadetector_spectra
+        parameters:
+          - in: path
+            name: spectrum_id
+            required: true
+            schema:
+              type: integer
+        requestBody:
+          content:
+            application/json:
+              schema: MMADetectorSpectrumPost
+        responses:
+          200:
+            content:
+              application/json:
+                schema: Success
+          400:
+            content:
+              application/json:
+                schema: Error
+        """
+
+        try:
+            spectrum_id = int(spectrum_id)
+        except TypeError:
+            return self.error('Could not convert spectrum id to int.')
+
+        data = self.get_json()
+
+        try:
+            data = MMADetectorSpectrumPost.load(data, partial=True)
+        except ValidationError as e:
+            return self.error(
+                'Invalid/missing parameters: ' f'{e.normalized_messages()}'
+            )
+
+        group_ids = data.pop("group_ids", None)
+        if group_ids == 'all':
+            group_ids = [g.id for g in self.current_user.accessible_groups]
+
+        with self.Session() as session:
+            stmt = MMADetectorSpectrum.select(self.current_user).where(
+                MMADetectorSpectrum.id == spectrum_id
+            )
+            spectrum = session.scalars(stmt).first()
+
+            if group_ids:
+                groups = (
+                    session.scalars(
+                        Group.select(self.current_user).where(Group.id.in_(group_ids))
+                    )
+                    .unique()
+                    .all()
+                )
+                if {g.id for g in groups} != set(group_ids):
+                    return self.error(
+                        f'Cannot find one or more groups with IDs: {group_ids}.'
+                    )
+
+                if groups:
+                    spectrum.groups = spectrum.groups + groups
+
+            for k in data:
+                setattr(spectrum, k, data[k])
+
+            session.commit()
+
+            self.push_all(
+                action='skyportal/REFRESH_MMADETECTOR',
+                payload={'detector_id': spectrum.detector.id},
+            )
+
+            self.push_all(
+                action='skyportal/REFRESH_MMADETECTOR_SPECTRA',
+                payload={'detector_id': spectrum.detector.id},
+            )
+
+            return self.success()
+
+    @permissions(['Upload data'])
+    def delete(self, spectrum_id):
+        """
+        ---
+        description: Delete an mmadetector spectrum
+        tags:
+          - mmadetector_spectra
+        parameters:
+          - in: path
+            name: spectrum_id
+            required: true
+            schema:
+              type: integer
+        responses:
+          200:
+            content:
+              application/json:
+                schema: Success
+          400:
+            content:
+              application/json:
+                schema: Error
+        """
+        with self.Session() as session:
+            spectrum = session.scalars(
+                MMADetectorSpectrum.select(self.current_user).where(
+                    MMADetectorSpectrum.id == spectrum_id
+                )
+            ).first()
+            detector_id = spectrum.detector.id
+            session.delete(spectrum)
+            session.commit()
+
+            self.push_all(
+                action='skyportal/REFRESH_MMADETECTOR',
+                payload={'detector_id': detector_id},
+            )
+
+            self.push_all(
+                action='skyportal/REFRESH_MMADETECTOR_SPECTRA',
+                payload={'detector_id': detector_id},
+            )
+
             return self.success()
