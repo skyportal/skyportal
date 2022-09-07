012fe634a5bb0ed165d4e9a82ffa4eaaa90c61ff
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-09-07 11:14:57 -0500

    Add properties table for ease of filtering events

--- skyportal/models/user_token.py
+++ skyportal/models/user_token.py
@@ -279,6 +279,12 @@ User.gcntags = relationship(
     passive_deletes=True,
     doc='The gcntags saved by this user',
 )
+User.gcnproperties = relationship(
+    'GcnProperty',
+    back_populates='sent_by',
+    passive_deletes=True,
+    doc='The gcnproperties saved by this user',
+)
 User.listings = relationship(
     'Listing',
     back_populates='user',
