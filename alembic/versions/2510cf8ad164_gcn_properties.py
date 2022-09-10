2156c31d2750fddd1fdec831eec36e5ac10baa9b
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-09-10 08:22:35 -0500

    alembic

--- alembic/versions/2510cf8ad164_gcn_properties.py
+++ alembic/versions/2510cf8ad164_gcn_properties.py
@@ -1,7 +1,7 @@
 """GCN properties migration
 
 Revision ID: 2510cf8ad164
-Revises: 44a7408c38a3
+Revises: c589cb7d70ec
 Create Date: 2022-09-07 16:24:44.397567
 
 """
@@ -11,7 +11,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = '2510cf8ad164'
-down_revision = '44a7408c38a3'
+down_revision = 'c589cb7d70ec'
 branch_labels = None
 depends_on = None
 
