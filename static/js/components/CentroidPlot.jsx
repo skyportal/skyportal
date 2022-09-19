fd3e530c715db00c08d45cf36a45aeb059299245
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-09-19 10:41:37 -0500

    Update static/js/components/CentroidPlot.jsx
    Co-authored-by: Guy Nir <37179063+guynir42@users.noreply.github.com>

--- static/js/components/CentroidPlot.jsx
+++ static/js/components/CentroidPlot.jsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles(() => ({
     height: props.height,
   }),
   infoLine: {
-    // Get it's own line
+    // Get its own line
     flexBasis: "100%",
     display: "flex",
     flexFlow: "row wrap",
