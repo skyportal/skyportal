7c026755ac8976c535f117a14ec06433d61089b0
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-09-26 19:28:02 -0500

    Include both request length and bytes sent plots (#3504)
    This PR adds a plot for the bytes sent.

--- tools/nginx_ip_and_requests.py
+++ tools/nginx_ip_and_requests.py
@@ -84,17 +84,33 @@ def create_ip_chart(filenames, outfile='requests.pdf', verb_to_report='ALL'):
     df_group = df_all.groupby(['ip'])
     labels = []
     values = []
+    bytes_sent = []
     requests = []
     for name, group in df_group:
         labels.append(name)
         values.append(group.count()['ip'])
+        bytes_sent.append(group['size'])
         requests.append(group['request_length'])
 
-    labels = [x for _, x in sorted(zip(values, labels), key=lambda pair: pair[0])]
-    requests = [x for _, x in sorted(zip(values, requests), key=lambda pair: pair[0])]
-    values = sorted(values)
+    labels = [
+        x
+        for _, x in sorted(zip(values, labels), key=lambda pair: pair[0], reverse=True)
+    ]
+    bytes_sent = [
+        x
+        for _, x in sorted(
+            zip(values, bytes_sent), key=lambda pair: pair[0], reverse=True
+        )
+    ]
+    requests = [
+        x
+        for _, x in sorted(
+            zip(values, requests), key=lambda pair: pair[0], reverse=True
+        )
+    ]
+    values = sorted(values, reverse=True)
 
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(90, 30))
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(120, 30))
     ax1.pie(
         values,
         labels=labels,
@@ -106,20 +122,61 @@ def create_ip_chart(filenames, outfile='requests.pdf', verb_to_report='ALL'):
     ax1.set_title('IP Addresses')
 
     bins = np.logspace(-6, 3, 20)
-    for name, request in zip(labels[:10], requests[:10]):
+    largest_request = -1
+    largest_request_ip = ""
+    for ii, (name, request) in enumerate(zip(labels, requests)):
         request_lengths = []
         for value in request:
             try:
-                request_lengths.append(int(value.replace("rl=", "")) / 1e6)
+                request_length = int(value.replace("rl=", "")) / 1e6
             except ValueError:
                 continue
+            request_lengths.append(request_length)
+            if request_length > largest_request:
+                largest_request = request_length
+                largest_request_ip = name
+
+        if ii > 10:
+            continue
+
         hist, bin_edges = np.histogram(request_lengths, bins=bins)
         bin_centers = (bin_edges[1:] + bin_edges[:-1]) / 2.0
         hist = hist / np.sum(hist)
         ax3.step(bin_centers, hist, label=name)
     ax3.set_xscale('log')
+    ax3.set_yscale('log')
     ax3.set_xlabel('Request Size [MB]', fontsize=36)
     ax3.legend(loc='upper right')
+
+    print(f'Largest request length by {largest_request_ip} of {largest_request} MB')
+
+    largest_bytes_sent = -1
+    largest_bytes_sent_ip = ""
+    for ii, (name, request) in enumerate(zip(labels, bytes_sent)):
+        request_sizes = []
+        for value in request:
+            try:
+                request_size = int(value) / 1e6
+            except ValueError:
+                continue
+            request_sizes.append(request_size)
+            if request_size > largest_bytes_sent:
+                largest_bytes_sent = request_size
+                largest_bytes_sent_ip = name
+
+        if ii > 10:
+            continue
+
+        hist, bin_edges = np.histogram(request_sizes, bins=bins)
+        bin_centers = (bin_edges[1:] + bin_edges[:-1]) / 2.0
+        hist = hist / np.sum(hist)
+        ax4.step(bin_centers, hist, label=name)
+    ax4.set_xscale('log')
+    ax4.set_yscale('log')
+    ax4.set_xlabel('Bytes Sent [MB]', fontsize=36)
+    ax4.legend(loc='upper right')
+
+    print(f'Largest bytes sent by {largest_bytes_sent_ip} of {largest_bytes_sent} MB')
 
     df_group = df_all.groupby(['request_parsed'])
     labels = []
