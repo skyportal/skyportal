c1661b7455f3b3154522702ef5ce30666cfa5125
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-11-17 17:04:27 -0600

    Finish component refactor

--- static/js/components/CustomDataTable.jsx
+++ static/js/components/CustomDataTable.jsx
@@ -1,24 +1,35 @@
-import React, { memo } from "react";
+import React, { Component, memo } from "react";
+import PropTypes from "prop-types";
+
 import MUIDataTable from "mui-datatables";
 
-class CustomDataTable extends React.Component {
+class CustomDataTable extends Component {
   constructor(props) {
     super(props);
+    this.state = {};
   }
 
   render() {
+    const { title, data, columns, options } = this.props;
     return (
       <div>
         <MUIDataTable
-          title={this.props.title}
-          data={this.props.data}
-          columns={this.props.columns}
-          options={this.props.options}
+          title={title}
+          data={data}
+          columns={columns}
+          options={options}
         />
       </div>
     );
   }
 }
+
+CustomDataTable.propTypes = {
+  title: PropTypes.string.isRequired,
+  data: PropTypes.objectOf(PropTypes.any).isRequired, // eslint-disable-line react/forbid-prop-types,
+  columns: PropTypes.objectOf(PropTypes.any).isRequired, // eslint-disable-line react/forbid-prop-types,
+  options: PropTypes.objectOf(PropTypes.any).isRequired, // eslint-disable-line react/forbid-prop-types,
+};
 
 const customComparator = (prevProps, nextProps) => {
   function areEqual(array1, array2) {
