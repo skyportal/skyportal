d9e6620175d6f6458b8a4be5cb4db2da727f90bd
Theophile du Laz <theophile.dulaz@gmail.com>
2023-01-02 13:40:09 +0100

    Photometry upload form (#3806)
    This PR improves the photometry upload form to be consistent with our other forms.

--- static/js/ducks/photometry.js
+++ static/js/ducks/photometry.js
@@ -11,6 +11,8 @@ const FETCH_ALL_ORIGINS_OK = "skyportal/FETCH_ALL_ORIGINS_OK";
 
 const DELETE_PHOTOMETRY = "skyportal/DELETE_PHOTOMETRY";
 
+const SUBMIT_PHOTOMETRY = "skyportal/SUBMIT_PHOTOMETRY";
+
 // eslint-disable-next-line import/prefer-default-export
 export function fetchSourcePhotometry(id) {
   return API.GET(`/api/sources/${id}/photometry`, FETCH_SOURCE_PHOTOMETRY);
@@ -30,6 +32,10 @@ export function fetchAllOrigins() {
 
 export function deletePhotometry(id) {
   return API.DELETE(`/api/photometry/${id}`, DELETE_PHOTOMETRY);
+}
+
+export function submitPhotometry(photometry) {
+  return API.POST("/api/photometry", SUBMIT_PHOTOMETRY, photometry);
 }
 
 // Websocket message handler
