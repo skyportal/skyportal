24d792bb5fa91693ddb0dc7f39847d8e178c68d3
lydie10 <lydie.pont@edu.devinci.fr>
2023-01-05 12:16:33 +0100

    instrument page improvement

--- static/js/components/InstrumentPage.jsx
+++ static/js/components/InstrumentPage.jsx
@@ -1,223 +1,118 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import DeleteIcon from "@mui/icons-material/Delete";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import makeStyles from "@mui/styles/makeStyles";
-import PropTypes from "prop-types";
-import { showNotification } from "baselayer/components/Notifications";
-import CircularProgress from "@mui/material/CircularProgress";
-import NewInstrument from "./NewInstrument";
-// eslint-disable-next-line import/no-cycle
-import ModifyInstrument from "./ModifyInstrument";
-import Button from "./Button";
-import ConfirmDeletionDialog from "./ConfirmDeletionDialog";
 
-import * as instrumentActions from "../ducks/instrument";
+import Paper from "@mui/material/Paper";
+import makeStyles from "@mui/styles/makeStyles";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+import InstrumentTable from "./InstrumentTable";
+import Spinner from "./Spinner";
+import * as instrumentsActions from "../ducks/instruments";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    maxWidth: "22.5rem",
-    backgroundColor: theme.palette.background.paper,
-  },
-  paperContent: {
+  paperDiv: {
     padding: "1rem",
+    height: "100%",
   },
-  instrumentDelete: {
-    cursor: "pointer",
-    fontSize: "2em",
-    position: "absolute",
-    padding: 0,
-    right: 0,
-    top: 0,
+  tableGrid: {
+    width: "100%",
   },
-  instrumentDeleteDisabled: {
-    opacity: 0,
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  paper: {
+    padding: "1rem",
+    marginTop: "0.625rem",
+  },
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& .MuiTextField-root": {
+      margin: theme.spacing(0.2),
+      width: "10rem",
+    },
+  },
+  blockWrapper: {
+    width: "100%",
+    marginBottom: "0.5rem",
+  },
+  title: {
+    margin: "0.5rem 0rem 0rem 0rem",
   },
 }));
 
-const textStyles = makeStyles(() => ({
-  primary: {
-    fontWeight: "bold",
-    fontSize: "110%",
-  },
-}));
-
-export function instrumentTitle(instrument, telescopeList) {
-  const telescope_id = instrument?.telescope_id;
-  const telescope = telescopeList?.filter((t) => t.id === telescope_id)[0];
-
-  if (!(instrument?.name && telescope?.name)) {
-    return (
-      <div>
-        <CircularProgress color="secondary" />
-      </div>
-    );
-  }
-
-  const result = `${instrument?.name}/${telescope?.nickname}`;
-
-  return result;
-}
-
-export function instrumentInfo(instrument, telescopeList) {
-  const telescope_id = instrument?.telescope_id;
-  const telescope = telescopeList?.filter((t) => t.id === telescope_id)[0];
-
-  if (!(instrument?.name && telescope?.name)) {
-    return (
-      <div>
-        <CircularProgress color="secondary" />
-      </div>
-    );
-  }
-
-  let result = "";
-
-  if (
-    instrument?.filters ||
-    instrument?.api_classname ||
-    instrument?.api_classname_obsplan ||
-    instrument?.fields
-  ) {
-    result += "( ";
-    if (instrument?.filters) {
-      const filters_str = instrument.filters.join(", ");
-      result += `filters: ${filters_str}`;
-    }
-    if (instrument?.api_classname) {
-      result += ` / API Classname: ${instrument?.api_classname}`;
-    }
-    if (instrument?.api_classname_obsplan) {
-      result += ` / API Observation Plan Classname: ${instrument?.api_classname_obsplan}`;
-    }
-    if (instrument?.fields && instrument?.fields.length > 0) {
-      result += ` / # of Fields: ${instrument?.fields.length}`;
-    }
-    result += " )";
-  }
-
-  return result;
-}
-
-const InstrumentList = ({ instruments, telescopes, deletePermission }) => {
+const InstrumentList = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
-  const classes = useStyles();
-  const textClasses = textStyles();
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [instrumentToDelete, setInstrumentToDelete] = useState(null);
-  const openDialog = (id) => {
-    setDialogOpen(true);
-    setInstrumentToDelete(id);
-  };
-  const closeDialog = () => {
-    setDialogOpen(false);
-    setInstrumentToDelete(null);
+
+  const instrumentsState = useSelector((state) => state.instruments);
+  const telescopesState = useSelector((state) => state.telescopes);
+
+  const [rowsPerPage, setRowsPerPage] = useState(100);
+
+  useEffect(() => {
+    dispatch(instrumentsActions.fetchInstruments());
+  }, [dispatch]);
+
+  const handleInstrumentTablePagination = (
+    pageNumber,
+    numPerPage,
+    sortData,
+    filterData
+  ) => {
+    setRowsPerPage(numPerPage);
+    const data = {
+      ...filterData,
+      pageNumber,
+      numPerPage,
+    };
+    if (sortData && Object.keys(sortData).length > 0) {
+      data.sortBy = sortData.name;
+      data.sortOrder = sortData.direction;
+    }
+    dispatch(instrumentsActions.fetchInstruments(data));
   };
 
-  const deleteInstrument = () => {
-    dispatch(instrumentActions.deleteInstrument(instrumentToDelete)).then(
-      (result) => {
-        if (result.status === "success") {
-          dispatch(showNotification("Instrument deleted"));
-          closeDialog();
-        }
-      }
-    );
+  const handleInstrumentTableSorting = (sortData, filterData) => {
+    const data = {
+      ...filterData,
+      pageNumber: 1,
+      rowsPerPage,
+      sortBy: sortData.name,
+      sortOrder: sortData.direction,
+    };
+    dispatch(instrumentsActions.fetchInstruments(data));
   };
+
+  if (!instrumentsState.instrumentList) {
+    return <Spinner />;
+  }
 
   return (
-    <div className={classes.root}>
-      <List component="nav">
-        {instruments?.map((instrument) => (
-          <ListItem button key={instrument.id}>
-            <ListItemText
-              primary={instrumentTitle(instrument, telescopes)}
-              secondary={instrumentInfo(instrument, telescopes)}
-              classes={textClasses}
+    <Paper elevation={1} className={classes.paper}>
+      <div className={classes.paperDiv}>
+        <Typography variant="h6" display="inline">
+          Instruments
+        </Typography>
+        {instrumentsState.instrumentList && (
+          <Grid item className={classes.tableGrid}>
+            <InstrumentTable
+              instruments={instrumentsState.instrumentList}
+              telescopes={telescopesState.telescopeList}
+              paginateCallback={handleInstrumentTablePagination}
+              totalMatches={instrumentsState.totalMatches}
+              pageNumber={instrumentsState.pageNumber}
+              numPerPage={instrumentsState.numPerPage}
+              sortingCallback={handleInstrumentTableSorting}
             />
-            <Button
-              key={instrument.id}
-              id="delete_button"
-              classes={{
-                root: classes.instrumentDelete,
-                disabled: classes.instrumentDeleteDisabled,
-              }}
-              onClick={() => openDialog(instrument.id)}
-              disabled={!deletePermission}
-            >
-              <DeleteIcon />
-            </Button>
-            <ConfirmDeletionDialog
-              deleteFunction={deleteInstrument}
-              dialogOpen={dialogOpen}
-              closeDialog={closeDialog}
-              resourceName="instrument"
-            />
-          </ListItem>
-        ))}
-      </List>
-    </div>
+          </Grid>
+        )}
+        {!instrumentsState.instrumentList && <Spinner />}
+      </div>
+    </Paper>
   );
 };
 
-const InstrumentPage = () => {
-  const { instrumentList } = useSelector((state) => state.instruments);
-  const { telescopeList } = useSelector((state) => state.telescopes);
-  const currentUser = useSelector((state) => state.profile);
-
-  const post_permission =
-    currentUser.permissions?.includes("Manage instruments") ||
-    currentUser.permissions?.includes("System admin");
-
-  const delete_permission =
-    currentUser.permissions?.includes("Delete instrument") ||
-    currentUser.permissions?.includes("System admin");
-
-  const classes = useStyles();
-  return (
-    <Grid container spacing={3}>
-      <Grid item md={6} sm={12}>
-        <Paper elevation={1}>
-          <div className={classes.paperContent}>
-            <Typography variant="h6">List of Instruments</Typography>
-            <InstrumentList
-              instruments={instrumentList}
-              telescopes={telescopeList}
-              deletePermission={delete_permission}
-            />
-          </div>
-        </Paper>
-      </Grid>
-      {post_permission && (
-        <Grid item md={6} sm={12}>
-          <Paper>
-            <div className={classes.paperContent}>
-              <Typography variant="h6">Add a New Instrument</Typography>
-              <NewInstrument />
-            </div>
-          </Paper>
-          <Paper>
-            <div className={classes.paperContent}>
-              <Typography variant="h6">Modify an Instrument</Typography>
-              <ModifyInstrument />
-            </div>
-          </Paper>
-        </Grid>
-      )}
-    </Grid>
-  );
-};
-
-InstrumentList.propTypes = {
-  instruments: PropTypes.arrayOf(PropTypes.any).isRequired, // eslint-disable-line react/forbid-prop-types
-  telescopes: PropTypes.arrayOf(PropTypes.any).isRequired, // eslint-disable-line react/forbid-prop-types
-  deletePermission: PropTypes.bool.isRequired,
-};
-
-export default InstrumentPage;
+export default InstrumentList;
