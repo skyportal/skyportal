bb3977d2c5e3e3bc5f8bf714ecd9055093bcd106
aolrc-vinci <113455524+aolrc-vinci@users.noreply.github.com>
2023-01-05 11:51:00 +0100

    Display telescopes list

--- static/js/components/TelescopeInfo.jsx
+++ static/js/components/TelescopeInfo.jsx
@@ -228,8 +228,13 @@ AllocationList.propTypes = {
 const TelescopeInfo = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
+
   const currentTelescopes = useSelector(
     (state) => state.telescope.currentTelescopes
+  );
+
+  const telescopeList = useSelector(
+    (state) => state.telescopes.telescopeList
   );
 
   const currentUser = useSelector((state) => state.profile);
@@ -376,7 +381,118 @@ const TelescopeInfo = () => {
       ))}
     </List>
   ) : (
-    <h2 className={classes.h2}>No telescope selected</h2>
+    <List className={classes.root}>
+      {telescopeList.telescopes.map((telescope) => (
+        <div key={`${telescope.id}_list_item`}>
+          <ListItem
+            id={`${telescope.name}_info`}
+            className={classes.listItem}
+            key={`${telescope.id}_info`}
+          >
+            <div
+              className={classes.telescope_header}
+              key={`${telescope.id}_header`}
+            >
+              {telescope.is_night_astronomical && telescope.fixed_location && (
+                <span className={classes.canObserveFixed} />
+              )}
+              {!telescope.is_night_astronomical && telescope.fixed_location && (
+                <span className={classes.cannotObserveFixed} />
+              )}
+              {!telescope.fixed_location && (
+                <span className={classes.cannotObserveNonFixed} />
+              )}
+              <h2 className={classes.h2}>
+                {telescope.name} ({telescope.nickname})
+              </h2>
+            </div>
+            {telescope.fixed_location && (
+              <div
+                className={classes.telescope_time}
+                key={`${telescope.id}_time`}
+              >
+                <i>
+                  {telescope.next_twilight_morning_astronomical &&
+                    `Next Sunrise (Astronomical): ${telescope.next_twilight_morning_astronomical.slice(
+                      0,
+                      -4
+                    )} UTC`}
+                </i>
+                <i>
+                  {telescope.next_twilight_evening_astronomical &&
+                    `Next Sunset (Astronomical): ${telescope.next_twilight_evening_astronomical.slice(
+                      0,
+                      -4
+                    )} UTC`}
+                </i>
+              </div>
+            )}
+            <h3 className={classes.h3} key={`${telescope.id}_diameter`}>
+              Diameter :{" "}
+              {telescope.diameter ? telescope.diameter.toFixed(1) : null}
+            </h3>
+            {telescope.fixed_location && (
+              <h3 className={classes.h3} key={`${telescope.id}_location`}>
+                Location : {telescope.lat ? telescope.lat.toFixed(4) : null},{" "}
+                {telescope.lon ? telescope.lon.toFixed(4) : null}
+              </h3>
+            )}
+            {telescope.fixed_location && (
+              <h3 className={classes.h3} key={`${telescope.id}_elevation`}>
+                Elevation :{" "}
+                {telescope.elevation ? telescope.elevation.toFixed(1) : null}
+              </h3>
+            )}
+            {telescope.robotic ? (
+              <h3 className={classes.h3} key={`${telescope.id}_robotic`}>
+                Robotic : Yes
+              </h3>
+            ) : (
+              <h3 className={classes.h3} key={`${telescope.id}_robotic`}>
+                Robotic : No
+              </h3>
+            )}
+            {telescope.fixed_location ? (
+              <h3 className={classes.h3} key={`${telescope.id}_fixed_location`}>
+                Fixed Location : Yes
+              </h3>
+            ) : (
+              <h3 className={classes.h3} key={`${telescope.id}_fixed_location`}>
+                Fixed Location : No
+              </h3>
+            )}
+            {telescope.skycam_link && (
+              <a
+                className={classes.a}
+                href={telescope.skycam_link}
+                key={`${telescope.id}_skycam_link`}
+              >
+                skycam link
+              </a>
+            )}
+            <Button
+              key={telescope.id}
+              id="delete_button"
+              classes={{
+                root: classes.telescopeDelete,
+                disabled: classes.telescopeDeleteDisabled,
+              }}
+              onClick={() => openDialog(telescope.id)}
+              disabled={!permission}
+            >
+              <DeleteIcon />
+            </Button>
+            <ConfirmDeletionDialog
+              deleteFunction={deleteTelescope}
+              dialogOpen={dialogOpen}
+              closeDialog={closeDialog}
+              resourceName="telescope"
+            />
+          </ListItem>
+          <Divider />
+        </div>
+      ))}
+    </List>
   );
 };
 
