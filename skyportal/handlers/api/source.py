a5257278ab6aeff6d845dca3d553e3a2ffb49e18
Michael Coughlin <michael.w.coughlin@gmail.com>
2023-01-16 16:08:20 -0600

    Add try except back in

--- skyportal/handlers/api/source.py
+++ skyportal/handlers/api/source.py
@@ -2492,8 +2492,7 @@ class SourceHandler(BaseHandler):
                 return self.success(data=source_info)
 
         with self.Session() as session:
-            # try:
-            if True:
+            try:
                 query_results = await get_sources(
                     self.associated_user_object.id,
                     session,
@@ -2567,8 +2566,8 @@ class SourceHandler(BaseHandler):
                     total_matches=total_matches,
                     includeGeoJSON=includeGeoJSON,
                 )
-            # except Exception as e:
-            #    return self.error(f'Cannot retrieve sources: {str(e)}')
+            except Exception as e:
+                return self.error(f'Cannot retrieve sources: {str(e)}')
 
             query_size = sizeof(query_results)
             if query_size >= SIZE_WARNING_THRESHOLD:
