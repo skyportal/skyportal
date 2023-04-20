b3f473b3aebcdf37b28b864ff970d3b29c85dfdb
Michael Coughlin <michael.w.coughlin@gmail.com>
2023-04-20 10:56:46 -0500

    Add email/slack request when making follow-up requests

--- static/js/components/EditFollowupRequestDialog.jsx
+++ static/js/components/EditFollowupRequestDialog.jsx
@@ -38,6 +38,8 @@ const EditFollowupRequestDialog = ({
       obj_id: followupRequest.obj_id,
       payload: formData,
     };
+    console.log("json", json);
+    console.log("followupRequest", followupRequest);
     dispatch(Actions.editFollowupRequest(json, followupRequest.id));
     handleClose();
   };
@@ -56,19 +58,21 @@ const EditFollowupRequestDialog = ({
 
   // we do the same for formSchema.dependencies, where each key is a value that has dependencies, under a key called "oneOf"
   // in the oneOf.properties, if any key isnt in formSchema.properties, we set the their default value to the value in the existing request's payload
-  Object.keys(formSchema.dependencies).forEach((key) => {
-    formSchema.dependencies[key].oneOf.forEach((oneOf) => {
-      Object.keys(oneOf.properties).forEach((oneOfKey) => {
-        if (
-          !formSchema.properties[oneOfKey] &&
-          followupRequest.payload[oneOfKey]
-        ) {
-          oneOf.properties[oneOfKey].default =
-            followupRequest.payload[oneOfKey];
-        }
+  if (formSchema?.dependencies) {
+    Object.keys(formSchema.dependencies).forEach((key) => {
+      formSchema.dependencies[key].oneOf.forEach((oneOf) => {
+        Object.keys(oneOf.properties).forEach((oneOfKey) => {
+          if (
+            !formSchema.properties[oneOfKey] &&
+            followupRequest.payload[oneOfKey]
+          ) {
+            oneOf.properties[oneOfKey].default =
+              followupRequest.payload[oneOfKey];
+          }
+        });
       });
     });
-  });
+  }
 
   const validate = (formData, errors) => {
     if (
