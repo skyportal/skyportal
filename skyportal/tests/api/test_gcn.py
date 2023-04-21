f8d829635375dce137257469901c98b70305c27a
Michael Coughlin <michael.w.coughlin@gmail.com>
2023-04-21 16:51:06 -0500

    ObservationPlanRequest query parameters (#4143)
    This PR adds query parameters for ObservationPlanRequests.

--- skyportal/tests/api/test_gcn.py
+++ skyportal/tests/api/test_gcn.py
@@ -882,7 +882,7 @@ def test_gcn_summary_observations(
 
             data = [
                 d
-                for d in data['data']
+                for d in data['data']['requests']
                 if d['gcnevent_id'] == gcnevent_id
                 and d['allocation_id'] == allocation_id
             ]
