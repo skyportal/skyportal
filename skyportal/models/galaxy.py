935de9bae4f59006b637d5c1c607217556b59c81
Michael Coughlin <michael.w.coughlin@gmail.com>
2023-05-15 16:42:25 -0500

    Add some NED columns

--- skyportal/models/galaxy.py
+++ skyportal/models/galaxy.py
@@ -29,9 +29,15 @@ class Galaxy(Base, ca.Point):
     redshift_error = sa.Column(sa.Float, nullable=True, doc="Redshift error.")
 
     sfr_fuv = sa.Column(sa.Float, nullable=True, doc="SFR based on FUV [Msol/yr]")
+    sfr_w4 = sa.Column(sa.Float, nullable=True, doc="SFR based on W4 [Msol/yr]")
+
     mstar = sa.Column(sa.Float, nullable=True, doc="Stellar mass [log(Msol)]")
     magb = sa.Column(sa.Float, nullable=True, doc="B band magnitude [mag]")
     magk = sa.Column(sa.Float, nullable=True, doc="K band magnitude")
+
+    mag_fuv = sa.Column(sa.Float, nullable=True, doc="FUV band magnitude")
+    mag_nuv = sa.Column(sa.Float, nullable=True, doc="NUV band magnitude")
+
     a = sa.Column(sa.Float, nullable=True, doc="semi-major axis in arcsec [arcsec]")
     b2a = sa.Column(sa.Float, nullable=True, doc="semi-minor to semi-major axis ratio")
     pa = sa.Column(sa.Float, nullable=True, doc="position angle in degrees")
