d6def038b9ee08a5f42dd11a4a4715754ab6d760
Theodlz <theophile.dulaz@gmail.com>
2023-05-25 11:04:03 -0700

    pin baselayer to 0375720960c3db2d333aaeb9bf383b47093fab60

--- skyportal/initial_setup.py
+++ skyportal/initial_setup.py
@@ -1,13 +1,14 @@
-import os
-from pathlib import Path
 import argparse
+import os
 from email.utils import parseaddr
-from baselayer.app.env import load_env
-from baselayer.app.model_util import status, drop_tables, create_tables
-from social_tornado.models import TornadoStorage
-from skyportal.models import init_db, Base, User, DBSession
+from pathlib import Path
 
 import model_util
+
+from baselayer.app.env import load_env
+from baselayer.app.model_util import create_tables, drop_tables, status
+from baselayer.app.psa import TornadoStorage
+from skyportal.models import Base, DBSession, User, init_db
 
 """
 usage: initial_setup.py [-h] [--nodrop] [--adminusername ADMINUSER]
