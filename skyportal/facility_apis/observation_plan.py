2c34beee8851d9c8639392b4f2dc98aad07d86a5
Theodlz <theophile.dulaz@gmail.com>
2023-05-30 12:46:10 -0700

    uncomment doMindifFilt param

--- skyportal/facility_apis/observation_plan.py
+++ skyportal/facility_apis/observation_plan.py
@@ -443,7 +443,9 @@ def generate_plan(
             'powerlaw_cl': request.payload["integrated_probability"],
             'telescopes': [request.instrument.name for request in requests],
             # minimum difference between observations of the same field
-            # 'doMindifFilt': True if request.payload["minimum_time_difference"] > 0 else False, TODO: Understand how to use this parameter
+            'doMindifFilt': True
+            if request.payload["minimum_time_difference"] > 0
+            else False,
             'mindiff': request.payload["minimum_time_difference"] * 60,
             # maximum airmass with which to observae
             'airmass': request.payload["maximum_airmass"],
@@ -753,13 +755,6 @@ def generate_plan(
                 tile_structs = gwemopt.skyportal.create_galaxy_from_skyportal(
                     params, map_struct, catalog_struct, regions=regions
                 )
-
-        # if min_diff_filt is set, we need to add the 'epochs_filter' key to the tile_structs
-        # TODO: Fix gwemopt so that this does not need to be done in SkyPortal
-        if params.get("doMindifFilt", False):
-            for key in tile_structs.keys():
-                for key2 in tile_structs[key].keys():
-                    tile_structs[key][key2]["epochs_filters"] = []
 
         log(f"Creating schedule(s) for ID(s): {','.join(observation_plan_id_strings)}")
 
