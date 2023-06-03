d6b247d70180b9675e8a02f9fdc54f18759ec5b9
Michael Coughlin <michael.w.coughlin@gmail.com>
2023-06-03 18:30:45 +0200

    Bulk TNS query

--- skyportal/handlers/api/__init__.py
+++ skyportal/handlers/api/__init__.py
@@ -172,7 +172,7 @@ from .sysinfo import SysInfoHandler
 from .config_handler import ConfigHandler
 from .taxonomy import TaxonomyHandler
 from .telescope import TelescopeHandler
-from .tns import ObjTNSHandler, SpectrumTNSHandler, TNSRobotHandler
+from .tns import ObjTNSHandler, BulkTNSHandler, SpectrumTNSHandler, TNSRobotHandler
 from .thumbnail import ThumbnailHandler, ThumbnailPathHandler
 from .user import (
     UserHandler,
