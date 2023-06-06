1cc40eb67d7d9f733a7643addf969de918a6377a
Theodlz <theophile.dulaz@gmail.com>
2023-06-06 10:47:22 -0700

    minor frontend improvements source page

---
+++ static/js/components/SourceGCNCrossmatchList.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import makeStyles from "@mui/styles/makeStyles";
+import AddIcon from "@mui/icons-material/Add";
+import Button from "./Button";
+
+const useStyles = makeStyles(() => ({
+  row: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+}));
+
+const SourceGCNCrossmatchList = ({ gcn_crossmatches }) => {
+  const classes = useStyles();
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  if (gcn_crossmatches?.length === 1) {
+    // show just the latest crossmatch, and a plus button to show the dialog with all of them
+    return (
+      <>
+        <div className={classes.row}>
+          <Link
+            to={`/gcn_events/${gcn_crossmatches[0].replace(" ", "T")}`}
+            role="link"
+            key={gcn_crossmatches[0]}
+          >
+            <Button size="small">{gcn_crossmatches[0]}</Button>
+          </Link>
+          <AddIcon
+            data-testid="addGcnEventAliasIconButton"
+            fontSize="small"
+            onClick={() => {
+              setDialogOpen(true);
+            }}
+          />
+        </div>
+        <Dialog
+          open={dialogOpen}
+          onClose={() => {
+            setDialogOpen(false);
+          }}
+          style={{ position: "fixed" }}
+        >
+          <DialogTitle>GCN Event Crossmatch</DialogTitle>
+          <DialogContent>
+            {gcn_crossmatches && (
+              <div>
+                {gcn_crossmatches.map((dateobs) => (
+                  <div key={dateobs}>
+                    <Link
+                      to={`/gcn_events/${dateobs.replace(" ", "T")}`}
+                      role="link"
+                      key={dateobs}
+                    >
+                      <Button size="small">{dateobs}</Button>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            )}
+          </DialogContent>
+        </Dialog>
+      </>
+    );
+  }
+  if (gcn_crossmatches?.length === 1) {
+    return (
+      <Link
+        to={`/gcn_events/${gcn_crossmatches[0].replace(" ", "T")}`}
+        role="link"
+        key={gcn_crossmatches[0]}
+      >
+        <Button size="small">{gcn_crossmatches[0]}</Button>
+      </Link>
+    );
+  }
+  return <></>;
+};
+
+SourceGCNCrossmatchList.propTypes = {
+  gcn_crossmatches: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default SourceGCNCrossmatchList;
