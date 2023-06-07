f6a8f1fc20ebd577e1a15d73dc67fc38c9fc1f57
Michael Coughlin <michael.w.coughlin@gmail.com>
2023-06-07 20:29:53 +0200

    Theo comments

--- skyportal/handlers/api/obj.py
+++ skyportal/handlers/api/obj.py
@@ -4,7 +4,16 @@ from sqlalchemy import func
 from baselayer.app.access import auth_or_token
 from baselayer.app.env import load_env
 from ..base import BaseHandler
-from ...models import Comment, Obj, PhotometricSeries, Photometry, Spectrum
+from ...models import (
+    Annotation,
+    Classification,
+    Comment,
+    Obj,
+    PhotometricSeries,
+    Photometry,
+    Spectrum,
+    SourcesConfirmedInGCN,
+)
 
 _, cfg = load_env()
 
@@ -40,9 +49,15 @@ class ObjHandler(BaseHandler):
             if obj is None:
                 return self.error(f"Cannot find object with ID {obj_id}.")
 
-            stmt = Spectrum.select(session.user_or_token).where(
-                Spectrum.obj_id == obj.id
-            )
+            stmt = sa.select(Annotation).where(Annotation.obj_id == obj.id)
+            count_stmt = sa.select(func.count()).select_from(stmt.distinct())
+            total_annotations = session.execute(count_stmt).scalar()
+            if total_annotations > 0:
+                return self.error(
+                    f"Please remove all associated annotations to object with ID {obj_id} before removing."
+                )
+
+            stmt = sa.select(Spectrum).where(Spectrum.obj_id == obj.id)
             count_stmt = sa.select(func.count()).select_from(stmt.distinct())
             total_spectrum = session.execute(count_stmt).scalar()
             if total_spectrum > 0:
@@ -50,9 +65,7 @@ class ObjHandler(BaseHandler):
                     f"Please remove all associated spectra to object with ID {obj_id} before removing."
                 )
 
-            stmt = Photometry.select(session.user_or_token).where(
-                Photometry.obj_id == obj.id
-            )
+            stmt = sa.select(Photometry).where(Photometry.obj_id == obj.id)
             count_stmt = sa.select(func.count()).select_from(stmt.distinct())
             total_photometry = session.execute(count_stmt).scalar()
             if total_photometry > 0:
@@ -60,7 +73,7 @@ class ObjHandler(BaseHandler):
                     f"Please remove all associated photometry to object with ID {obj_id} before removing."
                 )
 
-            stmt = PhotometricSeries.select(session.user_or_token).where(
+            stmt = sa.select(PhotometricSeries).where(
                 PhotometricSeries.obj_id == obj.id
             )
             count_stmt = sa.select(func.count()).select_from(stmt.distinct())
@@ -70,12 +83,30 @@ class ObjHandler(BaseHandler):
                     f"Please remove all associated photometric series to object with ID {obj_id} before removing."
                 )
 
-            stmt = Comment.select(session.user_or_token).where(Comment.obj_id == obj.id)
+            stmt = sa.select(Comment).where(Comment.obj_id == obj.id)
             count_stmt = sa.select(func.count()).select_from(stmt.distinct())
             total_comments = session.execute(count_stmt).scalar()
             if total_comments > 0:
                 return self.error(
                     f"Please remove all associated comments on object with ID {obj_id} before removing."
+                )
+
+            stmt = sa.select(Classification).where(Classification.obj_id == obj.id)
+            count_stmt = sa.select(func.count()).select_from(stmt.distinct())
+            total_classifications = session.execute(count_stmt).scalar()
+            if total_classifications > 0:
+                return self.error(
+                    f"Please remove all associated classifications on object with ID {obj_id} before removing."
+                )
+
+            stmt = sa.select(SourcesConfirmedInGCN).where(
+                SourcesConfirmedInGCN.obj_id == obj.id
+            )
+            count_stmt = sa.select(func.count()).select_from(stmt.distinct())
+            total_sources_in_gcn = session.execute(count_stmt).scalar()
+            if total_sources_in_gcn > 0:
+                return self.error(
+                    f"Please remove all associated sources in gcns associated with the object with ID {obj_id} before removing."
                 )
 
             session.delete(obj)
