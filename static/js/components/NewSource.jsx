f208fe7a329983bb32d10b0f36f798349b7bd4b7
Theophile du Laz <theophile.dulaz@gmail.com>
2023-06-16 13:02:04 -0700

    Frontend: Minor user requests (#4369)
    * ordered allocations in lists, avoid GCN notification issues, NewSource form bugfix

--- static/js/components/NewSource.jsx
+++ static/js/components/NewSource.jsx
@@ -25,55 +25,72 @@ const NewSource = ({ classes }) => {
   const dispatch = useDispatch();
   const groups = useSelector((state) => state.groups.userAccessible);
   const [selectedGroupIds, setSelectedGroupIds] = useState([]);
+  const [selectedFormData, setSelectedFormData] = useState({
+    id: "",
+    ra: "",
+    dec: "",
+  });
 
   const handleSubmit = async ({ formData }) => {
     let data = null;
-    if (formData.ra?.includes(":")) {
-      formData.ra = hours_to_ra(formData.ra);
+    if (formData?.ra?.includes(":")) {
+      formData.ra = hours_to_ra(formData?.ra);
     } else {
-      formData.ra = parseFloat(formData.ra);
+      formData.ra = parseFloat(formData?.ra);
     }
-    if (formData.dec?.includes(":")) {
-      formData.dec = dms_to_dec(formData.dec);
+    if (formData?.dec?.includes(":")) {
+      formData.dec = dms_to_dec(formData?.dec);
     } else {
-      formData.dec = parseFloat(formData.dec);
+      formData.dec = parseFloat(formData?.dec);
     }
-    data = await dispatch(checkSource(formData.id, formData));
-    if (data.data !== "A source of that name does not exist.") {
-      dispatch(showNotification(data.data, "error"));
+    if (
+      formData?.id === "" ||
+      formData?.id === null ||
+      formData?.id === undefined ||
+      !formData?.id
+    ) {
+      dispatch(showNotification("Please enter a source ID.", "error"));
     } else {
-      if (selectedGroupIds.length > 0) {
-        formData.group_ids = selectedGroupIds;
-      }
-      const result = await dispatch(saveSource(formData));
-      if (result.status === "success") {
-        dispatch(showNotification("Source saved"));
-        navigate(`/source/${formData.id}`);
+      data = await dispatch(checkSource(formData?.id, formData));
+      if (data.data !== "A source of that name does not exist.") {
+        dispatch(showNotification(data.data, "error"));
+      } else {
+        if (selectedGroupIds.length > 0) {
+          formData.group_ids = selectedGroupIds;
+        }
+        const result = await dispatch(saveSource(formData));
+        if (result.status === "success") {
+          dispatch(showNotification("Source saved"));
+          navigate(`/source/${formData.id}`);
+        }
       }
     }
   };
 
   function validate(formData, errors) {
-    if (selectedGroupIds.length === 0) {
+    if ((formData?.ra !== "" || formData?.dec !== "") && formData?.id === "") {
+      errors.id.addError("Please enter a source ID.");
+    }
+    if (selectedGroupIds?.length === 0 && formData?.id !== "") {
       errors.id.addError("Select at least one group.");
     }
-    if (formData.id.indexOf(" ") >= 0) {
+    if ((formData?.id || "").indexOf(" ") >= 0) {
       errors.id.addError("IDs are not allowed to have spaces, please fix.");
     }
-    if (formData.ra?.includes(":")) {
+    if ((formData?.ra || "").includes(":")) {
       formData.ra = hours_to_ra(formData.ra);
     } else {
       formData.ra = parseFloat(formData.ra);
     }
-    if (formData.ra < 0 || formData.ra >= 360) {
+    if (formData?.ra < 0 || formData?.ra >= 360) {
       errors.ra.addError("0 <= RA < 360, please fix.");
     }
-    if (formData.dec?.includes(":")) {
+    if ((formData?.dec || "").includes(":")) {
       formData.dec = dms_to_dec(formData.dec);
     } else {
       formData.dec = parseFloat(formData.dec);
     }
-    if (formData.dec < -90 || formData.dec > 90) {
+    if (formData?.dec < -90 || formData?.dec > 90) {
       errors.ra.addError("-90 <= Declination <= 90, please fix.");
     }
     return errors;
@@ -114,6 +131,8 @@ const NewSource = ({ classes }) => {
             />
             <Form
               schema={sourceFormSchema}
+              formData={selectedFormData}
+              onChange={({ formData }) => setSelectedFormData(formData)}
               validator={validator}
               onSubmit={handleSubmit}
               // eslint-disable-next-line react/jsx-no-bind
