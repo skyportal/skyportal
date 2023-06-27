62e294df3b37a1e99bdf5c8966b843beec6be618
Michael Coughlin <michael.w.coughlin@gmail.com>
2023-06-27 17:30:44 +0200

    numberObservations to TreasureMap submission query (#4394)
    This PR adds the number of observations to the treasuremap submission query.

--- static/js/components/GcnSelectionForm.jsx
+++ static/js/components/GcnSelectionForm.jsx
@@ -606,6 +606,7 @@ const GcnSelectionForm = ({
       localizationCumprob: filterParams.localizationCumprob,
       localizationName: filterParams.localizationName,
       localizationDateobs: dateobs,
+      numberObservations: filterParams?.numberDetections || 1,
     };
     await dispatch(observationsActions.submitObservationsTreasureMap(id, data));
     setIsSubmittingTreasureMap(null);
