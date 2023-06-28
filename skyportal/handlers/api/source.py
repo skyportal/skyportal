cb148bd228272fb8e0b47193fcd48b34fa61609d
Michael Coughlin <michael.w.coughlin@gmail.com>
2023-06-28 10:30:52 +0200

    remove commented out code

--- skyportal/handlers/api/source.py
+++ skyportal/handlers/api/source.py
@@ -2735,8 +2735,7 @@ class SourceHandler(BaseHandler):
                 return self.success(data=source_info)
 
         with self.Session() as session:
-            # try:
-            if True:
+            try:
                 query_results = await get_sources(
                     self.associated_user_object.id,
                     session,
@@ -2813,8 +2812,8 @@ class SourceHandler(BaseHandler):
                     total_matches=total_matches,
                     includeGeoJSON=includeGeoJSON,
                 )
-            # except Exception as e:
-            #    return self.error(f'Cannot retrieve sources: {str(e)}')
+            except Exception as e:
+                return self.error(f'Cannot retrieve sources: {str(e)}')
 
             query_size = sizeof(query_results)
             if query_size >= SIZE_WARNING_THRESHOLD:
