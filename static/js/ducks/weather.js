7c0d539fdd779116229030c46d2a0951103c83ab
Michael Coughlin <michael.w.coughlin@gmail.com>
2023-07-05 12:22:21 -0500

    Add telescope summary page (#4430)
    This PR adds a telescope summary page with associated instruments and allocations tables.

--- static/js/ducks/weather.js
+++ static/js/ducks/weather.js
@@ -7,7 +7,10 @@ const FETCH_WEATHER = "skyportal/FETCH_WEATHER";
 const FETCH_WEATHER_OK = "skyportal/FETCH_WEATHER_OK";
 
 // eslint-disable-next-line import/prefer-default-export
-export function fetchWeather() {
+export function fetchWeather(telescope_id = null) {
+  if (telescope_id) {
+    return API.GET(`/api/weather?telescope_id=${telescope_id}`, FETCH_WEATHER);
+  }
   return API.GET(`/api/weather`, FETCH_WEATHER);
 }
 
