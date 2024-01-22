c7accf804cac686e4f6b16ef71dbab5b018ddead
Theodlz <theophile.dulaz@gmail.com>
2024-01-22 07:40:47 -0800

    3rd attempt to fix tests, won't work until new baselayer is pinned

--- skyportal/tests/frontend/test_quick_search.py
+++ skyportal/tests/frontend/test_quick_search.py
@@ -1,6 +1,16 @@
 import pytest
 
-# Adding a comment here to make flake8 and black play nicely together (spacing below)
+
+def remove_notification(driver):
+    notification_xpath = '//*[contains(@data-testid, "notification-")]'
+    while True:
+        try:
+            driver.wait_for_xpath(notification_xpath, timeout=3)
+            driver.click_xpath(notification_xpath)
+            driver.wait_for_xpath_to_disappear(notification_xpath, timeout=3)
+        except Exception:
+            driver.wait_for_xpath_to_disappear(notification_xpath, timeout=3)
+            break
 
 
 @pytest.mark.flaky(reruns=3)
@@ -12,6 +22,8 @@ def test_quick_search(
 ):
     driver.get(f"/become_user/{super_admin_user.id}")
     driver.get("/")
+    remove_notification(driver)
+
     driver.wait_for_xpath('//*[@id="quick-search-bar"]').send_keys(public_source.id)
     driver.click_xpath('//*[@id="quick-search-bar-listbox"]')
     # Should be redirected to source page; check for elements that should render
