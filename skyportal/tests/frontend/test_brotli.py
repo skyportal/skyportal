df34d4586bd4646c064d665044c6c31bcc8e3f3f
Theodlz <theophile.dulaz@gmail.com>
2024-02-10 14:50:40 -0800

    remove print from test

--- skyportal/tests/frontend/test_brotli.py
+++ skyportal/tests/frontend/test_brotli.py
@@ -8,10 +8,6 @@ server_url = f'http://localhost:{cfg["ports.app"]}'
 
 
 def test_brotli():
-    # Test that the server is using Brotli compression
-    # when requested by the client
     r = requests.get(server_url, headers={"Accept-Encoding": "br"})
-    print(r.status_code)
-    print(r.headers)
     assert r.status_code == 200
     assert r.headers.get("Content-Encoding") == "br"
