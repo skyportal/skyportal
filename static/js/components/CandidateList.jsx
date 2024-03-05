d6b6ddf3edf7b67fe5434600d859cdfcdffe4896
Theodlz <theophile.dulaz@gmail.com>
2024-03-04 20:27:27 -0800

    reset numPerPage to 25

--- static/js/components/CandidateList.jsx
+++ static/js/components/CandidateList.jsx
@@ -36,8 +36,8 @@ import { ra_to_hours, dec_to_dms } from "../units";
 
 import * as candidatesActions from "../ducks/candidates";
 
-const numPerPage = 2;
-const numPerPageOffset = 1;
+const numPerPage = 25;
+const numPerPageOffset = 5;
 
 const useStyles = makeStyles((theme) => ({
   listPaper: {
@@ -837,6 +837,8 @@ const CandidateList = () => {
   filterGroups?.forEach((g) => {
     groupIds.push(g.id);
   });
+
+  console.log("length of candidates", candidates?.length);
 
   return (
     <div>
