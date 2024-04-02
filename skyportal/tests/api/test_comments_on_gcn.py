53048b3ba722afe1a89b9a19c7a1773da59abe5b
Theodlz <theophile.dulaz@gmail.com>
2024-04-02 16:35:56 -0700

    attempt to fix test comments on gcn

--- skyportal/tests/api/test_comments_on_gcn.py
+++ skyportal/tests/api/test_comments_on_gcn.py
@@ -6,51 +6,78 @@ import numpy as np
 from skyportal.tests import api
 
 
+def add_gw190425_gcn_event(super_admin_token):
+    dateobs = "2019-04-25T08:18:05"
+    status, data = api('GET', f'gcn_event/{dateobs}', token=super_admin_token)
+
+    if status == 404:
+        datafile = f'{os.path.dirname(__file__)}/../data/GW190425_initial.xml'
+        with open(datafile, 'rb') as fid:
+            payload = fid.read()
+        data = {'xml': payload}
+
+        status, data = api('POST', 'gcn_event', data=data, token=super_admin_token)
+        assert status == 200
+        assert data['status'] == 'success'
+        gcnevent_id = data['data']['gcnevent_id']
+
+        # wait for event to load
+        n_times = 0
+        while n_times < 5:
+            status, data = api(
+                'GET', "gcn_event/2019-04-25T08:18:05", token=super_admin_token
+            )
+            if data['status'] == 'success':
+                break
+            time.sleep(2)
+            n_times += 1
+        assert n_times < 5
+
+        # wait for the localization to load
+        params = {"include2DMap": True}
+
+        n_times_2 = 0
+        while n_times_2 < 5:
+            status, data = api(
+                'GET',
+                'localization/2019-04-25T08:18:05/name/bayestar.fits.gz',
+                token=super_admin_token,
+                params=params,
+            )
+
+            if data['status'] == 'success':
+                data = data["data"]
+                assert data["dateobs"] == "2019-04-25T08:18:05"
+                assert data["localization_name"] == "bayestar.fits.gz"
+                assert np.isclose(np.sum(data["flat_2d"]), 1)
+                break
+            else:
+                time.sleep(2)
+                n_times_2 += 1
+        assert n_times_2 < 5
+
+        return gcnevent_id
+    else:
+        return data['data']['gcnevent_id']
+
+
+def delete_gw190425_gcn_event(super_admin_token):
+    # delete the event
+    status, data = api(
+        'DELETE', 'gcn_event/2019-04-25T08:18:05', token=super_admin_token
+    )
+    print(status)
+    print(data)
+
+    # query it to make sure that it is gone
+    status, data = api('GET', 'gcn_event/2019-04-25T08:18:05', token=super_admin_token)
+    assert status == 404
+
+
 def test_add_and_retrieve_comment_on_gcn(
     comment_token, upload_data_token, public_group, super_admin_token
 ):
-    datafile = f'{os.path.dirname(__file__)}/../data/GW190425_initial.xml'
-    with open(datafile, 'rb') as fid:
-        payload = fid.read()
-    data = {'xml': payload}
-
-    status, data = api('POST', 'gcn_event', data=data, token=super_admin_token)
-    assert status == 200
-    assert data['status'] == 'success'
-    gcnevent_id = data['data']['gcnevent_id']
-
-    # wait for event to load
-    n_times = 0
-    for n_times in range(26):
-        status, data = api(
-            'GET', "gcn_event/2019-08-14T21:10:39", token=super_admin_token
-        )
-        if data['status'] == 'success':
-            break
-        time.sleep(2)
-    assert n_times < 25
-
-    # wait for the localization to load
-    params = {"include2DMap": True}
-
-    n_times_2 = 0
-    for n_times_2 in range(26):
-        status, data = api(
-            'GET',
-            'localization/2019-08-14T21:10:39/name/LALInference.v1.fits.gz',
-            token=super_admin_token,
-            params=params,
-        )
-
-        if data['status'] == 'success':
-            data = data["data"]
-            assert data["dateobs"] == "2019-08-14T21:10:39"
-            assert data["localization_name"] == "LALInference.v1.fits.gz"
-            assert np.isclose(np.sum(data["flat_2d"]), 1)
-            break
-        else:
-            time.sleep(2)
-    assert n_times_2 < 25
+    gcnevent_id = add_gw190425_gcn_event(super_admin_token)
 
     status, data = api(
         'POST',
@@ -72,21 +99,11 @@ def test_add_and_retrieve_comment_on_gcn(
     assert data['data']['text'] == 'Comment text'
 
     # delete the event
-    status, data = api(
-        'DELETE', 'gcn_event/2019-04-25T08:18:05', token=super_admin_token
-    )
+    delete_gw190425_gcn_event(super_admin_token)
 
 
 def test_delete_comment_on_gcn(comment_token, public_group, super_admin_token):
-    datafile = f'{os.path.dirname(__file__)}/../data/GW190425_initial.xml'
-    with open(datafile, 'rb') as fid:
-        payload = fid.read()
-    data = {'xml': payload}
-
-    status, data = api('POST', 'gcn_event', data=data, token=super_admin_token)
-    assert status == 200
-    assert data['status'] == 'success'
-    gcnevent_id = data['data']['gcnevent_id']
+    gcnevent_id = add_gw190425_gcn_event(super_admin_token)
 
     status, data = api(
         'POST',
@@ -128,6 +145,4 @@ def test_delete_comment_on_gcn(comment_token, public_group, super_admin_token):
     assert status == 403
 
     # delete the event
-    status, data = api(
-        'DELETE', 'gcn_event/2019-04-25T08:18:05', token=super_admin_token
-    )
+    delete_gw190425_gcn_event(super_admin_token)
