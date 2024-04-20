a0e3bb1d6881fe2fd9d129ff1418248429402fa9
Michael Coughlin <michael.w.coughlin@gmail.com>
2024-04-20 12:45:29 -0500

    Add 0 as swift trigger option

--- skyportal/facility_apis/swift.py
+++ skyportal/facility_apis/swift.py
@@ -162,8 +162,8 @@ class UVOTXRTRequest:
         too.exp_time_just = request.payload["exp_time_just"]
         too.immediate_objective = request.payload["immediate_objective"]
 
-        if request.payload["urgency"] not in ["1", "2", "3", "4"]:
-            raise ValueError('urgency not in ["1", "2", "3", "4"].')
+        if request.payload["urgency"] not in ["0", "1", "2", "3", "4"]:
+            raise ValueError('urgency not one of 0, 1, 2, 3, or 4.')
         too.urgency = int(request.payload["urgency"])
 
         if request.payload["obs_type"] not in [
