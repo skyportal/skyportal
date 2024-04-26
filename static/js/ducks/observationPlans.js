e257d91fffd181cc8cbdaf5aed0e5ddae37a110b
Michael Coughlin <michael.w.coughlin@gmail.com>
2024-04-26 12:38:36 -0500

    Allocation observation plan table (#4897)
    This PR adds a table for plan status for individual allocations.

--- static/js/ducks/observationPlans.js
+++ static/js/ducks/observationPlans.js
@@ -1,11 +1,39 @@
 import * as API from "../API";
+import store from "../store";
 
 const EXISTING_PLAN_WITH_NAME = "skyportal/EXISTING_PLAN_WITH_NAME";
 
-const planWithSameNameExists = (planName) =>
+const FETCH_ALLOCATION_OBSERVATION_PLANS =
+  "skyportal/FETCH_ALLOCATION_OBSERVATION_PLANS";
+const FETCH_ALLOCATION_OBSERVATION_PLANS_OK =
+  "skyportal/FETCH_ALLOCATION_OBSERVATION_PLANS_OK";
+
+export const planWithSameNameExists = (planName) =>
   API.GET(
     `/api/observation_plan/plan_names?name=${planName}`,
     EXISTING_PLAN_WITH_NAME,
   );
 
-export default planWithSameNameExists;
+export const fetchAllocationObservationPlans = (id, params = {}) =>
+  API.GET(
+    `/api/allocation/observation_plans/${id}`,
+    FETCH_ALLOCATION_OBSERVATION_PLANS,
+    params,
+  );
+
+const reducer = (state = { observationPlanList: [] }, action) => {
+  switch (action.type) {
+    case FETCH_ALLOCATION_OBSERVATION_PLANS_OK: {
+      const { observation_plan_requests, totalMatches } = action.data;
+      return {
+        ...state,
+        observation_plan_requests,
+        totalMatches,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+store.injectReducer("observation_plans", reducer);
