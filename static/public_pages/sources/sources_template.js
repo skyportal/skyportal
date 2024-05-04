406449b102a406ddb7294d7c2c99e8d92721d10a
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-05-04 14:14:02 -0700

    Add a "view all" button to display all the versions of a source. #1

--- static/public_pages/sources/sources_template.js
+++ static/public_pages/sources/sources_template.js
@@ -21,3 +21,13 @@ function filterSources() {
     }
   });
 }
+
+// Display all the versions of a source
+function displayVersions(sourceId) {
+  const versions = document.getElementById(`versions-of-source-${sourceId}`);
+  if (versions.style.display === "none") {
+    versions.style.display = "block";
+  } else {
+    versions.style.display = "none";
+  }
+}
