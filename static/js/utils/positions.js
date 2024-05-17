65102a63e817d392d2acc7860c9b1987bbdd672b
Theodlz <theophile.dulaz@gmail.com>
2024-05-17 10:25:10 -0700

    remove unnecessary comment

--- static/js/utils/positions.js
+++ static/js/utils/positions.js
@@ -168,39 +168,6 @@ function moonGeoJSON(d) {
   };
 }
 
-// implement this python method to calculate a great circle distance in javascript:
-// def great_circle_distance(ra1_deg, dec1_deg, ra2_deg, dec2_deg):
-// """
-//     Distance between two points on the sphere
-// :param ra1_deg:
-// :param dec1_deg:
-// :param ra2_deg:
-// :param dec2_deg:
-// :return: distance in degrees
-// """
-// # this is orders of magnitude faster than astropy.coordinates.Skycoord.separation
-// DEGRA = np.pi / 180.0
-// ra1, dec1, ra2, dec2 = (
-//     ra1_deg * DEGRA,
-//     dec1_deg * DEGRA,
-//     ra2_deg * DEGRA,
-//     dec2_deg * DEGRA,
-// )
-// delta_ra = np.abs(ra2 - ra1)
-// distance = np.arctan2(
-//     np.sqrt(
-//         (np.cos(dec2) * np.sin(delta_ra)) ** 2
-//         + (
-//             np.cos(dec1) * np.sin(dec2)
-//             - np.sin(dec1) * np.cos(dec2) * np.cos(delta_ra)
-//         )
-//         ** 2
-//     ),
-//     np.sin(dec1) * np.sin(dec2) + np.cos(dec1) * np.cos(dec2) * np.cos(delta_ra),
-// )
-
-// return distance * 180.0 / np.pi
-
 function greatCircleDistance(
   ra1_deg,
   dec1_deg,
