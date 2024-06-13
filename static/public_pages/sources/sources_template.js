3b51292a6aa8926c950285c8e0d4055ef231494f
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-06-13 12:48:38 -0700

    Improve source_template javascript.

--- static/public_pages/sources/sources_template.js
+++ static/public_pages/sources/sources_template.js
@@ -6,7 +6,7 @@ function filterSources() {
     .getElementById("search-bar")
     .value.trim()
     .toLowerCase();
-  const sources = document.getElementsByClassName("sourceAndVersions");
+  const sources = document.getElementsByClassName("source");
 
   Array.from(sources).forEach((source) => {
     const sourceId = source.id.toLowerCase();
@@ -15,7 +15,6 @@ function filterSources() {
       const h2 = source.querySelector("h2");
       const re = new RegExp(searchValue, "gi");
       h2.innerHTML = source.id.replace(re, (match) => `<mark>${match}</mark>`);
-      // Display the source versions
       source.style.display = "";
     } else {
       source.style.display = "none";
