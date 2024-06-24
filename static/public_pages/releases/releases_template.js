484c04d6448168f1e86511e583633eaeafda1678
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-06-24 12:14:05 -0700

    Improve search bar for releases to include the possibility to search in the description.

--- static/public_pages/releases/releases_template.js
+++ static/public_pages/releases/releases_template.js
@@ -7,15 +7,27 @@ function filterReleases() {
     .value.trim()
     .toLowerCase();
   const releases = document.getElementsByClassName("release");
+  const includeDescription =
+    document.getElementById("includeDescription").checked;
 
   Array.from(releases).forEach((release) => {
     const releaseName = release.getElementsByClassName("releaseName")[0];
-    if (releaseName.textContent.toLowerCase().includes(searchValue)) {
+    const releaseDescription =
+      release.getElementsByClassName("releaseDescription")[0];
+    if (
+      releaseName.textContent.toLowerCase().includes(searchValue) ||
+      (includeDescription &&
+        releaseDescription.textContent.toLowerCase().includes(searchValue))
+    ) {
       // Mark the search value in the source name
       const re = new RegExp(searchValue, "gi");
       releaseName.innerHTML = releaseName.textContent.replace(
         re,
         (match) => `<mark>${match}</mark>`,
+      );
+      releaseDescription.innerHTML = releaseDescription.textContent.replace(
+        re,
+        (match) => (includeDescription ? `<mark>${match}</mark>` : match),
       );
       release.style.display = "";
     } else {
