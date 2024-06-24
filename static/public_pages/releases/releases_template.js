4596b887b8c08e5357f9869c5fcd91775e291649
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-06-24 11:37:53 -0700

    Create the template for the list of all the releases with name and description. Filter by name with search bar.

---
+++ static/public_pages/releases/releases_template.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+
+// Filter releases based on the search bar value
+function filterReleases() {
+  const searchValue = document
+    .getElementById("search-bar")
+    .value.trim()
+    .toLowerCase();
+  const releases = document.getElementsByClassName("release");
+
+  Array.from(releases).forEach((release) => {
+    const releaseName = release.querySelector("h2");
+    if (releaseName.textContent.toLowerCase().includes(searchValue)) {
+      // Mark the search value in the source name
+      const re = new RegExp(searchValue, "gi");
+      releaseName.innerHTML = releaseName.textContent.replace(
+        re,
+        (match) => `<mark>${match}</mark>`,
+      );
+      release.style.display = "";
+    } else {
+      release.style.display = "none";
+    }
+  });
+}
