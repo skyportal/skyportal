9c49c650fbc43cca766cdab9d4ad489c8a83b6dc
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-06-24 18:59:12 -0700

    Add patch method to js duck.

--- static/js/ducks/public_pages/public_release.js
+++ static/js/ducks/public_pages/public_release.js
@@ -1,14 +1,22 @@
 import * as API from "../../API";
 
 const FETCH_PUBLIC_RELEASES = "skyportal/FETCH_PUBLIC_RELEASES";
-const CREATE_PUBLIC_RELEASE = "skyportal/CREATE_PUBLIC_RELEASE";
+const SUBMIT_PUBLIC_RELEASE = "skyportal/SUBMIT_PUBLIC_RELEASE";
+const UPDATE_PUBLIC_RELEASE = "skyportal/UPDATE_PUBLIC_RELEASE";
 const DELETE_PUBLIC_RELEASE = "skyportal/DELETE_PUBLIC_RELEASE";
 
 export const fetchPublicReleases = () =>
   API.GET("/api/public_pages/release", FETCH_PUBLIC_RELEASES);
 
-export const createPublicRelease = (payload) =>
-  API.POST("/api/public_pages/release", CREATE_PUBLIC_RELEASE, payload);
+export const submitPublicRelease = (payload) =>
+  API.POST("/api/public_pages/release", SUBMIT_PUBLIC_RELEASE, payload);
+
+export const updatePublicRelease = (releaseId, payload) =>
+  API.PATCH(
+    `/api/public_pages/release/${releaseId}`,
+    UPDATE_PUBLIC_RELEASE,
+    payload,
+  );
 
 export const deletePublicRelease = (releaseId) =>
   API.DELETE(`/api/public_pages/release/${releaseId}`, DELETE_PUBLIC_RELEASE);
