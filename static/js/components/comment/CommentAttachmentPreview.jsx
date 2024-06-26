0f82a6bd8afc7d267f77bbb3f29084e2bdb9c20e
Theophile du Laz <theophile.dulaz@gmail.com>
2024-06-26 13:15:37 -0700

    Comment Attachments: Preview Improvements (#5059)
    * drop the react-file-preview dependency in favor of using the browser's native support of PDFs (ala arxiv)
    * support more files for preview
    * render skymap fits files
    * nicer frontend to render text, video, and images
    * update Menu icon import in SidebarAndHeader.jsx.template

--- static/js/components/comment/CommentAttachmentPreview.jsx
+++ static/js/components/comment/CommentAttachmentPreview.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -14,7 +14,6 @@ import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import Typography from "@mui/material/Typography";
 import Tooltip from "@mui/material/Tooltip";
-import FilePreviewer, { FilePreviewerThumbnail } from "react-file-previewer";
 
 import ReactJson from "react-json-view";
 import { grey } from "@mui/material/colors";
@@ -63,7 +62,70 @@ const useStyles = makeStyles((theme) => ({
     border: "1px solid lightgray",
     padding: theme.spacing(1),
   },
+  textArea: {
+    width: "100%",
+    height: "50vh",
+  },
+  attachmentPreview: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+  },
 }));
+
+const VIDEO_EXTENSION_TO_TYPE = {
+  mp4: "video/mp4",
+  ogg: "video/ogg",
+  ogv: "video/ogg",
+  webm: "video/webm",
+};
+const AUDIO_EXTENSION_TO_TYPE = {
+  aac: "audio/aac",
+  mp3: "audio/mpeg",
+  oga: "audio/ogg",
+  wav: "audio/wav",
+};
+const IMAGE_EXTENSION_TO_TYPE = {
+  apng: "image/apng",
+  avif: "image/avif",
+  bmp: "image/bmp",
+  gif: "image/gif",
+  ico: "image/x-icon",
+  jpeg: "image/jpeg",
+  jpg: "image/jpeg",
+  png: "image/png",
+  svg: "image/svg+xml",
+  webp: "image/webp",
+};
+const SUPPORTED_VIDEO_TYPES = Object.keys(VIDEO_EXTENSION_TO_TYPE);
+const SUPPORTED_AUDIO_TYPES = Object.keys(AUDIO_EXTENSION_TO_TYPE);
+const SUPPORTED_IMAGE_TYPES = Object.keys(IMAGE_EXTENSION_TO_TYPE);
+const SUPPORTED_TEXT_TYPES = [
+  "txt",
+  "log",
+  "logs",
+  "csv",
+  "tsv",
+  "htm",
+  "html",
+  "js",
+  "mjs",
+  "json",
+  "xml",
+];
+const SUPPORTED_FITS_TYPES = ["fit", "fits", "fz"];
+
+const SUPPORTED_TYPES = [
+  ...SUPPORTED_AUDIO_TYPES,
+  ...SUPPORTED_VIDEO_TYPES,
+  ...SUPPORTED_IMAGE_TYPES,
+  ...SUPPORTED_TEXT_TYPES,
+  ...SUPPORTED_FITS_TYPES,
+  "pdf",
+];
 
 const dialogTitleStyles = (theme) => ({
   root: {
@@ -118,6 +180,34 @@ export const shortenFilename = (filename) => {
   return `${filename.slice(0, 12)}...`;
 };
 
+const readJsonFile = (commentAttachment, commentId) => {
+  let jsonFile = {};
+  try {
+    jsonFile =
+      commentId === commentAttachment?.commentId
+        ? JSON.parse(commentAttachment.attachment)
+        : {};
+  } catch (e) {
+    jsonFile = {
+      "JSON Preview Parsing Error": `${e.message}. Please download the file if you want to inspect it.`,
+    };
+  }
+  return jsonFile;
+};
+
+const readTextFile = (commentAttachment, commentId) => {
+  let txtFile = "";
+  try {
+    txtFile =
+      commentId === commentAttachment?.commentId
+        ? commentAttachment.attachment
+        : "";
+  } catch (e) {
+    txtFile = `Error reading text file: ${e.message}`;
+  }
+  return txtFile;
+};
+
 const CommentAttachmentPreview = ({
   filename,
   commentId,
@@ -130,6 +220,7 @@ const CommentAttachmentPreview = ({
   const classes = useStyles();
   const theme = useTheme();
   const darkTheme = theme.palette.mode === "dark";
+  const dispatch = useDispatch();
 
   function resourceType(state) {
     let type = "";
@@ -145,19 +236,11 @@ const CommentAttachmentPreview = ({
     return type;
   }
 
-  const dispatch = useDispatch();
   const commentAttachment = useSelector((state) => resourceType(state));
-  const cachedAttachmentCommentId = commentAttachment
-    ? commentAttachment.commentId
-    : null;
-  const isCached = commentId === cachedAttachmentCommentId;
-
   const [open, setOpen] = useState(false);
 
   const getURLs = () => {
-    const type = filename.includes(".") ? filename.split(".").pop() : "";
     let baseUrl = "";
-    // The FilePreviewer expects a url ending with .pdf for PDF files
     if (associatedResourceType === "gcn_event") {
       baseUrl = `/api/${associatedResourceType}/${gcnEventID}/comments/${commentId}/attachment`;
     } else if (associatedResourceType === "shift") {
@@ -168,74 +251,60 @@ const CommentAttachmentPreview = ({
       baseUrl = `/api/${associatedResourceType}/${objectID}/comments/${commentId}/attachment`;
     }
     return {
-      previewUrl: `${baseUrl}?preview=True`,
-      url: type === "pdf" ? `${baseUrl}.pdf` : baseUrl,
+      previewUrl: `${baseUrl}?preview=True&download=False`,
+      url: baseUrl,
     };
   };
 
+  const fileType = filename.includes(".")
+    ? filename.split(".").pop().toLowerCase()
+    : "";
+
+  useEffect(() => {
+    if (
+      SUPPORTED_TEXT_TYPES.includes(fileType) &&
+      commentId !== commentAttachment?.commentId &&
+      open
+    ) {
+      if (associatedResourceType === "sources") {
+        dispatch(sourceActions.getCommentTextAttachment(objectID, commentId));
+      } else if (associatedResourceType === "spectra") {
+        dispatch(
+          sourceActions.getCommentOnSpectrumTextAttachment(objectID, commentId),
+        );
+      } else if (associatedResourceType === "gcn_event") {
+        dispatch(
+          gcnEventActions.getCommentOnGcnEventTextAttachment(
+            gcnEventID,
+            commentId,
+          ),
+        );
+      } else if (associatedResourceType === "earthquake") {
+        dispatch(
+          earthquakeActions.getCommentOnEarthquakeTextAttachment(
+            earthquakeID,
+            commentId,
+          ),
+        );
+      } else if (associatedResourceType === "shift") {
+        dispatch(
+          shiftActions.getCommentOnShiftTextAttachment(shiftID, commentId),
+        );
+      }
+    }
+  }, [open]);
+
   const handleClickOpen = () => {
+    if (fileType === "pdf") {
+      window.open(getURLs().previewUrl, "_blank");
+      return;
+    }
     setOpen(true);
   };
 
   const handleClose = () => {
     setOpen(false);
   };
-
-  const fileType = filename.includes(".") ? filename.split(".").pop() : "";
-  const supportedType = [
-    "png",
-    "jpg",
-    "jpeg",
-    "pdf",
-    "gif",
-    "json",
-    "fit",
-    "fits",
-    "fz",
-  ].includes(fileType.toLowerCase());
-
-  let jsonFile = {};
-  try {
-    jsonFile = isCached
-      ? JSON.parse(Object.entries(commentAttachment)[1][1])
-      : {};
-  } catch (e) {
-    jsonFile = {
-      "JSON Preview Parsing Error": `${e.message}. Please download the file if you want to inspect it.`,
-    };
-  }
-
-  if (fileType.toLowerCase() === "json" && !isCached && open) {
-    if (associatedResourceType === "sources") {
-      dispatch(sourceActions.getCommentAttachmentPreview(objectID, commentId));
-    } else if (associatedResourceType === "spectra") {
-      dispatch(
-        sourceActions.getCommentOnSpectrumAttachmentPreview(
-          objectID,
-          commentId,
-        ),
-      );
-    } else if (associatedResourceType === "gcn_event") {
-      dispatch(
-        gcnEventActions.getCommentOnGcnEventAttachmentPreview(
-          gcnEventID,
-          commentId,
-        ),
-      );
-    } else if (associatedResourceType === "earthquake") {
-      dispatch(
-        earthquakeActions.getCommentOnEarthquakeAttachmentPreview(
-          earthquakeID,
-          commentId,
-        ),
-      );
-    } else if (associatedResourceType === "shift") {
-      dispatch(
-        shiftActions.getCommentOnShiftAttachmentPreview(shiftID, commentId),
-      );
-    }
-  }
-  const { previewUrl, url } = getURLs();
 
   return (
     <div>
@@ -258,6 +327,10 @@ const CommentAttachmentPreview = ({
           onClose={handleClose}
           style={{ position: "fixed" }}
           maxWidth="xlg"
+          fullWidth={
+            SUPPORTED_TYPES.includes(fileType) &&
+            !SUPPORTED_AUDIO_TYPES.includes(fileType)
+          }
         >
           <DialogTitle onClose={handleClose}>Attachment Preview</DialogTitle>
           <DialogContent dividers>
@@ -265,47 +338,70 @@ const CommentAttachmentPreview = ({
               <Typography variant="subtitle1" className={classes.filename}>
                 {filename}
               </Typography>
-              {supportedType && fileType === "pdf" && (
-                // Using FilePreviewerThumbnail results with PDF in very small
-                // preview with no way to edit it without losing resolution due
-                // to hard-coded in-line styling, so use the FilePreviewer
-                // component for PDF
-                <FilePreviewer file={{ url }} hideControls />
-              )}
-              {supportedType && fileType === "json" && (
-                <ReactJson
-                  src={jsonFile}
-                  name={false}
-                  theme={darkTheme ? "monokai" : "rjv-default"}
-                />
-              )}
-              {supportedType &&
-                ["jpeg", "jpg", "png", "gif"].includes(fileType) && (
+              <div className={classes.attachmentPreview}>
+                {fileType === "json" && (
+                  <ReactJson
+                    src={readJsonFile(commentAttachment, commentId)}
+                    name={false}
+                    theme={darkTheme ? "monokai" : "rjv-default"}
+                  />
+                )}
+                {SUPPORTED_TEXT_TYPES.includes(fileType) &&
+                  fileType !== "json" && (
+                    <textarea
+                      className={classes.textArea}
+                      value={readTextFile(commentAttachment, commentId)}
+                      readOnly
+                    />
+                  )}
+                {SUPPORTED_VIDEO_TYPES.includes(fileType) &&
+                  VIDEO_EXTENSION_TO_TYPE[fileType] && (
+                    <video
+                      controls
+                      style={{
+                        height: "68vh",
+                        maxWidth: "100%",
+                        width: "auto",
+                      }}
+                    >
+                      <source
+                        src={getURLs().previewUrl}
+                        type={VIDEO_EXTENSION_TO_TYPE[fileType]}
+                      />
+                      <track kind="captions" />
+                      Your browser does not support the video tag.
+                    </video>
+                  )}
+                {SUPPORTED_AUDIO_TYPES.includes(fileType) &&
+                  AUDIO_EXTENSION_TO_TYPE[fileType] && (
+                    <audio controls>
+                      <source
+                        src={getURLs().previewUrl}
+                        type={AUDIO_EXTENSION_TO_TYPE[fileType]}
+                      />
+                      <track kind="captions" />
+                      Your browser does not support the audio element.
+                    </audio>
+                  )}
+                {[...SUPPORTED_IMAGE_TYPES, ...SUPPORTED_FITS_TYPES].includes(
+                  fileType,
+                ) && (
                   <img
-                    src={previewUrl}
+                    src={getURLs().previewUrl}
                     alt={filename}
                     style={{
-                      maxHeight: "68vh",
+                      height: "68vh",
                       maxWidth: "100%",
                       width: "auto",
                     }}
                   />
                 )}
-              {supportedType &&
-                fileType !== "pdf" &&
-                fileType !== "json" &&
-                !["jpeg", "jpg", "png", "gif"].includes(fileType) && (
-                  <FilePreviewerThumbnail
-                    file={{ url: previewUrl }}
-                    style={{ width: "100px", height: "100px" }}
-                    hideControls
-                  />
+                {!SUPPORTED_TYPES.includes(fileType) && (
+                  <div className={classes.unsupportedType}>
+                    Previews are unavailable for {fileType.toUpperCase()} files.
+                  </div>
                 )}
-              {!supportedType && (
-                <div className={classes.unsupportedType}>
-                  Previews are unavailable for {fileType.toUpperCase()} files.
-                </div>
-              )}
+              </div>
             </div>
           </DialogContent>
           <DialogActions>
@@ -314,7 +410,7 @@ const CommentAttachmentPreview = ({
                 primary
                 size="large"
                 endIcon={<CloudDownloadIcon />}
-                href={url}
+                href={getURLs(fileType).url}
                 onClick={handleClose}
                 data-testid={`attachmentDownloadButton_${
                   filename.split(".")[0]
