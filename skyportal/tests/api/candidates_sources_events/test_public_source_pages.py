fcd5d49f08f6e86e8bba42ec2fc3c996efa401e7
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-07-27 14:27:59 -0700

    Instantiation of the public source page delete tests.

--- skyportal/tests/api/candidates_sources_events/test_public_source_pages.py
+++ skyportal/tests/api/candidates_sources_events/test_public_source_pages.py
@@ -6,7 +6,7 @@ def test_source_with_zero_public_pages(view_only_token, public_source):
         "GET", f"public_pages/source/{public_source.id}", token=view_only_token
     )
     assert_api(status, data)
-    assert data["data"] == []
+    assert len(data["data"]) == 0
 
 
 def test_non_group_manage_sources_cannot_create_page(view_only_token, public_source):
@@ -45,3 +45,55 @@ def test_group_manage_sources_create_page_with_data(
     )
     assert_api(status, data)
     assert len(data["data"]) == 1
+
+
+def test_non_group_manage_sources_cannot_delete_page(
+    view_only_token, manage_sources_token, public_source
+):
+    status, data = api(
+        "POST",
+        f"public_pages/source/{public_source.id}",
+        data={'options': {}},
+        token=manage_sources_token,
+    )
+    assert_api(status, data)
+
+    status, data = api(
+        "GET", f"public_pages/source/{public_source.id}", token=view_only_token
+    )
+    assert_api(status, data)
+    assert len(data["data"]) == 1
+    page_id = data["data"][0]["id"]
+
+    status, data = api(
+        "DELETE", f"public_pages/source/{page_id}", token=view_only_token
+    )
+    assert_api_fail(status, data, 401, "HTTP 401: Unauthorized")
+
+
+def test_group_manage_sources_delete_page(manage_sources_token, public_source):
+    status, data = api(
+        "POST",
+        f"public_pages/source/{public_source.id}",
+        data={'options': {}},
+        token=manage_sources_token,
+    )
+    assert_api(status, data)
+
+    status, data = api(
+        "GET", f"public_pages/source/{public_source.id}", token=manage_sources_token
+    )
+    assert_api(status, data)
+    assert len(data["data"]) == 1
+    page_id = data["data"][0]["id"]
+
+    status, data = api(
+        "DELETE", f"public_pages/source/{page_id}", token=manage_sources_token
+    )
+    assert_api(status, data)
+
+    status, data = api(
+        "GET", f"public_pages/source/{public_source.id}", token=manage_sources_token
+    )
+    assert_api(status, data)
+    assert len(data["data"]) == 0
