bf4f8893f2ffa2f591156b0a8d2bb5a5bb39d46f
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-07-27 14:11:40 -0700

    Instantiation of the public source page retrieve and create test.

---
+++ skyportal/tests/api/candidates_sources_events/test_public_source_pages.py
@@ -0,0 +1,47 @@
+from skyportal.tests import api, assert_api, assert_api_fail
+
+
+def test_source_with_zero_public_pages(view_only_token, public_source):
+    status, data = api(
+        "GET", f"public_pages/source/{public_source.id}", token=view_only_token
+    )
+    assert_api(status, data)
+    assert data["data"] == []
+
+
+def test_non_group_manage_sources_cannot_create_page(view_only_token, public_source):
+    status, data = api(
+        "POST",
+        f"public_pages/source/{public_source.id}",
+        data={},
+        token=view_only_token,
+    )
+    assert_api_fail(status, data, 401, "HTTP 401: Unauthorized")
+
+
+def test_group_manage_sources_create_page_no_data(manage_sources_token, public_source):
+    status, data = api(
+        "POST",
+        f"public_pages/source/{public_source.id}",
+        data={},
+        token=manage_sources_token,
+    )
+    assert_api_fail(status, data, 400, "No data provided")
+
+
+def test_group_manage_sources_create_page_with_data(
+    manage_sources_token, public_source
+):
+    status, data = api(
+        "POST",
+        f"public_pages/source/{public_source.id}",
+        data={'options': {}},
+        token=manage_sources_token,
+    )
+    assert_api(status, data)
+
+    status, data = api(
+        "GET", f"public_pages/source/{public_source.id}", token=manage_sources_token
+    )
+    assert_api(status, data)
+    assert len(data["data"]) == 1
