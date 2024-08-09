021ed7efcc084efed0c17adfced212d7d8b9a717
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-08-09 14:27:12 -0700

    Correct some bad coding line on public_source_page and source_groups.

--- skyportal/handlers/api/public_pages/public_source_page.py
+++ skyportal/handlers/api/public_pages/public_source_page.py
@@ -31,6 +31,8 @@ def calculate_hash(data):
 
 
 def process_thumbnails(thumbnails, ra, dec):
+    if thumbnails is None or ra is None or dec is None:
+        return []
     # Sort thumbnails by type, and remove 'DR8' thumbnail if 'LS' (that corresponds to DR9) thumbnail are present
     has_ls = any("ls" in thumbnail["type"] for thumbnail in thumbnails)
     thumbnails = sorted(
--- skyportal/handlers/api/source_groups.py
+++ skyportal/handlers/api/source_groups.py
@@ -49,7 +49,11 @@ class SourceGroupsHandler(BaseHandler):
                       List of group IDs from which specified source is to be unsaved.
                 required:
                   - objId
-                  - inviteGroupIds
+                anyOf:
+                  - required:
+                    - inviteGroupIds
+                  - required:
+                    - unsaveGroupIds
         responses:
           200:
             content:
@@ -192,9 +196,6 @@ class SourceGroupsHandler(BaseHandler):
             self.push_all(
                 action="skyportal/REFRESH_SOURCE", payload={"obj_key": obj.internal_key}
             )
-            # self.push_all(
-            #    action="skyportal/REFRESH_CANDIDATE", payload={"id": obj.internal_key}
-            # )
             return self.success()
 
     @permissions(['Upload data'])
