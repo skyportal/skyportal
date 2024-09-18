3bcf89c018ac09a5500200adf55e7c93689380ba
Theodlz <theophile.dulaz@gmail.com>
2024-09-18 14:15:19 -0700

    temporarely remove the Try It feature until we figure out the CORS in a future PR

--- tools/docs/patch-template.py
+++ tools/docs/patch-template.py
@@ -2,6 +2,10 @@ import json
 
 import jinja2
 
+from baselayer.app.env import load_env
+
+_, cfg = load_env()
+
 # open the openapi.json file
 # and patch the doc/api.html.template file with the new content
 with open('openapi.json') as f:
@@ -9,7 +13,17 @@ with open('openapi.json') as f:
 
 # here, we could add a list of servers to the openapi_spec
 # for the scalar docs to let the users point to when testing an endpoint
-openapi_spec['servers'] = []
+servers = cfg.get("docs.servers", [])
+# validate that servers have only 2 keys: url and description
+if not isinstance(servers, list) and servers is not None:
+    raise ValueError("API servers must be a list.")
+if servers is not None:
+    for server in servers:
+        if not all(k in server for k in ("url", "description")):
+            raise ValueError("Each server must have 'url' and 'description' keys.")
+    openapi_spec['servers'] = servers
+else:
+    openapi_spec['servers'] = []
 
 # Create a Jinja2 template environment
 template_env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath="./"))
