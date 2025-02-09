0eb770a086fb7ed24c25156a2b03b043de2abef5
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-09 00:08:36 -0600

    Improve naming.

--- static/js/ducks/candidate/candidate_scan_report.js
+++ static/js/ducks/candidate/candidate_scan_report.js
@@ -1,7 +1,7 @@
 import * as API from "../../API";
 
 const SUBMIT_CANDIDATE_TO_REPORT = "skyportal/SUBMIT_CANDIDATE_TO_REPORT";
-const FETCH_SCAN_REPORT = "skyportal/FETCH_SCAN_REPORT";
+const FETCH_CANDIDATE_SCAN_REPORT = "skyportal/FETCH_CANDIDATE_SCAN_REPORT";
 const DELETE_CANDIDATE_FROM_REPORT = "skyportal/DELETE_CANDIDATE_FROM_REPORT";
 
 export const submitCandidateToReport = (candidateId, payload) =>
@@ -11,8 +11,8 @@ export const submitCandidateToReport = (candidateId, payload) =>
     payload,
   );
 
-export const fetchScanReport = (params) =>
-  API.GET(`/api/candidates/scan_report`, FETCH_SCAN_REPORT, params);
+export const fetchCandidatesScanReport = (params) =>
+  API.GET(`/api/candidates/scan_report`, FETCH_CANDIDATE_SCAN_REPORT, params);
 
 export const deleteCandidateFromReport = (scanId) =>
   API.DELETE(
