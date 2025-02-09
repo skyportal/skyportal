34dd33fcfcc62bf94541081f3ec30b0da397ea23
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-08 22:48:26 -0600

    Improve naming.

--- static/js/ducks/candidate/candidate_scan_report.js
+++ static/js/ducks/candidate/candidate_scan_report.js
@@ -1,14 +1,17 @@
 import * as API from "../../API";
 
-const SAVE_SCAN_TO_REPORT = "skyportal/SAVE_SCAN_TO_REPORT";
+const SUBMIT_CANDIDATE_TO_REPORT = "skyportal/SUBMIT_CANDIDATE_TO_REPORT";
 const FETCH_SCAN_REPORT = "skyportal/FETCH_SCAN_REPORT";
-const DELETE_SCAN_FROM_REPORT = "skyportal/DELETE_SCAN_FROM_REPORT";
+const DELETE_CANDIDATE_FROM_REPORT = "skyportal/DELETE_CANDIDATE_FROM_REPORT";
 
-export const saveScanToReport = (payload) =>
-  API.POST(`/api/candidates/scan_report`, SAVE_SCAN_TO_REPORT, payload);
+export const submitCandidateToReport = (scanId, payload) =>
+  API.POST(`/api/candidates/scan_report`, SUBMIT_CANDIDATE_TO_REPORT, payload);
 
 export const fetchScanReport = (params) =>
   API.GET(`/api/candidates/scan_report`, FETCH_SCAN_REPORT, params);
 
-export const deleteScanFromReport = (scanId) =>
-  API.DELETE(`/api/candidates/scan_report/${scanId}`, DELETE_SCAN_FROM_REPORT);
+export const deleteCandidateFromReport = (scanId) =>
+  API.DELETE(
+    `/api/candidates/scan_report/${scanId}`,
+    DELETE_CANDIDATE_FROM_REPORT,
+  );
