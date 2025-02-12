5dc836b25af4040a8a8abe4bb02c10472373e07c
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-12 13:51:13 -0600

    adding notification for unchanged status.

--- skyportal/facility_apis/tarot.py
+++ skyportal/facility_apis/tarot.py
@@ -440,6 +440,18 @@ class TAROTAPI(FollowUpAPI):
                 new_request_status = f"complete"
 
         if new_request_status is None:
+            try:
+                flow = Flow()
+                flow.push(
+                    request.last_modified_by_id,
+                    "baselayer/SHOW_NOTIFICATION",
+                    payload={
+                        "note": "TAROT request status has not changed",
+                        "type": "warning",
+                    },
+                )
+            except Exception as e:
+                log(f"Failed to send notification: {e}")
             return
 
         request.status = new_request_status
