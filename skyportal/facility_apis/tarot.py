32cbf788843a6357e7b2c3ae67c6e93484810d79
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-13 21:32:45 -0600

    Update Tarot request status message for clarity.

--- skyportal/facility_apis/tarot.py
+++ skyportal/facility_apis/tarot.py
@@ -338,7 +338,7 @@ class TAROTAPI(FollowUpAPI):
                 )
             request.status = error_response
         else:
-            request.status = "submitted: please retrieve status"
+            request.status = "submitted: use retrieve to check status"
 
         transaction = FacilityTransaction(
             request=http.serialize_requests_request(response.request),
