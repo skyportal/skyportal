685a47d18ede887c51d0a1b188d1927e991e23dd
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-19 19:19:48 -0600

    Add generateCandidateScanReport function to handle report generation.

--- static/js/ducks/candidate/candidate_scan_report.js
+++ static/js/ducks/candidate/candidate_scan_report.js
@@ -6,12 +6,21 @@ import store from "../../store";
 const FETCH_CANDIDATE_SCAN_REPORT = "skyportal/FETCH_CANDIDATE_SCAN_REPORT";
 const FETCH_CANDIDATE_SCAN_REPORT_OK =
   "skyportal/FETCH_CANDIDATE_SCAN_REPORT_OK";
+const GENERATE_CANDIDATE_SCAN_REPORT =
+  "skyportal/GENERATE_CANDIDATE_SCAN_REPORT";
 const UPDATE_CANDIDATE_FROM_REPORT = "skyportal/UPDATE_CANDIDATE_FROM_REPORT";
 
 const REFRESH_CANDIDATE_SCAN_REPORT = "skyportal/REFRESH_CANDIDATE_SCAN_REPORT";
 
 export const fetchCandidatesScanReport = (params) =>
   API.GET(`/api/candidates/scan_report`, FETCH_CANDIDATE_SCAN_REPORT, params);
+
+export const generateCandidateScanReport = (payload) =>
+  API.POST(
+    `/api/candidates/scan_report`,
+    GENERATE_CANDIDATE_SCAN_REPORT,
+    payload,
+  );
 
 export const updateCandidateFromReport = (candidateFromReportId, payload) =>
   API.PATCH(
