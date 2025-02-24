fe4604b0aa7ae5c94af2311995ec1159b3452cb3
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-24 12:08:38 -0600

    Rename API endpoints for candidate scan reports to use plural form.

--- static/js/ducks/candidate/candidate_scan_report.js
+++ static/js/ducks/candidate/candidate_scan_report.js
@@ -13,18 +13,18 @@ const UPDATE_CANDIDATE_FROM_REPORT = "skyportal/UPDATE_CANDIDATE_FROM_REPORT";
 const REFRESH_CANDIDATE_SCAN_REPORT = "skyportal/REFRESH_CANDIDATE_SCAN_REPORT";
 
 export const fetchCandidatesScanReports = (params) =>
-  API.GET(`/api/candidates/scan_report`, FETCH_CANDIDATE_SCAN_REPORTS, params);
+  API.GET(`/api/candidates/scan_reports`, FETCH_CANDIDATE_SCAN_REPORTS, params);
 
 export const generateCandidateScanReport = (payload) =>
   API.POST(
-    `/api/candidates/scan_report`,
+    `/api/candidates/scan_reports`,
     GENERATE_CANDIDATE_SCAN_REPORT,
     payload,
   );
 
 export const updateCandidateFromReport = (candidateFromReportId, payload) =>
   API.PATCH(
-    `/api/candidates/scan_report/${candidateFromReportId}`,
+    `/api/candidates/scan_reports/${candidateFromReportId}`,
     UPDATE_CANDIDATE_FROM_REPORT,
     payload,
   );
