4b7c6b60bfa7afa72a2494f2428e25cb18c38338
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-03-09 22:56:03 -0500

    Add payload validation for observation types and parameters in Binospec API.

--- skyportal/facility_apis/mmt/binospec.py
+++ skyportal/facility_apis/mmt/binospec.py
@@ -6,6 +6,60 @@ from .utils import base_mmt_properties, catch_timeout_and_no_endpoint
 env, cfg = load_env()
 
 
+def check_payload(payload):
+    keys = payload.keys()
+
+    if "observation_type" not in keys or payload["observation_type"] not in [
+        "Imaging",
+        "Spectroscopy",
+    ]:
+        raise ValueError("A valid observation type must be provided")
+
+    if payload["observation_type"] == "Spectroscopy":
+        valid_ranges = {
+            270: range(5501, 7839),
+            600: range(5146, 8784),
+            1000: [
+                range(4108, 4684),
+                range(5181, 7274),
+                range(7363, 7968),
+                range(8153, 8773),
+                range(8897, 9280),
+            ],
+        }
+        if "grating" not in keys or payload["grating"] not in valid_ranges:
+            raise ValueError("A valid grating must be provided")
+        if payload["grating"] != 1000:
+            if (
+                "central_wavelength" not in keys
+                or payload["central_wavelength"] not in valid_ranges[payload["grating"]]
+            ):
+                raise ValueError("A valid central wavelength must be provided")
+        else:
+            if "central_wavelength" not in keys or not any(
+                payload["central_wavelength"] in r
+                for r in valid_ranges[payload["grating"]]
+            ):
+                raise ValueError("A valid central wavelength must be provided")
+        if "slit_width" not in keys or payload["slit_width"] not in [
+            "Longslit0_75",
+            "Longslit1",
+            "Longslit1_25",
+            "Longslit1_5",
+            "Longslit5",
+        ]:
+            raise ValueError("A valid slit width must be provided")
+        if "filter" not in keys or payload["filter"] not in ["LP3800", "LP3500"]:
+            raise ValueError("A valid filter must be provided")
+    else:
+        if "maskid" not in keys or payload["maskid"] not in [110]:
+            raise ValueError("A valid mask id must be provided")
+        if "exposure_time" not in keys or payload["exposure_time"] not in [400]:
+            raise ValueError("A valid exposure time must be provided")
+        if "filter" not in keys or payload["filter"] not in ["g", "r", "i", "z"]:
+            raise ValueError("A valid filter must be provided")
+
+
 class BINOSPECAPI(FollowUpAPI):
     """SkyPortal interface to BINOSPEC"""
 
