92f8b8afff4bacb9430a3738fe049d4addf86a1f
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-03-09 22:33:04 -0500

    Add payload validation for observation types and related parameters in MMIRS API.

--- skyportal/facility_apis/mmt/mmirs.py
+++ skyportal/facility_apis/mmt/mmirs.py
@@ -6,6 +6,66 @@ from .utils import base_mmt_properties, catch_timeout_and_no_endpoint
 env, cfg = load_env()
 
 
+def check_payload(payload):
+    keys = payload.keys()
+
+    if "observation_type" not in keys or payload["observation_type"] not in [
+        "Imaging",
+        "Spectroscopy",
+    ]:
+        raise ValueError("A valid observation type must be provided")
+
+    if payload["observation_type"] == "Specroscopy":
+        if "grism" not in keys or payload["grism"] not in ["J", "HK", "HK3"]:
+            raise ValueError("A valid grism must be provided")
+        if "readtab" not in keys or payload["readtab"] not in ["ramp_4.426"]:
+            raise ValueError("A valid read tab must be provided")
+        if "slitwidth" not in keys or payload["slitwidth"] not in [
+            "1pixel",
+            "2pixel",
+            "3pixel",
+            "4pixel",
+            "5pixel",
+            "6pixel",
+            "12pixel",
+        ]:
+            raise ValueError("A valid slit width must be provided")
+        if "slitwidthproperty" not in keys or payload["slitwidthproperty"] not in [
+            "long",
+            "short",
+        ]:
+            raise ValueError("A valid slit width property must be provided")
+        if "filter" not in keys or payload["filter"] not in ["HK", "zJ"]:
+            raise ValueError("A valid filter must be provided")
+    else:
+        if "dithersize" in keys and payload["dithersize"] not in [
+            5,
+            7,
+            10,
+            15,
+            20,
+            30,
+            60,
+            120,
+            210,
+        ]:
+            raise ValueError("A valid dither size must be provided")
+
+        if "readtab" not in keys or payload["readtab"] not in ["ramp_4.426"]:
+            raise ValueError("A valid read tab must be provided")
+        if "maskid" not in keys or payload["maskid"] not in [110]:
+            raise ValueError("A valid mask id must be provided")
+        if "exposure_time" not in keys or payload["exposure_time"] not in [400]:
+            raise ValueError("A valid exposure time must be provided")
+        if "nb_visits_per_night" not in keys or payload["nb_visits_per_night"] not in [
+            0,
+            1,
+        ]:
+            raise ValueError("A valid number of visits per night must be provided")
+        if "filter" not in keys or payload["filter"] not in ["J", "H", "K", "Ks"]:
+            raise ValueError("A valid filter must be provided")
+
+
 class MMIRSAPI(FollowUpAPI):
     """SkyPortal interface to the MMIRS"""
 
