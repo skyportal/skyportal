b8ea3e1c653c6c800980f32d151e819c3f07778b
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-03-13 22:16:29 -0500

    delete useless test.

--- skyportal/tests/api/test_hermes.py
+++ skyportal/tests/api/test_hermes.py
@@ -3,21 +3,11 @@ from skyportal.handlers.api.hermes.hermes import create_payload_and_header
 
 def test_hermes_payload_creation_and_validation(public_obj):
     data = {
-        "hermes_token": "Bad token",
+        "hermes_token": "BadToken",
         "topic": "hermes.test",
         "title": "Title test",
         "submitter": "Test user",
     }
-
-    try:
-        payload, header = create_payload_and_header(public_obj, data)
-    except ValueError as e:
-        assert (
-            str(e)
-            == 'Failed to validate payload: 403: {"detail":"Invalid token header. Token string should not contain spaces."}'
-        )
-
-    data["hermes_token"] = "BadToken"
 
     try:
         payload, header = create_payload_and_header(public_obj, data)
