a3c6a911573f5c81f4aa4c9ee6882169ecfe81c5
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-03-13 22:40:54 -0500

    Add validation for Hermes payload and enhance test coverage.

--- skyportal/tests/api/test_hermes.py
+++ skyportal/tests/api/test_hermes.py
@@ -1,4 +1,8 @@
-from skyportal.handlers.api.hermes.hermes import create_payload_and_header
+from skyportal.handlers.api.hermes.hermes import (
+    create_payload_and_header,
+    validate_payload_and_header,
+)
+from skyportal.tests import api
 
 
 def test_hermes_payload_creation_and_validation(public_obj):
@@ -9,7 +13,33 @@ def test_hermes_payload_creation_and_validation(public_obj):
         "submitter": "Test user",
     }
 
+    payload, header = create_payload_and_header(public_obj, data)
+
+    assert isinstance(payload, str)
+    assert header == {
+        "Authorization": f"Token BadToken",
+        "Content-Type": "application/json",
+    }
+
     try:
-        payload, header = create_payload_and_header(public_obj, data)
+        validate_payload_and_header(payload, header)
+    except ValueError as e:
+        assert str(e) == 'Failed to validate payload: 403: {"detail":"Invalid token."}'
+
+
+def test_hermes_publishing(public_obj, super_admin_token):
+    assert public_obj.photometry is not None
+
+    data = {
+        "hermes_token": "BadToken",
+        "topic": "hermes.test",
+        "title": "Title test",
+        "submitter": "Test user",
+    }
+
+    try:
+        status, data = api(
+            "POST", f"hermes/{public_obj.id}", data, token=super_admin_token
+        )
     except ValueError as e:
         assert str(e) == 'Failed to validate payload: 403: {"detail":"Invalid token."}'
