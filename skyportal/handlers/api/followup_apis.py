f7294e3e8a640e1fc7bf9635c6237e6bb2a3d7bd
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-03-24 12:03:18 -0500

    Refactor FollowupAPIsHandler in followup_apis.py to improve code structure by moving API dictionary initialization outside the class definition.

--- skyportal/handlers/api/followup_apis.py
+++ skyportal/handlers/api/followup_apis.py
@@ -4,13 +4,13 @@ from skyportal.enum_types import api_classnames
 
 from ..base import BaseHandler
 
+apis = {
+    api_name: getattr(facility_apis, api_name).frontend_render_api_info()
+    for api_name in api_classnames.enums
+}
+
 
 class FollowupAPIsHandler(BaseHandler):
     @auth_or_token
     def get(self):
-        apis = {
-            api_name: getattr(facility_apis, api_name).frontend_render_api_info()
-            for api_name in api_classnames.enums
-        }
-
         return self.success(data=apis)
