05308b6ed593655a3ebbe303cf1a0b58e8885fe7
Camille DOUZET <camille.douzet@ijclab.in2p3.fr>
2025-04-24 13:42:22 +0200

    enh : remove patch fof ObjTagHandler and refactor code

--- skyportal/models/tag.py
+++ skyportal/models/tag.py
@@ -1,33 +1,29 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import relationship
 
-from baselayer.app.models import AccessibleIfUserMatches, Base, join_model
+from baselayer.app.models import Base, join_model
 from skyportal.models import User
 from skyportal.models.obj import Obj
 
 
 class ObjTagOption(Base):
-    """A record of a list of tags that are available."""
+    """Store available tags that can be associated to an object."""
 
-    name = sa.Column(sa.String, nullable=False, doc="Tags list.", unique=True)
+    name = sa.Column(
+        sa.String,
+        nullable=False,
+        doc="Available tags that can be associated to an object.",
+        unique=True,
+    )
 
 
 ObjTag = join_model("obj_tag", ObjTagOption, Obj)
-
-read = update = delete = AccessibleIfUserMatches("user")
 
 ObjTag.author_id = sa.Column(
     "author_id",
     sa.ForeignKey("users.id", ondelete="SET NULL"),
     nullable=True,
-    index=True,
     doc="ID of the user who created the tag association",
 )
 
-ObjTag.author = relationship(
-    User,
-    doc="The associated User",
-    foreign_keys=[ObjTag.author_id],
-)
-
-ObjTag.__table_args__ = (sa.Index("obj_tags", "name", "tag_id", unique=True),)
+ObjTag.author = relationship(User, doc="The associated User")
