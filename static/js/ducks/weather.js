152d1a1622d1d1539eb6fefc85e59084ec8acd9f
Antoine Le Calloch <71392407+antoine-le-calloch@users.noreply.github.com>
2025-05-20 16:33:31 +0200

    Fix Infinite loop fetching weather (#5614)
    * Refactor weather fetching logic for clarity and efficiency.
    
    * Simplify weather data fetching logic in TelescopeSummary component
    
    * Fix stale check logic in WeatherWidget component.

--- static/js/ducks/weather.js
+++ static/js/ducks/weather.js
@@ -6,7 +6,6 @@ import store from "../store";
 const FETCH_WEATHER = "skyportal/FETCH_WEATHER";
 const FETCH_WEATHER_OK = "skyportal/FETCH_WEATHER_OK";
 
-// eslint-disable-next-line import/prefer-default-export
 export function fetchWeather(telescope_id = null) {
   if (telescope_id) {
     return API.GET(`/api/weather?telescope_id=${telescope_id}`, FETCH_WEATHER);
