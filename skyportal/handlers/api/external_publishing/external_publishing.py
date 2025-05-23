14ccf27505d12bab6b0b1140b846b411b93e5a3c
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-05-23 17:35:51 -0500

    Refactor external publishing handler to integrate Hermes and use external publishing bot functionality.

--- skyportal/handlers/api/external_publishing/external_publishing.py
+++ skyportal/handlers/api/external_publishing/external_publishing.py
@@ -1,12 +1,15 @@
 import sqlalchemy as sa
 
 from baselayer.app.access import auth_or_token
+from baselayer.app.env import load_env
 from baselayer.log import make_log
 
 from ....models import (
     Obj,
-    TNSRobot,
-    TNSRobotSubmission,
+)
+from ....models.external_publishing_bot import (
+    ExternalPublishingBot,
+    ExternalPublishingSubmission,
 )
 from ...base import BaseHandler
 from ..tns.tns_robot import (
@@ -15,7 +18,51 @@ from ..tns.tns_robot import (
     validate_photometry_options,
 )
 
+_, cfg = load_env()
+
 log = make_log("api/external_publishing")
+
+is_configured = (
+    cfg.get("app.hermes.endpoint")
+    and cfg.get("app.hermes.topic")
+    and cfg.get("app.hermes.token")
+)
+
+
+def is_existing_submission_request(session, obj, external_publishing_bot_id, service):
+    """Check if there is an existing submission request for the given object and bot and external service.
+    session: SQLAlchemy
+        session
+    obj: Obj
+        object to check
+    external_publishing_bot_id: int
+        ID of the external publishing bot
+    service: str
+        Name of the external service to check (TNS or Hermes)
+
+    Returns:
+        ExternalPublishingSubmission or None:
+            The existing submission request if found, None otherwise.
+    """
+    if service not in ["TNS", "Hermes"]:
+        raise ValueError("Invalid service name. Must be 'TNS' or 'Hermes'.")
+    if service == "TNS":
+        service_status = ExternalPublishingSubmission.tns_status
+    else:
+        service_status = ExternalPublishingSubmission.hermes_status
+    return session.scalars(
+        ExternalPublishingSubmission.select(session.user_or_token).where(
+            ExternalPublishingSubmission.obj_id == obj.id,
+            ExternalPublishingSubmission.external_publishing_bot_id
+            == external_publishing_bot_id,
+            sa.or_(
+                service_status == "pending",
+                service_status == "processing",
+                service_status.like("submitted%"),
+                service_status.like("complete%"),
+            ),
+        )
+    ).first()
 
 
 class ExternalPublishingHandler(BaseHandler):
@@ -44,10 +91,10 @@ class ExternalPublishingHandler(BaseHandler):
               application/json:
                 schema: Error
         """
+        data = self.get_json()
 
         with self.Session() as session:
-            data = self.get_json()
-            tnsrobotID = data.get("tnsrobotID")
+            external_publishing_bot_id = data.get("external_publishing_bot_id")
             reporters = data.get("reporters", "")
             remarks = data.get("remarks", "")
             archival = data.get("archival", False)
@@ -58,15 +105,17 @@ class ExternalPublishingHandler(BaseHandler):
             publish_to_tns = data.get("publish_to_tns", False)
             publish_to_hermes = data.get("publish_to_hermes", False)
 
-            if publish_to_tns and self.current_user.affiliation is None:
-                return self.error("User affiliation is required to publish to TNS")
             if not publish_to_tns and not publish_to_hermes:
                 return self.error(
                     "Either publish to tns or publish to hermes must be set to True"
                 )
+            if publish_to_tns and self.current_user.affiliation is None:
+                return self.error("User affiliation is required to publish to TNS")
+            if publish_to_hermes and not is_configured:
+                return self.error("This instance is not configured to use Hermes")
 
-            if tnsrobotID is None:
-                return self.error("tnsrobotID is required")
+            if external_publishing_bot_id is None:
+                return self.error("external_publishing_bot_id is required")
             if reporters == "" or not isinstance(reporters, str):
                 return self.error(
                     "reporters is required and must be a non-empty string"
@@ -81,11 +130,23 @@ class ExternalPublishingHandler(BaseHandler):
             if obj is None:
                 return self.error(f"No object available with ID {obj_id}")
 
-            tnsrobot = session.scalars(
-                TNSRobot.select(session.user_or_token).where(TNSRobot.id == tnsrobotID)
+            external_publishing_bot = session.scalars(
+                ExternalPublishingBot.select(session.user_or_token).where(
+                    ExternalPublishingBot.id == external_publishing_bot_id
+                )
             ).first()
-            if tnsrobot is None:
-                return self.error(f"No TNSRobot available with ID {tnsrobotID}")
+
+            if publish_to_tns:
+                altdata = external_publishing_bot.altdata
+                if not altdata:
+                    return self.error("Missing TNS information.")
+                if "api_key" not in altdata:
+                    return self.error("Missing TNS API key.")
+
+            if external_publishing_bot is None:
+                return self.error(
+                    f"No publishing bot available with ID {external_publishing_bot_id}"
+                )
 
             if archival is True:
                 if len(archival_comment) == 0:
@@ -93,46 +154,33 @@ class ExternalPublishingHandler(BaseHandler):
                         "If source flagged as archival, archival_comment is required"
                     )
 
-            if publish_to_tns:
-                altdata = tnsrobot.altdata
-                if not altdata:
-                    return self.error("Missing TNS information.")
-                if "api_key" not in altdata:
-                    return self.error("Missing TNS API key.")
-
             photometry_options = validate_photometry_options(
-                photometry_options, tnsrobot.photometry_options
+                photometry_options, external_publishing_bot.photometry_options
             )
 
-            # verify that there isn't already a TNSRobotSubmission for this object
-            # and TNSRobot, that is:
-            # 1. pending
-            # 2. processing
-            # 3. submitted
-            # 4. complete
-            # if so, do not add another request
-            existing_submission_request = session.scalars(
-                TNSRobotSubmission.select(session.user_or_token).where(
-                    TNSRobotSubmission.obj_id == obj.id,
-                    TNSRobotSubmission.tnsrobot_id == tnsrobot.id,
-                    sa.or_(
-                        TNSRobotSubmission.status == "pending",
-                        TNSRobotSubmission.status == "processing",
-                        TNSRobotSubmission.status.like("submitted%"),
-                        TNSRobotSubmission.status.like("complete%"),
-                    ),
+            if publish_to_tns:
+                existing_submission_request = is_existing_submission_request(
+                    session, obj, external_publishing_bot_id, "TNS"
                 )
-            ).first()
-            if existing_submission_request is not None:
-                return self.error(
-                    f"TNSRobotSubmission request for obj_id {obj.id} and tnsrobot_id {tnsrobot.id} already exists and is: {existing_submission_request.status}"
+                if existing_submission_request is not None:
+                    return self.error(
+                        f"Submission request for TNS for obj_id {obj.id} and bot_id {external_publishing_bot.id} already exists and is: {existing_submission_request.tns_status}"
+                    )
+            if publish_to_hermes:
+                existing_submission_request = is_existing_submission_request(
+                    session, obj, external_publishing_bot_id, "Hermes"
                 )
-            # create a TNSRobotSubmission entry with that information
-            tnsrobot_submission = TNSRobotSubmission(
-                tnsrobot_id=tnsrobot.id,
+                if existing_submission_request is not None:
+                    return self.error(
+                        f"Submission request for Hermes for obj_id {obj.id} and bot_id {external_publishing_bot.id} already exists and is: {existing_submission_request.hermes_status}"
+                    )
+
+            # create a ExternalPublishingSubmission entry with that information
+            external_publishing_submission = ExternalPublishingSubmission(
+                external_publishing_bot_id=external_publishing_bot.id,
                 obj_id=obj.id,
                 user_id=self.associated_user_object.id,
-                custom_reporting_string=reporters,
+                custom_publishing_string=reporters,
                 custom_remarks_string=remarks,
                 archival=archival,
                 archival_comment=archival_comment,
@@ -141,9 +189,9 @@ class ExternalPublishingHandler(BaseHandler):
                 photometry_options=photometry_options,
                 auto_submission=False,
             )
-            session.add(tnsrobot_submission)
+            session.add(external_publishing_submission)
             session.commit()
             log(
-                f"Added TNSRobotSubmission request for obj_id {obj.id} (manual submission) with tnsrobot_id {tnsrobot.id} for user_id {self.associated_user_object.id}"
+                f"Added external publishing request for obj_id {obj.id} (manual submission) with bot_id {external_publishing_bot.id} for user_id {self.associated_user_object.id}"
             )
             return self.success()
