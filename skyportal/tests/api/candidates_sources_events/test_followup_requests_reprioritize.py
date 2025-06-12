85930e7408b1014480604c938a2a5da98723a11c
Theophile du Laz <theophile.dulaz@gmail.com>
2025-06-11 19:54:05 -0700

    SEDM Allocation based unit tests failures: fix (#5671)
    * fix some followup related tests that rely on the SEDMAPI when we test update/deletes, which can fail if the SEDM server is down. Instead we add a generic_instrument and allocation that use the GENERICAPI

--- skyportal/tests/api/candidates_sources_events/test_followup_requests_reprioritize.py
+++ skyportal/tests/api/candidates_sources_events/test_followup_requests_reprioritize.py
@@ -2,25 +2,27 @@ from skyportal.tests import api
 
 
 def test_reprioritize_followup_request(
-    public_group_sedm_allocation,
+    public_group_generic_allocation,
     public_source,
     upload_data_token,
-    super_admin_token,
     gcn_GW190425,
 ):
     localization_id = gcn_GW190425.localizations[0].id
 
     request_data = {
-        "allocation_id": public_group_sedm_allocation.id,
+        "allocation_id": public_group_generic_allocation.id,
         "obj_id": public_source.id,
         "payload": {
             "priority": 1,
             "start_date": "3020-09-01",
             "end_date": "3022-09-01",
-            "observation_type": "IFU",
+            "observation_choices": public_group_generic_allocation.instrument.to_dict()[
+                "filters"
+            ],
             "exposure_time": 300,
+            "exposure_counts": 1,
             "maximum_airmass": 2,
-            "maximum_fwhm": 1.2,
+            "minimum_lunar_distance": 30,
         },
     }
 
