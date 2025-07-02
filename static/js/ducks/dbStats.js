0eb6e61d93c5f60de89ff1a8ac6b8f24db3ce4d3
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-07-02 14:34:40 -0500

    Remove unnecessary eslint-disable comments in dbStats.js, store.js, and sysInfo.js.

--- static/js/ducks/dbStats.js
+++ static/js/ducks/dbStats.js
@@ -4,7 +4,6 @@ import store from "../store";
 const FETCH_DB_STATS = "skyportal/FETCH_DB_STATS";
 const FETCH_DB_STATS_OK = "skyportal/FETCH_DB_STATS_OK";
 
-// eslint-disable-next-line import/prefer-default-export
 export const fetchDBStats = () => API.GET("/api/db_stats", FETCH_DB_STATS);
 
 const reducer = (state = null, action) => {
