b2e607c13596bb25107a2b5e18d41fb9eb761c21
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-07-02 23:17:58 -0500

    Refactor Button component to use forwardRef for improved ref handling and to avoid inspector warning.

--- static/js/components/Button.jsx
+++ static/js/components/Button.jsx
@@ -1,35 +1,41 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import PropTypes from "prop-types";
 import MuiButton from "@mui/material/Button";
 import LoadingButton from "@mui/lab/LoadingButton";
 
-const Button = ({ primary, secondary, async, ...muiButtonProps }) => {
-  if (muiButtonProps.startIcon) {
-    throw new Error(
-      "Error: startIcon used in Button props. Please use endIcon as specified in the SkyPortal style documentation: https://skyportal.io/docs/styling.html#buttons",
-    );
-  }
-  if (async) {
-    return (
-      <LoadingButton
-        loadingIndicator="Loading..."
-        variant="contained"
-        color="primary"
-        {...muiButtonProps}
-      />
-    );
-  }
-  if (primary || secondary) {
-    return (
-      <MuiButton
-        variant="contained"
-        color={primary ? "primary" : "grey"}
-        {...muiButtonProps}
-      />
-    );
-  }
-  return <MuiButton {...muiButtonProps} />;
-};
+const Button = forwardRef(
+  ({ primary, secondary, async, ...muiButtonProps }, ref) => {
+    if (muiButtonProps.startIcon) {
+      throw new Error(
+        "Error: startIcon used in Button props. Please use endIcon as specified in the SkyPortal style documentation: https://skyportal.io/docs/styling.html#buttons",
+      );
+    }
+    if (async) {
+      return (
+        <LoadingButton
+          ref={ref}
+          loadingIndicator="Loading..."
+          variant="contained"
+          color="primary"
+          {...muiButtonProps}
+        />
+      );
+    }
+    if (primary || secondary) {
+      return (
+        <MuiButton
+          ref={ref}
+          variant="contained"
+          color={primary ? "primary" : "grey"}
+          {...muiButtonProps}
+        />
+      );
+    }
+    return <MuiButton ref={ref} {...muiButtonProps} />;
+  },
+);
+
+Button.displayName = "Button";
 
 Button.propTypes = {
   primary: PropTypes.bool,
