c9dd95795a45054e8e664c097dabb4529a03a9a8
Camille DOUZET <camille.douzet@ijclab.in2p3.fr>
2026-02-02 16:59:49 +0100

    fix typo

--- static/js/components/EditTagGroups.jsx
+++ static/js/components/EditTagGroups.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 
