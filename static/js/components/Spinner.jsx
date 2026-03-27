5c413ed1c54063bde889067ce73435e90cbac377
antoine-le-calloch <antoine.lecalloch@gmail.com>
2026-03-27 01:26:03 -0500

    Improve Spinner.jsx.

--- static/js/components/Spinner.jsx
+++ static/js/components/Spinner.jsx
@@ -1,26 +1,19 @@
 import React from "react";
-import makeStyles from "@mui/styles/makeStyles";
 import CircularProgress from "@mui/material/CircularProgress";
 
-const useStyles = makeStyles(() => ({
-  spinner: {
-    marginLeft: "auto",
-    marginRight: "auto",
-    display: "flex",
-    top: "50%",
-    left: "50%",
-    position: "fixed",
-    transform: "translate(-50%, -50%)",
-  },
-}));
-
-const Spinner = () => {
-  const classes = useStyles();
-  return (
-    <div className={classes.spinner}>
-      <CircularProgress />
-    </div>
-  );
-};
+const Spinner = () => (
+  <div
+    style={{
+      position: "fixed",
+      inset: 0,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      zIndex: 9999,
+    }}
+  >
+    <CircularProgress />
+  </div>
+);
 
 export default Spinner;
