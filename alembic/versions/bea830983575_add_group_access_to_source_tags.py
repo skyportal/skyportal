dd577b631e5533e340b80d8e872430f58781ef1c
Camille DOUZET <camille.douzet@ijclab.in2p3.fr>
2026-05-22 17:21:06 +0200

    update alembic migration head

--- alembic/versions/bea830983575_add_group_access_to_source_tags.py
+++ alembic/versions/bea830983575_add_group_access_to_source_tags.py
@@ -12,7 +12,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "bea830983575"
-down_revision = "2b8addba3e86"
+down_revision = "6928ae655672"
 branch_labels = None
 depends_on = None
 
