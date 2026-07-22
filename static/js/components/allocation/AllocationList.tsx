4e1b7d2e836c789ff438bd90eed6579f9a36960d
antoine-le-calloch <antoine.lecalloch@gmail.com>
2026-07-22 14:15:03 -0400

    Clean up.

--- static/js/components/allocation/AllocationList.tsx
+++ static/js/components/allocation/AllocationList.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
-import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
 
 import { useGetDefaultSurveyEfficienciesQuery } from "../../ducks/default_survey_efficiencies";
@@ -10,39 +9,10 @@ import { useGetTelescopesQuery } from "../../ducks/telescopes";
 import { useGetProfileQuery } from "../../ducks/profile";
 import { useGetGroupsQuery } from "../../ducks/groups";
 import Spinner from "../Spinner";
-import AllocationTableComponent from "./AllocationTable";
-import DefaultObservationPlanTableComponent from "../observation_plan/DefaultObservationPlanTable";
-import DefaultSurveyEfficiencyTableComponent from "../survey_efficiency/DefaultSurveyEfficiencyTable";
 import { useGetInstrumentsQuery } from "../../ducks/instruments";
-
-const AllocationTable = AllocationTableComponent as any;
-const DefaultObservationPlanTable = DefaultObservationPlanTableComponent as any;
-const DefaultSurveyEfficiencyTable =
-  DefaultSurveyEfficiencyTableComponent as any;
-
-interface AllocationsTabProps {
-  managePermission?: boolean;
-}
-
-const AllocationsTab = ({ managePermission = false }: AllocationsTabProps) => {
-  const { data: instrumentList = [] } = useGetInstrumentsQuery();
-  const { data: telescopeList = [] } = useGetTelescopesQuery();
-  const groups = useGetGroupsQuery().data?.all ?? null;
-  const { data: allocationList } = useGetAllocationsQuery();
-
-  if (allocationList == null) return <Spinner />;
-
-  return (
-    <AllocationTable
-      instruments={instrumentList}
-      telescopes={telescopeList}
-      groups={groups}
-      allocations={allocationList}
-      managePermission={managePermission}
-      fixedHeader={true}
-    />
-  );
-};
+import AllocationTable from "./AllocationTable";
+import DefaultObservationPlanTable from "../observation_plan/DefaultObservationPlanTable";
+import DefaultSurveyEfficiencyTable from "../survey_efficiency/DefaultSurveyEfficiencyTable";
 
 const AllocationList = () => {
   const { data: defaultObservationPlanList = [] } =
@@ -51,30 +21,37 @@ const AllocationList = () => {
     useGetDefaultSurveyEfficienciesQuery();
   const { data: instrumentList = [] } = useGetInstrumentsQuery();
   const { data: telescopeList = [] } = useGetTelescopesQuery();
+  const { data: allocationList } = useGetAllocationsQuery();
   const { data: currentUser } = useGetProfileQuery();
+  const groups = useGetGroupsQuery().data?.all ?? null;
 
   const hasPermission = (specificPermission: string) =>
     currentUser?.permissions?.includes("System admin") ||
     currentUser?.permissions?.includes(specificPermission) ||
     false;
 
-  const [tabIndex, setTabIndex] = React.useState("1");
+  const [tabIndex, setTabIndex] = React.useState(1);
+
+  if (tabIndex == 1 && allocationList == null) return <Spinner />;
 
   return (
     <TabContext value={tabIndex}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <TabList onChange={(_, newValue) => setTabIndex(newValue)} centered>
-          <Tab label="Allocations" value="1" />
-          <Tab label="Default Observation Plans" value="2" />
-          <Tab label="Default Survey Efficiencies" value="3" />
-        </TabList>
-      </Box>
-      <TabPanel value="1">
-        <AllocationsTab
+      <TabList onChange={(_, newValue) => setTabIndex(newValue)} centered>
+        <Tab label="Allocations" value={1} />
+        <Tab label="Default Observation Plans" value={2} />
+        <Tab label="Default Survey Efficiencies" value={3} />
+      </TabList>
+      <TabPanel value={1}>
+        <AllocationTable
+          instruments={instrumentList}
+          telescopes={telescopeList}
+          groups={groups as any}
+          allocations={allocationList as any}
           managePermission={hasPermission("Manage allocations")}
+          fixedHeader={true}
         />
       </TabPanel>
-      <TabPanel value="2">
+      <TabPanel value={2}>
         <DefaultObservationPlanTable
           default_observation_plans={defaultObservationPlanList}
           instruments={instrumentList}
@@ -82,7 +59,7 @@ const AllocationList = () => {
           deletePermission={hasPermission("Manage observation plans")}
         />
       </TabPanel>
-      <TabPanel value="3">
+      <TabPanel value={3}>
         <DefaultSurveyEfficiencyTable
           default_survey_efficiencies={defaultSurveyEfficiencyList}
           deletePermission={hasPermission("Manage observation plans")}
