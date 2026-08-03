dc54f2e4a4b16a09146da2e85cd0f4f432edf23a
Michael Coughlin <michael.w.coughlin@gmail.com>
2026-08-03 10:09:41 -0500

    instrument.telescope greenlet issue (#6463)
    This PR eager-loads instrument.telescope in api_classname instrument_forms to fix MissingGreenlet.

--- skyportal/handlers/api/internal/robotic_instruments.py
+++ skyportal/handlers/api/internal/robotic_instruments.py
@@ -15,9 +15,10 @@ class RoboticInstrumentsHandler(BaseHandler):
             if apitype is not None:
                 if apitype == "api_classname":
                     result = await session.scalars(
-                        Instrument.select(session.user_or_token).where(
-                            Instrument.api_classname.isnot(None)
-                        )
+                        Instrument.select(session.user_or_token)
+                        # custom_json_schema reads instrument.telescope
+                        .options(joinedload(Instrument.telescope))
+                        .where(Instrument.api_classname.isnot(None))
                     )
                     instruments = result.all()
                     retval = {
@@ -28,8 +29,6 @@ class RoboticInstrumentsHandler(BaseHandler):
                     result = await session.scalars(
                         Instrument.select(session.user_or_token)
                         # custom_json_schema reads instrument.telescope
-                        # (next_twilight_morning_nautical); eager-load it so it
-                        # doesn't lazy-load (MissingGreenlet) under async.
                         .options(joinedload(Instrument.telescope))
                         .where(Instrument.api_classname_obsplan.isnot(None))
                     )
