1a8a8a24ca910baef5f22ea638fc9c191325358f
Camille DOUZET <camille.douzet@ijclab.in2p3.fr>
2026-08-04 17:30:22 +0200

    enh: uniformize form

--- static/js/components/photometry/UpdatePhotometry.tsx
+++ static/js/components/photometry/UpdatePhotometry.tsx
@@ -5,8 +5,6 @@ import DialogTitle from "@mui/material/DialogTitle";
 import { makeStyles } from "tss-react/mui";
 import SaveIcon from "@mui/icons-material/Save";
 import TextField from "@mui/material/TextField";
-import Select from "@mui/material/Select";
-import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import EditIcon from "@mui/icons-material/Edit";
 import IconButton from "@mui/material/IconButton";
@@ -18,9 +16,6 @@ import { useUpdatePhotometryMutation } from "../../ducks/photometry";
 import { useGetInstrumentsQuery } from "../../ducks/instruments";
 
 const useStyles = makeStyles()(() => ({
-  Select: {
-    width: "100%",
-  },
   SelectItem: {
     whiteSpace: "break-spaces",
   },
@@ -243,7 +238,7 @@ const UpdatePhotometry = ({ phot, magsys }: UpdatePhotometryProps) => {
       </IconButton>
       <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
         <DialogTitle>Update Photometry</DialogTitle>
-        <DialogContent>
+        <DialogContent sx={{ pt: 2 }}>
           {invalid && (
             <FormValidationError message="Please enter a valid float" />
           )}
@@ -333,7 +328,8 @@ const UpdatePhotometry = ({ phot, magsys }: UpdatePhotometryProps) => {
             <TextField
               data-testid="updatePhotometryAltdataTextfield"
               size="small"
-              label='Alternative json data (i.e. {"note": "poor subtraction"})'
+              label="Alternative json data"
+              placeholder='{"note": "poor subtraction"}'
               value={state.altdata}
               name="altdata"
               onChange={handleChange}
@@ -344,15 +340,16 @@ const UpdatePhotometry = ({ phot, magsys }: UpdatePhotometryProps) => {
           </div>
           <p />
           <div className={classes.formField}>
-            <InputLabel id="instrumentSelectLabel">Instrument</InputLabel>
-            <Select
-              inputProps={{ MenuProps: { disableScrollLock: true } }}
-              labelId="instrumentSelectLabel"
-              value={state.instrument_id}
+            <TextField
+              select
+              size="small"
+              label="Instrument"
+              value={state.instrument_id ?? ""}
               onChange={handleChange}
               name="instrument_id"
               data-testid="instrumentSelect"
-              className={classes.Select}
+              fullWidth
+              slotProps={{ select: { MenuProps: { disableScrollLock: true } } }}
             >
               {instrumentList?.map((instrument: any) => (
                 <MenuItem
@@ -363,18 +360,20 @@ const UpdatePhotometry = ({ phot, magsys }: UpdatePhotometryProps) => {
                   {instrument.name}
                 </MenuItem>
               ))}
-            </Select>
+            </TextField>
           </div>
-          <div className={classes.formField}>
-            <InputLabel id="filterSelectLabel">Filter</InputLabel>
-            <Select
-              inputProps={{ MenuProps: { disableScrollLock: true } }}
-              labelId="filterSelectLabel"
-              value={state.filter}
+          <p />
+          <div>
+            <TextField
+              select
+              size="small"
+              label="Filter"
+              value={state.filter ?? ""}
               onChange={handleChange}
               name="filter"
               data-testid="filterSelect"
-              className={classes.Select}
+              fullWidth
+              slotProps={{ select: { MenuProps: { disableScrollLock: true } } }}
             >
               {selectedInstrument?.filters?.map((filt: any) => (
                 <MenuItem
@@ -385,7 +384,7 @@ const UpdatePhotometry = ({ phot, magsys }: UpdatePhotometryProps) => {
                   {filt}
                 </MenuItem>
               ))}
-            </Select>
+            </TextField>
           </div>
           <div className={classes.saveButton}>
             <Button
