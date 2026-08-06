bc7ccc804453b59839697f8b34382619bdb3778c
Michael Coughlin <michael.w.coughlin@gmail.com>
2026-08-05 20:00:41 -0500

    fritz filter translation

--- static/js/components/filter/FilterPlugins.tsx
+++ static/js/components/filter/FilterPlugins.tsx
@@ -1,7 +1,26 @@
+import { useParams } from "react-router-dom";
+
+import { useGetFilterQuery } from "../../ducks/filter";
+import { setBrokerFilterTarget } from "../../ducks/brokerFilterTarget";
+import BoomFilterPlugins from "./boom/BoomFilterPlugins";
+
 interface FilterPluginsProps {
   group?: any;
 }
 
-const FilterPlugins = (_props: FilterPluginsProps) => <></>;
+// A filter attached to a broker gets the same builder the
+// /brokers/:brokerId/filter/:fid route renders; anything else has no plugins.
+const FilterPlugins = (_props: FilterPluginsProps) => {
+  const { fid } = useParams();
+  const { data: filter } = useGetFilterQuery(fid ?? "", { skip: !fid }) as any;
+
+  if (!filter?.broker_id) {
+    return <></>;
+  }
+
+  // Set synchronously, before BoomFilterPlugins' mount effects read it.
+  setBrokerFilterTarget(filter.broker_id);
+  return <BoomFilterPlugins />;
+};
 
 export default FilterPlugins;
