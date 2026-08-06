7670cd27de298e7908f94a745c07e27ce9d8cce6
Michael Coughlin <michael.w.coughlin@gmail.com>
2026-08-06 10:24:17 -0500

    fritz filter translation (#6484)
    This PR continues the fritz filter translation.
    
    ---------
    
    Co-authored-by: Thomas Culino <thomas.culino@gmail.com>

--- static/js/components/filter/FilterPlugins.tsx
+++ static/js/components/filter/FilterPlugins.tsx
@@ -1,7 +1,34 @@
+import { useParams } from "react-router-dom";
+
+import { useGetFilterQuery } from "../../ducks/filter";
+import { useGetBrokersQuery } from "../../ducks/brokers";
+import { setBrokerFilterTarget } from "../../ducks/brokerFilterTarget";
+import BoomFilterPlugins from "./boom/BoomFilterPlugins";
+
 interface FilterPluginsProps {
   group?: any;
 }
 
-const FilterPlugins = (_props: FilterPluginsProps) => <></>;
+// A filter attached to a broker gets that broker's builder; anything else
+// defaults to BOOM (the backend attaches the filter to it on first version
+// creation - see BrokerFiltersHandler.post in skyportal/handlers/api/broker.py).
+const FilterPlugins = (_props: FilterPluginsProps) => {
+  const { fid } = useParams();
+  const { data: filter } = useGetFilterQuery(fid ?? "", { skip: !fid }) as any;
+  const { data: brokers } = useGetBrokersQuery();
+
+  const boomBrokerId = brokers?.find(
+    (broker) => broker.broker_classname === "BOOMBROKER",
+  )?.id;
+  const brokerId = filter?.broker_id ?? boomBrokerId;
+
+  if (!brokerId) {
+    return <></>;
+  }
+
+  // Set synchronously, before BoomFilterPlugins' mount effects read it.
+  setBrokerFilterTarget(brokerId);
+  return <BoomFilterPlugins />;
+};
 
 export default FilterPlugins;
