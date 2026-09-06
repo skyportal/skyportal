e34433dd9207266c3c09e20fc46711503c9e12e8
antoine-le-calloch <antoine.lecalloch@gmail.com>
2026-09-06 15:21:35 -0500

    Store assistant conversations in their own table instead of comment channels

--- skyportal/models/__init__.py
+++ skyportal/models/__init__.py
@@ -6,6 +6,7 @@ from .allocation import *
 from .analysis import *
 from .annotation import *
 from .assignment import *
+from .assistant import *
 from .broker import *
 from .candidate import *
 from .classification import *
