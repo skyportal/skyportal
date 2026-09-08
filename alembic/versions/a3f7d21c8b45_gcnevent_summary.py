8fb57da362c94fb2fafe6882f328f4aede2c93f0
Michael Coughlin <michael.w.coughlin@gmail.com>
2026-09-08 09:50:50 -0500

    point the gcnevent summary migration at the current head

--- alembic/versions/a3f7d21c8b45_gcnevent_summary.py
+++ alembic/versions/a3f7d21c8b45_gcnevent_summary.py
@@ -1,7 +1,7 @@
 """Give a GcnEvent a summary, mirroring objs.summary/summary_history
 
 Revision ID: a3f7d21c8b45
-Revises: b1e4a7c92f10
+Revises: e5c1a83f7d92
 Create Date: 2026-09-04
 
 """
@@ -12,7 +12,7 @@ from sqlalchemy.dialects import postgresql
 from alembic import op
 
 revision = "a3f7d21c8b45"
-down_revision = "b1e4a7c92f10"
+down_revision = "e5c1a83f7d92"
 branch_labels = None
 depends_on = None
 
