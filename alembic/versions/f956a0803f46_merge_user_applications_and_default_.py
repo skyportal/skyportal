1098dbf0272bb91b3e1c2c69ef7ba5a49481b4a6
antoine-le-calloch <antoine.lecalloch@gmail.com>
2026-09-16 22:33:54 -0500

    Join the two alembic heads main was left with

---
+++ alembic/versions/f956a0803f46_merge_user_applications_and_default_.py
@@ -0,0 +1,21 @@
+"""merge user applications and default analysis resource type
+
+Revision ID: f956a0803f46
+Revises: a3d7c1f04b92, c3d9e7a1f4b6
+Create Date: 2026-09-16 22:33:32.046521
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "f956a0803f46"
+down_revision = ("a3d7c1f04b92", "c3d9e7a1f4b6")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
